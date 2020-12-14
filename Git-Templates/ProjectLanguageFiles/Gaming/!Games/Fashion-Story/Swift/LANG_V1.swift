b7e5df3a69c62494e8fff6aa31bd75690c8b9f79
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-14 15:13:56 -0800

    LANG_V1.swift
    An archive of the first version of the project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Fashion-Story/Swift/LANG_V1.swift
@@ -0,0 +1,10 @@
+// Start of script
+// Fashion Story is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of an action (Swift) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Swift.
+
+var str = "Language file: "
+str += " Fashion Story is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of an action (Swift) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Swift."
+
+// File version: 1 (Monday, October 26th 2020 at 9:59 am)
+// File type: Swift Script file (*.swift)
+// Line count (including blank lines and compiler line): 11
+// End of script
