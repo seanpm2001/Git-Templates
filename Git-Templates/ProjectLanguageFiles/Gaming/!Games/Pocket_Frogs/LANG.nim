d6a9709586da55d9a26274be6ed4e06f55608603
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-14 15:32:19 -0800

    LANG.nim
    The project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Pocket_Frogs/LANG.nim
@@ -0,0 +1,9 @@
+# Start of script
+# Pocket Frogs is closed source, and I don't know what language it is written in, so I chose a random programming language (Nim) as I couldn't figure out what else to make the official project language. I want this project to have a language assigned to it, so I wrote this file in Nim. It feels like it fits.
+
+echo ("Pocket Frogs is closed source, and I don't know what language it is written in, so I chose a random programming language (Nim) as I couldn't figure out what else to make the official project language. I want this project to have a language assigned to it, so I wrote this file in Nim. It feels like it fits.")
+  
+# File version: 1 (Wednesday, October 28th 2020 at 4:09 pm)
+# File type: Nim script file (*.nim)
+# Line count (including blank lines and compiler line): 10
+# End of script
