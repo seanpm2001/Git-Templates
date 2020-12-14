af5ac8ac1bb583838d6f73ac34bd008dd3bad9dd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-14 14:58:03 -0800

    LANG_V1.go
    An archive of the first version of the project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/ModernSkyBurger/Go/LANG_V1.go
@@ -0,0 +1,15 @@
+// Start of script
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Modern Sky Burger is closed source, and I don't know what language it is written in, so I chose the one programming language that sucks and I hate the most (Go) as I do not like the modern version of this game, and I want this project to have a language assigned to it, so I wrote this file in Go")
+}
+
+// Modern Sky Burger is closed source, and I don't know what language it is written in, so I chose the one programming language that sucks and I hate the most (Go) as I do not like the modern version of this game, and I want this project to have a language assigned to it, so I wrote this file in Go
+
+// File version: 1 (Monday, October 26th 2020 at 11:02 am)
+// File type: Google Go script file (*.go)
+// Line count (including blank lines and compiler line): 16
+// End of script
