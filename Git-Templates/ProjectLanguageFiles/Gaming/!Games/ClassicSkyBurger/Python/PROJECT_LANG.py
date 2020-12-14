8c7a29f2a3c818951ab9ed69e98bedeec0f61f94
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-14 15:05:24 -0800

    PROJECT_LANG.py
    The project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/ClassicSkyBurger/Python/PROJECT_LANG.py
@@ -0,0 +1,10 @@
+# Start of script
+print("Classic Sky Burger is closed source, and I don't know what language it is written in, so I chose the one programming language that I like the most (Python) as I do not like the modern version of this game, and I want this project to have a language assigned to it, so the classic version gets a good.")
+noMore = input("Press [ENTER] key to quit")
+print ("The window should now be closed. If it is not closed, press the close button. If this doesn't work, use a process manager/task manager and end the process/task")
+# End
+# Modern Sky Burger is closed source, and I don't know what language it is written in, so I chose the one programming language that I like the most (Python) as I do not like the modern version of this game, and I want this project to have a language assigned to it, so the classic version gets a good.
+# File version: 1 (Tuesday, November 24th 2020 at 2:20 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 11
+# End of script
