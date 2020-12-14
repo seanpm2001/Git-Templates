1a349df6f92c4fdcf81ae2a0b3b0fe0a79f196b4
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-14 15:24:47 -0800

    LANG_V1.pas
    An archive of the first version of the project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Merge-Plane/Pascal/LANG_V1.pas
@@ -0,0 +1,14 @@
+// Start of script
+program LanguageFile(output);
+begin
+    Write('Merge Plane is closed source, and I do not know what language it is written in, so I chose the one programming language that is also a unit of distance (Pascal) as I want this project to have a language assigned to it, so I wrote this file in Pascal')
+    {No ";" is required after the last statement of a block -
+        adding one adds a "null statement" to the program, which is ignored by the compiler.}
+end.
+
+// Merge Plane is closed source, and I don't know what language it is written in, so I chose the one programming language that is also a unit of distance (Pascal) as I want this project to have a language assigned to it, so I wrote this file in Pascal
+
+// File version: 1 (Monday, October 26th 2020 at 11:12 am)
+// File type: Pascal script file (*.pas)
+// Line count (including blank lines and compiler line): 15
+// End of script
