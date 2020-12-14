c63355e5c7b913c1b1cafed42fdd1ba9d0e58f3f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-14 15:16:13 -0800

    LANG.rs
    The project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Bit-City/Rust/LANG.rs
@@ -0,0 +1,11 @@
+// Start of script
+// Bit City is a closed source game, and I don't know what language it is written in, so I chose the one programming language with the name of something industrial (Rust) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Rust.
+
+fn main() {
+    println!("Bit City is a closed source game, and I don't know what language it is written in, so I chose the one programming language with the name of something industrial (Rust) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Rust.");
+}
+
+// File version: 1 (Monday, October 26th 2020 at 1:21 pm)
+// File type: Rust Script file (*.rs)
+// Line count (including blank lines and compiler line): 12
+// End of script
