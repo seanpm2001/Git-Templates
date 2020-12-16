fd859dca55133e90c8ac490aebef184d5685af8b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-15 18:07:24 -0800

    1 sub-directory (7 files) - DDG.GG
    1 sub-directory for this directory, containing 7 files in Perl, Python, and JavaScript formats, from my DuckDuckGo image repository.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/DuckDuckGo_Images/JavaScript/PROJECTLANG2_V1.js
@@ -0,0 +1,7 @@
+// Start of script
+// DuckDuckGo is written in Perl, JavaScript, and Python. I have decided to split the project language for this project across these 3 languages. This project language file is for the JavaScript portion of DuckDuckGo
+console.log("DuckDuckGo is written in Perl, JavaScript, and Python. I have decided to split the project language for this project across these 3 languages. This project language file is for the JavaScript portion of DuckDuckGo");
+// File version: 1 (Tuesday, December 8th 2020 at 3:32 pm)
+// File type: JavaScript script file (*.js)
+// Line count (including blank lines and compiler line): 8
+// End of script
