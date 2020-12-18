3c7e89e4f5d28812214ae96201b8aa442d36273f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-17 16:21:18 -0800

    Sunset image repo (2 files)
    2 project language files from the sunset image repo, in Java format.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Sunset_ImageRepo/Java/PROJECT_LANG.java
@@ -0,0 +1,12 @@
+// Start of script
+// I have decided to write this language file in Java for sunset images, as Java was originally developed by **Sun** Microsystems, which is fitting for **sun**set pictures.
+public class ProjectLanguageFile {
+    public static void main(String[] args) {
+        System.out.println("I have decided to write this language file in Java for sunset images, as Java was originally developed by **Sun** Microsystems, which is fitting for **sun**set pictures."); // Prints the string to the console.
+}}
+/* File version: 1 (Thursday, December 17th 2020 at 4:17 pm)
+* File type: Java source file (*.java)
+* Line count (including blank lines and compiler line): 12
+*/
+// End of script
+
