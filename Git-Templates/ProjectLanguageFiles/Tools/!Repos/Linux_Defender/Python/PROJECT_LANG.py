c3568dbbcc722a1b2cd08fa1834f8e1e6e676805
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-24 14:33:00 -0800

    PROJECT_LANG.py
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/Linux_Defender/Python/PROJECT_LANG.py
@@ -0,0 +1,10 @@
+# Start of script
+# Linux Defender is written in Python, as that is the preferred and most efficient programming language for this task
+print ("Linux Defender is written in Python, as that is the preferred and most efficient programming language for this task")
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (Thursday, December 24th 2020 at 4:31 pm)
+# Line count (including blank lines and compiler line): 11
+"""
+# End of script
