01debd1d6626fe6169aede8536cc2141ae0bb622
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-23 20:50:50 -0800

    PROJECT_LANG_V1.asm
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Laptop_Images/Assembly/PROJECT_LANG_V1.asm
@@ -0,0 +1,6 @@
+; I have decided to make the Assembly language the official language for this project, as laptops require assembly.
+
+; File info
+; File version: 1 (Wednesday, December 23rd 2020 ay 8:49 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
