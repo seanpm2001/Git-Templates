ce3010fbd8abffbebd8fad4b8ec5b441b85d52bb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-24 20:56:39 -0800

    PROJECT_LANG_V1.py
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/BluetoothExperience_Images/Python/PROJECT_LANG_V1.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this project in Python, as it is my go to language and is highly functional and matches the language of my BluPhone client project, which is related to Bluetooth, which in turn is related to this project.
+''' '''
+print("I have decided to write this project in Python, as it is my go to language and is highly functional and matches the language of my BluPhone client project, which is related to Bluetooth, which in turn is related to this project.")
+""" """
+# File version: 1 (Thursday, December 24th 2020 at 8:55 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
