5e1076cc664f76db82b701e2d3ed232cf5850420
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-25 15:58:46 -0800

    PROJECT_LANG_V1.d
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/Dentist_Images/D/PROJECT_LANG_V1.d
@@ -0,0 +1,16 @@
+// Start of script
+import std.stdio : writeln;
+import std.range : iota;
+import std.parallelism : parallel;
+/* DIVIDER */
+void main()
+{
+	writeln ("This project is for dentistry images. I could have gone with Diff, but I felt like the D programming language is as close as I could get for a project language file for this project.");
+}
+// This project is for dentistry images. I could have gone with Diff, but I felt like the D programming language is as close as I could get for a project language file for this project.
+/* DIVIDER */
+// File info
+// File version: 1 (Friday, December 25th 2020 at 3:57 pm)
+// File type: D programming language script file (*.d)
+// Line count (including blank lines and compiler line): 17
+// End of script
