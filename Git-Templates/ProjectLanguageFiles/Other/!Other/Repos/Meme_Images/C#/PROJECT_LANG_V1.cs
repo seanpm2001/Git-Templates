21a60c7d865803e24ecf2c58824de7d8d8928ee2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-24 20:42:22 -0800

    PROJECT_LANG_V1.cs
    An archive of the first version of the project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Meme_Images/C#/PROJECT_LANG_V1.cs
@@ -0,0 +1,14 @@
+// Start of script
+using System;
+// I decided to write this file based on my favorite programming language bashing meme. Out of all the "Java bad, JavaScript bad, PHP Bad" memes I saw on r/programmerhumor, my favorite meme is titled "The Java bros" the joke is that Mario is Java and Luigi is C#. Luigi goes: Mama why you never remember my name and the punchline is I'ma sorry **MICROSOFT JAVA**
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("I decided to write this file based on my favorite programming language bashing meme. Out of all the "Java bad, JavaScript bad, PHP Bad" memes I saw on r/programmerhumor, my favorite meme is titled "The Java bros" the joke is that Mario is Java and Luigi is C#. Luigi goes: Mama why you never remember my name and the punchline is I'ma sorry **MICROSOFT JAVA**");
+    }
+}
+// File version: 1 (Thursday, December 24th 2020 at 8:40 pm)
+// File type: C Sharp Script file (*.cs)
+// Line count (including blank lines and compiler line): 15
+// End of script
