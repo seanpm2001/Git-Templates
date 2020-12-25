60431dffe0e71d3052a2ace530c4931879287c40
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-24 20:29:24 -0800

    PROJECT_LANG.c
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Christmas_Images/C/PROJECT_LANG.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Start of script
+// I don't know of any languages named with a religious naming scheme, so I defaulted to C, as this project language file puts the C in hristmas.
+int main(void)
+{
+    printf("I don't know of any languages named with a religious naming scheme, so I defaulted to C, as this project language file puts the C in hristmas.\n");
+}
+// File version: 1 (Thursday, December 24th 2020 at 8:28 pm)
+// File type: C Script file (*.c)
+// Line count (including blank lines and compiler line): 12
+// End of script
