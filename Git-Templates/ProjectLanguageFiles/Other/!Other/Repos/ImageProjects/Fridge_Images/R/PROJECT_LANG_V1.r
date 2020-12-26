283d7c6ce88ed3704d4665bae7ed7e50a43469c3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-25 16:16:32 -0800

    PROJECT_LANG_V1.r
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/Fridge_Images/R/PROJECT_LANG_V1.r
@@ -0,0 +1,8 @@
+# Start of script
+print ("This project is for Refridgerator images. It took me a long time to figure out to go with R for this project. I considered several languages, such as Fortran, IronPython, CoffeeScript, AppleScript, Lisp Flavored Erlang, Rust, and REBOL before I settled on R.");
+# This project is for Refridgerator images. It took me a long time to figure out to go with R for this project. I considered several languages, such as Fortran, IronPython, CoffeeScript, AppleScript, Lisp Flavored Erlang, Rust, and REBOL before I settled on R
+# File info
+# File version: 1 (Friday, December 25th 2020 at 4:14 pm)
+# File type: R programming language script file (*.r)
+# Line count (including blank lines and compiler line): 9
+# End of script
