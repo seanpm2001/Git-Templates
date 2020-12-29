d02be4e6c7c323c69932bd9fe4bb760699c7c6c8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-28 16:33:21 -0800

    PROJECT_LANG_V1.py
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/Advanced_Eye_Chart/Python/PROJECT_LANG_V1.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this project (Advanced Eye Chart) in Python, as it is my go to language and is highly functional and meets all the needs for this project.
+''' '''
+print("I have decided to write this project (Advanced Eye Chart) in Python, as it is my go to language and is highly functional and meets all the needs for this project.")
+""" """
+# File version: 1 (Monday, December 28th 2020 at 4:31 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
