e25f0c21691b4f37fef4b4b3f34cb2bf7709691b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-31 18:32:46 -0800

    PROJECT_LANG_V1.js
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/KhanAcademyData/JavaScript/PROJECT_LANG_V1.js
@@ -0,0 +1,9 @@
+// Start of script
+// This project is mostly written in JavaScript, so I chose JavaScript as the project language.
+console.log ("This project is mostly written in JavaScript, so I chose JavaScript as the project language.");
+/* File info
+* File version: 1 (Thursday, December 31st 2020 at 6:31 pm)
+* File type: JavaScript script file (*.js)
+* Line count (including blank lines and compiler line): 10
+*/
+// End of script
