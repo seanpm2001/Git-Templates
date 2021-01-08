c87b4d7d0daa68abde055eac6c8a13a864808a15
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-08 13:28:17 -0800

    PROJECT_LANG_V1.java
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/SNU/SNU_2D_TV_Remote/Java/PROJECT_LANG_V1.java
@@ -0,0 +1,11 @@
+// Start of script
+// I have decided to write this language file in Java for the SNU TV Remote. I feel that it is extremely fitting, as Java was originally made due to limitations with TV remotes at the time. This project will be written in Java to honor this.
+public class ProjectLanguageFile {
+    public static void main(String[] args) {
+        System.out.println("I have decided to write this language file in Java for the SNU TV Remote. I feel that it is extremely fitting, as Java was originally made due to limitations with TV remotes at the time. This project will be written in Java to honor this."); // Prints the string to the console.
+}}
+/* File version: 1 (Friday, January 8th 2021 at 1:26 pm)
+* File type: Java source file (*.java)
+* Line count (including blank lines and compiler line): 12
+*/
+// End of script
