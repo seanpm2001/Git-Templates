b4c125f2e50fe6bb430eea1a6d1fb48838276215
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-14 18:51:29 -0800

    PROJECT_LANG.cpp
    The current version (version 1) of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/SVG_Video/C++/PROJECT_LANG.cpp
@@ -0,0 +1,13 @@
+// Start of script
+#include <iostream>
+// The majority language for this project is C++ so I wrote this projects Project language file in C++
+int main()
+{
+    std::cout << "The majority language for this project is C++ so I wrote this projects Project language file in C++";
+}
+/* File info
+* File type: C++ Source code file (*.cpp)
+* File version: 1 (Thursday, January 14th 2021 at 6:50 pm)
+* Line count (including blank lines and compiler line): 14
+*/
+// End of script
