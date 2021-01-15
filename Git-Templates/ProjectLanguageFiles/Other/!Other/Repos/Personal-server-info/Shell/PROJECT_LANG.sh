e1a69578952926f7b2ac8f5234ea8e1b6f9670a0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-14 19:03:59 -0800

    PROJECT_LANG.sh
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Personal-server-info/Shell/PROJECT_LANG.sh
@@ -0,0 +1,7 @@
+# Start of script
+# I have decided to write this project (personal server info) in Shell, as it is my go-to language for Linux configuration, and it fits well with the management of my server.
+echo ("I have decided to write this project (personal server info) in Shell, as it is my go-to language for Linux configuration, and it fits well with the management of my server.")
+# File version: 1 (Thursday, January 14th 2021 at 7:02 pm)
+# File type: Shell script file (*.sh)
+# Line count (including blank lines and compiler line): 8
+# End of script
