20b8faa33c440253c2942cec012e6e148ae9ee28
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-16 14:55:24 -0800

    PROJECT_LANG_V1.cpp
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Economica/C++/PROJECT_LANG_V1.cpp
@@ -0,0 +1,13 @@
+// Start of script
+#include <iostream>
+// The majority language for this project (Economica) is C++ so I wrote this projects Project language file in C++
+int main()
+{
+    std::cout << "The majority language for this project (Economica) is C++ so I wrote this projects Project language file in C++";
+}
+/* File info
+* File type: C++ Source code file (*.cpp)
+* File version: 1 (Saturday, January 16th 2021 at 2:53 pm)
+* Line count (including blank lines and compiler line): 14
+*/
+// End of script
