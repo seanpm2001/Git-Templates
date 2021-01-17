44c69f483d61f8fc58faf8fbd3584ac48b195715
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-17 14:50:27 -0800

    V2 update
    Updated this project language file to version 2, with minor styling fixes.

--- Git-Templates/ProjectLanguageFiles/Tools/!Repos/Candroid/Python/PROJECT_LANG_1.py
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/Candroid/Python/PROJECT_LANG_1.py
@@ -1,10 +1,10 @@
 # Start of script
-# 
-print ("Python is one of the 3 main project languages for Candroid. This is the python project language file")
+# Python is one of the 3 main project languages for Candroid. This is the python project language file.
+print ("Python is one of the 3 main project languages for Candroid. This is the python project language file.")
 """
 File info
 # File type: Python source file (*py)
-# File version: 1 (Saturday, January 16th 2021 at 7:35 pm)
+# File version: 2 (Sunday, January 17th 2021 at 2:48 pm)
 # Line count (including blank lines and compiler line): 11
 """
 # End of script
