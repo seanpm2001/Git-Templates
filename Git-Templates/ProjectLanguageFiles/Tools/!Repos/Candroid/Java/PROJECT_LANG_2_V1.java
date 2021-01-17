e1ef5bf502756a4847373fa6ac062473699626e8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-16 19:39:44 -0800

    PROJECT_LANG_2_V1.java
    An archive of the first version of the project language file for this directory,

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/Candroid/Java/PROJECT_LANG_2_V1.java
@@ -0,0 +1,11 @@
+// Start of script
+// Java is one of the 3 project languages for Candroid, as it is one of the 3 top languages. This is the Java project language file.
+public class ProjectLanguageFile {
+    public static void main(String[] args) {
+        System.out.println("Java is one of the 3 project languages for Candroid, as it is one of the 3 top languages. This is the Java project language file."); // Prints the string to the console.
+}}
+/* File version: 1 (Saturday, January 16th 2021 at 7:38 pm)
+* File type: Java source file (*.java)
+* Line count (including blank lines and compiler line): 12
+*/
+// End of script
