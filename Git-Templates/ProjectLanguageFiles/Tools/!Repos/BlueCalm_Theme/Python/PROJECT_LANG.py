d27b30dca37037a05504568feec0d4888bfba5f8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-02-10 15:28:58 -0800

    PROJECT_LANG.py
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/BlueCalm_Theme/Python/PROJECT_LANG.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this project (BlueCalm Theme) in Python, as it is my go to language and is highly functional and meets all the needs for this project.
+''' '''
+print("I have decided to write this project (BlueCalm Theme) in Python, as it is my go to language and is highly functional and meets all the needs for this project.")
+""" """
+# File version: 1 (Wednesday, February 10th 2021 at 3:28 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
