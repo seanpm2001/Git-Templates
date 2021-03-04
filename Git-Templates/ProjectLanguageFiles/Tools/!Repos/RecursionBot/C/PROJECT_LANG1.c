de443f56ba2bb0161d4d86c53f33efbfe73095f9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-03 18:04:32 -0800

    PROJECT_LANG1.c
    The current version (version 1) of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/RecursionBot/C/PROJECT_LANG1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// Start of script
+// This project (RecursionBot) contains source code in C, as it is one of the 9 languages needed to be used to build the program.
+int main(void) {
+    printf("This project (RecursionBot) contains source code in C, as it is one of the 9 languages needed to be used to build the program.\n");
+}
+// File version: 1 (Wednesday, March 3rd 2021 at 3:28 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 11
+// End of script
