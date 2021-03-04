7eac0654b58e401aa1d2169bcc2068b39bd0b21a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-03 18:11:55 -0800

    PROJECT_LANG3_V1.py
    An archive of the 1st version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/RecursionBot/Python/PROJECT_LANG3_V1.py
@@ -0,0 +1,9 @@
+# Start of script
+# This project (RecursionBot) contains source code in Python, as it is one of the 9 languages needed to be used to build the program.
+''' '''
+print("This project (RecursionBot) contains source code in Python, as it is one of the 9 languages needed to be used to build the program.")
+""" """
+# File version: 1 (Wednesday, March 3rd 2021 at 6:10 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
