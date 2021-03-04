908badd5edb7671d1ae1d2025f101b63c4ec872a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-03 18:55:35 -0800

    PROJECT_LANG9_V1.vhdl
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/RecursionBot/VHDL/PROJECT_LANG9_V1.vhdl
@@ -0,0 +1,6 @@
+-- Start of script
+-- This project (RecursionBot) contains source code in Virtual Hard Disk Language (VHDL) format, as it is one of the 9 languages needed to be used to build the program.");
+-- File version: 1 (Wednesday, March 3rd 2021 at 6:54 pm)
+-- File type: Virtual Hard Disk Language (VHDL) Source file (*.vhdl)
+-- Line count (including blank lines and compiler line): 7
+-- End of script
