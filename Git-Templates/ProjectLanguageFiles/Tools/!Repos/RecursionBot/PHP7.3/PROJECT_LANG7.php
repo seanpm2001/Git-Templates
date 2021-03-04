555e13d14e16074832cff1625c18ec76190a2fa8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-03 18:47:04 -0800

    PROJECT_LANG7.php
    The current version (version 1) of the project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/RecursionBot/PHP7.3/PROJECT_LANG7.php
@@ -0,0 +1,8 @@
+<?php // Start of script
+echo ("This project (RecursionBot) contains source code in PHP 7.3, as it is one of the 9 languages needed to be used to build the program.");
+// This project (RecursionBot) contains source code in PHP 7.3, as it is one of the 9 languages needed to be used to build the program.
+// File version: 1 (Wednesday, March 3rd 2021 at 6:45 pm)
+// File type: PHP script file (*.php)
+// Line count (including blank lines and compiler line): 9
+// End of script
+?>
