ffc9b5ab14992f90a22f85d70d6e408b9987a7d6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-04 18:43:01 -0800

    PROJECT_LANG_V1.py
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Pynuke128/PROJECT_LANG_V1.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this project (Pynuke128) in Python, as it is already the main language of the project.
+''' '''
+print("I have decided to write this project (Pynuke128) in Python, as it is already the main language of the project.")
+""" """
+# File version: 1 (Thursday, March 4th 2021 at 6:41 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
