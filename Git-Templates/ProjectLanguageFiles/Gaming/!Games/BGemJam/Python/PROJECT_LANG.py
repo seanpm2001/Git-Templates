04d2756341c809201c20727a9685fd391cc736a7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-17 19:00:31 -0700

    PROJECT_LANG.py
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/BGemJam/Python/PROJECT_LANG.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this project (BGemJam) in Python, as it is my go to language and is highly functional and meets all the needs for this project.
+''' '''
+print("I have decided to write this project (BGemJam) in Python, as it is my go to language and is highly functional and meets all the needs for this project.")
+""" """
+# File version: 1 (Wednesday, March 17th 2020 at 6:59 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
