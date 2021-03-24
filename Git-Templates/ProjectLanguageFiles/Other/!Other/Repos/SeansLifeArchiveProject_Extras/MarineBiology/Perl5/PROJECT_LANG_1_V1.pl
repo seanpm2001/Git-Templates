d80a37d5c1e1ceb9cfad0abce109c5919f73735f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-24 16:48:42 -0700

    PROJECT_LANG_1_V1.pl
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SeansLifeArchiveProject_Extras/MarineBiology/Perl5/PROJECT_LANG_1_V1.pl
@@ -0,0 +1,7 @@
+# Start of script
+# I have chosen to have 6 aquatic themed project language files for this project. Perl is the first one, as Oysters produce Pe~~a~~rls.
+print ("I have chosen to have 6 aquatic themed project language files for this project. Perl is the first one, as Oysters produce Pe~~a~~rls.");
+# File version: 1 (Wednesday, March 24th 2021 at 4:46 pm)
+# File type: Perl 5 Source file (*.pl)
+# Line count (including blank lines and compiler line): 8
+# End of script
