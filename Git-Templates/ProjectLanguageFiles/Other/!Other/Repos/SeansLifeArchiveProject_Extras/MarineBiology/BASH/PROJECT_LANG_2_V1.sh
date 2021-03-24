a096c3306b28c1cd11d0ee403e99e08fc8bdf102
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-24 16:55:14 -0700

    PROJECT_LANG_2_V1.sh
    An archive of the 2nd version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SeansLifeArchiveProject_Extras/MarineBiology/BASH/PROJECT_LANG_2_V1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Start of script
+# I have chosen to have 6 aquatic themed project language files for this project. Shell (BASH) is the first one, as sea turtles have shells.
+echo ("# I have chosen to have 6 aquatic themed project language files for this project. Shell (BASH) is the first one, as sea turtles have shells.");
+# File version: 1 (Wednesday, March 24th 2021 at 4:52 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 9
+# End of script
