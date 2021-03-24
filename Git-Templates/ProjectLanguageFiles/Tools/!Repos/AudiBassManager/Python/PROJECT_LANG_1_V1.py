b86fc80a597501b5a898910797ef102367ab50ed
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-23 17:17:14 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/AudiBassManager/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this project (AudiBass Manager) in Python as the main language, as it is my go to language and is highly functional and meets all the needs for this project.
+''' '''
+print("I have decided to write this project (AudiBass Manager) in Python as the main language, as it is my go to language and is highly functional and meets all the needs for this project.")
+""" """
+# File version: 1 (Tuesday, March 23rd 2020 at 7:14 pm)
+# File type: Python script file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
