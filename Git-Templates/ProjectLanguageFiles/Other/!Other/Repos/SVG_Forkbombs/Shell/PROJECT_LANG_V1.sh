47e6bae25079c1c7fc542075ef48a31b3462ec4a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-23 20:30:00 -0700

    PROJECT_LANG_V1.sh
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SVG_Forkbombs/Shell/PROJECT_LANG_V1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -x
+# Start of script
+# I have decided to write this project in Shell script, as the first forkbomb was written in Shell, and this project is about forkbombs. I chose this, as I didn't see any fitting languages related to W3 standards that would fit the forkbomb category.
+echo ("I have decided to write this project in Shell script, as the first forkbomb was written in Shell, and this project is about forkbombs. I chose this, as I didn't see any fitting languages related to W3 standards that would fit the forkbomb category.");
+# File version: 1 (Tuesday, March 23rd 2021 at 8:28 pm)
+# File type: Shell script file (*.sh)
+# Line count (including blank lines and compiler line): 9
+# End of script
