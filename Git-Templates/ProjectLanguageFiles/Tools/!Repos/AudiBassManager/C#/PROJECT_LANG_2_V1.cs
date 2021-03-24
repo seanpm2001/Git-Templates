060ce444eec047c4e26beccb16ea0f882a18ab4e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-23 17:19:59 -0700

    PROJECT_LANG_2_V1.cs
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/AudiBassManager/C#/PROJECT_LANG_2_V1.cs
@@ -0,0 +1,13 @@
+// Start of script
+using System;
+// I decided to make C# the secondary language of this project (AudiBass Manager) as C# is named after a music note and this is a music project. Also, the Windows port is codenamed Musi`c#` due to this. A third project language is being considered.
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("I decided to make C# the secondary language of this project (AudiBass Manager) as C# is named after a music note and this is a music project. Also, the Windows port is codenamed Musi`c#` due to this. A third project language is being considered.");
+}}
+// File version: 1 (Tuesday, March 23rd 2021 at 5:18 pm)
+// File type: C Sharp Source file (*.cs)
+// Line count (including blank lines and compiler line): 14
+// End of script
