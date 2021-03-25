b288c81a967d09d8cf5e204e422de3791e020fd9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-24 17:26:39 -0700

    PROJECT_LANG_6_V1.scala
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SeansLifeArchiveProject_Extras/MarineBiology/Scala2.1/PROJECT_LANG_6_V1.scala
@@ -0,0 +1,13 @@
+// Start of script
+// I have chosen to have 6 aquatic themed project language files for this project. Scala is the sixth one, as it is similar to scale, and fish have scales.
+import users // Not necessary, just getting used to the language
+package users // Not necessary, just getting used to the language
+object ProjectLanguageSix extends App {
+   println("I have chosen to have 6 aquatic themed project language files for this project. Scala is the sixth one, as it is similar to scale, and fish have scales.")
+ }
+return object(ProjectLanguageSix)
+// Note: I am not very good at Scala or Java, so this script may not work.
+// File version: 1 (Wednesday, March 24th 2021 at 5:24 pm)
+// File type: Scala 2.1 Source file (*.scala)
+// Line count (including blank lines and compiler line): 14
+// End of script
