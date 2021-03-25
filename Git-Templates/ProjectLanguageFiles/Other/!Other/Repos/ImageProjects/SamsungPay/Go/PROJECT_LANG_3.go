989bdb00008ccd8c29a59e0fd0fbb9e7316bbdd9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-24 18:20:10 -0700

    PROJECT_LANG_3.go
    The 3rd project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/SamsungPay/Go/PROJECT_LANG_3.go
@@ -0,0 +1,13 @@
+// Start of script
+// I have chosen to have 3 languages for this project, the main, main one being C, due to Android and Samsungs reliance on Linux, which is written heavily in C. The 2nd project language file is written in Java, as the API Samsung writes is in Java (as far as I know) The 3rd language is written in Go, as I want this feature to Go away. This is the third project language file, and it is written in Go (2008 Google language, instead of the 2004 Francis McCabe language, I chose not to use it this time)
+package main
+import "fmt"
+func main() {
+    fmt.Println("I have chosen to have 3 languages for this project, the main, main one being C, due to Android and Samsungs reliance on Linux, which is written heavily in C. The 2nd project language file is written in Java, as the API Samsung writes is in Java (as far as I know) The 3rd language is written in Go, as I want this feature to Go away. This is the third project language file, and it is written in Go (2008 Google language, instead of the 2004 Francis McCabe language, I chose not to use it this time)")
+}
+/* File info
+* File version: 1 (Wednesday, March 24th 2021 at 6:18 pm)
+* File type: Google Go Source file (*.go)
+* Line count (including blank lines and compiler line): 14
+*/
+// End of script
