8818fad3769aaefd07792c00e48edcac5bdbc11b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-24 17:11:41 -0700

    PROJECT_LANG_4.rb
    The 4th project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SeansLifeArchiveProject_Extras/MarineBiology/Ruby3.0/PROJECT_LANG_4.rb
@@ -0,0 +1,7 @@
+# Start of script
+# I have chosen to have 6 aquatic themed project language files for this project. Ruby is the fourth one, as Rubys are common treasure in the sea.
+puts 'I have chosen to have 6 aquatic themed project language files for this project. Ruby is the fourth one, as Rubys are common treasure in the sea.'
+# File version: 1 (Wednesday, March 24th 2021 at 5:10 pm)
+# File type: Ruby 3.0 Source file (*.rb)
+# Line count (including blank lines and compiler line): 8
+# End of script
