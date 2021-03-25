dd51e54799a7ac5f2ee901d8b4fd0f50135de4c0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-24 17:18:51 -0700

    PROJECT_LANG_5.swift
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SeansLifeArchiveProject_Extras/MarineBiology/Swift5.3/PROJECT_LANG_5.swift
@@ -0,0 +1,11 @@
+// Start of script
+// I have chosen to have 6 aquatic themed project language files for this project. Swift is the fifth one, as the small fish have to be swift to survive
+var str = "I have chosen to have 6 aquatic themed project language files for this project. Swift is the fifth one, as the small fish have to be swift to survive";
+myValue = @String
+let myValue = str();
+return myValue;
+// I don't know how to make working strings in Swift
+// File version: 1 (Wednesday, March 24th 2021 at 5:17 pm)
+// File type: Swift 5.3 Source file (*.swift)
+// Line count (including blank lines and compiler line): 12
+// End of script
