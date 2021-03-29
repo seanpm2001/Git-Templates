f360303b19a7d2ab7091c533691502152e0311eb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-28 18:28:34 -0700

    PROJECT_LANG_4.bat
    The 4th project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Team-TRM/Batchfile/PROJECT_LANG_4.bat
@@ -0,0 +1,10 @@
+REM Start of script
+@ECHO OFF
+REM I have decided to have the fourth project language for this project be Batch file (DOS/Windows) as I used to write some batch files back in the era this repository is based on.
+ECHO "I have decided to have the fourth project language for this project be Batch file (DOS/Windows) as I used to write some batch files back in the era this repository is based on."
+PAUSE
+REM File info
+REM File version: 1 (Sunday, March 28th 2021 at 6:27 pm)
+REM File type: MS-DOS Batch file (*.bat)
+REM Line count (including blank lines and compiler line): 11
+REM End of script
