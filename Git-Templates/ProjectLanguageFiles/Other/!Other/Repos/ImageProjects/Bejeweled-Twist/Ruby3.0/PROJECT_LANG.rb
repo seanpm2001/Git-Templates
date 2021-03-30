b57357c548d595b1b42f9ca129d86e255cc2e5d0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-29 18:52:37 -0700

    PROJECT_LANG.rb
    The project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/Bejeweled-Twist/Ruby3.0/PROJECT_LANG.rb
@@ -0,0 +1,8 @@
+# Start of script
+# I decided to write this project (SeansLifeArchive_Images_Bejeweled-Twist) in Ruby, as a ruby is a type of gem/jewel, and it fits here.
+puts "I decided to write this project (SeansLifeArchive_Images_Bejeweled-Twist) in Ruby, as a ruby is a type of gem/jewel, and it fits here.";
+# File info
+# File version: 1 (Monday, March 29th 2021 at 6:51 pm)
+# File type: Ruby 3.0 script file (*.rb)
+# Line count (including blank lines and compiler line): 9
+# End of script
