f86bac656b6ca952dff54e13b06d88cef83c9c31
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-30 14:28:51 -0700

    PROJECT_LANG_2.cpp
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/YKDefender/C++/PROJECT_LANG_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// Start of script
+// I decided to make C++ the second project language for this project (YKDefender) as C++ is needed to pair with C to gain access to the system time.
+class projectLanguageFile {
+	int main() {
+    		std::cout << "I decided to make C++ the second project language for this project (YKDefender) as C++ is needed to pair with C to gain access to the system time.\n";
+    		break;
+	}
+return main();
+break;
+}
+// File info
+// File version: 1 (Tuesday, March 30th 2021 at 2:26 pm)
+// File type: C++ Source file (*.cpp)
+// Line count (including blank lines and compiler line): 17
+// End of script
