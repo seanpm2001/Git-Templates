838d5d0c8d5f92ebb85614c2d235075f0c9ad9a1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-30 14:13:20 -0700

    PROJECT_LANG_2_V1.php
    An archive of the first version of the 2nd project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/AdBool/PHP8.0.3/PROJECT_LANG_2_V1.php
@@ -0,0 +1,11 @@
+<?php
+// Start of script
+// I chose PHP as the second language for this project (AdBool) as it is the 2nd most common language to use for access to the source needed for this project.
+echo ("I chose PHP as the second language for this project (AdBool) as it is the 2nd most common language to use for access to the source needed for this project.");
+/* File info
+* File type: PHP 8.0.3 Source file (*.php)
+* File version: 1 (Tuesday, March 30th 2021 at 2:11 pm)
+* Line count (including blank lines and compiler line): 12
+*/
+// End of script
+?>
