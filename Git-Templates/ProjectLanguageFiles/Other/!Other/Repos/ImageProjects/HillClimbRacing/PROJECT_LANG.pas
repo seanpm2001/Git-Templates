ab52fb434dea1f194e7414880f3a6866debc5640
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-29 21:29:52 -0700

    PROJECT_LANG.pas
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/HillClimbRacing/PROJECT_LANG.pas
@@ -0,0 +1,12 @@
+// Start of script
+program LanguageFile(output);
+begin
+    Write('A lot of the games mechanics rely on weight and speed, so for this project (SeansLifeArchive_Images_Hill-Climb-Racing) I chose Pascal as the project language.')
+    No ";" is required after the last statement of a block -
+        adding one adds a "null statement" to the program, which is ignored by the compiler.}
+end.
+// A lot of the games mechanics rely on weight and speed, so for this project (SeansLifeArchive_Images_Hill-Climb-Racing) I chose Pascal as the project language.
+// File version: 1 (Monday, March 29th 2021 at 9:26 pm)
+// File type: Pascal source file (*.pas)
+// Line count (including blank lines and compiler line): 13
+// End of script
