eb0534a730167150baa3c52f9bbd7bb147a2e742
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-29 21:51:51 -0700

    PROJECT_LANG_V1.py
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/FeedingFrenzy/Python3/PROJECT_LANG_V1.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to write this project (SeansLifeArchive_Images_Feeding-Frenzy) in Python, as it is my go to language and is highly functional and meets all the needs for this project. Also, this is now a Python feeding frenzy.
+''' '''
+print("I have decided to write this project (SeansLifeArchive_Images_Feeding-Frenzy) in Python, as it is my go to language and is highly functional and meets all the needs for this project. Also, this is now a Python feeding frenzy.")
+""" """
+# File version: 1 (Monday, March 29th 2020 at 9:50 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
