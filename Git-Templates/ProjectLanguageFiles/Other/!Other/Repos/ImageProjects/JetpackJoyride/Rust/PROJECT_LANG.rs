8b5f25566362a7bfb0f2a969557cfc76e5468c20
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-29 21:10:50 -0700

    PROJECT_LANG.rs
    The project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/JetpackJoyride/Rust/PROJECT_LANG.rs
@@ -0,0 +1,11 @@
+// Start of script
+// I don't know of any programming languages with a metal name (other than IronPython) so I chose Rust for this projects (SeansLifeArchive_Images_Jetpack-joyride) project language file, as the lab is going to get rusty before it has time to be repaired, and the main jetpack also looks a tad bit rusty.
+
+fn main() {
+    println!("I don't know of any programming languages with a metal name (other than IronPython) so I chose Rust for this projects (SeansLifeArchive_Images_Jetpack-joyride) project language file, as the lab is going to get rusty before it has time to be repaired, and the main jetpack also looks a tad bit rusty.");
+}
+
+// File version: 1 (Monday, March 29th 2021 at 9:09 pm)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 12
+// End of script
