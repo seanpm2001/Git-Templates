a336b42d2c74d154a7ee8acfe607770778613c0b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-29 21:22:25 -0700

    PROJECT_LANG_V1.cob
    An archive of the first version of the project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/PlantsVsZombies/PROJECT_LANG_V1.cob
@@ -0,0 +1,12 @@
+      *> Start of script
+      *> I chose COBOL as the project language file for this project (SeansLifeArchive_Images_PlantsVsZombies) as the zombies are as dead as COBOL, but are still alive.
+      IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello-world.
+       PROCEDURE DIVISION.
+           DISPLAY "I chose COBOL as the project language file for this project (SeansLifeArchive_Images_PlantsVsZombies) as the zombies are as dead as COBOL, but are still alive."
+           .
+      *> File info
+      *> File version: 1 (Monday, March 29th 2021 at 9:21 pm)
+      *> File type: COBOL Source file (*.cob)
+      *> Line count (including blank lines and compiler line): 13
+      *> End of script
