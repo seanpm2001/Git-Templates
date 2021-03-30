6d6edd48d02db229626e2c919b4a3ee22ec19c8a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-29 18:23:38 -0700

    PROJECT_LANG_1_V1.cpp
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/Keyoff/C++/PROJECT_LANG_1_V1.cpp
@@ -0,0 +1,13 @@
+// Start of script
+#include <iostream>
+// I decided to make the first project language file for this project (KeyOff) C++ as it is most needed to get high level access to the machine to perform the one needed task.
+int main()
+{
+    std::cout << "I decided to make the first project language file for this project (KeyOff) C++ as it is most needed to get high level access to the machine to perform the one needed task.";
+}
+/* File info
+* File type: C++ Source file (*.cpp)
+* File version: 1 (Monday, March 29th 2021 at 6:21 pm)
+* Line count (including blank lines and compiler line): 14
+*/
+// End of script
