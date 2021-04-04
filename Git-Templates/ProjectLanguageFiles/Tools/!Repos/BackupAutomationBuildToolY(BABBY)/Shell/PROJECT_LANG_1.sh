ec2e70fd2887d3a8a9e757287d2fd990f8191c60
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-03 19:27:13 -0700

    PROJECT_LANG_1.sh
    The first project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/BackupAutomationBuildToolY(BABBY)/Shell/PROJECT_LANG_1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the first project language file for this project due to it being the first language used, and due to its usefulness in automation.
+echo (" I have chosen Shell as the first project language file for this project due to it being the first language used, and due to its usefulness in automation.");
+# File version: 1 (Saturday, April 3rd 2021 at 7:25 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 9
+# End of script
