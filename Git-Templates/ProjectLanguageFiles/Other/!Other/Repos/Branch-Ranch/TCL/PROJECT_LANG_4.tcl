1b62d6d6dd03a5362f5ae4d0f5fc5e07d0d28ac1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-05 13:56:43 -0700

    PROJECT_LANG_4.tcl
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Branch-Ranch/TCL/PROJECT_LANG_4.tcl
@@ -0,0 +1,7 @@
+# Start of script
+# I have chosen TCL as the fourth project language file for this project (Branch-Ranch) because it is the 4th (of 4) languages used to write Git. Since this project is based solely on Git, it fits.
+puts "I have chosen TCL as the third project language file for this project (Branch-Ranch) because it is the 3rd (of 4) languages used to write Git. Since this project is based solely on Git, it fits."
+# File version: 1 (Monday, April 5th 2021 at 1:55 pm)
+# File type: Tool Command Language source file (*.tcl)
+# Line count (including blank lines and compiler line): 8
+# End of script
