eb8bdf160027751a183683796da2d7ec067f8f59
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-05 13:52:15 -0700

    PROJECT_LANG_2_V1.sh
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Branch-Ranch/Shell/PROJECT_LANG_2_V1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the second project language file for this project (Branch-Ranch) because it is the 2nd (of 4) languages used to write Git. Since this project is based solely on Git, it fits.
+echo ("I have chosen Shell as the second project language file for this project (Branch-Ranch) because it is the 2nd (of 4) languages used to write Git. Since this project is based solely on Git, it fits.");
+# File version: 1 (Monday, April 5th 2021 at 1:51 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 9
+# End of script
