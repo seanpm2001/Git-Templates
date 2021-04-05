69829caddcd12937beb09ae969aa608013983c35
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-05 13:49:39 -0700

    PROJECT_LANG_1.c
    The first project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Branch-Ranch/C/PROJECT_LANG_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// Start of script
+// I decided to make C the first project language file for this project (Branch Ranch) as it is one of the 4 languages that Git is written in. This project is based solely on Git, so it fits.
+union projectLanguageFile() {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: Branch-Ranch");
+    printf("I decided to make C the first project language file for this project (Branch Ranch) as it is one of the 4 languages that Git is written in. This project is based solely on Git, so it fits.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Monday, April 5th 2021 at 1:48 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 26
+// End of script
