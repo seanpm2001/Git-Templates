39a2ff50bc0105c350fb74b0c7e2436e098933ec
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-05 13:55:14 -0700

    PROJECT_LANG_3_V1.pl
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/Branch-Ranch/Perl5/PROJECT_LANG_3_V1.pl
@@ -0,0 +1,7 @@
+# Start of script
+# I have chosen Perl as the third project language file for this project (Branch-Ranch) because it is the 3rd (of 4) languages used to write Git. Since this project is based solely on Git, it fits.
+print ("I have chosen Perl as the third project language file for this project (Branch-Ranch) because it is the 3rd (of 4) languages used to write Git. Since this project is based solely on Git, it fits.");
+# File version: 1 (Monday, April 5th 2021 at 1:53 pm)
+# File type: Perl 5 Source file (*.pl)
+# Line count (including blank lines and compiler line): 8
+# End of script
