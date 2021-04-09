e1a41e5f2e6af6caefcd4a34a1baed6666d4d0fb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-08 18:50:34 -0700

    PROJECT_LANG_3_V1.asm
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/SeansLifeArchive_Images_VirtualBox/Assembly/PROJECT_LANG_3_V1.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the third project language for this project (SeansLifeArchive_Images_VirtualBox) as it is the 3rd language that VirtualBox is written in. This project is all about VirtualBox, so it fits.
+
+; File info
+; File version: 1 (Thursday, April 8th 2021 ay 6:49 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
