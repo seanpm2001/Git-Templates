0abc8908c3b1a4ace6415cd94d03c7fb7ea845b1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-08 18:54:38 -0700

    PROJECT_LANG_4.py
    The project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/SeansLifeArchive_Images_VirtualBox/Python3/PROJECT_LANG_4.py
@@ -0,0 +1,9 @@
+# Start of script
+# I have decided to make Python the 4th project language file for this project (SeansLifeArchive_Images_VirtualBox) as Python is the 4th language that VirtualBox is written in. This project is all about VirtualBox, so it fits.
+''' '''
+print("I have decided to make Python the 4th project language file for this project (SeansLifeArchive_Images_VirtualBox) as Python is the 4th language that VirtualBox is written in. This project is all about VirtualBox, so it fits.")
+""" """
+# File version: 1 (Thursday, April 8th 2021 at 6:53 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 10
+# End of script
