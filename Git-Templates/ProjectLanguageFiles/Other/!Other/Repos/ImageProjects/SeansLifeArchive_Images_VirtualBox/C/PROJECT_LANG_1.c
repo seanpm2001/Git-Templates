95c9a8aa489a9705450dd3e786d92d3ceb938051
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-08 18:41:29 -0700

    PROJECT_LANG_1.c
    The current version (version 1) of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/SeansLifeArchive_Images_VirtualBox/C/PROJECT_LANG_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// Start of script
+// I decided to make C the first project language file for this project (SeansLifeArchive_Images_VirtualBox) as it is one of the 4 languages that VirtualBox is written in. This project is based solely on VirtualBox, so it fits.
+union projectLanguageFile() {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: SeansLifeArchive_Images_VirtualBox");
+    printf("I decided to make C the first project language file for this project (Branch Ranch) as it is one of the 4 languages that Git is written in. This project is based solely on Git, so it fits.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Thursday, April 8th 2021 at 6:40 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 26
+// End of script
