406dc771ac120c30093bef6133d208e63340b5bc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-11 16:51:22 -0700

    PROJECT_LANG_V1.py
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/PoliticalProjects/Profa_or_Antifa/Python3/OldVersions/PROJECT_LANG_V1.py
@@ -0,0 +1,10 @@
+# Start of script
+# I chose Python as the project language for this project (Profa_Or_Antifa) as it is my preferred language, and works for this project.
+print ("I chose Python as the project language for this project (Profa_Or_Antifa) as it is my preferred language, and works for this project.")
+"""
+File info
+# File type: Python 3 source file (*py)
+# File version: 1 (Sunday, April 11th 2021 at 4:49 pm)
+# Line count (including blank lines and compiler line): 11
+"""
+# End of script
