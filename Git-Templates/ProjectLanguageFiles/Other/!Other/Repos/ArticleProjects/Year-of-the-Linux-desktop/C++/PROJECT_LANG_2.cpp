0dbff98c557cc303c43c8cfed6b6ddccb42dd344
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-13 16:23:51 -0700

    PROJECT_LANG_2.cpp
    The 2nd project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ArticleProjects/Year-of-the-Linux-desktop/C++/PROJECT_LANG_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// Start of script
+// I decided to make C++ the second project language for this project (Year-of-the-Linux-desktop) as C++ is the second language Linux is written in, and this project is directly about Linux, so it fits.
+class projectLanguageFile {
+	int main() {
+    		std::cout << "I decided to make C++ the second project language for this project (Year-of-the-Linux-desktop) as C++ is the second language Linux is written in, and this project is directly about Linux, so it fits.\n";
+    		break;
+	}
+return main();
+break;
+}
+// File info
+// File version: 1 (Tuesday, April 13th 2021 at 4:21 pm)
+// File type: C++ Source file (*.cpp)
+// Line count (including blank lines and compiler line): 17
+// End of script
