fd8b15544688900cc29e64524ec71a54b1f3cbf2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-13 16:19:36 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ArticleProjects/Year-of-the-Linux-desktop/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+// Start of script
+// I decided to make the main project language for this project (Year-of-the-Linux-desktop) the C programming language, as it is heavily influenced by UNIX, and has been the most used language for Linux development since inception. 
+union projectLanguageFile() {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: Year-of-the-Linux-desktop");
+    printf("I decided to make the main project language for this project (Year-of-the-Linux-desktop) the C programming language, as it is heavily influenced by UNIX, and has been the most used language for Linux development since inception.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+}
+printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Tuesday, April 13th 2021 at 4:13 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 27
+// End of script
