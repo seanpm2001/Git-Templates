821e7e905953531821a74ef51e51a13c4f45e8f1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-12 17:37:17 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/SeansLifeArchive_Images_Reddit/Python3/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the first project language for this project (SeansLifeArchive_Images_Reddit) to be Python, as Python is one of the 2 languages Reddit is written in, and this project is about Reddit, so it fits.
+''' '''
+class projectLanguageFile()
+print("Project Language File\n")
+print("For: SeansLifeArchive_Images_Reddit")
+print("I have decided to have the first project language for this project (SeansLifeArchive_Images_Reddit) to be Python, as Python is one of the 2 languages Reddit is written in, and this project is about Reddit, so it fits.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/proces manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, April 12th 2021 at 5:35 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
