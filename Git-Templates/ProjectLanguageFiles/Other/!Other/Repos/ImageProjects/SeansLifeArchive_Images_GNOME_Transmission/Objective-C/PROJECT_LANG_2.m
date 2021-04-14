a6ef4983b1c7c945cfb012d923aabfece8c36b77
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-13 17:06:43 -0700

    PROJECT_LANG_2.m
    The 2nd project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/SeansLifeArchive_Images_GNOME_Transmission/Objective-C/PROJECT_LANG_2.m
@@ -0,0 +1,21 @@
+@interface classname : projectLanguageFileTwo {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @MacOS_10_1_ObjectiveC classname : superclassname {
+    // I chose Objective-C as the second project language file for this project (SeansLifeArchive_Images_GNOME_Transmission) as it is the 2nd language that GNOME Transmission is written in, which this project is about.
+	NSLog(@"I chose Objective-C as the second project language file for this project (SeansLifeArchive_Images_GNOME_Transmission) as it is the 2nd language that GNOME Transmission is written in, which this project is about.");
+}
+@end
+/* File info
+* File version: `1 (Tuesday, April 13th 2021 at 5:05 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 22
+* This script is incomplete, and needs lots of work */
+// End of script
