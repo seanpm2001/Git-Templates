93cc8761ffab44c94871483d459fb784e19e2d55
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-14 16:38:59 -0700

    PROJECT_LANG_1_V1.js
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/SeansLifeArchive_Images_Discord/JavaScript/PROJECT_LANG_1_V1.js
@@ -0,0 +1,25 @@
+// Start of script
+// I chose JavaScript as the main first language for this project (SeansLifeArchive_Images_Discord) as JavaScript is one of the 4 main languages that Discord is written in. This project is all about Discord, so it fits.
+class projectLanguageFileOne() {
+  void main() {
+    console.log ("I chose JavaScript as the main first language for this project (SeansLifeArchive_Images_Discord) as JavaScript is one of the 4 main languages that Discord is written in. This project is all about Discord, so it fits.");
+    /* Garbage syntax, so I can test out 2 new keywords I recently learned. Uncomment to highlight
+    x = bool("true");
+    y = bool("false");
+    if (x == "false"); {
+      try y
+    } else: {
+      catch x;
+    }
+    */
+    break;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Tuesday, March 30th 2021 at 4:36 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 26
+*/
+// End of script
