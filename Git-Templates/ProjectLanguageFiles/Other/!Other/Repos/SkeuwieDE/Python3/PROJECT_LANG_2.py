c70e4467ae9305f718c1abe69d3e42d85b5043c1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-26 16:57:19 -0700

    PROJECT_LANG_2.py
    The 2nd project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SkeuwieDE/Python3/PROJECT_LANG_2.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to have the second project language for this project (Skeuwie) to be Python, as Python is a good second language for the application level, among other things, and I don't want to mimick KDE exactly. I want to stick with a language I know well.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileDesc():
+	print("Project Language File\n")
+	print("For: Skeuwie (Desktop Environment)")
+	print("I have decided to have the second project language for this project (Skeuwie) to be Python, as Python is a good second language for the application level, among other things, and I don't want to mimick KDE exactly. I want to stick with a language I know well.")
+projectLanguageFileDesc()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/proces manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, April 26th 2021 at 4:55 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
