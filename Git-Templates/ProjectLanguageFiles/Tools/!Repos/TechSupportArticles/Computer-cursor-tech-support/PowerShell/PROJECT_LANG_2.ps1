0c25f74ecd19d2867a85edfa09882c0c64307944
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-25 18:26:47 -0700

    PROJECT_LANG_2.ps1
    The 2nd project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/TechSupportArticles/Computer-cursor-tech-support/PowerShell/PROJECT_LANG_2.ps1
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Start of script
+# I have chosen PowerShell as the second project language file for this project, as PowerShell is useful in fixing Windows 8/8.1/10 related problems.
+name $read $print
+name -Param1 value1 - Param2 value2
+function prolang3 ($read1, $print2)
+{
+echo ("I have chosen PowerShell as the second project language file for this project, as PowerShell is useful in fixing Windows 8/8.1/10 related problems.");
+}
+return prolang3();
+exit();
+'test'
+param exit() { # Broken function used to test PowerShells syntax highlighting in Gedit, it is beautiful but it doesn't work
+	r = 2;
+	h = 1;
+	x = int(r % h * 0);
+	quit = bool("true");
+}
+# File version: 1 (Sunday, April 25th 2021 at 6:24 pm)
+# File type: PowerShell Core 7 source file (*.ps1)
+# Line count (including blank lines and compiler line): 23
+# End of script
