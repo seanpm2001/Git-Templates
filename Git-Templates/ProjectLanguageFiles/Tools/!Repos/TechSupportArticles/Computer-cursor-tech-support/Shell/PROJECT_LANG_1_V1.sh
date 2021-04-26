8d90e136903a6c76a36254c0241848c77ef9bad9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-25 18:22:09 -0700

    PROJECT_LANG_1_V1.sh
    An archive of the first version of the project language file for this directory.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/TechSupportArticles/Computer-cursor-tech-support/Shell/PROJECT_LANG_1_V1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the first project language file for this project due to it being the first language used, and due to its usefulness in repairing problems on Linux and MacOS.
+echo ("I have chosen Shell as the first project language file for this project due to it being the first language used, and due to its usefulness in repairing problems on Linux and MacOS.");
+# File version: 1 (Sunday, April 25th 2021 at 6:20 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 9
+# End of script
