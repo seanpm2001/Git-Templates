ff1228baae3ec060d873ca25b4e23299a72d4440
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-29 19:40:57 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/BluPhone/Python3/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the first project language for this project (BluPhone) to be Python, as Python is the main language used for the application level of this project.
+''' '''
+class projectLanguageFile()
+print("Project Language File\n")
+print("For: BluPhone")
+print("I have decided to have the first project language for this project (BluPhone) to be Python, as Python is the main language used for the application level of this project.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/proces manager")
+break
+""" """
+# File info
+# File version: 1 (Thursday, April 29th 2021 at 7:39 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
