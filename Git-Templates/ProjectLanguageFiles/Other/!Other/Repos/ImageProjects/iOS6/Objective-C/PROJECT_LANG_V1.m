e0f5d8056c1eb623ff10972f602e5f0696243d98
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-03 16:29:13 -0700

    PROJECT_LANG_V1.m
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/iOS6/Objective-C/PROJECT_LANG_V1.m
@@ -0,0 +1,21 @@
+@interface classname : projectLanguageFileTwo {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @iOS6_ObjectiveC classname : superclassname {
+    // I chose Objective-C as the main project language file for this project (SeansLifeArchive_Images_iOS_6) as it is the main language iOS 6 is written in (I think, if not, I still want to have more Objective-C projects) and this project is directly related to iOS 6, so it fits.
+	NSLog(@"I chose Objective-C as the third project language file for this project (SeansLifeArchive_Images_iOS_6) as it is the main language iOS 6 is written in (I think, if not, I still want to have more Objective-C projects) and this project is directly related to iOS 6, so it fits.");
+}
+@end
+/* File info
+* File version: `1 (Monday, May 3rd 2021 at 4:27 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 22
+* This script is incomplete, and needs lots of work */
+// End of script
