65c57d562c7eea566718753e5c330ca1fbbd4086
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-08 17:49:45 -0700

    PROJECT_LANG_V1.cs
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/ImageProjects/OneNote/C#/PROJECT_LANG_V1.cs
@@ -0,0 +1,14 @@
+// Start of script
+using System;
+// I decided to make C sharp the project language for this project (SeansLifeArchive_Images_OneNote-collection) as I couldn't find what OneNote is written in, and it is Microsoft, so I just decided to use C# as the project language, as that might be what is used, and if not, it is a Microsoft language for a Microsoft-related project.
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("I decided to make C sharp the project language for this project (SeansLifeArchive_Images_OneNote-collection) as I couldn't find what OneNote is written in, and it is Microsoft, so I just decided to use C# as the project language, as that might be what is used, and if not, it is a Microsoft language for a Microsoft-related project.");
+    }
+}
+// File version: 1 (Saturday, May 8th 2021 at 5:48 pm)
+// File type: C Sharp Source file (*.cs)
+// Line count (including blank lines and compiler line): 15
+// End of script
