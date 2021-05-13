0f8c13f72f440190dd0c273e23b44ce5619a9bc5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-12 17:39:48 -0700

    PROJECT_LANG.swift
    The project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/SEGA/Sonic_Advance/1/Swift/PROJECT_LANG.swift
@@ -0,0 +1,14 @@
+// Start of script
+// I have chosen Swift as the project language file for this project (SeansLifeArchive/Images/SonicAdvance1) as the game is about speed, speed can be swift, and I ruled out Assembly, C, and C++ as project language file options.
+class projectLanguageFile {
+var str = "have chosen Swift as the project language file for this project (SeansLifeArchive/Images/SonicAdvance1) as the game is about speed, speed can be swift, and I ruled out Assembly, C, and C++ as project language file options.";
+myValue = @String
+let myValue = str();
+return myValue;
+break;
+}
+// I don't know how to make working strings in Swift
+// File version: 1 (Wednesday, May 12th 2021 at7 5:38 pm)
+// File type: Swift 5.3 Source file (*.swift)
+// Line count (including blank lines and compiler line): 15
+// End of script
