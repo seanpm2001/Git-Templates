8406ce5c2f5142406e212c5bad119cfa47c51d8d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-18 15:44:19 -0700

    PROJECT_LANG_3.js
    Version 1 of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/SeansLifeArchive_Extras/Website-archives/JavaScript/PROJECT_LANG_3.js
@@ -0,0 +1,25 @@
+// Start of script
+// I chose JavaScript as the third project language for this project (SeansLifeArchive_Extras_Website_Archives) as JavaScript is one of the 3 common languages that most webpages are written in, and this project is about website archival, so it fits.
+class projectLanguageFileThree() {
+  void main() {
+    console.log ("I chose JavaScript as the third project language for this project (SeansLifeArchive_Extras_Website_Archives) as JavaScript is one of the 3 common languages that most webpages are written in, and this project is about website archival, so it fits.");
+    /* Garbage syntax, so I can test out 2 new keywords I recently learned. Uncomment to highlight
+    x = bool("true");
+    y = bool("false");
+    if (x == "false"); {
+      try y
+    } else: {
+      catch x;
+    }
+    */
+    break;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Tuesday, May 18th 2021 at 3:43 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 26
+*/
+// End of script
