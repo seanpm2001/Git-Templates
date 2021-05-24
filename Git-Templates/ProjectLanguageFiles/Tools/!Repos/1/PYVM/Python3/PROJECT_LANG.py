ebb775ec66f1fd9ec1f7f103647be2e99c14b3a0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-23 20:10:42 -0700

    PROJECT_LANG.py
    The project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/PYVM/Python3/PROJECT_LANG.py
@@ -0,0 +1,19 @@
+# Start of script
+# I have decided to make Python the project language file for this project (PYVM/Python Virtual Machine) as this project is directly about Python.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileOne():
+  print("Project Language File\n")
+  print("For: Senior Design")
+  print("I have decided to make Python the project language file for this project (PYVM/Python Virtual Machine) as this project is directly about Python.")
+  break
+return projectLanguageFileOne()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Sunday, May 23rd 2021 at 8:09 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 20
+# End of script
