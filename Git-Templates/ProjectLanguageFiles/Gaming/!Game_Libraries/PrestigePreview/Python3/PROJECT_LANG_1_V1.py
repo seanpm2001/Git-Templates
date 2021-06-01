4c6e519512be6d2093033b2986d1b017a7f92ebe
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-31 18:46:35 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Game_Libraries/PrestigePreview/Python3/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the first project language for this project (Prestige Preview) to be Python, as Python is the main language used for the application level of this project other than QML and Java.
+''' '''
+class projectLanguageFile()
+print("Project Language File\n")
+print("For: PrestigePreview")
+print("I have decided to have the first project language for this project (Prestige Preview) to be Python, as Python is the main language used for the application level of this project other than QML and Java.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/proces manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, May 31st 2021 at 6:44 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
