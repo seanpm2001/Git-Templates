9ecd583b503ae1b79cc22709034419643e3a1158
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-31 19:03:24 -0700

    PROJECT_LANG_3_V1.java
    An archive of the 1st version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Game_Libraries/PrestigePreview/Java/PROJECT_LANG_3_V1.java
@@ -0,0 +1,17 @@
+// Start of script
+// I decided to make Java the 3rd project language file for this project (PrestigePreview) due to development on the Android platform, and Androids reliance on Java. Kotlin will be an alternative 4th option.
+public class projectLanguageFileThree {
+	public static void main(String args) {
+		System.out.println("I decided to make Java the 3rd project language file for this project (PrestigePreview) due to development on the Android platform, and Androids reliance on Java. Kotlin will be an alternative 4th option.");
+}
+return main();
+int y = 3;
+System.out.println("Current project language file: " + int(y));
+break; } // No exit command exists
+// I am not the best with Java, so this script may not work
+/* File info
+* File version: 1 (Monday, May 31st 2021 at 7:01 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
