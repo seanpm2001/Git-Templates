ac4b3b27be85f7ccb7bb892c404ac7837d027d00
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-31 18:42:09 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/App-backpack/Python3/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the first project language for this project (App-Backpack) to be Python, as Python is the main language used for the application level of this project other than QML, and I want to learn file systems with Python.
+''' '''
+class projectLanguageFile()
+print("Project Language File\n")
+print("For: App-backpack")
+print("I have decided to have the first project language for this project (App-Backpack) to be Python, as Python is the main language used for the application level of this project other than QML, and I want to learn file systems with Python.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/proces manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, May 31st 2021 at 6:40 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
