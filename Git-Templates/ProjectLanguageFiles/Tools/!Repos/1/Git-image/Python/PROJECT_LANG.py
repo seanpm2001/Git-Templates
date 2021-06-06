ad09a03a97a0d8aadb6498034f5d25ecd34bea13
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-05 20:20:31 -0700

    PROJECT_LANG.py
    The project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/Git-image/Python/PROJECT_LANG.py
@@ -0,0 +1,19 @@
+# Start of script
+# I have decided to make Python the project language file for this project (Git-image) as this project is an article about stock and Artificial Intelligence image data sets, and for Artificial Intelligence, Python is a good high level language. For stock images and personal progress, Python is preferred, as it is my main goto language.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileOne():
+  print("Project Language File\n")
+  print("For: Git-image")
+  print("I have decided to make Python the project language file for this project (Git-image) as this project is an article about stock and Artificial Intelligence image data sets, and for Artificial Intelligence, Python is a good high level language. For stock images and personal progress, Python is preferred, as it is my main goto language.")
+  break
+return projectLanguageFileOne()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Saturday, June 5th 2021 at 8:18 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 20
+# End of script
