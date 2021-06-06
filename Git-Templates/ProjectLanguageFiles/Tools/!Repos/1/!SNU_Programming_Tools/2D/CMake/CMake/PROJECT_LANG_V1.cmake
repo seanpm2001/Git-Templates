c3fd8a8432936320f5020065157b7ef2ad561a47
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-05 18:28:20 -0700

    PROJECT_LANG_V1.cmake
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/!SNU_Programming_Tools/2D/CMake/CMake/PROJECT_LANG_V1.cmake
@@ -0,0 +1,19 @@
+# Start of script
+# This file does not contain valid CMake Syntax. Error on line 12, unable to create an output without a non-existent file.
+# src/CMakeLists.txt
+cmake_minimum_required(VERSION 3.10)
+
+# set the project name
+project("Hello World")
+
+# I decided to make CMake the project language file for this project (SNU/2D/Programming Tools/IDE/CMake) as this is a CMake IDE, and its main language should be CMake.
+
+# specify the executable and corresponding source file
+add_executable("I decided to make CMake the project language file for this project (SNU/2D/Programming Tools/IDE/CMake) as this is a CMake IDE, and its main language should be CMake.")
+
+# File info
+# File type: CMake source file (*.cmake)
+# File version: 1 (Saturday, June 5th 2021 at 6:26 pm
+# Line count (including blank lines and compiler line): 20
+
+# End of script
