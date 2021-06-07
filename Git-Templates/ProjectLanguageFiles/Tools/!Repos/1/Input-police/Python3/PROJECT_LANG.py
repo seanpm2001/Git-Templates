6126e4b022646ccd5aeb69e7970aedfbe5d3259f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-07 16:38:10 -0700

    PROJECT_LANG.py
    The project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/Input-police/Python3/PROJECT_LANG.py
@@ -0,0 +1,19 @@
+# Start of script
+# I have decided to make Python the project language file for this project (Input-police) as Python is my go-to language, and a fuzzing tool in Python would be in demand, plus it is a language I am good at.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileOne():
+  print("Project Language File\n")
+  print("For: Input-police")
+  print("I have decided to make Python the project language file for this project (Input-police) as Python is my go-to language, and a fuzzing tool in Python would be in demand, plus it is a language I am good at.")
+  break
+return projectLanguageFileOne()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, June 7th 2021 at 4:37 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 20
+# End of script
