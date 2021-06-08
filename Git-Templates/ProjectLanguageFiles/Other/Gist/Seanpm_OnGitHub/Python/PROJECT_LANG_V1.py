79b8898810c2073fceeef8278f31ec16e16f057b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-07 19:16:46 -0700

    PROJECT_LANG_V1.py
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/Gist/Seanpm_OnGitHub/Python/PROJECT_LANG_V1.py
@@ -0,0 +1,25 @@
+# Start of script
+x = int(2)
+y = int(16)
+z = int(2)
+a = int(0)
+a == (x ** y * z)
+print(a)
+"""
+PROJECT_LANG.py
+I made this project language file for this project (Gist/Seanpm_OnGitHub) so that a programming language is associated with this project. I chose to make only 1, prominently with Python. I chose the language I am best at. I feel it fits best here.
+"""
+class projectLanguageFile
+print ("PROJECT_LANG.py")
+print ("I made this project language file for this project (Gist/Seanpm_OnGitHub) so that a programming language is associated with this project. I chose to make only 1, prominently with Python. I chose the language I am best at. I feel it fits best here.")
+noMore = input("Press [ENTER] key to quit")
+print ("The program should have stopped. If the program has not yet stopped, try ending the process, or closing the terminal window")
+break
+# End of script
+"""
+File info
+File type: Python 3 source file (*.py)
+File version: 1 (Monday, June 7th 2021 at 7:15 pm)
+Line count (including blank lines and compiler line): 26
+Language: Python 3.10
+"""
