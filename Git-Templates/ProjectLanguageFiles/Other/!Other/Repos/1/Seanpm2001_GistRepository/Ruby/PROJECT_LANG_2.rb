4f86a54d66c479552ded81a89d4d721b1dd35652
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-08 18:59:33 -0700

    PROJECT_LANG_2.rb
    The project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/1/Seanpm2001_GistRepository/Ruby/PROJECT_LANG_2.rb
@@ -0,0 +1,21 @@
+# Start of script
+# GitHub is written mostly in Ruby, not Go (2009 Google language) and I want this project (Seanpm2001_GistRepository) to have a language assigned to it that isn't Go this time, so I created this Ruby source file (I don't want it to be Go, as I hate Google, and I am not going to repeat the mistake of having a key project (SeansLifeArchive_Images_GitHub) having a bad language associated with it. This file will override the Go file in majority.
+
+# Old Go file kept for reference
+# // Gist.github.com (GistHub0 is part of GitHub, and GitHub is written mostly in Go (2009 Google language) and I want this project to have a language assigned to it, so I created this Go (2009 Google language) file
+# package main
+# import "fmt"
+# func main() {
+#    // Project language file
+#    fmt.Println("Gist.github.com (GistHub0 is part of GitHub, and GitHub is written mostly in Go (2009 Google language) and I want this project to have a language assigned to it, so I created this Go (2009 Google language) file")
+# }
+# // End of script
+
+puts "GitHub is written mostly in Ruby, not Go (2009 Google language) and I want this project (Seanpm2001_GistRepository) to have a language assigned to it that isn't Go this time, so I created this Ruby source file (I don't want it to be Go, as I hate Google, and I am not going to repeat the mistake of having a key project (SeansLifeArchive_Images_GitHub) having a bad language associated with it. This file will override the Go file in majority."
+
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (Tuesday, June 8th 2021 at 6:58 pm)
+# Line count (including blank lines and compiler line): 22
+
+# End of script
