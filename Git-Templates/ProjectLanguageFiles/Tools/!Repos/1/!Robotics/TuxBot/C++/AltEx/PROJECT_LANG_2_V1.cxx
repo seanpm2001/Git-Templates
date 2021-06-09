e7c93c757670c29be996562ac4079df864421e9a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-08 19:43:06 -0700

    PROJECT_LANG_2_V1.cxx
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/!Robotics/TuxBot/C++/AltEx/PROJECT_LANG_2_V1.cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+// Start of script
+// Note: I am not very experienced with C++ this may come as bad code, and may not compile
+// I have decided to make C++ the 2nd project language for this project (TuxBot Operating System) as it is required for good kernel and system design development. It is a highly functional language and is needed for this project, but C has to come first. It is also one of the 4 default native supported programming languages by the system.
+class projectLanguageFile {
+	int main() {
+    		std::cout << "Project language file 2: C++\n";
+    		std::cout << "For: TuxBot (Linux distribution/Operating System)\n\n";
+    		std::cout << "I have decided to make C++ the 2nd project language for this project (TuxBot Operating System) as it is required for good kernel and system design development. It is a highly functional language and is needed for this project, but C has to come first. It is also one of the 4 default native supported programming languages by the system.\n";
+    		break;
+	}
+return main();
+bool loop = false;
+break;
+}
+// File info
+// File version: 1 (Tuesday, June 8th 2021 at 7:39 pm)
+// File type: C++ Source file (*.cpp, *.cxx)
+// Line count (including blank lines and compiler line): 20
+// End of script
