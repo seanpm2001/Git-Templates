2cbe803282a5d8658f08d3652cd9ae686fb587af
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-09 17:45:59 -0700

    PROJECT_LANG_V1.for
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/1/Fortran-Fortress/PROJECT_LANG_V1.for
@@ -0,0 +1,15 @@
+C Start of script
+
+C I decided to make FORTRAN the main programming language for this project (FORTRAN FORTRESS) as this project is all about Fortran and playing around with it. The fortress of FORTRAN should be and will be written in Fortran.
+program prolang
+     print *, "I decided to make FORTRAN the main programming language for this project (FORTRAN FORTRESS) as this project is all about Fortran and playing around with it. The fortress of FORTRAN should be and will be written in Fortran."
+end program prolang
+STOP
+END
+
+C File info
+C File type: FORTRAN 90 source file (*.f90, *.f, *.for)
+C File version: 1 (Wednesday, June 9th 2021 at 5:43 pm)
+C Line count (including blank lines and compiler line): 16
+
+C End of script
