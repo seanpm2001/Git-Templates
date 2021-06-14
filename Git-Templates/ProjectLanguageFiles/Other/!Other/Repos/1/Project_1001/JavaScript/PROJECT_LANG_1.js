281f47d87cb008d71756debd02839b44ac3640d7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-13 20:23:51 -0700

    PROJECT_LANG_1.js
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/1/Project_1001/JavaScript/PROJECT_LANG_1.js
@@ -0,0 +1,16 @@
+// Start of script
+// I chose JavaScript as the main first language for this project (Project 1001) as JavaScript is one of the first languages used by this project, and it is useful for web development.
+class projectLanguageFileOne() {
+  void main() {
+    console.log ("I chose JavaScript as the main first language for this project (Project 1001) as JavaScript is one of the first languages used by this project, and it is useful for web development.");
+    break;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Sunday, June 13th 2021 at 8:22 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 17
+*/
+// End of script
