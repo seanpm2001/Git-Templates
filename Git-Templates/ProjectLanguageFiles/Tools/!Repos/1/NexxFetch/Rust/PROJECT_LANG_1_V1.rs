5500477a7010b537e6c845e590172ce8e007ad0f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-16 20:44:20 -0700

    PROJECT_LANG_1_V1.rs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/NexxFetch/Rust/PROJECT_LANG_1_V1.rs
@@ -0,0 +1,16 @@
+// Start of script
+// I chose Rust as the first project language for this project (NexxFetch) as Firefox is notably mostly written in Rust, and I intend to support Firefox heavily in this project, and is the first browser to be tested.
+
+fn main() {
+    println!("I chose Rust as the first project language for this project (NexxFetch) as Firefox is notably mostly written in Rust, and I intend to support Firefox heavily in this project, and is the first browser to be tested.");
+    break;
+}
+return main();
+break;
+
+// File info
+// File version: 1 (Wednesday, June 16th 2021 at 8:41 pm)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 17
+
+// End of script
