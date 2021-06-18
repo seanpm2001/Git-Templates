265584a0e12a44fa70fea5ee85d9652110e47a15
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-17 19:41:14 -0700

    PROJECT_LANG_V1.c
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Other/!Other/Repos/1/SNU/2D/DennisRitchieMemorial/C/PROJECT_LANG_V1.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+// Start of script
+// I decided to make C the main project language for this project (SNU_2D_DennisRitchieMemorial) as it is the most influential invention that Dennis Ritchie made, and it significantly adds to his legacy and should be respected in a memorial project as the only project language file (exceptions.override)
+union projectLanguageFile() {
+int main(void) {
+    printf("Project language file: C");
+    printf("For: SNU/2D/DennisRitchieMemorial");
+    printf("I decided to make C the main project language for this project (SNU_2D_DennisRitchieMemorial) as it is the most influential invention that Dennis Ritchie made, and it significantly adds to his legacy and should be respected in a memorial project as the only project language file (exceptions.override)\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+}
+printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Thursday, June 17th 2021 at 7:39 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 27
+// End of script
