1a7b2b64071de6916cb638138f63d270ffbe6a50
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-19 16:48:30 -0700

    PROJECT_LANG_3_V1.m
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/TouchPetsDogs2Rewritten/Objective-C/PROJECT_LANG_3_V1.m
@@ -0,0 +1,21 @@
+@interface classname : projectLanguageFileThree {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @iOS6_ObjectiveC classname : superclassname {
+    // I chose Objective-C as the third project language file for this project (Touch Pets Dogs 2 Rewritten) as it is the language required for writing older iOS and MacOS applications.
+	NSLog(@"I chose Objective-C as the third project language file for this project (Touch Pets Dogs 2 Rewritten) as it is the language required for writing older iOS and MacOS applications.");
+}
+@end
+/* File info
+* File version: `1 (Saturday, June 19th 2021 at 4:46 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 22
+* This script is incomplete, and needs lots of work */
+// End of script
