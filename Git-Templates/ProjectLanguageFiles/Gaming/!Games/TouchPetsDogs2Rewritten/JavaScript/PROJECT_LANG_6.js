9076b4361b850433334d3d5d0a7dcbe08cb26e81
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-19 17:03:12 -0700

    PROJECT_LANG_6.js
    The current version (version 1) of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/TouchPetsDogs2Rewritten/JavaScript/PROJECT_LANG_6.js
@@ -0,0 +1,16 @@
+// Start of script
+// I chose JavaScript as the sixth project language for this project (Touch Pets Dogs 2 Rewritten) as JavaScript is one of the three languages used for the web port of this project.
+class projectLanguageFileSix() {
+  void main() {
+    console.log ("I chose JavaScript as the sixth project language for this project (Touch Pets Dogs 2 Rewritten) as JavaScript is one of the three languages used for the web port of this project.");
+    break;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Saturday, June 19th 2021 at 5:02 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 17
+*/
+// End of script
