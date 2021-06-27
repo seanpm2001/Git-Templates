6b4f6bdbb897ef910453a8a9627a118f5b3645a9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-27 16:12:08 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/Engineer_Superdesk/Python/PROJECT_LANG_1.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to make Python 3 the 1st project language for this project (Engineer Superdesk) as it is good for application development, and has many uses for this project.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileFourDesc():
+	print("Project Language File 1\n")
+	print("For: Engineer Superdesk")
+	print("I have decided to make Python 3 the 1st project language for this project (Engineer Superdesk) as it is good for application development, and has many uses for this project.")
+projectLanguageFileDesc()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Sunday, June 27th 2021 at 4:10 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
