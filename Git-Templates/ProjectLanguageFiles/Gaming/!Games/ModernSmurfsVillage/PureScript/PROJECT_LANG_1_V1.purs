7263ef4d020ebc9c5bc22aa9f3bc1ebee779e363
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-02 16:26:17 -0700

    PROJECT_LANG_1_V1.purs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/ModernSmurfsVillage/PureScript/PROJECT_LANG_1_V1.purs
@@ -0,0 +1,17 @@
+-- Start of script
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+
+-- I decided to make PureScript for the project language file for this project (SeansLifeArchive_Images_ModernSmurfsVillage) it took a lot of decision, and I went through many languages (PHP, Idris, PostScript, Racket, FORTRAN, Scheme, Python, R, REBOL, Isabelle, Inform-7 just to name a few) I went with PureScript due to the purity of the game and my memories associated with it.
+main :: Effect Unit
+main = log "I decided to make PureScript for the project language file for this project (SeansLifeArchive_Images_ModernSmurfsVillage) it took a lot of decision, and I went through many languages (PHP, Idris, PostScript, Racket, FORTRAN, Scheme, Python, R, REBOL, Isabelle, Inform-7 just to name a few) I went with PureScript due to the purity of the game and my memories associated with it."
+{- File info
+File type: PureScript source file (*.purs)
+File version: 1 (Friday, July 2nd 2021 at 4:24 pm)
+Line count (including blank lines and compiler line): 18
+}-
+-- End of script
