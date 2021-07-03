9d33596236c80a234fadbe60427624c8a0c095a5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-02 18:03:47 -0700

    PROJECT_LANG_2_V1.cpp
    An archive of the 1st version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Tools/!Repos/1/Cyborg-System-Monitor/C++/PROJECT_LANG_2_V1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+// Start of script
+// Note: I am not very experienced with C++ this may come as bad code, and may not compile
+// I have decided to make C++ the 2nd project language for this project (Cyborg System Monitor) as it is required for good kernel and system design development when C can't be used. It is a highly functional language and is needed for this project, but C has to come first.
+class projectLanguageFileTwo {
+	int main() {
+    		std::cout << "Project language file 2: C++\n";
+    		std::cout << "For: Cyborg System Monitor\n\n";
+    		std::cout << "I have decided to make C++ the 2nd project language for this project (Cyborg System Monitor) as it is required for good kernel and system design development when C can't be used. It is a highly functional language and is needed for this project, but C has to come first.\n";
+    		break;
+	}
+return main();
+bool loop = false;
+break;
+}
+// File info
+// File version: 1 (Friday, July 2nd 2021 at 6:02 pm)
+// File type: C++ Source file (*.cpp, *.cxx)
+// Line count (including blank lines and compiler line): 21
+// End of script
