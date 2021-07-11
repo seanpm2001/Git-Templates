991a179e0689376c425bc6485347d953c37a7f55
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-10 17:59:28 -0700

    Added support for Rust
    Added support for Rust, the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/Seanpm2001-Firefox.github.io/Rust/PROJECT_LANG_V1.rs
@@ -0,0 +1,16 @@
+// Start of script
+// I chose Rust as the project language for this project (SeansLifeArchive_Images-Mozilla-firefox) as Firefox is notably mostly written in Rust, and I don't have very many projects written in this language.
+
+fn main() {
+    println!("I chose Rust as the project language for this project (SeansLifeArchive_Images-Mozilla-firefox) as Firefox is notably mostly written in Rust, and I don't have very many projects written in this language.");
+    break;
+}
+return main();
+break;
+
+// File info
+// File version: 1 (Thursday, June 3rd 2021 at 7:05 pm)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 17
+
+// End of script
