7c7a891c2ee3f3184da94a919ff35e8d36b91b7c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-12 21:19:08 -0700

    PROJECT_LANG_1.glsl
    The current version (version 1) of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/Stock-Elevator-LIBrary/GLSL/PROJECT_LANG_1.glsl
@@ -0,0 +1,11 @@
+// Start of script
+
+#define PROJECT_LANGUAGE_FILE
+// I decided to make GLSL the first project language file for this project (Stock Elevator Software LIBrary) as OpenGL shading will be a key feature in the development and portability of this projects advanced and intermediate elevator models.
+
+// File info
+// File type: OpenGL Shading Language File (*.glsl)
+// File version: 1 (Monday, July 12th 2021 at 9:17 pm)
+// Line count (including blank lines and compiler line): 12
+
+// End of script
