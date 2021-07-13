45719cba9b151a33c0e21004d744906849633ceb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-12 21:24:19 -0700

    PROJECT_LANG_1.c
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/System_Tools/GitMan/C/PROJECT_LANG_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the first project language for this project (GitMan/GitManager) as it is the best language to use for system functions, especially for this project, but a secondary and third language will also be used.
+union projectLanguageFileOne() {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: GitMan - Git Manager");
+    printf("I decided to make C the first project language for this project (GitMan/GitManager) as it is the best language to use for system functions, especially for this project, but a secondary and third language will also be used.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Monday, July 12th 2021 at 9:22 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 27
+// End of script
