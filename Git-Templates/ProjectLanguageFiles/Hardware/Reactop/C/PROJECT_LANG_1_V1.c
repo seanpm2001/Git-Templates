575a2585d52156215e38e349f6821bbd6815fcba
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-14 14:15:13 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/Reactop/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the first project language for this project (Reactop) as it is the best language to use for system functions, especially for this project, and it is also the main language used to write ReactOS, which is very important to this project.
+union projectLanguageFileOne() {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: GitMan - Reactop");
+    printf("I decided to make C the first project language for this project (Reactop) as it is the best language to use for system functions, especially for this project, and it is also the main language used to write ReactOS, which is very important to this project.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Wednesday, July 14th 2021 at 2:13 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 27
+// End of script
