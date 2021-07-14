286264064f7bd7cb721cdd8402a8e0bd6c884953
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-13 20:06:07 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/NUNIX/Python3/PROJECT_LANG_1_V1.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to make Python 3 the 1st project language for this project (NUNIX) as it is one of the preferred languages from the original perception of the project.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileFourDesc():
+	print("Project Language File 1\n")
+	print("For: NUNIX")
+	print("I have decided to make Python 3 the 1st project language for this project (NUNIX) as it is one of the preferred languages from the original perception of the project.")
+projectLanguageFileDesc()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Tuesday, July 13th 2021 at 8:04 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
