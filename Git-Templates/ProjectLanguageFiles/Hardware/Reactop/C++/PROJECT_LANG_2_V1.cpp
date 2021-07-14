9456b3536a1ad4bf4c7455efdca1cc8e717f389e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-14 14:49:14 -0700

    PROJECT_LANG_2_V1.cpp
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/Reactop/C++/PROJECT_LANG_2_V1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// Start of script
+// I decided to make C++ the second project language for this project (Reactop) as C++ is the second primary language that ReactOS is written in, and this project relies heavily on ReactOS.
+class projectLanguageFile {
+	int main() {
+    		std::cout << "I decided to make C++ the second project language for this project (Reactop) as C++ is the second primary language that ReactOS is written in, and this project relies heavily on ReactOS.\n";
+    		break;
+	}
+return main();
+break;
+}
+// File info
+// File version: 1 (Wednesday, July 14th 2021 at 2:47 pm)
+// File type: C++ Source file (*.cpp, *.cxx)
+// Line count (including blank lines and compiler line): 17
+// End of script
