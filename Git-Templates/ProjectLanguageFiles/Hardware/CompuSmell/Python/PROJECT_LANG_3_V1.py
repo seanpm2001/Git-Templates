a40051276031ce842b5ff686a41105bf97c5ed8b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-14 18:03:20 -0700

    PROJECT_LANG_3_V1.py
    An archive of the 1st version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/CompuSmell/Python/PROJECT_LANG_3_V1.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to make Python 3 the 3rd project language for this project (CompuSmell) as Python is one of the 2 common languages that inspired the design of the VSmell language invented through this project.
+''' '''
+class projectLanguageFile()
+def projectLanguageFileThreeDescription():
+	print("Project Language File 3\n")
+	print("For: CompuSmell")
+	print("I have decided to make Python 3 the 3rd project language for this project (CompuSmell) as Python is one of the 2 common languages that inspired the design of the VSmell language invented through this project.")
+projectLanguageFileThreeDescription()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Wednesday, July 14th 2021 at 6:02 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
