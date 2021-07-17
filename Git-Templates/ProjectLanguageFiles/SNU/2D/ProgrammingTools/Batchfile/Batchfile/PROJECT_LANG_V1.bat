ae0ed0727ea4134b98ed28ee6d2de51dae872ea7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-16 19:06:20 -0700

    PROJECT_LANG_V1.bat
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/Batchfile/Batchfile/PROJECT_LANG_V1.bat
@@ -0,0 +1,10 @@
+REM Start of script
+@ECHO OFF
+REM I have decided to have the first and main project language file for this project (SNU / 2D / ProgrammingTools / IDE / Batchfile) to be Batchfile, as it is the main language used for this IDE, and may need to be supplemented with another language, as I am not sure how effective bootstrapping batchfile is.
+ECHO "I have decided to have the first and main project language file for this project (SNU / 2D / ProgrammingTools / IDE / Batchfile) to be Batchfile, as it is the main language used for this IDE, and may need to be supplemented with another language, as I am not sure how effective bootstrapping batchfile is."
+PAUSE
+REM File info
+REM File version: 1 (Friday, July 16th 2021 at 7:04 pm)
+REM File type: MS-DOS Batch file (*.bat)
+REM Line count (including blank lines and compiler line): 11
+REM End of script
