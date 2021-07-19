2b93ce2e3b414f06c6fb115a3951e2495b3568d5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-18 20:55:05 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Documentation/Submissions/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (Seanpm2001/Submissions) as Python is my top language and will suit this project well.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: Seanpm2001/Submissions")
+print("# I have decided to have the main language for this project (Seanpm2001/Submissions) as Python is my top language and will suit this project well.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Sunday, July 18th 2021 at 8:52 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
