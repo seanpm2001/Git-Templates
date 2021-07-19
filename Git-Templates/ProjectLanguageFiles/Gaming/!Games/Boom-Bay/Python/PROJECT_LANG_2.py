3a67b27b10f9f3e15b6818fee166e23d5424152b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-19 16:28:04 -0700

    PROJECT_LANG_2.py
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Boom-Bay/Python/PROJECT_LANG_2.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to make Python 3 the 2nd project language for this project (Boom-Bay) as Python is one of the functional languages needed for programming the game in some areas, because Scheme currently isn't going to work everywhere.
+''' '''
+class projectLanguageFileTwo()
+def projectLanguageFileTwoDescription():
+	print("Project Language File 2\n")
+	print("For: Boom-Bay")
+	print("I have decided to make Python 3 the 2nd project language for this project (Boom-Bay) as Python is one of the functional languages needed for programming the game in some areas, because Scheme currently isn't going to work everywhere.")
+projectLanguageFileTwoDescription()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, July 19th 2021 at 4:27 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
