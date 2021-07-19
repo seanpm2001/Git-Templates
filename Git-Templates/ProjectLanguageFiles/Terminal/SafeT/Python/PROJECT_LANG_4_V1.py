59f709027fd97463f9aaf72a12a2b8ba0b6d81c8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-19 15:32:26 -0700

    PROJECT_LANG_4_V1.py
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Terminal/SafeT/Python/PROJECT_LANG_4_V1.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to make Python 3 the 4th project language for this project (SafeT) as Python is one of the functional languages needed for programming the terminal plugin, and also for terminal support. It is also one of my top languages.
+''' '''
+class projectLanguageFileFour()
+def projectLanguageFileFourDescription():
+	print("Project Language File 4\n")
+	print("For: SafeT")
+	print("I have decided to make Python 3 the 4th project language for this project (SafeT) as Python is one of the functional languages needed for programming the terminal plugin, and also for terminal support. It is also one of my top languages.")
+projectLanguageFileFourDescription()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, July 19th 2021 at 3:31 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
