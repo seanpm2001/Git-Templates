1890ff0d44e742b0c83d811ca43bbb5ffb7d3a94
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-18 21:27:04 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/Dimensional-Converter/Python/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (SNU-Dimensional-Converter) as Python is my top language and is necessary for the data processes of this project and suits well, but may need to be supplemented with an additional language.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: SNU-Dimensional-Converter")
+print("I have decided to have the main language for this project (SNU-Dimensional-Converter) as Python is my top language and is necessary for the data processes of this project and suits well, but may need to be supplemented with an additional language.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Sunday, July 18th 2021 at 9:26 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
