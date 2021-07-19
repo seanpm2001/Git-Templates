2cfc7fd836ba753ae3012be6e3daa30905dc7bbd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-19 15:07:28 -0700

    PROJECT_LANG_1.sh
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Terminal/SafeT/Shell/PROJECT_LANG_1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the first project language file for this project (SafeT) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project represents. It is also highly represented in most Linux terminals.
+function projectLanguageFileOne() {
+  echo ("I have chosen Shell as the first project language file for this project (SafeT) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project represents. It is also highly represented in most Linux terminals.");
+  break;
+}
+return projectLanguageFileOne();
+break;
+exit;
+# File info
+# File version: 1 (Monday, July 19th 2021 at 3:06 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 16
+# End of script
