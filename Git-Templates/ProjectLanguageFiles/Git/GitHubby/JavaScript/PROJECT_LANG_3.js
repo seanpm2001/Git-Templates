5f79cd3eb09ac63425b5aa46c77dba6caf95ec11
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-20 19:57:25 -0700

    PROJECT_LANG_3.js
    The current version (version 1) of the project language for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git/GitHubby/JavaScript/PROJECT_LANG_3.js
@@ -0,0 +1,25 @@
+// Start of script
+// I chose JavaScript as the third project language for this project (GitHubby) as JavaScript is one of the 3 common languages that most webpages are written in, and JavaScript is one of the portable functional programming languages used for this project.
+class projectLanguageFileThree() {
+  void main() {
+    console.log ("I chose JavaScript as the third project language for this project (GitHubby) as JavaScript is one of the 3 common languages that most webpages are written in, and JavaScript is one of the portable functional programming languages used for this project.");
+    /* Garbage syntax, so I can test out 2 new keywords I recently learned. Uncomment to highlight
+    x = bool("true");
+    y = bool("false");
+    if (x == "false"); {
+      try y
+    } else: {
+      catch x;
+    }
+    */
+    break;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Tuesday, July 20th 2021 at 7:56 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 26
+*/
+// End of script
