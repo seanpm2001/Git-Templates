1f91105bee17d5817af9f5414af9b22acfaaaff7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-22 19:11:50 -0700

    PROJECT_LANG_1_V1.m
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Documentation/Metric-System/Mathematica/PROJECT_LANG_1_V1.m
@@ -0,0 +1,27 @@
+(* ::Package:: *)
+(* Start of script *)
+(* Paclet Info File *)
+
+(* created Sept. 14 2018 *)
+
+(* Modified Jul. 22 2021 from https://github.com/WolframResearch/WolframLanguageForJupyter/blob/master/WolframLanguageForJupyter/PacletInfo.m *)
+
+Paclet[
+    Name -> "WolframLanguageForJupyter",
+    Version -> "0.9.2",
+    MathematicaVersion -> "11.2+",
+    Extensions -> {
+        {"Kernel", Context -> {"WolframLanguageForJupyter`"}},
+        {"Documentation", Language -> "English"}
+    }
+]
+
+(* I decided to make Mathematica the project language for this project (Metric-System) to be Mathematica, as I don't have a lot of representation for this language yet, and math is a key part of the metric system. *)
+
+(* File info
+* File type: Wolfram Mathematic source file (*.m)
+* File version: 1 (Thursday, July 22nd 2021 at 7:09 pm
+* Line count (including blank lines and compiler line): 27
+*)
+(* End of script *)
+ 
