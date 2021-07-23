cf12f74b932d115fd1945886bcc8714aa2bc4c69
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-22 19:48:37 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/Gitty-images/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (Gitty-images) be Python, as Python is my top language and will suit this project well in the backend.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: Gitty-images")
+print("I have decided to have the main language for this project (Gitty-images) be Python, as Python is my top language and will suit this project well in the backend.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Thursday, July 22nd 2021 at 7:47 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
