89b5d15ace0be81d805b4a56e325da15a9bea886
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-25 15:14:49 -0700

    PROJECT_LANG_1.m
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Articles/Numeric-Liberation-Front/Mathematica/PROJECT_LANG_1.m
@@ -0,0 +1,26 @@
+(* ::Package:: *)
+(* Start of script *)
+(* Paclet Info File *)
+
+(* created Sept. 14 2018 *)
+
+(* Modified Jul. 22 2021 from https://github.com/WolframResearch/WolframLanguageForJupyter/blob/master/WolframLanguageForJupyter/PacletInfo.m *)
+
+Paclet[
+    Name -> "WolframLanguageForJupyter",
+    Version -> "0.9.2",
+    MathematicaVersion -> "11.2+",
+    Extensions -> {
+        {"Kernel", Context -> {"WolframLanguageForJupyter`"}},
+        {"Documentation", Language -> "English"}
+    }
+]
+
+(* I decided to make Mathematica the first project language for this project (Numeric-Liberation-Front) to be Mathematica, as I don't have a lot of representation for this language yet, and numerals are math related, so mathematica fits best. A second language will also be used.*)
+
+(* File info
+* File type: Wolfram Mathematic source file (*.m)
+* File version: 1 (Sunay, July 25th 2021 at 3:12 pm
+* Line count (including blank lines and compiler line): 27
+*)
+(* End of script *)
