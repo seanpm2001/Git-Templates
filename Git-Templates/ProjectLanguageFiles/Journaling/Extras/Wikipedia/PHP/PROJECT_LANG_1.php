f79bd5d6a4c7e9eca9e78660a3d358654a659cd2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-24 21:39:00 -0700

    PROJECT_LANG_1.php
    The current version (version 1) of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Journaling/Extras/Wikipedia/PHP/PROJECT_LANG_1.php
@@ -0,0 +1,12 @@
+<?php
+// Start of script
+// Since Wikmedia is written mostly in PHP, this project (SeansLifeArchive / Extras / Wikipedia) has its own main first project language file in PHP, as it is heavily related to Wikipedia, and this project needs a project language file.
+echo ("Since Wikmedia is written mostly in PHP, this project (SeansLifeArchive / Extras / Wikipedia) has its own main first project language file in PHP, as it is heavily related to Wikipedia, and this project needs a project language file.");
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (Saturday, July 24th 2021 at 9:37 pm)
+* Line count (including blank lines and compiler line): 13
+*/
+// End of script
+?>
