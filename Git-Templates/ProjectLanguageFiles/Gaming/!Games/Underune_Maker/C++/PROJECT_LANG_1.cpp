36458e41e15cdcb495da4dd588f78c765ed80b79
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-31 15:49:24 -0700

    PROJECT_LANG_1.cpp
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Underune_Maker/C++/PROJECT_LANG_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// Start of script
+// I decided to make C++ the first project language for this project (Underune) as C++ is the main primary language that ReactOS is written in, and this project can make use of C++ due to the projects simplicity. It may be supplemented for/with another language in the future.
+class projectLanguageFile {
+	int main() {
+    		std::cout << "I decided to make C++ the first project language for this project (Underune) as C++ is the main primary language that ReactOS is written in, and this project can make use of C++ due to the projects simplicity. It may be supplemented for/with another language in the future.\n";
+    		break;
+	}
+return main();
+break;
+}
+// File info
+// File version: 1 (Saturday, July 31st 2021 at 3:48 pm)
+// File type: C++ Source file (*.cpp, *.cxx)
+// Line count (including blank lines and compiler line): 17
+// End of script
