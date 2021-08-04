c462837f0d28a6af32b582d5eba186d344d7617e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-04 15:41:06 -0700

    PROJECT_LANG_5.cs
    The current version (version 1) of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Malware/StartFsck/C#/PROJECT_LANG_5.cs
@@ -0,0 +1,17 @@
+// Start of script
+// using namespace;
+using System;
+// I decided to make C sharp the fifth project language for this project (StartFsck) as Windows NT is now mostly written in C# and it is a common Microsoft language that I decided to use for certain application level parts of this project for modern Windows (Windows XP and onward)
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("I decided to make C sharp the fifth project language for this project (StartFsck) as Windows NT is now mostly written in C# and it is a common Microsoft language that I decided to use for certain application level parts of this project for modern Windows (Windows XP and onward)");
+        break;
+    }
+}
+break;
+// File version: 1 (Wednesday, August 4th 2021 at 3:40 pm)
+// File type: C Sharp Source file (*.cs)
+// Line count (including blank lines and compiler line): 18
+// End of script
