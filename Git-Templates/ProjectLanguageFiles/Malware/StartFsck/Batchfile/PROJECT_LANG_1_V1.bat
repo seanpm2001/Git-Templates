1425431a6d75d81de3b832aa038a9363dfd400c6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-04 15:17:13 -0700

    PROJECT_LANG_1_V1.bat
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Malware/StartFsck/Batchfile/PROJECT_LANG_1_V1.bat
@@ -0,0 +1,10 @@
+REM Start of script
+@ECHO OFF
+REM I have decided to have the first and main project language file for this project (StartFsck) to be Batchfile, as it is a useful language for modifying the startup folder on both old and new versions of Windows, and will be needed for compatibility purposes on this project.
+ECHO "I have decided to have the first and main project language file for this project (StartFsck) to be Batchfile, as it is a useful language for modifying the startup folder on both old and new versions of Windows, and will be needed for compatibility purposes on this project."
+PAUSE
+REM File info
+REM File version: 1 (Wednesday, August 4th 2021 at 3:15 pm)
+REM File type: MS-DOS Batch file (*.bat)
+REM Line count (including blank lines and compiler line): 11
+REM End of script
