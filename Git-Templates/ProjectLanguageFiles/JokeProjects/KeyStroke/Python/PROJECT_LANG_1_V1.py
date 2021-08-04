2ced49eab22fa0367aa2f5488de423e1a738ac71
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-03 18:20:09 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/JokeProjects/KeyStroke/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (KeyStroke) be Python, as Python is a good language for a simple project, and I chose it, since I know it well enough to write the sample. Not much is needed for this project.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: KeyStroke")
+print("I have decided to have the main language for this project (KeyStroke) be Python, as Python is a good language for a simple project, and I chose it, since I know it well enough to write the sample. Not much is needed for this project.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Tuesday, August 3rd 2021 at 6:18 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
