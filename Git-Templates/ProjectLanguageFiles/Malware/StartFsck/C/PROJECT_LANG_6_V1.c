aae8e0b9b3d94753738a42b1d9fdd4e7781d70dc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-04 15:46:39 -0700

    PROJECT_LANG_6_V1.c
    An archive of the first version of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Malware/StartFsck/C/PROJECT_LANG_6_V1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the sxith project language for this project (StartFsck) as it is the best language to use for hardware level functions, and is a stable and portable enough language to work. It is compatible with the majority of operating systems, and can be a replacement for C# for Windows XP and below.
+union projectLanguageFileSix() {
+int main(void) {
+    printf("Project language file 6: C");
+    printf("For: StartFsck");
+    printf("I decided to make C the sxith project language for this project (StartFsck) as it is the best language to use for hardware level functions, and is a stable and portable enough language to work. It is compatible with the majority of operating systems, and can be a replacement for C# for Windows XP and below..\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Wednesday, August 4th 2021 at 3:45 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
