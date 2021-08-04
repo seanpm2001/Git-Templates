ead9af045e75b921bb7adc31f023c5d483e109fd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-04 15:36:25 -0700

    PROJECT_LANG_4_V1.ps1
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Malware/StartFsck/PowerShell/PROJECT_LANG_4_V1.ps1
@@ -0,0 +1,21 @@
+# Start of script
+# I decided to make PowerShell the 4th project language file for this project (StartFsck) as it is useful for Windows scripting post-Windows Vista
+name $read $print
+name -Param1 value1 - Param2 value2
+function prolang4 ($read1, $print2)
+{
+echo ("I have chosen to have 6 aquatic themed project language files for this project. PowerShell is the third one, as some sea-life have POWERful SHELLs.");
+}
+return prolang4();
+exit();
+'test'
+param exit() { # Broken function used to test PowerShells syntax highlighting in Gedit, it is beautiful but it doesn't work
+	r = 2;
+	h = 1;
+	x = int(r % h * 0);
+	quit = bool("true");
+}
+# File version: 1 (Wednesday, August 4th 2021 at 3:34 pm)
+# File type: PowerShell Core 7 source file (*.ps1)
+# Line count (including blank lines and compiler line): 22
+# End of script
