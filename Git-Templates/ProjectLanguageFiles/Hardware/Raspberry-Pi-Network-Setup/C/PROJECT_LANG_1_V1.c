fb02973bc5e1333e5cdc7259c6a5e37faa025c62
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-04 16:11:32 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/Raspberry-Pi-Network-Setup/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the first project language file for this project (Raspberry-Pi-Network-Setup) as it is probably the best language to write Raspberry Pi setups in, and it is a stable portable language that works on the majority of hardware.
+union projectLanguageFileOne) {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: Raspberry-Pi-Network-Setup");
+    printf("I decided to make C the first project language file for this project (Raspberry-Pi-Network-Setup) as it is probably the best language to write Raspberry Pi setups in, and it is a stable portable language that works on the majority of hardware.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Wednesday, August 4th 2021 at 4:10 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
