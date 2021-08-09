950818acd61ec63120dc31ac7c061aba7581d870
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-09 16:53:35 -0700

    PROJECT_LANG_1_V1.nim
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/NimbleBit-Glossary/Nim/PROJECT_LANG_1_V1.nim
@@ -0,0 +1,9 @@
+# Start of script
+# NimbleBit produces closed source games, and I don't know what language any of them are written in, so I chose the Nim programming language, as it containis the fiest 3 letters of the companys name. Nim is this projects official language. I want this project to have a language assigned to it, so I wrote this file in Nim. It feels like it fits.
+
+echo ("NimbleBit produces closed source games, and I don't know what language any of them are written in, so I chose the Nim programming language, as it containis the fiest 3 letters of the companys name. Nim is this projects official language. I want this project to have a language assigned to it, so I wrote this file in Nim. It feels like it fits.")
+  
+# File version: 1 (Monday, August 9th 2021 at 4:52 pm)
+# File type: Nim source file (*.nim)
+# Line count (including blank lines and compiler line): 10
+# End of script
