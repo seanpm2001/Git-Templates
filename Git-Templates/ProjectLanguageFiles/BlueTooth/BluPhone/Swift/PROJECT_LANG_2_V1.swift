bd18c7f2d05dcf8463d42904c79f71852f5ef678
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-09 20:21:30 -0700

    PROJECT_LANG_2_V1.swift
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/BlueTooth/BluPhone/Swift/PROJECT_LANG_2_V1.swift
@@ -0,0 +1,14 @@
+// Start of script
+// I have chosen Swift as the second project language file for this project (BluPhone) as this project needs Swift support to work well natively on Apple platforms.
+class projectLanguageFileTwo {
+var str = "I have chosen Swift as the second project language file for this project (BluPhone) as this project needs Swift support to work well natively on Apple platforms.";
+myValue = @String
+let myValue = str();
+return myValue;
+break;
+}
+// I don't know how to make working strings in Swift
+// File version: 1 (Monday, August 9th 2021 at 8:20 pm)
+// File type: Swift 5.3 Source file (*.swift)
+// Line count (including blank lines and compiler line): 15
+// End of script
