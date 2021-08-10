e859e7789377534c46a79f57e39d051cb7bc32fd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-09 20:25:59 -0700

    PROJECT_LANG_3.sh
    The current version (version 1) of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/BlueTooth/BluPhone/Shell/PROJECT_LANG_3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the third project language file for this project (BluPhone) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project uses.
+function projectLanguageFileThree() {
+  echo ("I have chosen Shell as the fourth project language file for this project (KeyOff) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project represents. It is also highly represented in most Linux terminals.");
+  break;
+}
+return projectLanguageFileThree();
+break;
+exit;
+# File info
+# File version: 1 (Monday, August 9th 2021 at 8:25 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 16
+# End of script
