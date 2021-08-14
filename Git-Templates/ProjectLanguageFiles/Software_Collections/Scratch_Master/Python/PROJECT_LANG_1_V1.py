3c8fe93c11cdcd8aac1e21758a7b6d17a0a892fa
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-13 21:00:46 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_Collections/Scratch_Master/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (Scratch_Master) be Python, as Python is already the main language for this project (other than Scratch) and already consists of a majority of the projects source code. This project needs a project language file, so here is the first one.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: Scratch_Master")
+print("I have decided to have the main language for this project (Scratch_Master) be Python, as Python is already the main language for this project (other than Scratch) and already consists of a majority of the projects source code. This project needs a project language file, so here is the first one.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Friday, August 13th 2021 at 8:58 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
