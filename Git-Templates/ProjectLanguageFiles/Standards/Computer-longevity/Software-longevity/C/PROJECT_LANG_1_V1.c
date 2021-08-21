9048a9d312f2513f168d583b5a1f3440b3f86e21
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-20 18:04:45 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Standards/Computer-longevity/Software-longevity/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the first project language file for this project (Software-longevity) as it is the most classic programming language that has stood a good test of time, and represents this project well.
+union projectLanguageFileOne) {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: Software-longevity");
+    printf("I decided to make C the first project language file for this project (Software-longevity) as it is the most classic programming language that has stood a good test of time, and represents this project well.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Friday, August 20th 2021 at 6:03 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
