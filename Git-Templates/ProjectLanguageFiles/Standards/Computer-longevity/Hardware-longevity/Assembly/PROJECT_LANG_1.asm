a67a6e162116635b8a709e5b58905dd2d40f53b6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-20 17:14:01 -0700

    PROJECT_LANG_1.asm
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Standards/Computer-longevity/Hardware-longevity/Assembly/PROJECT_LANG_1.asm
@@ -0,0 +1,6 @@
+; I have decided to make the Assembly language the official language for this project, as computer hardware requires assembly, and it fits this projects name well.
+
+; File info
+; File version: 1 (Friday, August 20th 2021 ay 5:12 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
