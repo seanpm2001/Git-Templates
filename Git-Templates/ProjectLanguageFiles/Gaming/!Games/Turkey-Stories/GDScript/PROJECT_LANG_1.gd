d11f30639d19e1c3aa3e90edb7afe564d3f23f2d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-25 21:05:46 -0700

    PROJECT_LANG_1.gd
    The current version (version 1) of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Turkey-Stories/GDScript/PROJECT_LANG_1.gd
@@ -0,0 +1,17 @@
+# Start of script
+
+# I decided to make this game (Turkey Stories) in GoDot/GDScript, as I decided between this, Rust, and Python, and decided I wanted to develop on the GoDot platform for this game, so GDScript is the first project language file for this project.
+
+# Project language file output
+func _ready():
+	print("I decided to make this game (Turkey Stories) in GoDot/GDScript, as I decided between this, Rust, and Python, and decided I wanted to develop on the GoDot platform for this game, so GDScript is the first project language file for this project.")
+  break
+return _ready()
+break
+
+# File info
+# File type: GDscript (GoDot Script) source file (*.gs)
+# File version: 1 (2021 Wednesday August 25th at 7:27 pm)
+# Line count (including blank lines and compiler line): 18
+
+# End of script
