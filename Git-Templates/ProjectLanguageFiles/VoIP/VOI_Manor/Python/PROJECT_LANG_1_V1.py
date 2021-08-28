098fe36020aec794061d6d446ceadecb74d14afd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-27 19:50:28 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/VoIP/VOI_Manor/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (VOI Manor) to be Pythn, as Python is a language I am most familiar with, and it can give nearly direct access to current services such as Discord. Due to tje cross-compatibility of Python and its support, I feel it is a good language to use here.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: VOI Manor")
+print("I have decided to have the main language for this project (VOI Manor) to be Pythn, as Python is a language I am most familiar with, and it can give nearly direct access to current services such as Discord. Due to tje cross-compatibility of Python and its support, I feel it is a good language to use here.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Friday, August 27th 2021 at 7:49 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
