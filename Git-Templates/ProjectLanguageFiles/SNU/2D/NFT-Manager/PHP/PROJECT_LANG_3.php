f76c1e46a7fd99273f1cc2df28c2c1639766c279
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-27 18:29:59 -0700

    PROJECT_LANG_3.php
    The current version (version 1) of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/NFT-Manager/PHP/PROJECT_LANG_3.php
@@ -0,0 +1,18 @@
+<?php
+// Start of script
+// I chose PHP as the 3rd project language for this project (SNU / 2D / NFT Manager) as it seems like a fitting language for both backend and frontend processing, and I wanted to alternate from JavaScript.
+$PROJECT_LANGUAGE_THREE; {
+  echo ("Project language 3");
+  echo ("For SNU: 2D / NFT Manager");
+  echo ("I chose PHP as the 3rd project language for this project (SNU / 2D / NFT Manager) as it seems like a fitting language for both backend and frontend processing, and I wanted to alternate from JavaScript.");
+  break;
+}
+return $PROJECT_LANGUAGE_THREE;
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (Friay, August 27th 2021 at 6:29 pm)
+* Line count (including blank lines and compiler line): 19
+*/
+// End of script
+?>
