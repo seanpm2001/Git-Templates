75054d2330def4e08b5a17d3e75ce7d47e95925d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-04 20:57:49 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Medicinal-Medicine/TrueEEG/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the first project language file for this project (TrueEEG) as I need a stable, reliable, and powerful language to run on hospital computers, and what better language is there to choose than C (specifically Portable C) although difficult, this software will be programmed in C, rather than Python. The software has to be as reliable as possible for the hospitals and medical centers using it. It must pass evaluation before being used in a hospital, and needs to undergo tests when it gets to a working state.
+union projectLanguageFileOne) {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: TrueEEG");
+    printf("I decided to make C the first project language file for this project (TrueEEG) as I need a stable, reliable, and powerful language to run on hospital computers, and what better language is there to choose than C (specifically Portable C) although difficult, this software will be programmed in C, rather than Python. The software has to be as reliable as possible for the hospitals and medical centers using it. It must pass evaluation before being used in a hospital, and needs to undergo tests when it gets to a working state.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Saturday, 2021 September 4th at 8:56 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
