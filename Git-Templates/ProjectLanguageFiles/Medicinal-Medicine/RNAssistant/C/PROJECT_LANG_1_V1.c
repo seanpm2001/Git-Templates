5ab53011c7d6818fa88639400e4a64b3f153798e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-06 20:18:19 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Medicinal-Medicine/RNAssistant/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the first project language file for this project (RNAssistant) as I need a stable, reliable, and powerful language to run on hospital  and patient computers, and what better language is there to choose than C (specifically Portable C) although difficult, this software will be programmed in C, rather than Python, Prolog, or C++. The software has to be as reliable as possible for the hospitals and medical centers using it, and for the patients using it. It must pass evaluation before being used in a hospital, and needs to undergo tests when it gets to a working state. This software is a lot safer and would have a lesser impact upon failure, as long as a backup method is provided.
+union projectLanguageFileOne) {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: RNAssistant");
+    printf("I decided to make C the first project language file for this project (RNAssistant) as I need a stable, reliable, and powerful language to run on hospital  and patient computers, and what better language is there to choose than C (specifically Portable C) although difficult, this software will be programmed in C, rather than Python, Prolog, or C++. The software has to be as reliable as possible for the hospitals and medical centers using it, and for the patients using it. It must pass evaluation before being used in a hospital, and needs to undergo tests when it gets to a working state. This software is a lot safer and would have a lesser impact upon failure, as long as a backup method is provided.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Monday, 2021 September 6th at 8:17 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
