d6457532bc35ed65a0fe313e28a55e190b103e5e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-08 15:56:43 -0700

    PROJECT_LANG_2.c
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Medicinal-Medicine/Hospital-Defender/C/PROJECT_LANG_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the second project language file for this project (Hospital-Defender) as I need a stable, portable language to use as a backend, and portable C is the best language for this. It will still be supplemented with Assembly and Shell.
+union projectLanguageFileTwo {
+int main(void) {
+    printf("Project language file 2: C");
+    printf("For: Hospital_Defender");
+    printf("I decided to make C the second project language file for this project (Hospital-Defender) as I need a stable, portable language to use as a backend, and portable C is the best language for this. It will still be supplemented with Assembly and Shell.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Wednesday, 2021 September 8th at 3:56 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
