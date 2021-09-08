33147741361b41636d29fa4c083b90805bc38a4a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-08 16:11:19 -0700

    PROJECT_LANG_4.sh
    The current version (version 1) of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Medicinal-Medicine/Hospital-Defender/Shell/PROJECT_LANG_4.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the fourth project language file for this project (Hospital Defender) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop that this project promotes and supports.
+function projectLanguageFileFour() {
+  echo ("I have chosen Shell as the fourth project language file for this project (Hospital Defender) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop that this project promotes and supports.");
+  break;
+}
+return projectLanguageFileFour();
+break;
+exit;
+# File info
+# File version: 1 (Wednesday, 2021 September 8th at 4:10 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 16
+# End of script
