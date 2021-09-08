9a65b9fe8892dcffb74fa07cc84b70edf848e9f0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-08 15:51:06 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Medicinal-Medicine/Hospital-Defender/Python/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (Hospital Defender) to be Python, as Python is a language I am most familiar with, and it is good for writing the main software with (front-end) I will still use C, Assembly, and Shell for the backend.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: Hospital Defender")
+print("I have decided to have the main language for this project (Hospital Defender) to be Python, as Python is a language I am most familiar with, and it is good for writing the main software with (front-end) I will still use C, Assembly, and Shell for the backend.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Wednesday, 2021 September 8th at 3:50 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
