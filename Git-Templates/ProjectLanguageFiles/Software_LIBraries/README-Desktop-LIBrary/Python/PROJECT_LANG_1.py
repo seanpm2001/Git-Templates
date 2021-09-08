5082563f778da4ebc495b974781717f4320e8b2b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-08 15:24:57 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/README-Desktop-LIBrary/Python/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (README Desktop Library) to be Pythn, as Python is a language I am most familiar with, and it is good for writing libraries with, with little dependency.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: README-Desktop-LIBrary")
+print("I have decided to have the main language for this project (README Desktop Library) to be Pythn, as Python is a language I am most familiar with, and it is good for writing libraries with, with little dependency.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Wednesday, 2021 September 8th at 3:24 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
