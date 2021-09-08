d0ec38b06900acf554caadf2060f9fb35d90b147
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-08 16:04:23 -0700

    PROJECT_LANG_3_V1.asm
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Medicinal-Medicine/Hospital-Defender/Assembly/PROJECT_LANG_3_V1.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the third project language for this project (Hospital Defender) as Assembly is required for certain parts of this projects back-end.
+
+; File info
+; File version: 1 (Wednesday, 2021 September 8th ay 4:03 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
