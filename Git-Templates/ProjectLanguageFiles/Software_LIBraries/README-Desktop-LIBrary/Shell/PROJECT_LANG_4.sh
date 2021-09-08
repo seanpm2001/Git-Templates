3350ad9f0c663385bbb8691c5dd0aa7c5477c737
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-08 15:44:10 -0700

    PROJECT_LANG_4.sh
    The current version (version 1) of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/README-Desktop-LIBrary/Shell/PROJECT_LANG_4.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the fourth project language file for this project (README Desktop LIBrary) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project represents. It is also highly represented in most Linux terminals.
+function projectLanguageFileFour() {
+  echo ("I have chosen Shell as the fourth project language file for this project (README Desktop LIBrary) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project represents. It is also highly represented in most Linux terminals.");
+  break;
+}
+return projectLanguageFileFour();
+break;
+exit;
+# File info
+# File version: 1 (Wednesday, 2021 September 8th at 3:43 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 16
+# End of script
