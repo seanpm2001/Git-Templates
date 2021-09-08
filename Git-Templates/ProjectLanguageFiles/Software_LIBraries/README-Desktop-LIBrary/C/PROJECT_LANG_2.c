1526e06365773aa714289c7659b0b8206a1597bd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-08 15:33:56 -0700

    PROJECT_LANG_2.c
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/README-Desktop-LIBrary/C/PROJECT_LANG_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the second project language file for this project (README-Desktop-LIBrary) as I need a stable, portable language to use as a backup, and Portable C is a good language for several functions of this program.
+union projectLanguageFileOne) {
+int main(void) {
+    printf("Project language file 2: C");
+    printf("For: README Desktop LIBrary");
+    printf("I decided to make C the second project language file for this project (README-Desktop-LIBrary) as I need a stable, portable language to use as a backup, and Portable C is a good language for several functions of this program.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Wednesday, 2021 September 8th at 3:33 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
