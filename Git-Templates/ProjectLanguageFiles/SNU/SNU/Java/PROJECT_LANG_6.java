304efa15d4c9fe17671d74210c2ddec46b3acedb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-10 20:46:03 -0700

    PROJECT_LANG_6.java
    The current version (version 1) of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/SNU/Java/PROJECT_LANG_6.java
@@ -0,0 +1,18 @@
+// Start of script
+// I chose Java as the sixth project language file for this project (SNU) as Java is one of the experimental languages used on SNU early on, and serves as a legacy language. I also was learning Java at the same time I started working on SNU. 
+public class projectLanguageFileSix {
+	public static void main(String args) {
+		System.out.println("I chose Java as the sixth project language file for this project (SNU) as Java is one of the experimental languages used on SNU early on, and serves as a legacy language. I also was learning Java at the same time I started working on SNU. ");
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+main();
+int y = 6;
+System.out.println("Current project language file: " + int(y));
+break } // No exit command exists
+// I am not experienced with C, so this script may not work
+/* File info
+* File version: 1 (Friday, 2021 September 10th at 8:45 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 19
+*/
+// End of script
