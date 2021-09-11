a6a74b9937071c5917e5a81295e780a1a73c0544
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-10 20:54:43 -0700

    PROJECT_LANG_7_V1.py
    An archive of the first version of the 7th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/SNU/Python/PROJECT_LANG_7_V1.py
@@ -0,0 +1,17 @@
+# Start of script
+# I have decided to have the seventh language for this project (SNU) to be Python, as Python is a language I am most familiar with, and it is good for writing the main software with, and it is a very common language in SNUs programmed backend.
+''' '''
+class projectLanguageFileSeven()
+print("Project Language File 7\n")
+print("For: SNU")
+print("I have decided to have the seventh language for this project (SNU) to be Python, as Python is a language I am most familiar with, and it is good for writing the main software with, and it is a very common language in SNUs programmed backend.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+# Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+""" """
+# File info
+# File version: 1 (Friday, 2021 September 10th at 8:51 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 18
+# End of script
