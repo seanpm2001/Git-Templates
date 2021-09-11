a522ae9be1beefc3cd625f1874cd73e2cf94e72d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-10 20:34:03 -0700

    PROJECT_LANG_5_V1.rb
    An archive of the first version of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/SNU/Ruby/PROJECT_LANG_5_V1.rb
@@ -0,0 +1,18 @@
+# Start of script
+# I chose Ruby as the fifth project language file for this project (SNU) as Ruby is SNUs 5th default front-end language, and is commonly used within SNU.
+class project_language_file_5():
+  def projectLanguageFile5():
+    puts "Project language file 5"
+    puts "For: SNU"
+    puts "I chose Ruby as the fifth project language file for this project (SNU) as Ruby is SNUs 5th default front-end language, and is commonly used within SNU."
+    break
+  return projectLanguageFile5()
+  break
+end # end class
+# Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (Friday, 2021 September 10th at 8:32 pm)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
