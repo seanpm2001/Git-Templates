f57c4e09c842a1a7d58d67abf41d8a50931fc7d9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-10 20:20:08 -0700

    PROJECT_LANG_3.js
    The current version (version 1) of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/SNU/JavaScript/PROJECT_LANG_3.js
@@ -0,0 +1,26 @@
+// Start of script
+// I chose JavaScript as the third project language for this project (SNU) as JavaScript is one of the 3 common languages that most webpages are written in, and JavaScript is one of the portable functional programming languages used for this project. It also has a lot of use in SNU development.
+class projectLanguageFileThree() {
+  void main() {
+    console.log ("I chose JavaScript as the third project language for this project (SNU) as JavaScript is one of the 3 common languages that most webpages are written in, and JavaScript is one of the portable functional programming languages used for this project. It also has a lot of use in SNU development.");
+    /* Garbage syntax, so I can test out 2 new keywords I recently learned. Uncomment to highlight
+    x = bool("true");
+    y = bool("false");
+    if (x == "false"); {
+      try y
+    } else: {
+      catch x;
+    }
+    */
+    break;
+  }
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+/* File info
+* File version: 1 (Friday, 2021 September 10th at 8:19 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
