7fa2a0fb60c8cddec4aaa043e5229487f66c81b2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-10 20:27:44 -0700

    PROJECT_LANG_4_V1.php
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/SNU/PHP/PROJECT_LANG_4_V1.php
@@ -0,0 +1,19 @@
+<?php
+// Start of script
+// I chose PHP as the 4th project language for this project (SNU) as it seems like a fitting language for both backend and frontend processing, and I wanted to alternate from JavaScript. It is also used in SNUs development following the July 2020 update.
+$PROJECT_LANGUAGE_FOUR; {
+  echo ("Project language 4");
+  echo ("For: SNU");
+  echo ("I chose PHP as the 4th project language for this project (SNU) as it seems like a fitting language for both backend and frontend processing, and I wanted to alternate from JavaScript. It is also used in SNUs development following the July 2020 update.");
+  break;
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return $PROJECT_LANGUAGE_FOUR;
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (Friday, 2021 September 10th at 8:26 pm)
+* Line count (including blank lines and compiler line): 20
+*/
+// End of script
+?>
