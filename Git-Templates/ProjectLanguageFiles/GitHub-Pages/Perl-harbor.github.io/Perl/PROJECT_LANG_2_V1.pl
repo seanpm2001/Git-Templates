4073c6c4044b59d757791440271b038c84a7269c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-12 15:15:38 -0700

    PROJECT_LANG_2_V1.pl
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/Perl-harbor.github.io/Perl/PROJECT_LANG_2_V1.pl
@@ -0,0 +1,8 @@
+# Start of script
+# Perl Harbor is written in Perl, due to its 1 letter difference with Pearl, and is used to remember and commemorate the attacks on Pearl Harbor on Sunday, December 7th 1941, with the 2399 people killed (2335 when not counting the attacking Kamikaze pilots) I decided to make Perl the second project language file for the projects website in accordance with this.
+print "Perl Harbor is written in Perl, due to its 1 letter difference with Pearl, and is used to remember and commemorate the attacks on Pearl Harbor on Sunday, December 7th 1941, with the 2399 people killed (2335 when not counting the attacking Kamikaze pilots) I decided to make Perl the second project language file for the projects website in accordance with this.";
+# File info
+# File type: Perl source file (*pl)
+# File version: 1 (Sunday, 2021 September 12th at 3:14 pm)
+# Line count (including blank lines and compiler line): 9
+# End of script
