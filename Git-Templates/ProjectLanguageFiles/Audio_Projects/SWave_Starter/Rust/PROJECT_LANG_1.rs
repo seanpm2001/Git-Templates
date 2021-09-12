872dcac7d8812c573e02a8d2b79b0acd9d9fbfb4
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-11 20:58:15 -0700

    PROJECT_LANG_1.rs
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Audio_Projects/SWave_Starter/Rust/PROJECT_LANG_1.rs
@@ -0,0 +1,16 @@
+// Start of script
+// I chose Rust as the first main project language for this project (SWave Starter) as Rust is a stable and fast language that I decided would be good for this project. I also wanted to maintain software diversity and use Rust instead of Python.
+fn main() {
+  println!("Project language file 1 (Rust)");
+  println!("For: SWave Starter");
+  println!("About:");
+  println!("I chose Rust as the first main project language for this project (SWave Starter) as Rust is a stable and fast language that I decided would be good for this project. I also wanted to maintain software diversity and use Rust instead of Python.");
+  break;
+}
+return main();
+break;
+// File info
+// File version: 1 (Saturday, 2021 September 11th at 8:57 pm)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 17
+// End of script
