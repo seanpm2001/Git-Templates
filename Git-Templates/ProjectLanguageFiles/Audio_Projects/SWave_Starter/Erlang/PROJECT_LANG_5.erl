8080dd08d5ddb01af51538eb3d509a8e61e381b3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-11 21:29:00 -0700

    PROJECT_LANG_5.erl
    The current version (version 1) of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Audio_Projects/SWave_Starter/Erlang/PROJECT_LANG_5.erl
@@ -0,0 +1,8 @@
+%% Start of script
+%% I decided to have Erlang as the 5th project language for this project (SWave Starter) for basic functional uses, and for software diversity.
+io:format("I decided to have Erlang as the 5th project language for this project (SWave Starter) for basic functional uses, and for software diversity.~n").
+%% File info
+%% File type: Erlang source file (*.erl)
+%% File version: 1 (Saturday, 2021 September 11th at 9:28 pm)
+%% Line count (including blank lines and compiler line): 9
+%% End of script
