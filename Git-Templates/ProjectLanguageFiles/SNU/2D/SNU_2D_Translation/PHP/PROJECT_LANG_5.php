15ae291c286388014ca86f6c40591fbe8467185a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-16 15:46:25 -0700

    PROJECT_LANG_5.php
    The current version (version 1) of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/SNU_2D_Translation/PHP/PROJECT_LANG_5.php
@@ -0,0 +1,19 @@
+<?php
+// Start of script
+// I chose PHP as the 5th project language for this project (SNU / 2D / Translation) as it seems like a fitting language for both backend and frontend processing, and I wanted to alternate from JavaScript. It is also used in SNUs development following the July 2020 update.
+$PROJECT_LANGUAGE_FIVE; {
+  echo ("Project language 5");
+  echo ("For: SNU / 2D / Translation");
+  echo ("I chose PHP as the 5th project language for this project (SNU) as it seems like a fitting language for both backend and frontend processing, and I wanted to alternate from JavaScript. It is also used in SNUs development following the July 2020 update.");
+  break;
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return $PROJECT_LANGUAGE_FOUR;
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (Thursday, 2021 September 16th at 3:45 pm)
+* Line count (including blank lines and compiler line): 20
+*/
+// End of script
+?>
