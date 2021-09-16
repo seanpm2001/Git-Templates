13bfe61052b3ee9e1d5a6d99c1e241ea89b0d57d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-16 15:53:52 -0700

    PROJECT_LANG_6.rb
    The current version (version 1) of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/SNU_2D_Translation/Ruby/PROJECT_LANG_6.rb
@@ -0,0 +1,20 @@
+# Start of script
+# I chose Ruby as the sixth project language file for this project (SNU / 2D / Translation) as Ruby is SNUs 5th default front-end language, and is commonly used within SNU. This project uses 1 language earlier than the project I am basing these languages files off of, so the number is 1 off.
+class project_language_file_6():
+  def projectLanguageFile5():
+    puts "Project language file 6"
+    puts "For: SNU / 2D / Translation"
+    puts "About:"
+    puts "I chose Ruby as the sixth project language file for this project (SNU / 2D / Translation) as Ruby is SNUs 5th default front-end language, and is commonly used within SNU. This project uses 1 language earlier than the project I am basing these languages files off of, so the number is 1 off."
+    break
+  return projectLanguageFile6()
+  return 0
+  break
+end # end class
+# Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (Thursday, 2021 September 16th at 3:52 pm)
+# Line count (including blank lines and compiler line): 21
+
+# End of script
