7aed5ff5c3ce73cb7c1bea487c57d2ddaf5d951c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-16 16:13:50 -0700

    PROJECT_LANG_8_V1.pl
    An archive of the first version of the 8th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/SNU_2D_Translation/Perl/PROJECT_LANG_8_V1.pl
@@ -0,0 +1,8 @@
+# Start of script
+# I chose Perl as the 8th project language file for this project (SNU / 2D / Translation) as Perl is a good back-end language for machine translation, although I am considering also sideloading Prolog or another language as well.
+print "I chose Perl as the 8th project language file for this project (SNU / 2D / Translation) as Perl is a good back-end language for machine translation, although I am considering also sideloading Prolog or another language as well.";
+# File info
+# File type: Perl source file (*pl)
+# File version: 1 (Thursday, 2021 September 16th at 4:12 pm)
+# Line count (including blank lines and compiler line): 9
+# End of script
