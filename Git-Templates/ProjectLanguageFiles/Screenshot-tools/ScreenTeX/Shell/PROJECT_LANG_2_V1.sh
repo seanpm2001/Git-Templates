eee1255efd7174f2a69ee300ebc0a66d80d05a13
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-20 13:19:40 -0700

    PROJECT_LANG_2_V1.sh
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Screenshot-tools/ScreenTeX/Shell/PROJECT_LANG_2_V1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the second project language file for this project (ScreenTeX) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project uses.
+function projectLanguageFileTwo() {
+  echo ("I have chosen Shell as the second project language file for this project (ScreenTeX) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project uses.");
+  break;
+}
+return projectLanguageFileTwo();
+break;
+exit;
+# File info
+# File version: 1 (Monday, 2021 September 20th at 1:18 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 16
+# End of script
