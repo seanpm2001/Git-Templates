c335f3ecde3f09a1af5f5a3defc8f3350382139b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-20 13:01:13 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Screenshot-tools/ScreenTeX/Python/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (ScreenTeX) to be Python, as Python is a language I am most familiar with, and it is good for writing the main software with. Vala may be used to supplement some portions on certain desktop environments, and other languages may also be used, but Python will be the core language.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: ScreenTeX")
+print("I have decided to have the main language for this project (ScreenTeX) to be Python, as Python is a language I am most familiar with, and it is good for writing the main software with. Vala may be used to supplement some portions on certain desktop environments, and other languages may also be used, but Python will be the core language.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, 2021 September 20th at 1:00 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
