7e9d6c686153f53b435f4ee8430c5d4c0fffddb2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-20 14:01:26 -0700

    PROJECT_LANG_2_V1.vhdl
    An archive of the first version of the 2nd project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BootDown/VHDL/PROJECT_LANG_2_V1.vhdl
@@ -0,0 +1,6 @@
+-- Start of script
+-- I have chosen VHDL as the 2nd project language file for this project (BootDown) as along with Assembly, it is needed for hardware virtualization.
+-- File version: 1 (Monday, 2021 September 20th at 1:59 pm)
+-- File type: Virtual Hard Disk Language (VHDL) Source file (*.vhdl, *.vhd, *.vdi)
+-- Line count (including blank lines and compiler line): 7
+-- End of script
