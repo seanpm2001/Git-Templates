1312b540edf2c9489c4b320318b8b15a658433a8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-20 13:53:12 -0700

    PROJECT_LANG_1_V1.asm
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BootDown/Assembly/PROJECT_LANG_1_V1.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the first project language for this project (BootDown) as it is needed for many functions of this operating system project.
+
+; File info
+; File version: 1 (Monday, 2021 September 20th at 1:51 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
