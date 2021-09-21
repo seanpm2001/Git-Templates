0a05fd77733815d6b8758802d241a1e56c0ae855
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-21 15:13:31 -0700

    PROJECT_LANG_2.asm
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/BootDown-dev/Assembly/PROJECT_LANG_2.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the second project language for this project (BootDown-dev.github.io) because although the website is not written in Assembly, the project is is hosting relies heavily on it for many functions of the BootDown operating system project.
+
+; File info
+; File version: 1 (Tuesday, 2021 September 21st at 3:12 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
