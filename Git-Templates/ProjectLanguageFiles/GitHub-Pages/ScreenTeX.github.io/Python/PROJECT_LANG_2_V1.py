c87d4f9cae8eeb4e219ae5f0aef1dff6d0117b8e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-21 16:17:54 -0700

    PROJECT_LANG_2_V1.py
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/ScreenTeX.github.io/Python/PROJECT_LANG_2_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the second project language for this project (ScreenTeX.github.io) be Python, as although the website is not written in Python, the project it is hosting relies heavily on the language.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 2\n")
+print("For: ScreenTeX.github.io")
+print("I have decided to have the second project language for this project (ScreenTeX.github.io) be Python, as although the website is not written in Python, the project it is hosting relies heavily on the language.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Tuesday, 2021 September 21st at 4:16 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
