cecd5c401d3a11ff95f5597b565eff9f488231bb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-24 19:37:39 -0700

    PROJECT_LANG_2_V1.asm
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Graphic-Design/WacPaint/Assembly/PROJECT_LANG_2_V1.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the second project language for this project (WacPaint) because Assembly is the secondary language that WacPaint is written in, and one of the languages that the original software that this project is based on is written in,
+
+; File info
+; File version: 1 (Friday, 2021 September 24th at 7:35 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
