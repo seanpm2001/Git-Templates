2d19be73647c1cf6f9108b93dff1ff01299da8f8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-27 20:20:52 -0700

    PROJECT_LANG_3.sh
    The current version (version 1) of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/DesktopEnvironments/SkeuwieDE/Shell/PROJECT_LANG_3.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the third project language file for this project (Skeuowie) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project uses. It is important for a lot of backend functionality.
+function projectLanguageFileThree() {
+  echo ("Project language file #3");
+  echo ("For: Skeuowie");
+  echo ("About:");
+  echo ("I have chosen Shell as the third project language file for this project (Skeuowie) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project uses. It is important for a lot of backend functionality.");
+  return 0;
+  break;
+}
+return projectLanguageFileThree();
+break;
+return 0;
+exit;
+# File info
+# File version: 1 (Monday, 2021 September 27th at 8:20 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 21
+# End of script
