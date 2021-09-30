3a1097902807dd940e79b34230e5486e2a018c8a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-29 20:47:59 -0700

    PROJECT_LANG_1_V1.cpp
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Camera-software/CamCamPlus/C++/PROJECT_LANG_1_V1.cpp
@@ -0,0 +1,24 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// I chose C++ as the project language file for this project, as it is the language used for this project, and the projects name was heavily inspired by it (++C or C++ for CamCam+ ++Cam)
+// Start of code
+class projectLanguageFile {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file";
+    // Subheader
+    std::cout << "For: CamCamPlus";
+    // Neck
+    std::cout << "Reasoning:";
+    // Body
+    std::cout << "I chose C++ as the project language file for this project, as it is the language used for this project, and the projects name was heavily inspired by it (++C or C++ for CamCam+ ++Cam)";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}} // End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (Thursday, April 22nd 2021 at 6:05 pm)
+* Line count (including blank lines and compiler line): 25
+*/
+// End of script
