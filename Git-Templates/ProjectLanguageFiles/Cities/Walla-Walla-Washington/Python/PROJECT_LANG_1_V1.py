5ab9493b8c49e6a8f31c73d593dcd59a81103deb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-04 18:41:54 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Cities/Walla-Walla-Washington/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (Walla Walla Washington) to be Python, as Python is a language I am most familiar with, and it is a recommended choice for the software that is written here. Python is also already a majority language for this project. Fork the project and make another language the top if needed.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: Walla Walla Washington")
+print("I have decided to have the main language for this project (Walla Walla Washington) to be Python, as Python is a language I am most familiar with, and it is a recommended choice for the software that is written here. Python is also already a majority language for this project. Fork the project and make another language the top if needed.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, 2021 October 4th at 6:40 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
