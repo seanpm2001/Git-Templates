3042f682518390b91b3e90f64745eb76721ab1d2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-04 18:52:21 -0700

    PROJECT_LANG_3_V1.sh
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Cities/Walla-Walla-Washington/Shell/PROJECT_LANG_3_V1.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the third project language file for this project (Walla Walla Washington) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that some of this project uses.
+function projectLanguageFileThree() {
+  echo ("Project language file #3");
+  echo ("For: Walla Walla Washington");
+  echo ("About:");
+  echo ("I have chosen Shell as the third project language file for this project (Walla Walla Washington) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that some of this project uses.");
+  return 0;
+  break;
+}
+return projectLanguageFileThree();
+break;
+return 0;
+exit;
+# File info
+# File version: 1 (Monday, 2021 October 4th at 6:51 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 21
+# End of script
