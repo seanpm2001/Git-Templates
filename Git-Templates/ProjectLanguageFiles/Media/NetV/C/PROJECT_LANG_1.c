d35ba4992ab0dca531e32caadaf8457908e98f40
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-06 16:25:29 -0700

    PROJECT_LANG_1.c
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Media/NetV/C/PROJECT_LANG_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the main project language file for this project (NetV) as C is the primary language I am writing this extension in for compatibility, portability, and stability. This is specifically written in Portable-C. It may be supplemented with another language.
+union projectLanguageFileOne {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: NetV");
+    printf("I decided to make C the main project language file for this project (NetV) as C is the primary language I am writing this extension in for compatibility, portability, and stability. This is specifically written in Portable-C. It may be supplemented with another language.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Wednesday, 2021 October 6th at 4:24 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
