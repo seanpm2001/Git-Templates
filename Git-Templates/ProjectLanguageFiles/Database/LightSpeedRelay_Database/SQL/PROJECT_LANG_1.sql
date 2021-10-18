0d24a10106908646f49becd5183ef6af0c00d35c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-18 16:01:12 -0700

    PROJECT_LANG_1.sql
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Database/LightSpeedRelay_Database/SQL/PROJECT_LANG_1.sql
@@ -0,0 +1,11 @@
+--Start of script;
+--I chose SQL as the main project language for this project (LightSpeedRelay/Database) as the main database sets for this project require SQL as their base language.
+CREATE TABLE "PROJECT_LANGUAGE_FILE_1" WITH STRING "I chose SQL as the main project language for this project (LightSpeedRelay/Database) as the main database sets for this project require SQL as their base language.";
+SELECT * FROM PROJECT_LANGUAGE_FILE_1;
+--I am unable to test this program, as I don't have a proper database/server set up yet;
+-- File info
+-- File type: SQL source code (*.sql)
+-- File version: 1 (Monday, 2021 October 18th at 4:00 pm)
+-- Line count (including blank lines and compiler line): 12
+-- Language: English (US)
+--End of script;
