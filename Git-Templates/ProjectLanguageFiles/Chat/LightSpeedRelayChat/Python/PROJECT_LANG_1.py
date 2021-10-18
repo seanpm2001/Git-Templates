94dee8285436dfa2e5e296b6ceddc3ae0fd64303
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-18 16:09:47 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Chat/LightSpeedRelayChat/Python/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (LightSpeed Relay Chat) be Python, as Python is a good language to use for this purpose, as I prefer its usage. I could have done C, or most any other modern programming language.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: Seanpm2001/LightSpeedRelayCHat")
+print("I have decided to have the main language for this project (LightSpeed Relay Chat) be Python, as Python is a good language to use for this purpose, as I prefer its usage. I could have done C, or most any other modern programming language.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, 2021 October 18th at 4:09 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
