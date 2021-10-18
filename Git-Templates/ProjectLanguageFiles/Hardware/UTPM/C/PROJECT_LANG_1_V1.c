62d38aaf4419faadc5101279fea1ed6d5994024c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-18 14:38:45 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/UTPM/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Start of script
+// Warning: contains bad code, may not compile
+// I decided to make C the main project language file for this project (UTPM) as C is the primary language that most computers handle, and is a good language for fixing TPM hardware. It may be supplemented with C++, Assembly, and/or another language.
+union projectLanguageFileOne {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: UTPM");
+    printf("I decided to make C the main project language file for this project (UTPM) as C is the primary language that most computers handle, and is a good language for fixing TPM hardware. It may be supplemented with C++, Assembly, and/or another language..\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+  printf("Error. Loop has broken. Please close the program");
+  kill 0 // (end with a crash)
+}
+// File info
+// File version: 1 (Monday, 2021 October 18th at 2:37 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 28
+// End of script
