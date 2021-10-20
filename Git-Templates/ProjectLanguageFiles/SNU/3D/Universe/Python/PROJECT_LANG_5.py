c3df9908757ba9a13637dcc587c17c12ae7de337
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-19 19:05:09 -0700

    PROJECT_LANG_5.py
    The current version (version 1) of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/3D/Universe/Python/PROJECT_LANG_5.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the fifth language for this project (SNU / 3D / Universe) be Python, as Python is a good, powerful, Libre language to use for this project, and it can be used as an alternative to JavaScript or PHP.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: SNU / 3D / Universe")
+print("I have decided to have the fifth language for this project (SNU / 3D / Universe) be Python, as Python is a good, powerful, Libre language to use for this project, and it can be used as an alternative to JavaScript or PHP.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Tuesday, 2021 October 19th at 7:03 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
