c9aecc70cb0e15e64970fc922f2776dc24b7fe7c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-19 18:55:57 -0700

    PROJECT_LANG_3_V1.js
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/3D/Universe/JavaScript/PROJECT_LANG_3_V1.js
@@ -0,0 +1,30 @@
+// Start of script
+// I chose JavaScript as the third project language for this project (SNU / 3D / Universe) as JavaScript is one of the 3 common languages that most webpages are written in, and JavaScript is one of the portable functional programming languages used for this project. It also has a lot of use in SNU development. For this project, it will also be supplemented with some other high level languages. It was already one of the 2 existing languages.
+class projectLanguageFileTwo() {
+  void main() {
+    console.log ("Project language file 3");
+    console.log ("For: SNU / 3D / Universe");
+    console.log ("About:");
+    console.log ("I chose JavaScript as the third project language for this project (SNU / 3D / Universe) as JavaScript is one of the 3 common languages that most webpages are written in, and JavaScript is one of the portable functional programming languages used for this project. It also has a lot of use in SNU development. For this project, it will also be supplemented with some other high level languages. It was already one of the 2 existing languages.");
+    /* Garbage syntax, so I can test out 2 new keywords I recently learned. Uncomment to highlight
+    x = bool("true");
+    y = bool("false");
+    if (x == "false"); {
+      try y
+    } else: {
+      catch x;
+    }
+    */
+    break;
+    return 0;
+  }
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+/* File info
+* File version: 1 (Tuesday, 2021 October 19th at 6:54 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 31
+*/
+// End of script
