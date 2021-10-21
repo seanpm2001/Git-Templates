4fa1b1b5c4e85ce3f4016672dc40579d5285c13b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-21 15:50:39 -0700

    PROJECT_LANG_2_V1.java
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Office/uOffice/Java/PROJECT_LANG_2_V1.java
@@ -0,0 +1,21 @@
+// Start of script
+// I decided to make the 2nd project language file for this project (uOffice) to be Java, as the original prototype of this project was written in Java, and it was significant, as it was the only time I purposefully made 'closed source' software (it is now open source)
+public class projectLanguageFileTwo {
+	public static void main(String args) {
+		System.out.println("Project language file 2");
+		System.out.println("For: uOffice");
+		System.out.println("About:");
+		System.out.println("I decided to make the 2nd project language file for this project (uOffice) to be Java, as the original prototype of this project was written in Java, and it was significant, as it was the only time I purposefully made 'closed source' software (it is now open source)");
+		break;
+}
+return main();
+int y = 2;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break } // No exit command exists
+// I am not experienced with C, so this script may not work
+/* File info
+* File version: 1 (Thursday, 2021 October 21st at 3:49 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 22
+*/
+// End of script
