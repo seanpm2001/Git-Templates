77171c092b254b5849523f39b5ca696e4169e1c3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-24 15:05:03 -0700

    PROJECT_LANG_2.js
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Automation/AUTOMATE-2001/JavaScript/PROJECT_LANG_2.js
@@ -0,0 +1,20 @@
+// Start of script
+// I decided to make the 2nd project language file for this project (AUTOMATE-2001) to be JavaScript, as JavaScript is one of the languages needed for automation in connection with the GitHub API.
+class projectLanguageFileTwo() {
+  void main() {
+    console.log ("Project language file 2");
+    console.log ("AUTOMATE-2001");
+    console.log ("About:");
+    console.log ("I decided to make the 2nd project language file for this project (AUTOMATE-2001) to be JavaScript, as JavaScript is one of the languages needed for automation in connection with the GitHub API.");
+    break;
+    return 0;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Sunday, 2021 October 24th at 3:04 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 21
+*/
+// End of script
