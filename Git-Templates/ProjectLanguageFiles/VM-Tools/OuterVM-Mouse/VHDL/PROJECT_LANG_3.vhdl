42fa0ee9cf3cbab4b29bdb9a6dfb58af12f4fc55
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-24 17:30:17 -0700

    PROJECT_LANG_3.vhdl
    The current version (version 1) of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/VM-Tools/OuterVM-Mouse/VHDL/PROJECT_LANG_3.vhdl
@@ -0,0 +1,11 @@
+-- Start of script
+
+-- I have chosen VHDL as the 3rd project language file for this project (Seanpm2001/OuterVM-Mouse) as along with Assembly, it is needed for hardware virtualization.
+
+-- File info
+
+-- File version: 1 (Sunday, 2021 October 24th at 5:29 pm)
+-- File type: Virtual Hard Disk Language (VHDL) Source file (*.vhdl, *.vhd, *.vdi)
+-- Line count (including blank lines and compiler line): 12
+
+-- End of script
