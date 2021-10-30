14caf77867d664340312100cc84502e81da048c8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-29 19:09:31 -0700

    PROJECT_LANG_2.js
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Audio_Projects/SeansAudioDB/JavaScript/PROJECT_LANG_2.js
@@ -0,0 +1,20 @@
+// Start of script
+// I decided to make the 2nd project language file for this project (SeansAudioDB) to be JavaScript, as JavaScript is one of the languages used early on in development (jQuery is an implementation of JavaScript) JavaScript might not be used further in this project.
+class projectLanguageFileTwo() {
+  void main() {
+    console.log ("Project language file 2");
+    console.log ("SeansAudioDB");
+    console.log ("About:");
+    console.log ("I decided to make the 2nd project language file for this project (SeansAudioDB) to be JavaScript, as JavaScript is one of the languages used early on in development (jQuery is an implementation of JavaScript) JavaScript might not be used further in this project.");
+    break;
+    return 0;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Friday, 2021 October 29th at 7:08 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 21
+*/
+// End of script
