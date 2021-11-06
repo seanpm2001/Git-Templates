0f0fd2a6f1366d561d2517b12fe0962f3c6a6f35
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-05 17:08:19 -0700

    PROJECT_LANG_1.py
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Techlaration/Techlaration/Python/PROJECT_LANG_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Start of script
+# I have decided to have the first project language for this project (Techlaration) be Python, as Python is the main language that most projects of my projects are written in, and it works well with this set of projects. This is the core of all Techlaration projects, and Python should be represented here.
+''' '''
+class projectLanguageFileThree()
+print("Project Language File 1\n")
+print("For: Seanpm2001/Techlaration")
+print("I have decided to have the first project language for this project (Techlaration) be Python, as Python is the main language that most projects of my projects are written in, and it works well with this set of projects. This is the core of all Techlaration projects, and Python should be represented here.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Friday, 2021 November 5th at 5:07 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 18
+# End of script
