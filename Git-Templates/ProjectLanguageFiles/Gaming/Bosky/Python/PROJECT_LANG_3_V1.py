c0198a9d45d7995ec770f898a017d8d7ae9523d9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-10 17:06:42 -0800

    PROJECT_LANG_3_V1.py
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/Bosky/Python/PROJECT_LANG_3_V1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Start of script
+# I have decided to have the third project language for this project (Bosky) be Python, as Python is the third language used on this project for the main script and the GUI. I could have written the whole project in 1 language, but I decided to diversify and write in 3 different programming languages.
+''' '''
+class projectLanguageFileThree()
+print("Project Language File 3\n")
+print("For: Seanpm2001/Bosky")
+print("I have decided to have the third project language for this project (Bosky) be Python, as Python is the third language used on this project for the main script and the GUI. I could have written the whole project in 1 language, but I decided to diversify and write in 3 different programming languages.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Wednesday, 2021 November 10th at 5:05 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 18
+# End of script
