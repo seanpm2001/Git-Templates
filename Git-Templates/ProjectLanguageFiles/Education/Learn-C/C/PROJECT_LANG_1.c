7b3a8a409d02206fb1adb9eeb51a08485d49cbb0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-11 18:53:55 -0800

    PROJECT_LANG_1.c
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-C/C/PROJECT_LANG_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+// Start of script
+
+// Warning: contains bad code, may not compile
+
+// I decided to make C the main project language file for this project (Seanpm2001/Learn-C) as C is the language this project is dedicated to, because this project is about learning the C programming language. It only makes sense to make C the official language for this project.
+union projectLanguageFileOne {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: Seanpm2001/Learn-C");
+    printf("About:");
+    printf("I decided to make C the main project language file for this project (Seanpm2001/Learn-C) as C is the language this project is dedicated to, because this project is about learning the C programming language. It only makes sense to make C the official language for this project.\n");
+    break;
+    return 0;
+  }
+return main();
+printf("The program is going to close in 15 seconds");
+wait 15;
+return 0;
+break;
+exit;
+}
+// File info
+
+// File version: 1 (Thursday, 2021 November 11th at 6:53 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 30
+
+// End of script
