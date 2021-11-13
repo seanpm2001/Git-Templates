4e2782993faa6cc34759d2b24918a05a31d67b08
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-12 19:06:40 -0800

    PROJECT_LANG_1_V1.asm
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-Assembly/Assembly/PROJECT_LANG_1_V1.asm
@@ -0,0 +1,6 @@
+; I have decided to have the first project language file for this project (Seanpm2001/Learn-Assembly) be written in Assembly as this project is about learning the Assembly programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+
+; File info
+; File version: 1 (2021, Friday, November 12th at 7:05 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
