68b2672c9a86a15553bdf5d56066f72636aadf4c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-12 16:39:45 -0800

    PROJECT_LANG_1_V1.pl
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-Perl/Perl/PROJECT_LANG_1_V1.pl
@@ -0,0 +1,9 @@
+# Start of script
+# I chose Perl as the first project language file for this project (Seanpm2001/Learn-Perl) as this project is about learning the Perl programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+print "I chose Perl as the first project language file for this project (Seanpm2001/Learn-Perl) as this project is about learning the Perl programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.";
+break;
+# File info
+# File type: Perl source file (*.pl)
+# File version: 1 (Friday, 2021 November 12th at 4:38 pm)
+# Line count (including blank lines and compiler line): 10
+# End of script
