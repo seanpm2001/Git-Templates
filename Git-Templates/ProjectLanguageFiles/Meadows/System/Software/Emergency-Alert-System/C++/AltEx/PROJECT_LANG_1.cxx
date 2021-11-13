cd4b0c96b6dcccc0cf0ebb8b7fb565aac3d85f10
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-13 14:37:01 -0800

    PROJECT_LANG_1_V1.cxx
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Meadows/System/Software/Emergency-Alert-System/C++/AltEx/PROJECT_LANG_1.cxx
@@ -0,0 +1,24 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// I chose C++ as the first project language file for this project (Seanpm2001/Meadows_Emergency-Alert-System) as C++ seemed like a good language to write this program in, and the source code of the project started in C++ so I wanted to match it here.
+// Start of code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1";
+    // Subheader
+    std::cout << "For: Seanpm2001/Meadows_Emergency-Alert-System";
+    // Neck
+    std::cout << "Reasoning:";
+    // Body
+    std::cout << "I chose C++ as the first project language file for this project (Seanpm2001/Meadows_Emergency-Alert-System) as C++ seemed like a good language to write this program in, and the source code of the project started in C++ so I wanted to match it here.";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}} // End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (2021, Saturday, November 13th at 2:34 pm)
+* Line count (including blank lines and compiler line): 25
+*/
+// End of script
