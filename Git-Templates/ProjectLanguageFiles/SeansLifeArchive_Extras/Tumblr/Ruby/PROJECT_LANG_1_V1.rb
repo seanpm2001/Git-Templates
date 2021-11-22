8efbf07f5f84bdc6f5b0415859fad477e830aeb3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-21 19:42:39 -0800

    PROJECT_LANG_1_V1.rb
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/Tumblr/Ruby/PROJECT_LANG_1_V1.rb
@@ -0,0 +1,11 @@
+# Start of script
+# I chose Ruby as the first project language for this project (Seanpm2001/LifeArchive/Extras/Tumblr) as Ruby is the language that currently is the majority in the development of Tumblr (as far as open source goes) and this project is about Tumblr, so the language fits.
+class projectLanguageFileOne()
+puts "I chose Ruby as the first project language for this project (Seanpm2001/LifeArchive/Extras/Tumblr) as Ruby is the language that currently is the majority in the development of Tumblr (as far as open source goes) and this project is about Tumblr, so the language fits."
+break
+end
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (Sunday, 2021 November 21st at 7:41 pm)
+# Line count (including blank lines and compiler line): 12
+# End of script
