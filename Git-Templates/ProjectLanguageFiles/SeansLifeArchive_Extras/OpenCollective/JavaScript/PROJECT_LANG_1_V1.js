e64997f1745c313c62b559eb1910858e5de80100
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-21 19:33:47 -0800

    PROJECT_LANG_1_V1.js
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/OpenCollective/JavaScript/PROJECT_LANG_1_V1.js
@@ -0,0 +1,17 @@
+// Start of script
+// I chose JavaScript as the first project language for this project (Seanpm2001/LifeArchive/Extras/OpenCollective) as JavaScript is the language that currently is the majority in the development of OpenCollective (as far as open source goes) and this project is about OpenCollective, so the language fits.
+class projectLanguageFileOne() {
+  void main() {
+    console.log ("Project language file 1");
+    console.log ("For: Seanpm2001/LifeArchive/Extras/OpenCollective");
+    console.log ("About:");
+    console.log ("I chose JavaScript as the first project language for this project (Seanpm2001/LifeArchive/Extras/OpenCollective) as JavaScript is the language that currently is the majority in the development of OpenCollective (as far as open source goes) and this project is about OpenCollective, so the language fits.");
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+/* File info
+* File version: 1 (Sunday, 2021 November 21st at 7:32 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
