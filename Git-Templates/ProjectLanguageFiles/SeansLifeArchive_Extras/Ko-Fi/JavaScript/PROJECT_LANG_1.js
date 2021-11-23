94aa78ecdcb16afd2ff6feaf1cc9d2913ffefb35
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-22 16:24:20 -0800

    PROJECT_LANG_1.js
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/Ko-Fi/JavaScript/PROJECT_LANG_1.js
@@ -0,0 +1,17 @@
+// Start of script
+// I chose JavaScript as the first project language for this project (Seanpm2001/LifeArchive/Extras/Ko-Fi) as JavaScript is the language that currently is the majority in the development of Ko-Fi (as far as open source goes) and this project is about Ko-Fi, so the language fits.
+class projectLanguageFileOne() {
+  void main() {
+    console.log ("Project language file 1");
+    console.log ("For: Seanpm2001/LifeArchive/Extras/Ko-Fi");
+    console.log ("About:");
+    console.log ("I chose JavaScript as the first project language for this project (Seanpm2001/LifeArchive/Extras/Ko-Fi) as JavaScript is the language that currently is the majority in the development of Ko-Fi (as far as open source goes) and this project is about Ko-Fi, so the language fits.");
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+/* File info
+* File version: 1 (Monday, 2021 November 22nd at 4:23 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
