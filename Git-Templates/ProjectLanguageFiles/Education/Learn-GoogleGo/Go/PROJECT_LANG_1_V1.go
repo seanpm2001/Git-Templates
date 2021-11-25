4b1f2105f4de12c652af0a2054d759b6ba56fedd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-25 14:49:49 -0800

    PROJECT_LANG_1_V1.go
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-GoogleGo/Go/PROJECT_LANG_1_V1.go
@@ -0,0 +1,19 @@
+// Start of script
+// I decided to make Go the main project language file for this project (Seanpm2001/Learn-GoogleGo) as Google Golang is the language this project is dedicated to, because this project is about learning the Google Golang programming language. It only makes sense to make Google Golang the official language for this project.
+package main
+import "fmt"
+func main() {
+    // Project language file
+    fmt.println("Project language file 1")
+    fmt.println("For: Seanpm2001/Learn-GoogleGo")
+    fmt.println("About:")
+    fmt.Println("I decided to make Go the main project language file for this project (Seanpm2001/Learn-GoogleGo) as Google Golang is the language this project is dedicated to, because this project is about learning the Google Golang programming language. It only makes sense to make Google Golang the official language for this project.")
+    break;
+}
+// End of script
+/* File info
+* File type: Google Go source file (*.go)
+* File version: 1 (2021, Thursday, November 25th at 2:47 pm
+* Line count (including blank lines and compiler line): 20
+*/
+// End of script
