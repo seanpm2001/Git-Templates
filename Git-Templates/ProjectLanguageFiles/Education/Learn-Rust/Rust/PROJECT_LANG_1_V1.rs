ce83db9c09f773c80a1de16a5c67ba95f3b05f74
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-24 18:53:38 -0800

    PROJECT_LANG_1_V1.rs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-Rust/Rust/PROJECT_LANG_1_V1.rs
@@ -0,0 +1,16 @@
+// Start of script
+// I decided to make Rust the main project language file for this project (Seanpm2001/Learn-Rust) as Rust is the language this project is dedicated to, because this project is about learning the Rust programming language. It only makes sense to make Rust the official language for this project.
+fn main() {
+  println!("Project language file 1 (Rust)");
+  println!("For: Seanpm2001/Learn-Rust");
+  println!("About:");
+  println!("I decided to make Rust the main project language file for this project (Seanpm2001/Learn-Rust) as Rust is the language this project is dedicated to, because this project is about learning the Rust programming language. It only makes sense to make Rust the official language for this project.");
+  break;
+}
+return main();
+break;
+// File info
+// File version: 1 (Wednesday, 2021 November 24th at 6:52 pm)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 17
+// End of script
