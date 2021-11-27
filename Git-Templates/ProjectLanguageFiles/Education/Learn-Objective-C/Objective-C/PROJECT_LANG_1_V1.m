261796b6c9f762cf7a9b95486462061a930978d4
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-26 19:47:34 -0800

    PROJECT_LANG_1_V1.m
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-Objective-C/Objective-C/PROJECT_LANG_1_V1.m
@@ -0,0 +1,22 @@
+@interface classname : projectLanguageFileOne {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @MacOS_10_1_ObjectiveC classname : superclassname {
+    // I decided to make Objective-C the main project language file for this project (Seanpm2001/Learn-Objective-C) as Objective-C is the language this project is dedicated to, because this project is about learning the Objective-C programming language. It only makes sense to make Objective-C the official language for this project.
+	NSLog(@"I decided to make Objective-C the main project language file for this project (Seanpm2001/Learn-Objective-C) as Objective-C is the language this project is dedicated to, because this project is about learning the Objective-C programming language. It only makes sense to make Objective-C the official language for this project..");
+	break;
+}
+@end
+/* File info
+* File version: `1 (Friday, 2021 November 26th at 7:46 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 23
+* This script is incomplete, and needs lots of work */
+// End of script
