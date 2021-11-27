9bde13df8bbe3c8947ddb46d14f41da419e34ad5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-26 21:57:36 -0800

    PROJECT_LANG_7_V1.rb
    An archive of the first version of the 7th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/ShareBoxLIB/Ruby/PROJECT_LANG_7_V1.rb
@@ -0,0 +1,11 @@
+# Start of script
+# I chose Ruby as the seventh project language for this project (Seanpm2001/ShareBoxLIB) as Ruby is one of the languages used on certain variants of this projects framework, and can be an alternate to JavaScript, Python, PHP, and other languages to be decided in the future.
+class projectLanguageFileOne()
+puts "I chose Ruby as the seventh project language for this project (Seanpm2001/ShareBoxLIB) as Ruby is one of the languages used on certain variants of this projects framework, and can be an alternate to JavaScript, Python, PHP, and other languages to be decided in the future."
+break
+end
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (Sunday, 2021 November 21st at 9:56 pm)
+# Line count (including blank lines and compiler line): 12
+# End of script
