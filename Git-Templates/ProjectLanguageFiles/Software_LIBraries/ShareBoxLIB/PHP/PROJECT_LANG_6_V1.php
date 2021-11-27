e1b6c4ad80124b4280506de42eca1697fbb6ef3d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-26 21:53:33 -0800

    PROJECT_LANG_6_V1.php
    An archive of the first version of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/ShareBoxLIB/PHP/PROJECT_LANG_6_V1.php
@@ -0,0 +1,19 @@
+<?php
+// Start of script
+// I chose PHP as the 6th project language for this project (Seanpm2001/ShareBoxLIB) as PHP is one of the languages used on certain variants of this projects framework, and can be an alternate to JavaScript, Python, Ruby, and other languages to be decided in the future.
+$PROJECT_LANGUAGE_FOUR; {
+  echo ("Project language 6");
+  echo ("For: Seanpm2001/ShareBoxLIB");
+  echo ("I chose PHP as the 6th project language for this project (Seanpm2001/ShareBoxLIB) as PHP is one of the languages used on certain variants of this projects framework, and can be an alternate to JavaScript, Python, Ruby, and other languages to be decided in the future.");
+  break;
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return $PROJECT_LANGUAGE_FOUR;
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (Friday, 2021 November 26th at 9:52 pm)
+* Line count (including blank lines and compiler line): 20
+*/
+// End of script
+?>
