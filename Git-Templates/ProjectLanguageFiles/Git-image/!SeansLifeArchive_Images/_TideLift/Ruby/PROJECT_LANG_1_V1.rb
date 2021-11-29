93a735dbee4daaa3771d5e9702cdbed19072a69f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-28 21:23:54 -0800

    PROJECT_LANG_1_V1.rb
    An archive of the first version of the project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/_TideLift/Ruby/PROJECT_LANG_1_V1.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby -w
+# Start of script
+# I chose Ruby as the first project language for this project (Seanpm2001/LifeArchive/Images/TideLift) as Ruby is the language that currently is the majority in the development of TideLift (as far as open source goes) and this project is about TideLift, so the language fits.
+class projectLanguageFileOne()
+puts "I chose Ruby as the first project language for this project (Seanpm2001/LifeArchive/Images/TideLift) as Ruby is the language that currently is the majority in the development of TideLift (as far as open source goes) and this project is about TideLift, so the language fits."
+break
+end
+# File info
+# File type: Ruby source file (*.rb *.gemfile *.rakefile *.rake)
+# File version: 1 (Sunday, 2021 November 28th at 9:22 pm)
+# Line count (including blank lines and compiler line): 13
+# End of script
