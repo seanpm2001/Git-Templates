9dbdf498732fcad5246a8d828082b322b97fd68e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-07 14:58:41 -0800

    PROJECT_LANG_1_V1.fs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/ThePlayForge-ZombieLife/F#/PROJECT_LANG_1_V1.fs
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S dotnet fsi // Trying this out, as the last one was recognized as GLSL/Forth for some reason
+// Start of script
+// I decided to make F# the main programming language for this project (Seanpm2001LifeArchive/Images/ThePlayForge-ZombieLife) as I want to make a pun off the name, since there isn't much else I can do with the contents of the project. Forge has an F in it, and the forge is sharp, so F# is the project language here. I can't do much else for a project language file, as this is an image/information project, and not a software/web application project. I wanted to make a zombie pun, but I couldn't find a related language.
+printfn "I decided to make F# the main programming language for this project (Seanpm2001LifeArchive/Images/ThePlayForge-ZombieLife) as I want to make a pun off the name, since there isn't much else I can do with the contents of the project. Forge has an F in it, and the forge is sharp, so F# is the project language here. I can't do much else for a project language file, as this is an image/information project, and not a software/web application project. I wanted to make a zombie pun, but I couldn't find a related language."
+break
+"""
+File info
+File type: F# source file (.fs)
+File version: 1 (2021, Tuesday, December 7th at 2:52 pm)
+Line count (including blank lines and compiler line): 123
+"""
+// End of script
