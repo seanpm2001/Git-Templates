ae82c75a4e4b3c1f27e88abea12d4440919cff1d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-06 16:56:27 -0800

    PROJECT_LANG_1_V1.elm
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/ZooTree/Elm/PROJECT_LANG_1_V1.elm
@@ -0,0 +1,13 @@
+-- Start of script
+-- I decided to make Elm the main (first) project language for this project (ZooTree) as this project is about a game that is about trees, and it only makes sense to dedicate a language like Elm to it (since Elm is also named after a tree) this project needs a language file, so it works. I wanted to write this game in Elm as a challenge, and to further support the language. The main Linux version will be written in Elm. Other platforms will be written in other languages
+-- Start
+projectLanguageOne : String
+projectLanguageOne =
+    "Project language file 1\nFor: ZooTree\nAbout:\nI decided to make Elm the main (first) project language for this project (ZooTree) as this project is about a game that is about trees, and it only makes sense to dedicate a language like Elm to it (since Elm is also named after a tree) this project needs a language file, so it works. I wanted to write this game in Elm as a challenge, and to further support the language. The main Linux version will be written in Elm. Other platforms will be written in other languages."
+-- End
+{- File info
+File type: Elm source file (*.elm)
+File version: 1 (2021, Monday, December 6th at 4:55 pm)
+Line count (including blank lines and compiler line): 14
+-}
+-- End of script
