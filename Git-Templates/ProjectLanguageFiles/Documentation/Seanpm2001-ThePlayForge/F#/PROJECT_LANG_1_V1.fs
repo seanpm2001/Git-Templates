840a13165022bd555b1519bea254a9ad7d62ccb9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-07 14:46:26 -0800

    PROJECT_LANG_1_V1.fs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Documentation/Seanpm2001-ThePlayForge/F#/PROJECT_LANG_1_V1.fs
@@ -0,0 +1,11 @@
+// Start of script
+// I decided to make F# the main programming language for this project (Seanpm2001-ThePlayForge) as I want to make a pun off the name, since there isn't much else I can do with the contents of the project. Forge has an F in it, and the forge is sharp, so F# is the project language here. I can't do much else for a project language file, as this is an image/information project, and not a software/web application project.
+printfn "I decided to make F# the main programming language for this project (Seanpm2001-ThePlayForge) as I want to make a pun off the name, since there isn't much else I can do with the contents of the project. Forge has an F in it, and the forge is sharp, so F# is the project language here. I can't do much else for a project language file, as this is an image/information project, and not a software/web application project."
+break
+"""
+File info
+File type: F# source file (.fs)
+File version: 1 (2021, Tuesday, December 7th at 2:45 pm)
+Line count (including blank lines and compiler line): 12 
+"""
+// End of script
