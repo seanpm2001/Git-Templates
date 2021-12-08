067817226aa92e28f8de9851a1437ad6eb16f984
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-07 16:51:33 -0800

    PROJECT_LANG_1.c (V1)
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/LapLight/C/PROJECT_LANG_1.c
@@ -0,0 +1,21 @@
+// Start of script
+
+union projectLanguageFileOne() {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1");
+    printf("For: LapLight")
+}
+
+// I decided to make C the main project language file for this project (LapLight) as C is the most commonly supported and stable system language, so it would be good for this software, as it is designed to be interoperable across as many laptops as possible.
+printf("I decided to make C the main project language file for this project (LapLight) as C is the most commonly supported and stable system language, so it would be good for this software, as it is designed to be interoperable across as many laptops as possible.");
+}
+
+// File info
+
+// File version: 1 (Tuesday, 2021 December 7th at 4:50 pm)
+// File type: C programming language script file (*.c)
+// Line count (including blank lines and compiler line): 22
+
+// End of script
