261024cc8f23e961a38431636eeee9c50a8c0b28
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-07 22:21:30 -0800

    PROJECT_LANG_1_V1.cpp
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Zombie-Life-re-animated/C++/PROJECT_LANG_1_V1.cpp
@@ -0,0 +1,23 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// I chose C++ as the first project language file for this project (Zombie Life re-animated) as I wanted a good cross-platform language to use that would be compatible with as many operating systems as possible. I considered Java, but considered it too bloated for my needs, and possibly not compatible with iOS/iPadOS. I could have written multiple versions, but I am producing 4 games in one here, and want to keep it simple. There also were no zombie themed languages I could use as a pun.
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1";
+    // Subheader
+    std::cout << "For: Seanpm2001/Zombie-Life-re-animated";
+    // Neck
+    std::cout << "Reasoning:";
+    // Body
+    std::cout << "I chose C++ as the first project language file for this project (Zombie Life re-animated) as I wanted a good cross-platform language to use that would be compatible with as many operating systems as possible. I considered Java, but considered it too bloated for my needs, and possibly not compatible with iOS/iPadOS. I could have written multiple versions, but I am producing 4 games in one here, and want to keep it simple. There also were no zombie themed languages I could use as a pun..";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}} // End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (Tuesday, 2021 December 7th at 10:19 pm)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
