d1ae024beae81b763b118ef1d4f01dc40dd538e4
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-08 18:06:17 -0800

    PROJECT_LANG_1.py (V1)
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Notification-Tools/WakeNote/Ptython/PROJECT_LANG_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Start of script
+# I chose Python as the main (first) project language for this project (Seanpm2001/WakeNote) as Python is the preferred language for this projects development due to its stability, cross-platform support, and language features.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: WakeNote")
+print("I chose Python as the main (first) project language for this project (Seanpm2001/WakeNote) as Python is the preferred language for this projects development due to its stability, cross-platform support, and language features.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Wednesday, 2021 December 8th at 6:05 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 18
+# End of script
