a6840f172529aa03406a9a634726d75cceda5c73
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-10 15:47:54 -0800

    PROJECT_LANG_2_V1.glsl
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/ZooTree/GLSL/PROJECT_LANG_2_V1.glsl
@@ -0,0 +1,17 @@
+// Start of script
+// Sample file usage: https://www.lighthouse3d.com/tutorials/glsl-tutorial/hello-world/ (warning: insecure connection)
+#define Project_Language_File_Two
+// Project language file 2
+// For: ZooTree
+// I decided to make GLSL (OpenGL Shading Language) the second project language file for this project (ZooTree) as GLSL is the main shading language used on this project.
+void main()
+{
+    outputF = vec4(1.0, 0.0, 0.0, 1.0);
+}
+#endif
+/* File info
+* File type: GLSL source file (*.glsl)
+* File version: 1 (2021, Friday, December 10th at 3:46 pm)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
