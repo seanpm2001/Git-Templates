1a3c35441ec8dbe252885485f44c97368f88f375
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-12 19:13:54 -0800

    PROJECT_LANG_2.go (V1)
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/GitHub/2022/Go/PROJECT_LANG_2.go
@@ -0,0 +1,17 @@
+// Start of script
+// I chose Go as the second project language for this project (Seanpm2001/LifeArchive/Images/GitHub/2022) as Go is one of the 4 languages (Ruby, Go, JavaScript, C) used in the development of GitHub.
+package main
+import "fmt"
+func main() {
+    // Project language file
+    fmt.Println("I chose Go as the second project language for this project (Seanpm2001/LifeArchive/Images/GitHub/2022) as Go is one of the 4 languages (Ruby, Go, JavaScript, C) used in the development of GitHub.")
+    break
+}
+return main()
+break
+/* File info
+* File version: 1 (Sunday, 2021 December 12th at 7:13 pm)
+* File type: Google Go source file (*.go)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
