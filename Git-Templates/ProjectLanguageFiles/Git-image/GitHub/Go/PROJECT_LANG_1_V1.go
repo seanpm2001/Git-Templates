d821a834b8254312b7ad2b2e92412e0a0d94f714
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-12 16:47:26 -0800

    PROJECT_LANG_1_V1.go
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/GitHub/Go/PROJECT_LANG_1_V1.go
@@ -0,0 +1,8 @@
+// GitHub is written mostly in Go (2009 Google language) and I want this project to have a language assigned to it, so I created this Go (2009 Google language) file
+package main
+import "fmt"
+func main() {
+    // Project language file
+    fmt.Println("GitHub is written mostly in Go (2009 Google language) and I want this project to have a language assigned to it, so I created this Go (2009 Google language) file")
+}
+// End of script
