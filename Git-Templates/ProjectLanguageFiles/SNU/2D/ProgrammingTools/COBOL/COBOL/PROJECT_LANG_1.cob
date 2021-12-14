963dfcc04caa49dd18de3bae30a512c562a68447
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-13 18:27:24 -0800

    PROJECT_LANG_1.cob (V1)
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/COBOL/COBOL/PROJECT_LANG_1.cob
@@ -0,0 +1,14 @@
+*> Start of script
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROJECT_LANG_1.
+       PROCEDURE DIVISION.
+           DISPLAY "Project language file 1"
+           DISPLAY "For: SNU/2D/Programmingools/IDE/COBOL"
+           DISPLAY "About:"
+           DISPLAY "I decided to make COBOL the main project language for this project (SNU_2D_ProgrammingTools_IDE_COBOL) as COBOL is the main language of this projects IDE and should be represented here as well."
+           .
+*> File info
+      *> File type: COBOL Source file (*.cob *.cbl *.cpy)
+      *> File version: 1 (2021, Monday, December 13th at 6:26 pm)
+      *> Line count (including blank lines and compiler line): 15
+*> End of script
