720dec897f847a712e23692fe5d8fcfb4d585d97
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-17 18:44:31 -0800

    PROJECT_LANG_3_V1.cpp
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/Tetris128/C++/PROJECT_LANG_3_V1.cpp
@@ -0,0 +1,26 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// I chose C++ as the third project language file for this project (Seanpm2001/Tetris128) as C++ is one of the languages this project uses heavily (it has an engine written in the language) and I wanted to have a project language file to represent it.
+// Start of code
+class projectLanguageFileThreee {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 3";
+    // Subheader
+    std::cout << "For: Seanpm2001/Tetris128";
+    // Neck
+    std::cout << "About:";
+    // Body
+    std::cout << "I chose C++ as the third project language file for this project (Seanpm2001/Tetris128) as C++ is one of the languages this project uses heavily (it has an engine written in the language) and I wanted to have a project language file to represent it.";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+return main();
+break; // Not sure what this does, but I am using it as a stopping point.
+}} // End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.cc)
+* File version: 1 (2021, Friday, December 17th at 6:43 pm)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
