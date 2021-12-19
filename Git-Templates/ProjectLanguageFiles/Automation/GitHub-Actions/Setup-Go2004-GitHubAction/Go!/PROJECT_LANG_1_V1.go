efd30dc7e3f1da05cac38900da85cdbaf5e208d1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-18 18:14:28 -0800

    PROJECT_LANG_1_V1.go
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Automation/GitHub-Actions/Setup-Go2004-GitHubAction/Go!/PROJECT_LANG_1_V1.go
@@ -0,0 +1,22 @@
+// Start of script
+// I decided to make Go! the main project language file for this project (Setup-Go2004-GitHubAction) as Go! is the language this project is dedicated to, because this project is an online IDE for the GO! programming language. It only makes sense to make Go! the official language for this project.
+/*TODO 
+This file will need to be rewritten in the future when there is a way to write a similar program in Go!
+For now, this project is written in Googles Golang, 6 years later.
+*/
+package main
+import "fmt"
+func main() {
+    // Project language file
+    fmt.println("Project language file 1")
+    fmt.println("For: Setup-Go2004-GitHubAction")
+    fmt.println("About:")
+    fmt.Println("I decided to make Go! the main project language file for this project (Setup-Go2004-GitHubAction) as Go! is the language this project is dedicated to, because this project is an online IDE for the GO! programming language. It only makes sense to make Go! the official language for this project.")
+    break;
+}
+/* File info
+* File type: Go! source file (*.go)
+* File version: 1 (2021, Saturday, December 18th at 6:13 pm)
+* Line count (including blank lines and compiler line): 23
+*/
+// End of script
