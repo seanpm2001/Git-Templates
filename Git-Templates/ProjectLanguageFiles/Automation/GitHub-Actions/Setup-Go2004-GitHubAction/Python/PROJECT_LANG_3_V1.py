a851a9720672f15dcde18a0f74d05655d8771247
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-20 16:20:48 -0800

    PROJECT_LANG_3_V1.py
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Automation/GitHub-Actions/Setup-Go2004-GitHubAction/Python/PROJECT_LANG_3_V1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Start of script
+# I chose Python as the third project language for this project (Seanpm2001/Setup-Go2004-GitHubAction) as Python is the preferred language for writing the main script for this GitHub action.
+''' '''
+class projectLanguageFileThree()
+print("Project Language File 3\n")
+print("For: Seanpm2001/Setup-Go2004-GitHubAction")
+print("I chose Python as the third project language for this project (Seanpm2001/Setup-Go2004-GitHubAction) as Python is the preferred language for writing the main script for this GitHub action.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Wednesday, 2021 December 8th at 4:19 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 18
+# End of script
