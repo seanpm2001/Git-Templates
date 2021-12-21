827d2e6641c475c7290f835fa1a824bb0a00885b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-20 22:41:33 -0800

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/KindleFire/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,26 @@
+// Start of script
+
+union projectLanguageFileOne() {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1");
+    printf("For: SeansLifeArchive/Extras/KindleFIre")
+    break;
+}
+
+// I chose C as the first project language for this project (Seanpm2001/LifeArchive/Extras/KindleFire) as C is one of the 3 languages (C, C++, Java) that are known to have heavy use in the Kindle Fire operating system that this project is based around.
+return main();
+printf("I chose C as the first project language for this project (Seanpm2001/LifeArchive/Extras/KindleFire) as C is one of the 3 languages (C, C++, Java) that are known to have heavy use in the Kindle Fire operating system that this project is based around.");
+wait 10;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (Monday, 2021 December 20th at 10:40 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 27
+
+// End of script
