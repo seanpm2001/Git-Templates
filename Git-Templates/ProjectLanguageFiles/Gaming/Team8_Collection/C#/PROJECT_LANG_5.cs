58234b3435645b94edc9d1bddc31d4ac97c10a63
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-31 18:23:59 -0800

    PROJECT_LANG_5.cs (V1)
    The current version (version 1) of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/Team8_Collection/C#/PROJECT_LANG_5.cs
@@ -0,0 +1,17 @@
+// Start of script
+// using namespace;
+using System;
+// I decided to make C sharp the fifth project language for this project (Team8_Collection) as C# is the most compatible language with the Windows API and interface, and is a good language for the Windows port of this project.
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("I decided to make C sharp the fifth project language for this project (Team8_Collection) as C# is the most compatible language with the Windows API and interface, and is a good language for the Windows port of this project.");
+        break;
+    }
+}
+break;
+// File version: 1 (Friday, 2021 December 31st at 6:23 pm)
+// File type: C Sharp Source file (*.cs)
+// Line count (including blank lines and compiler line): 18
+// End of script
