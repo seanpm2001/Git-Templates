a87d73f4bbfb88394626271f8c239f4724425639
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-31 19:49:14 -0800

    PROJECT_LANG_3.sh (V1)
    The current version (version 1) of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/Seanpm2001-seanpm2001/Shell/PROJECT_LANG_3.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the third project language file for this project (Seanpm2001/Seanpm2001) because Shell is the go-to language for debug scripting on UNIX, and is native to some functionality on this project. It is also being represented as one of the computing languages I am good at.
+function projectLanguageFileThree() {
+  echo ("Project language file #3");
+  wait 1;
+  echo ("For: Seanpm2001/seanpm2001);
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the third project language file for this project (Seanpm2001/Seanpm2001) because Shell is the go-to language for debug scripting on UNIX, and is native to some functionality on this project. It is also being represented as one of the computing languages I am good at.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileThree();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Friday, 2021 December 31st at 7:48 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
