bcfc2e43fd1f83328e6a88c62f6ce8bada9db515
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-31 17:47:58 -0800

    PROJECT_LANG_2.java (V1)
    The current version (version 1) of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/Team8_Collection/Java/PROJECT_LANG_2.java
@@ -0,0 +1,21 @@
+// Start of script
+// I decided to make the 2nd project language file for this project (Team8_Collection) to be Java, as I prefer to use Java in making the Android port of this project. I could have used Kotlin, but to keep in line with the legacy structures of the base inspiration set of games, Java is preferred. Java will also be an unofficial secondary desktop language (the 1st one being Python)
+public class projectLanguageFileTwo {
+	public static void main(String args) {
+		System.out.println("Project language file 2");
+		System.out.println("For: Team8_Collection");
+		System.out.println("About:");
+		System.out.println("I decided to make the 2nd project language file for this project (Team8_Collection) to be Java, as I prefer to use Java in making the Android port of this project. I could have used Kotlin, but to keep in line with the legacy structures of the base inspiration set of games, Java is preferred. Java will also be an unofficial secondary desktop language (the 1st one being Python)");
+		break;
+}
+return main();
+int y = 2;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I amd oing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (Friday, 2021 December 21st at 5:47 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 22
+*/
+// End of script
