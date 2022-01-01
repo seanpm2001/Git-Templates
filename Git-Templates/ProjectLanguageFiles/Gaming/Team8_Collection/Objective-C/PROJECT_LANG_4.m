055b5023b9a047da8fcf90ce245ea3c727563b99
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-31 18:18:52 -0800

    PROJECT_LANG_4.m (V1)
    The current version (version 1) of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/Team8_Collection/Objective-C/PROJECT_LANG_4.m
@@ -0,0 +1,22 @@
+@interface classname : projectLanguageFileFour {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @MacOS_10_1_ObjectiveC classname : superclassname {
+    // I chose Objective-C as the fourth project language file for this project (Team8_Collection) as it is an additional language needed for native Apple platform support, especially XCode/MacOS (and partially iOS/iPhoneOS)
+	NSLog(@"I chose Objective-C as the fourth project language file for this project (Team8_Collection) as it is an additional language needed for native Apple platform support, especially XCode/MacOS (and partially iOS/iPhoneOS)");
+        break;
+}
+@end
+/* File info
+* File version: `1 (Friday, 2021 December 31st at 6:18 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 23
+* This script is incomplete, and needs lots of work */
+// End of script
