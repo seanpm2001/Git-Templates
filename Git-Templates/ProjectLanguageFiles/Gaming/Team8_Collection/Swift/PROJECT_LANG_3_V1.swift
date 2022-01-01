174f259871f4cc1a4bd1598fd1ee4f76891143f6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-31 18:14:38 -0800

    PROJECT_LANG_3_V1.swift
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/Team8_Collection/Swift/PROJECT_LANG_3_V1.swift
@@ -0,0 +1,14 @@
+// Start of script
+// I have chosen Swift as the third project language file for this project (Team8_Collection) as this project needs Swift support to work well natively on Apple platforms.
+class projectLanguageFileThree {
+var str = "I have chosen Swift as the third project language file for this project (Team8_Collection) as this project needs Swift support to work well natively on Apple platforms.";
+myValue = @String
+let myValue = str();
+return myValue;
+break;
+}
+// I don't know how to make working strings in Swift
+// File version: 1 (Friday, 2021 December 31st 6:13 pm)
+// File type: Swift 5.3 Source file (*.swift)
+// Line count (including blank lines and compiler line): 15
+// End of script
