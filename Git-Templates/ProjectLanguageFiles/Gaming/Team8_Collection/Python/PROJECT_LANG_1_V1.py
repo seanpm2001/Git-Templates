29bfc892028dceb25b654fdb1e991ef8da32f2ac
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-31 17:50:01 -0800

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/Team8_Collection/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,9 @@
+# Start of script
+# Project language file
+foo = int(3)
+bar = float(3.9)
+print ("This project is going to be written in Python. I feel like this is the most appropriate and efficient language for the recreation of these games.")
+print ("The game will start to be written in Python 3.9, but the version will upgrade when necessary")
+noMore = input("Press [ENTER] key to achknowledge this and exit")
+print ("The program should now be closed. If the program still isn't closed, try pressing the close button. If this still doesn't work, end the process with a process manager/task manager")
+# End of script
