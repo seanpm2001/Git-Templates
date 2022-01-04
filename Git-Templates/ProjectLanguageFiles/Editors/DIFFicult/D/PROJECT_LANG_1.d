19924b67d036548a38ef8845cb16ff314b714a85
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-03 16:31:06 -0800

    PROJECT_LANG_1.d (V1)
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Editors/DIFFicult/D/PROJECT_LANG_1.d
@@ -0,0 +1,23 @@
+#!/usr/bin/rdmd
+// Start of script
+import std.stdio : writeln;
+import std.range : iota;
+import std.parallelism : parallel;
+/* DIVIDER */
+void main()
+{
+  writeln("Project language file 1");
+  writeln("For: DIFFicult (Diff manager and editor)");
+  writeln("About:");
+	writeln ("I chose D as the first project language file for this project (DIFFicult) as I wanted to use the D language for this project, and I feel it also works well for this purpose.");
+}
+return main();
+return 0;
+break;
+// I chose D as the first project language file for this project (DIFFicult) as I wanted to use the D language for this project, and I feel it also works well for this purpose.
+/* DIVIDER */
+// File info
+// File version: 1 (2022, Monday, January 3rd at 4:30 pm)
+// File type: D programming language source file (*.d)
+// Line count (including blank lines and compiler line): 24
+// End of script
