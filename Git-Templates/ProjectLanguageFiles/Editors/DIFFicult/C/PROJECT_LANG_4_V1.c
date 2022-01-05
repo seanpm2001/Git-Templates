439f88322af16756773c248797e680dfc9806e41
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-04 17:14:33 -0800

    PROJECT_LANG_4_V1.c
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Editors/DIFFicult/C/PROJECT_LANG_4_V1.c
@@ -0,0 +1,27 @@
+// Start of script
+
+union projectLanguageFileFour) {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 4");
+    printf("For: Seanpm2001/DIFFicult")
+    break;
+}
+
+// I chose C as the fourth project language file for this project (DIFFicult) as C was one of the 5 languages I originally planned to write this project in, I just forgot to add it until day 2 of development. It is in use for headers and drivers.
+
+return main();
+printf("I chose C as the fourth project language file for this project (DIFFicult) as C was one of the 5 languages I originally planned to write this project in, I just forgot to add it until day 2 of development. It is in use for headers and drivers.");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Tuesday, January 4th at 5:13 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 28
+
+// End of script
