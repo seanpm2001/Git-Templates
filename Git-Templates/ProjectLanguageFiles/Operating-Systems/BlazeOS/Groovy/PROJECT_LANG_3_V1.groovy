9f626bce0a31f62937a5426eee97e464e194192f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-05 17:43:25 -0800

    PROJECT_LANG_3_V1.groovy
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/Groovy/PROJECT_LANG_3_V1.groovy
@@ -0,0 +1,13 @@
+// Start of script
+// I decided to make Groovy the 3rd project language file for this project (BlazeOS) as the model operating system (FireOS) heavily uses Java technologies, and to achieve similar performance and style, I will need to do similarly. This is considered an alternative languages to Java. It will also be supplemented with other languages, such as Java, Kotlin, C, C++, and Assembly.
+class ProjectLanguageFileThree {
+  println("I decided to make Groovy the 3rd project language file for this project (BlazeOS) as the model operating system (FireOS) heavily uses Java technologies, and to achieve similar performance and style, I will need to do similarly. This is considered an alternative languages to Java. It will also be supplemented with other languages, such as Java, Kotlin, C, C++, and Assembly.");
+  break;
+}
+assert ProjectLanguageFileThree;
+break;
+// File info
+// File version: 1 (2022, Wednesday, January 5th at 5:42 pm)
+// File type: Groovy source file (*.groovy *.gvy *.gy *.gsh)
+// Line count (including blank lines and compiler line): 14
+// End of script
