6c2d6a1a5570b353e3d31e8041153e0a9a398f7f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-05 17:59:12 -0800

    PROJECT_LANG_6.asm (V1)
    The current version (version 1) of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/Assembly/PROJECT_LANG_6.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the sixth project language for this project (BlazeOS) because Assembly is the best language for hardware interactions, alongside C and C++. It will be used for the lowest of level system calls and functions.
+
+; File info
+; File version: 1 (2022, Wednesday, January 5th at 5:58 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
