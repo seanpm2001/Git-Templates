eeb39d6838f57ea3e5e97a78775f5640281622c1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-05 17:30:58 -0800

    PROJECT_LANG_1.java (V1)
    The current version (version 1) of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/Java/PROJECT_LANG_1.java
@@ -0,0 +1,21 @@
+// Start of script
+// I decided to make Java the 1st project language file for this project (BlazeOS) as the model operating system (FireOS) heavily uses Java technologies, and to achieve similar performance and style, I will need to do similarly. Alternative languages for Java will include Kotlin and Groovy, and it will also be supplemented with other languages, such as C, C++, and Assembly.
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: BlazeOS");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (BlazeOS) as the model operating system (FireOS) heavily uses Java technologies, and to achieve similar performance and style, I will need to do similarly. Alternative languages for Java will include Kotlin and Groovy, and it will also be supplemented with other languages, such as C, C++, and Assembly.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I amd oing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Wednesday, January 4th at 5:30 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 22
+*/
+// End of script
