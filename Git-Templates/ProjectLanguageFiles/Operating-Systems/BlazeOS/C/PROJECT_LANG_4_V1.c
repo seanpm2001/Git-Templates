9fa745dfe6bc20fc37482dbefcc056cd63f114b8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-05 17:48:18 -0800

    PROJECT_LANG_4_V1.c
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/C/PROJECT_LANG_4_V1.c
@@ -0,0 +1,27 @@
+// Start of script
+
+union projectLanguageFileFour {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 4");
+    printf("For: BlazeOS")
+    break;
+}
+
+// I chose C as the fourth project language file for this project (BlazeOS) as C was one of the 6 languages I originally planned to write this project in (Java, Kotlin, Groovy, C, C++, Assembly) it is to be used for system functions and core programming. It will not be used for interfaces, that will be left up to Java, Kotlin, and Groovy respectively.
+
+return main();
+printf("I chose C as the fourth project language file for this project (BlazeOS) as C was one of the 6 languages I originally planned to write this project in (Java, Kotlin, Groovy, C, C++, Assembly) it is to be used for system functions and core programming. It will not be used for interfaces, that will be left up to Java, Kotlin, and Groovy respectively.");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Tuesday, January 5th at 5:47 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 28
+
+// End of script
