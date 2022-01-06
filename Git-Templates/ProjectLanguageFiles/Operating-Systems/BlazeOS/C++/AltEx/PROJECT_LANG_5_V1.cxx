6ca810d90fc8ac04337ad285b8429befad7918f2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-05 17:54:21 -0800

    PROJECT_LANG_5_V1.cxx
    An archive of the first version of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/C++/AltEx/PROJECT_LANG_5_V1.cxx
@@ -0,0 +1,26 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// I chose C++ as the fifth project language file for this project (BlazeOS) as C++ was one of the 6 languages I originally planned to write this project in (Java, Kotlin, Groovy, C, C++, Assembly) it is to be used for certain system functions, but not the majority of core programming. It will not be used for interfaces, that will be left up to Java, Kotlin, and Groovy respectively. In terms of system level importance, C++ ranks 2nd, C ranks 1st, and Assembly ranks 3rd
+// Start of code
+class projectLanguageFileFive {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 5";
+    // Subheader
+    std::cout << "For: BlazeOS";
+    // Neck
+    std::cout << "About:";
+    // Body
+    std::cout << "I chose C++ as the fifth project language file for this project (BlazeOS) as C++ was one of the 6 languages I originally planned to write this project in (Java, Kotlin, Groovy, C, C++, Assembly) it is to be used for certain system functions, but not the majority of core programming. It will not be used for interfaces, that will be left up to Java, Kotlin, and Groovy respectively. In terms of system level importance, C++ ranks 2nd, C ranks 1st, and Assembly ranks 3rd";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+return main();
+break; // Not sure what this does, but I am using it as a stopping point.
+}} // End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.cc)
+* File version: 1 (2022, Wednesday, January 5th at 5:52 pm)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
