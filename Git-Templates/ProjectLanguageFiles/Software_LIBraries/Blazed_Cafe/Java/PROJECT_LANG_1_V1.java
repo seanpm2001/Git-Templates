2793aa1e89d5cbde14bcc8e1f182aea3d4ccf62c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-09 20:52:54 -0800

    PROJECT_LANG_1_V1.java
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/Blazed_Cafe/Java/PROJECT_LANG_1_V1.java
@@ -0,0 +1,22 @@
+// Start of script
+// I decided to make Java the 1st project language file for this project (BlazeOS/Blazed_Cafe/) as this software library for BlazeOS is written mostly in Java, and is the first language used on the sub-project. It is being represented here in its own repository.
+
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: BlazeOS/Blazed_Cafe");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (BlazeOS/Blazed_Cafe/) as this software library for BlazeOS is written mostly in Java, and is the first language used on the sub-project. It is being represented here in its own repository.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I amd oing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Sunday, January 9th at 8:51 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 23
+*/
+// End of script
