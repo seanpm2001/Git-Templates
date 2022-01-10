5d2f490eef0ef8ed8163506ece0f5a5416c980c5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-10 15:45:40 -0800

    PROJECT_LANG_2_V1.java
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/IgniteBook/Java/PROJECT_LANG_2_V1.java
@@ -0,0 +1,22 @@
+// Start of script
+// I decided to make Java the 2nd project language file for this project (BlazeOS/IgniteBook/) as this software library for BlazeOS is written mostly in C, but is also written in Java. It is also the second language used on the sub-project. It is being represented here in its own repository.
+
+public class projectLanguageFileTwo {
+	public static void main(String args) {
+		System.out.println("Project language file 2");
+		System.out.println("For: BlazeOS/IgniteBook");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 2nd project language file for this project (BlazeOS/IgniteBook/) as this software library for BlazeOS is written mostly in C, but is also written in Java. It is also the second language used on the sub-project. It is being represented here in its own repository.");
+		break;
+}
+return main();
+int y = 2;
+System.out.println("Current project language file: " + int(y) + " (Java 10)");
+break } // No exit command exists
+// I am not experienced with Java, so this script may not work
+/* File info
+* File version: 1 (Monday, 2022 January 10th at 3:44 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 23
+*/
+// End of script
