f3acafff5c05fe03efc8da0b90a513dd7b49f6f8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-15 16:45:33 -0800

    PROJECT_LANG_2_V1.sh
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/Raspberry-Pi-Network-Setup/Shell/PROJECT_LANG_2_V1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the second project language file for this project (Seanpm2001/Raspberry-Pi-Network-Setup) because Shell is the go-to language for debug scripting on UNIX, and is native to some functionality on this project.
+function projectLanguageFileTwo() {
+  echo ("Project language file #2");
+  wait 1;
+  echo ("For: Seanpm2001/Raspberry-Pi-Network-Setup");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the second project language file for this project (Seanpm2001/Raspberry-Pi-Network-Setup) because Shell is the go-to language for debug scripting on UNIX, and is native to some functionality on this project.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileTwo();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Saturday, 2022 January 15th at 4:44 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
