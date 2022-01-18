5515e4ac132a84cec15e735ab78a7fba75ab92ee
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-17 17:33:14 -0800

    PROJECT_LANG_1_V1.cpp
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BluPhoneOS/C++/PROJECT_LANG_1_V1.cpp
@@ -0,0 +1,26 @@
+// Start of script
+#include <iostream>
+// I chose C++ as the first project language file for this project (BluPhoneOS) as C++ is the original planned language for this project due to its usability and functionality at an operating system level.\n
+// Start of source code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: BluPhoneOS\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I chose C++ as the first project language file for this project (BluPhoneOS) as C++ is the original planned language for this project due to its usability and functionality at an operating system level.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+return main();
+break; // Not sure what this does, but I am using it as a stopping point.
+return 0;
+}} // End of source code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.cc)
+* File version: 1 (2022, Monday, January 17th at 5:31 pm)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
