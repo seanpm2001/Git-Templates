d68838db42d70ce0377775def521f1fed604038d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-07 19:06:15 -0800

    PROJECT_LANG_1.py [V1]
    The current version [version one] of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/System_Tools/DeskLocker/Python/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (DeskLocker) to be Pythn, as Python is a language I am most familiar with, and it works with everything this basic projects needs for internal functionality.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: DeskLocker")
+print("I have decided to have the main language for this project (DeskLocker) to be Pythn, as Python is a language I am most familiar with, and it works with everything this basic projects needs for internal functionality.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Monday, 2022 February 7th at 7:05 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
