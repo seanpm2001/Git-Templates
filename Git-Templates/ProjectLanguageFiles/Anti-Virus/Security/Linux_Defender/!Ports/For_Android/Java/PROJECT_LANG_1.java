8bb73f07d3ce1fbba45ee0efaed558ebff37f3eb
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 15:33:00 -0800

    PROJECT_LANG_1.java [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Android/Java/PROJECT_LANG_1.java
@@ -0,0 +1,22 @@
+// Start of script
+// I decided to make Java the 1st project language file for this project (Linux_Defender_for_Android) as this software port is for Android, and the native language to Android is Java. Alternative functionality will also be available in Kotlin, CSS, Assembly, C, and possibly another language if needed.
+
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: Linux_Defender_for_Android");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (Linux_Defender_for_Android) as this software port is for Android, and the native language to Android is Java. Alternative functionality will also be available in Kotlin, CSS, Assembly, C, and possibly another language if needed.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I amd oing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Monday, February 21st at 3:32 pm)
+* File type: Java 8 Source file (*.java)
+* Line count (including blank lines and compiler line): 23
+*/
+// End of script
