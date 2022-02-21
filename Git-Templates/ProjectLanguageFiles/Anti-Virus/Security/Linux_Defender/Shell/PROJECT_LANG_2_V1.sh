31048a8d779323c8094173afe85f81efb25b7b73
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-20 18:34:23 -0800

    PROJECT_LANG_2_V1.sh
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/Shell/PROJECT_LANG_2_V1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the second project language file for this project (Linux_Defender) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.
+function projectLanguageFileTwo() {
+  echo ("Project language file #2");
+  wait 1;
+  echo ("For: Seanpm2001/Linux_Defender");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the second project language file for this project (Linux_Defender) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileTwo();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Sunday, 2022 February 20th at 6:33 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
