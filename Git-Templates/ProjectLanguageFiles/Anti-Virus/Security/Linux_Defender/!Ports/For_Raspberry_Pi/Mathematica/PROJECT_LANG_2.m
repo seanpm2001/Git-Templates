05f0d59f92ab97042ebdb0eb9dceae0008f0927f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 14:31:21 -0800

    PROJECT_LANG_2.m [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Raspberry_Pi/Mathematica/PROJECT_LANG_2.m
@@ -0,0 +1,26 @@
+(* ::Package:: *)
+(* Start of script *)
+(* Paclet Info File *)
+
+(* created Sept. 14 2018 *)
+
+(* Modified Jul. 22 2021 from https://github.com/WolframResearch/WolframLanguageForJupyter/blob/master/WolframLanguageForJupyter/PacletInfo.m *)
+
+Paclet[
+    Name -> "WolframLanguageForJupyter",
+    Version -> "0.9.2",
+    MathematicaVersion -> "11.2+",
+    Extensions -> {
+        {"Kernel", Context -> {"WolframLanguageForJupyter`"}},
+        {"Documentation", Language -> "English"}
+    }
+]
+
+(* I decided to make Mathematica the second language for this project (Linux Defender for Raspberry Pi) as the Raspberry Pi has default support for Mathematica, and I want to make use of it for calculations. It will work alongside Python in this project. *)
+
+(* File info
+* File type: Wolfram Mathematic source file (*.m)
+* File version: 1 (Monday, 2022 February 21st at 2:30 pm
+* Line count (including blank lines and compiler line): 27
+*)
+(* End of script *)
