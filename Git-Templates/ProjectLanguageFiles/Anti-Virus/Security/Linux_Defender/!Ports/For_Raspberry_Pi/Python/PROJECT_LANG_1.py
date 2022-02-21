614b6e7f43cd8b7945847fd9f68c2595f7250c08
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 14:17:18 -0800

    PROJECT_LANG_1.py [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Raspberry_Pi/Python/PROJECT_LANG_1.py
@@ -0,0 +1,16 @@
+# Start of script
+# Project language file 1
+# For: Linux Defender for Raspberry Pi
+# About
+# I decided to make Python the main project language file for this project (Linux Defender for Raspberry Pi) as Python is the best language for supporting the development of this port to the Raspberry Pi, but I will also use Mathematica for certain features, as it is also one of the default languages for the Raspberry Pi.
+print("I decided to make Python the main project language file for this project (Linux Defender for Raspberry Pi) as Python is the best language for supporting the development of this port to the Raspberry Pi, but I will also use Mathematica for certain features, as it is also one of the default languages for the Raspberry Pi.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed.")
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Monday, February 21st at 2:16 pm)
+# Line count (including blank lines and compiler line): 17
+"""
+# End of script
