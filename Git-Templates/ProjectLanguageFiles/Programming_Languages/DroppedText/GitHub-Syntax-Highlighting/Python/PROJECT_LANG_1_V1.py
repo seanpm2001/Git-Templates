4506bd815ad82b4aa872bd4d6e6ff7de1058bbb1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-20 16:36:34 -0800

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Programming_Languages/DroppedText/GitHub-Syntax-Highlighting/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+# I have decided to have the main language for this project (DroppedText-GitHub-Syntax-Highlighting) to be Python, as Python is the implementation language for this language, and needs to be represented here.
+''' '''
+class projectLanguageFileOne()
+print("Project Language File 1\n")
+print("For: DroppedText-GitHub-Syntax-Highlighting")
+print("I have decided to have the main language for this project (DroppedText-GitHub-Syntax-Highlighting) to be Python, as Python is the implementation language for this language, and needs to be represented here.")
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+""" """
+# File info
+# File version: 1 (Sunday, 2022 February 20th at 4:35 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 17
+# End of script
