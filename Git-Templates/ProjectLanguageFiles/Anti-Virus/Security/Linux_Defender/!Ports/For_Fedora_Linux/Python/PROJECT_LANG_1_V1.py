ca265e9030dab5ff86c0d28401f8909e55742a20
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 14:41:25 -0800

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Fedora_Linux/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: Linux Defender for Fedora Linux
+# About
+# I decided to make Python the first project language file for this project (Linux Defender for Fedora Linux) as Python is the best language for supporting the application level development of this project, but I will also use C++, and Shell for certain features. As a fallback, CSS will also be used.
+class projectLanguageFileTwo():
+	print("Project language file 2")
+	print("For: Linux Defender for Fedora Linux")
+	print("About:")
+	print("I decided to make Python the first project language file for this project (Linux Defender for Fedora Linux) as Python is the best language for supporting the application level development of this project, but I will also use C++, and Shell for certain features. As a fallback, CSS will also be used.\n")
+	break
+return projectLanguageFileTwo()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Tuesday, February 22nd at 2:40 pm)
+# Line count (including blank lines and compiler line): 24
+"""
+# End of script
