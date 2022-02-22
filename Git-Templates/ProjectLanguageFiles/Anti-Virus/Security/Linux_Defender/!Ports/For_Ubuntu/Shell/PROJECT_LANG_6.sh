9796561e154eed70e61f233deb572bd1de925635
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 18:31:11 -0800

    PROJECT_LANG_6.sh [V1]
    The current version [version 1] of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Ubuntu/Shell/PROJECT_LANG_6.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the sixth project language file for this project (Linux_Defender_For_Ubuntu) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.
+function projectLanguageFileSix() {
+  echo ("Project language file #6");
+  wait 1;
+  echo ("For: Seanpm2001/Linux_Defender_For_Ubuntu");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the sixth project language file for this project (Linux_Defender_For_Ubuntu) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileSix();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Monday, 2022 February 21st at 6:30 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
