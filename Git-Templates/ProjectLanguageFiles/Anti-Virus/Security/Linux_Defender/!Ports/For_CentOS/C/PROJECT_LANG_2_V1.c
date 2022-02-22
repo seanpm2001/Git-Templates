5e55cf2eaf97fb444bff8180ff25551d4419d161
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 17:36:28 -0800

    PROJECT_LANG_2_V1.c
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_CentOS/C/PROJECT_LANG_2_V1.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileTwo {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 2");
+    printf("For: Linux Defender for CentOS");
+    printf("About: ");
+    break;
+}
+
+// I chose C as the second project language file for this project (Linux Defender for CentOS) as C is a universal language used to be used for system functions and core programming, alongside Shell. It will not be used for interfaces, that will be left up to Python, and SCSS respectively.
+
+return main();
+printf("I chose C as the second project language file for this project (Linux Defender for CentOS) as C is a universal language used to be used for system functions and core programming, alongside Shell. It will not be used for interfaces, that will be left up to Python, and SCSS respectively.");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Monday, February 21st at 5:35 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
