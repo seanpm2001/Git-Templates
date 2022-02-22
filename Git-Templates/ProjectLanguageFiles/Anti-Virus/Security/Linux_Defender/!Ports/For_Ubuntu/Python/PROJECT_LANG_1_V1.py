0f33ac7cf795fd2cbed8e4bbecfd0f2c2575ef92
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 17:56:22 -0800

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Ubuntu/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: Linux Defender for Ubuntu
+# About
+# I decided to make Python the first project language file for this project (Linux Defender for Ubuntu) as Python is the best language for supporting the application level development of this project, but I will also use C, Vala, Shell, and Genie for certain features. As a fallback, CSS and Shell will also be used
+class projectLanguageFileTwo():
+	print("I decided to make Python the second project language file for this project (Linux Defender for Debian) as Python is the best language for supporting the application level development of this project, but I will also use C for certain features. As a fallback, CSS and Shell will also be used")
+	noMore = input("Press [ENTER] key to quit")
+	print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+	break
+return projectLanguageFileTwo()
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Monday, February 21st at 5:55 pm)
+# Line count (including blank lines and compiler line): 21
+"""
+# End of script
