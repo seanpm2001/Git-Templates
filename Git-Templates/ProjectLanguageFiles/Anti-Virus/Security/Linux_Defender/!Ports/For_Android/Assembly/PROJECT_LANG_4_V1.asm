bae5dd6760fa0c8888d281b8512a4eb5e57f97ee
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 16:00:16 -0800

    PROJECT_LANG_4_V1.asm
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Android/Assembly/PROJECT_LANG_4_V1.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 4
+; For: Linux Defender for Android
+; About:
+; I have decided to make Assembly the fourth project language for this project (Linux Defender For Android) because Assembly is the best language for hardware interactions, alongside C, and might be possible to use with Android.
+
+; File info
+; File version: 1 (2022, Monday, February 21st at 3:58 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
