944aee3689f6224435dbe94196b645d77b6c0b2b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 17:52:20 -0800

    PROJECT_LANG_4_V1.sh
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_CentOS/Shell/PROJECT_LANG_4_V1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the fourth project language file for this project (Linux_Defender_For_CentOS) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.
+function projectLanguageFileFour() {
+  echo ("Project language file #4");
+  wait 1;
+  echo ("For: Seanpm2001/Linux_Defender_For_Debian");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the fourth project language file for this project (Linux_Defender_For_CentOS) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileFour();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Monday, 2022 February 21st at 5:51 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
