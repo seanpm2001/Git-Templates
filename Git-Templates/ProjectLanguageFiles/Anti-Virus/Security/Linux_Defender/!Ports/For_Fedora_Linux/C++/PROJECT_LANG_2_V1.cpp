1d7f9eee403b738100f3517be507b9a0daf4623e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 14:45:39 -0800

    PROJECT_LANG_2_V1.cpp
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Fedora_Linux/C++/PROJECT_LANG_2_V1.cpp
@@ -0,0 +1,26 @@
+// Start of script
+#include <iostream>
+// I chose C++ as the second project language file for this project (Linux Defender for Fedora Linux) as C++ is planned to be used for core programming alongside Python. It was chosen for cross-compatibility and language diversity reasons.\n
+// Start of source code
+class projectLanguageFileTwo {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 2\n";
+    // Subheader
+    std::cout << "For: Linux Defender for Fedora Linux\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I chose C++ as the second project language file for this project (Linux Defender for Fedora Linux) as C++ is planned to be used for core programming alongside Python. It was chosen for cross-compatibility and language diversity reasons.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+return main();
+break; // Not sure what this does, but I am using it as a stopping point.
+return 0;
+}} // End of source code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.cc)
+* File version: 1 (2022, Tuesday, February 22nd at 2:44 pm)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
