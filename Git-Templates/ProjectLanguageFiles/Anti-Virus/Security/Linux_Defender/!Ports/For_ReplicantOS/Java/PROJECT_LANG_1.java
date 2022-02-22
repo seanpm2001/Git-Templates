7c59dbe54c83a8a9ab76428301545884bf4de670
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 15:51:07 -0800

    PROJECT_LANG_1.java [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_ReplicantOS/Java/PROJECT_LANG_1.java
@@ -0,0 +1,29 @@
+// Start of script
+// Project language file 1
+// For: Linux Defender for ReplicantOS
+// About:
+// I decided to make Java the 1st project language file for this project (Linux_Defender_for_ReplicantOS) as this software port is for ReplicantOS, which is based heavily on Android, and the native language to Android is Java. Alternative functionality will also be available in SCSS, Assembly, C, and possibly another language if needed.
+// Import section
+import java.util.*
+// Main function
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: Linux_Defender_for_ReplicantOS");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (Linux_Defender_for_ReplicantOS) as this software port is for ReplicantOS, which is based heavily on Android, and the native language to Android is Java. Alternative functionality will also be available in Kotlin, SCSS, Assembly, C++, and possibly another language if needed.");
+		break;
+}
+// Main section
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break;
+} // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Tuesday, February 22nd at 3:50 pm)
+* File type: Java 8 Source file (*.java)
+* Line count (including blank lines and compiler line): 30
+*/
+// End of script
