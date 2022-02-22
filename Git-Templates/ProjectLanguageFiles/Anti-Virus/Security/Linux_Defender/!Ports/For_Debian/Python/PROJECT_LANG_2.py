1af6f9b51cd05f49e4d456c22ff2a894463103f4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 16:52:01 -0800

    PROJECT_LANG_2.py [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Debian/Python/PROJECT_LANG_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 2
+# For: Linux Defender for Debian
+# About
+# I decided to make Python the second project language file for this project (Linux Defender for Debian) as Python is the best language for supporting the application level development of this project, but I will also use C for certain features. As a fallback, CSS and Shell will also be used
+class projectLanguageFileTwo():
+	print("I decided to make Python the second project language file for this project (Linux Defender for Debian) as Python is the best language for supporting the application level development of this project, but I will also use C for certain features. As a fallback, CSS and Shell will also be used")
+	noMore = input("Press [ENTER] key to quit")
+	print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+	break
+return projectLanguageFileTwo()
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Monday, February 21st at 4:51 pm)
+# Line count (including blank lines and compiler line): 21
+"""
+# End of script
