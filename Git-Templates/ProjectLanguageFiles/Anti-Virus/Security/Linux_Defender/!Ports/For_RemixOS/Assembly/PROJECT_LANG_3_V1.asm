9017129e0c911e5c3dde62be82d814a8f439a27a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 15:41:54 -0800

    PROJECT_LANG_3_V1.asm
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_RemixOS/Assembly/PROJECT_LANG_3_V1.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 3
+; For: Linux Defender for RemixOS
+; About:
+; I have decided to make Assembly the third project language for this project (Linux Defender For RemixOS) because Assembly is the best language for hardware interactions, alongside C, and might be possible to use with Android.
+
+; File info
+; File version: 1 (2022, Tuesday, February 22nd 3:40 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
