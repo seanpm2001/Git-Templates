2df5248af7aead080765267d796a5c0eb107c6c5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 15:11:09 -0800

    PROJECT_LANG_2.c [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Trisquel/C/PROJECT_LANG_2.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileTwo {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 2\n");
+    printf("For: Linux Defender for Trisquel\n");
+    printf("About: \n");
+    break;
+}
+
+// I chose C as the second project language file for this project (Linux Defender for Trisquel) as C is a universal language used to be used for system functions and core programming, alongside Shell. It will not be used for interfaces, that will be left up to Python, and CSS respectively.
+
+return main();
+printf("I chose C as the second project language file for this project (Linux Defender for Trisquel) as C is a universal language used to be used for system functions and core programming, alongside Shell. It will not be used for interfaces, that will be left up to Python, and CSS respectively.\n");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Tuesday, February 22nd at 3:10 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
