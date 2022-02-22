42d396d38c4dafb927a9e1639df5b6f0830203ab
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 15:45:52 -0800

    PROJECT_LANG_4.c [V1]
    The current version [version 1] of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_RemixOS/C/PROJECT_LANG_4.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileFour {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 4\n");
+    printf("For: Linux Defender for RemixOS\n");
+    printf("About: \n");
+    break;
+}
+
+// I chose C as the fourth project language file for this project (Linux Defender for RemixOS) as C is a universal language used to be used for system functions and core programming. It will not be used for interfaces, that will be left up to Java, Assembly, and CSS respectively.
+
+return main();
+printf("I chose C as the fourth project language file for this project (Linux Defender for RemixOS) as C is a universal language used to be used for system functions and core programming. It will not be used for interfaces, that will be left up to Java, Assembly, and CSS respectively.\n");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Tuesday, February 22nd at 3:45 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
