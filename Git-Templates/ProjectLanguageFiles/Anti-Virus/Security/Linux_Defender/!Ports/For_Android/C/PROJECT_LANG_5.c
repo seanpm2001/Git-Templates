0899088c643ebcae312fdd2117b732aece966b23
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 16:05:00 -0800

    PROJECT_LANG_5.c [V1]
    The current version [version 1] of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Android/C/PROJECT_LANG_5.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileFive {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 5");
+    printf("For: Linux Defender for Android");
+    printf("About: ");
+    break;
+}
+
+// I chose C as the fifth project language file for this project (Linux Defender for Android) as C is a universal language used to be used for system functions and core programming. It will not be used for interfaces, that will be left up to Java, Kotlin, and CSS respectively.
+
+return main();
+printf("I chose C as the fifth project language file for this project (Linux Defender for Android) as C is a universal language used to be used for system functions and core programming. It will not be used for interfaces, that will be left up to Java, Kotlin, and CSS respectively.");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Monday, February 21st at 4:04 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
