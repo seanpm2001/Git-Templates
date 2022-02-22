9634ce89b10d1c85dc503c92b969391bf8dabb22
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 15:18:44 -0800

    PROJECT_LANG_4.sh
    The current version [version 1] of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Trisquel/Shell/PROJECT_LANG_4.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the fourth project language file for this project (Linux_Defender_For_Trisquel) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.
+function projectLanguageFileFour() {
+  echo ("Project language file #4");
+  wait 1;
+  echo ("For: Seanpm2001/Linux_Defender_For_Trisquel");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the fourth project language file for this project (Linux_Defender_For_Trisquel) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileFour();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Tuesday, 2022 February 22nd at 3:18 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
