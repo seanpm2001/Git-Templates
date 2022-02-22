16602adef96bd7748a32b3e80b6c3c29db736724
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 17:30:51 -0800

    PROJECT_LANG_1.py [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_CentOS/Python/PROJECT_LANG_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: Linux Defender for CentOS
+# About
+# I decided to make Python the first project language file for this project (Linux Defender for CentOS) as Python is the best language for supporting the application level development of this project, but I will also use C for certain features. As a fallback, SCSS and Shell will also be used.
+class projectLanguageFileTwo():
+	print("I decided to make Python the first project language file for this project (Linux Defender for CentOS) as Python is the best language for supporting the application level development of this project, but I will also use C for certain features. As a fallback, SCSS and Shell will also be used.")
+	noMore = input("Press [ENTER] key to quit")
+	print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+	break
+return projectLanguageFileTwo()
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Monday, February 21st at 5:30 pm)
+# Line count (including blank lines and compiler line): 21
+"""
+# End of script
