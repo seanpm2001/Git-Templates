e04ad87557e46253fb84a3063abbcb1fbd622974
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-21 16:42:12 -0800

    PROJECT_LANG_1.c [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_Debian/C/PROJECT_LANG_1.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1");
+    printf("For: Linux Defender for Debian");
+    printf("About: ");
+    break;
+}
+
+// I chose C as the first project language file for this project (Linux Defender for Debian) as C is a universal language used to be used for system functions and core programming, alongside Shell. It will not be used for interfaces, that will be left up to Python, and CSS respectively.
+
+return main();
+printf("I chose C as the first project language file for this project (Linux Defender for Debian) as C is a universal language used to be used for system functions and core programming, alongside Shell. It will not be used for interfaces, that will be left up to Python, and CSS respectively.");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Monday, February 21st at 4:41 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
