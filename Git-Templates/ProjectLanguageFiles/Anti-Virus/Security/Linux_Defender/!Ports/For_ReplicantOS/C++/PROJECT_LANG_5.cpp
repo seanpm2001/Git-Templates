b7844b9306b5ae51c2f312ca9610e53845b96165
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 16:17:57 -0800

    PROJECT_LANG_5.cpp [V1]
    The current version [version 1] of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_ReplicantOS/C++/PROJECT_LANG_5.cpp
@@ -0,0 +1,26 @@
+// Start of script
+#include <iostream>
+// I chose C++ as the fifth project language file for this project (Linux Defender for ReplicantOS) as C++ is planned to be used for core programming alongside Java, Kotlin, and Assembly. It was chosen for cross-compatibility and language diversity reasons.\n
+// Start of source code
+class projectLanguageFileFive {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 5\n";
+    // Subheader
+    std::cout << "For: Linux Defender for ReplicantOS\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I chose C++ as the fifth project language file for this project (Linux Defender for ReplicantOS) as C++ is planned to be used for core programming alongside Java, Kotlin, and Assembly. It was chosen for cross-compatibility and language diversity reasons.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+return main();
+break; // Not sure what this does, but I am using it as a stopping point.
+return 0;
+}} // End of source code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.cc)
+* File version: 1 (2022, Tuesday, February 22nd at 4:17 pm)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
