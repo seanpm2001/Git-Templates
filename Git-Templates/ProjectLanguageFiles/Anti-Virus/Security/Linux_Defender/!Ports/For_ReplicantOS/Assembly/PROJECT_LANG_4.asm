b504cd0eaeb0674b999427d747c20cf13c73c2ab
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-22 16:11:19 -0800

    PROJECT_LANG_4.asm [V1]
    The current version [version 1] of the 4th project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Anti-Virus/Security/Linux_Defender/!Ports/For_ReplicantOS/Assembly/PROJECT_LANG_4.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 4
+; For: Linux Defender for ReplicantOS
+; About:
+; I have decided to make Assembly the fourth project language for this project (Linux Defender For ReplicantOS) because Assembly is the best language for hardware interactions, alongside C, and might be possible to use with Android/ReplicantOS.
+
+; File info
+; File version: 1 (2022, Tuesday, February 22nd 4:10 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
