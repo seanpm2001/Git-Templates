1e432836f157c6f628c59c6c39ded3983dc71d3c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-26 17:01:48 -0800

    PROJECT_LANG_1_V1.nim
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/Nim/Nim/PROJECT_LANG_1_V1.nim
@@ -0,0 +1,17 @@
+# Start of script
+# Project language file 1
+# For: SNU / 2D / Programming Tools / IDE / Nim
+# About:
+# I decided to make Nim the main project language file for this project (SNU / 2D / Programming Tools / IDE / Nim) as this is a Nim IDE, and it needs its main language to be represented here.
+
+class projectLanguageFileOne()
+echo ("Project language file 1")
+echo ("For: SNU / 2D / Programming Tools / IDE / Nim")
+echo ("About:")
+echo ("I decided to make Nim the main project language file for this project (SNU / 2D / Programming Tools / IDE / Nim) as this is a Nim IDE, and it needs its main language to be represented here.")
+break
+
+# File version: 1 (Saturday, 2022 February 26th at 5:00 pm)
+# File type: Nim source file (*.nim)
+# Line count (including blank lines and compiler line): 18
+# End of script
