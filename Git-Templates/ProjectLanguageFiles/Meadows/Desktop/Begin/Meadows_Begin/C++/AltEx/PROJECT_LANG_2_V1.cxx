04c1ea955a47f1cf8106b75c1af5c10a1686a013
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-01 18:39:40 -0800

    PROJECT_LANG_2_V1.cxx
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Meadows/Desktop/Begin/Meadows_Begin/C++/AltEx/PROJECT_LANG_2_V1.cxx
@@ -0,0 +1,27 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// I chose C++ as the second project language file for this project (Meadows/Begin) as C++ is used for some components of this project, not everything will be written in Python. Some parts will also be written in C.
+// Start of code
+class projectLanguageFileTwo {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 2\n";
+    // Subheader
+    std::cout << "For: Meadows/Begin\n";
+    // Neck
+    std::cout << "Reasoning:\n";
+    // Body
+    std::cout << "I chose C++ as the second project language file for this project (Meadows/Begin) as C++ is used for some components of this project, not everything will be written in Python. Some parts will also be written in C.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+return main();
+break;
+return 0;
+}} // End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (2022, Tuesday, March 1st at 6:37 pm)
+* Line count (including blank lines and compiler line): 28
+*/
+// End of script
