8df5b968620f58fc8c40379b00fa2d79a9598302
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-01 18:55:35 -0800

    PROJECT_LANG_3_V1.c
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Meadows/Desktop/Begin/Meadows_Begin/C/PROJECT_LANG_3_V1.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// Start of script
+// I decided to make C the third project language for this project (Meadows/Begin) the C programming language, as it is being used alongside Python and C++ for core functionality here.
+union projectLanguageFileThree() {
+int main(void) {
+    printf("Project language file 3: C\n");
+    printf("For: Meadows/Begin\n");
+    printf("About:");
+    printf("I decided to make C the third project language for this project (Meadows/Begin) the C programming language, as it is being used alongside Python and C++ for core functionality here.\n");
+    break;
+  }
+}
+return main();
+break;
+// File info
+// File version: 1 (Tuesday, 2022 March 1st at 6:54 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 20
+// End of script
