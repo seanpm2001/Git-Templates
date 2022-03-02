61766a05ca96909e3c983479627d34abef7ab810
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-01 16:11:29 -0800

    PROJECT_LANG_2.py [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Office/uOffice/!LIBraries/uOffice_LIBrary_DVCS/Python/PROJECT_LANG_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 2
+# For: uOffice LIBrary DVCS
+# About
+# I decided to make Python the second project language file for this project (uOffice LIBrary DVCS) as Python is the default language for writing uOffice components, as I chose it as the default language. Diff came first this time, as this is a version control software library.
+class projectLanguageFileTwo():
+	print("Project language file 2")
+	print("For: uOffice LIBrary DVCS")
+	print("About:")
+	print("I decided to make Python the second project language file for this project (uOffice LIBrary DVCS) as Python is the default language for writing uOffice components, as I chose it as the default language. Diff came first this time, as this is a version control software library.")
+	noMore = input("Press [ENTER] key to quit")
+	print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+	break
+return projectLanguageFileTwo()
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Tuesday, March 1st at 4:10 pm)
+# Line count (including blank lines and compiler line): 24
+"""
+# End of script
