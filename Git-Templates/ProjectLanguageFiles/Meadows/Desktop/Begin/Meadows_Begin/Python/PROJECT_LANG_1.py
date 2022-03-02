404630df396cda3ae34c7eb77574177aee8ff470
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-01 18:29:02 -0800

    PROJECT_LANG_1.py
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Meadows/Desktop/Begin/Meadows_Begin/Python/PROJECT_LANG_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: Meadows Begin
+# About
+# I decided to make Python the first project language file for this project (Meadows/Begin) as Python is the default language for writing Madows components, as I chose it as the default language. It is best suited here, but will be accompanied with C++, C, Less, CSS, SCSS, SASS, QML, and others.
+class projectLanguageFileOne():
+	print("Project language file 1")
+	print("For: Meaodws/Begin")
+	print("About:")
+	print("I decided to make Python the first project language file for this project (Meadows/Begin) as Python is the default language for writing Madows components, as I chose it as the default language. It is best suited here, but will be accompanied with C++, C, Less, CSS, SCSS, SASS, QML, and others.")
+	noMore = input("Press [ENTER] key to quit")
+	print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+	break
+return projectLanguageFileOne()
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Tuesday, March 1st at 6:28 pm)
+# Line count (including blank lines and compiler line): 24
+"""
+# End of script
