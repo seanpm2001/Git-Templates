4f5eeec682fc50fef2439345408be42e9f1e4ecc
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-02 16:43:25 -0800

    PROJECT_LANG_3.ts [V1]
    The current version [version 1] of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/SlideXMagic/TypeScript/PROJECT_LANG_3.ts
@@ -0,0 +1,16 @@
+// Start of script /* I decided to make TypeScript the third project language file for this project (SlideXMagic) as TypeScript is used to compile JavaScript, and is one of the planned languages for use on this project. It is getting its own project language file here.  
+*/ // WARNING: I am not very experienced with TypeScript. This program may not execute properly. /
+namespace ts {
+	public void projectLanguageFileThree() {
+	let message: string = 'Project language file 3\nFor: SlideXMagic\nAbout:\nI decided to make TypeScript the third project language file for this project (SlideXMagic) as TypeScript is used to compile JavaScript, and is one of the planned languages for use on this project. It is getting its own project language file here.  \n';	
+	console.log(message);
+	break;
+	}
+	return projectLanguageFileThree();
+	break;
+}
+/* File info
+* File type: TypeScript source file (*.ts)
+* File version: 1 (2022, Wednesday, March 2nd at 4:42 pm) 
+* Line count (including blank lines and compiler line): 17
+*/ // End of script
