7f37596bd3c0306777cf73b8a40f857f6333e403
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-02 16:32:38 -0800

    PROJECT_LANG_1.pl [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/SlideXMagic/Perl/PROJECT_LANG_1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+# Start of script
+# Project laguage file 1
+# For: SlideXMagic
+# About:
+# I chose Perl as the first project language file for this project (SlideXMagic) as this project is going to make use of the Perl language for software diversity reasons, and also because Perl is a good language to use on some things. It will be accompanied by many other languages, such as ActionScript, TypeScript, JavaScript, WebAssembly, and XHTML, just to name a few.
+print "Project laguage file 1";
+print "For: SlideXMagic";
+print "About:";
+print "I chose Perl as the first project language file for this project (SlideXMagic) as this project is going to make use of the Perl language for software diversity reasons, and also because Perl is a good language to use on some things. It will be accompanied by many other languages, such as ActionScript, TypeScript, JavaScript, WebAssembly, and XHTML, just to name a few.";
+# File info
+# File type: Perl source file (*.pl)
+# File version: 1 (Wednesday, 2022 March 2nd at 4:31 pm)
+# Line count (including blank lines and compiler line): 16
+# End of script
