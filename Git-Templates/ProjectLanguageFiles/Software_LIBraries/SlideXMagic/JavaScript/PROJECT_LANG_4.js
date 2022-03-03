37f91d210660e75def37095d2401138376b81bc1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-02 16:51:14 -0800

    PROJECT_LANG_4.js [V1]
    The current version [version 1] of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/SlideXMagic/JavaScript/PROJECT_LANG_4.js
@@ -0,0 +1,21 @@
+// Start of script // I chose JavaScript as the fourth project language for this project (SlideXMagic) as JavaScript is one of the key languages to use for web page scripting, although alternative languages will also be used in its place. It is used for connections to web pages and their existing frameworks. For people who block JavaScript, other languages will be supported as a backup.
+class projectLanguageFileFour() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 4");
+    alert("Project language file 4");
+    console.log ("For: SlideXMagic");
+    alert("For: SlideXMagic");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the fourth project language for this project (SlideXMagic) as JavaScript is one of the key languages to use for web page scripting, although alternative languages will also be used in its place. It is used for connections to web pages and their existing frameworks. For people who block JavaScript, other languages will be supported as a backup.");
+    alert("I chose JavaScript as the fourth project language for this project (SlideXMagic) as JavaScript is one of the key languages to use for web page scripting, although alternative languages will also be used in its place. It is used for connections to web pages and their existing frameworks. For people who block JavaScript, other languages will be supported as a backup.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+} /* File info
+* File version: 1 (Wednesday, 2022 March 2nd at 4:50 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 22
+*/ // End of script
