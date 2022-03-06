81e3b897dd2cb1fe786d0abb0922459005fe387e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-05 18:59:49 -0800

    PROJECT_LANG_2.js [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/BrowserNose/JavaScript/PROJECT_LANG_2.js
@@ -0,0 +1,21 @@
+// Start of script
+// I chose JavaScript as the second project language for this project (SNU/BrowserNose) as JavaScript is one of the key languages to use for web page scripting, and is currently the majority language for this project. It is the original scripting language used for this project. It will be supplemented with other languages in the future.
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 2");
+    alert("Project language file 2");
+    console.log ("For: SNU/BrowserNose");
+    alert("For: SNU/BrowserNose");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the second project language for this project (SNU/BrowserNose) as JavaScript is one of the key languages to use for web page scripting, and is currently the majority language for this project. It is the original scripting language used for this project. It will be supplemented with other languages in the future.");
+    alert("I chose JavaScript as the second project language for this project (SNU/BrowserNose) as JavaScript is one of the key languages to use for web page scripting, and is currently the majority language for this project. It is the original scripting language used for this project. It will be supplemented with other languages in the future.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+} /* File info
+* File version: 1 (Saturday, 2022 March 5th at 6:53 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 22
+*/ // End of script
