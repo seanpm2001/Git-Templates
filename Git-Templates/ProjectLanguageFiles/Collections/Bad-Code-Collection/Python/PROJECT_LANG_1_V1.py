67124d5185dbda1fbe694d8b0bb4d278295d0c01
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-14 15:21:36 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Collections/Bad-Code-Collection/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Start of script
+
+# Project language file 1
+# For: Seanpm2001/Bad-Code-Collection
+# About
+# I have decided to have the main language for this project (Seanpm2001/Bad-Code-Collection) as Python is the first programming language I learnt, and is a good basis for that reason, as that is where my bad code started.
+
+''' '''
+
+class projectLanguageFileOne()
+
+def plang1():
+  print("Project Language File 1\n")
+  print("For: Seanpm2001/Bad-Code-Collection")
+  print("About:")
+  print("I have decided to have the main language for this project (Seanpm2001/Bad-Code-Collection) as Python is the first programming language I learnt, and is a good basis for that reason, as that is where my bad code started.")
+  break
+
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+""" """
+
+# File info
+# File version: 1 (Monday, 2022 March 14th at 3:20 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 34
+
+# End of script
