30de600231bf0d7624b54ade3309e11aba8fa3f8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-22 18:31:02 -0700

    PROJECT_LANG_7.sh [V1]
    The current version [version 1] of the 7th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Audio_Projects/SeansAudioDB/Shell/PROJECT_LANG_7.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the seventh project language file for this project (seanpm2001/SeansAudioDB) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project. I am making it a project language, as I finally got around to documenting every language as a project language for this project. This language has common use, but falls below the top 6 languages.
+function projectLanguageFileSeven() {
+  echo ("Project language file #7");
+  wait 1;
+  echo ("For: Seanpm2001/SeansAUdioDB");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the seventh project language file for this project (seanpm2001/SeansAudioDB) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project. I am making it a project language, as I finally got around to documenting every language as a project language for this project. This language has common use, but falls below the top 6 languages.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileSeven();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Tuesday, 2022, March 22nd at 6:30 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
