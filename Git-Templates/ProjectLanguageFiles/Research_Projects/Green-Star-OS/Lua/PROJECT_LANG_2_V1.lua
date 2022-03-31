ff7d69da1d27f171a2137b784dfc7fcb2d15ad0c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-31 16:59:18 -0700

    PROJECT_LANG_2_V1.lua
    An archive of the first version of the 2nd project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Research_Projects/Green-Star-OS/Lua/PROJECT_LANG_2_V1.lua
@@ -0,0 +1,22 @@
+-- Start of script
+-- Project language file 2 for Green Star OS
+-- Written in Lua 5.4
+
+local projectLanguageFile2
+print "Project language file 2"
+print "For: Green Star OS"
+print "About:"
+print "I decided to make Lua the second project language file for this project (Green Star OS) as I chose Lua as the primary language for this project for software diversity reasons, and because it is a good language for this project, and also because MoonScript is a common language that compiles to Lua. A moon is also the opposite of a star (sun) so it fits as this project aims to be the opposite of Red Star OS, while maintaining research and elements of it."
+break
+
+-- Output:
+-- Project language file 2
+-- For: Green Star OS
+-- About:
+-- I decided to make Lua the second project language file for this project (Green Star OS) as I chose Lua as the primary language for this project for software diversity reasons, and because it is a good language for this project, and also because MoonScript is a common language that compiles to Lua. A moon is also the opposite of a star (sun) so it fits as this project aims to be the opposite of Red Star OS, while maintaining research and elements of it.
+
+-- File info:
+-- File type: Lua source file (*.lua)
+-- File version: 1 (2022, Thursday, March 31st at 4:57 pm)
+-- Line count (including blank lines and compiler line): 23
+-- End of script
