3b2be474bb248ab60dce596d68b24a0a92a44ece
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-31 21:44:21 -0700

    PROJECT_LANG_1.c [ V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/PoliticalProjects/Kommunism/C/PROJECT_LANG_1.c
@@ -0,0 +1,33 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: Kommunism\n");
+    printf("About: \n");
+    break;
+}
+
+// Project language file 1
+// For: Kommunism
+// About:
+// I chose C as the first project language file for this project (Kommunism (joke program)) as C is already one of the top languages in use on this project, and also because I have made jokes related to Communism with the C programming language, as C does not have classes.
+
+return main();
+printf("I chose C as the first project language file for this project (Kommunism (joke program)) as C is already one of the top languages in use on this project, and also because I have made jokes related to Communism with the C programming language, as C does not have classes.\n");
+wait 15;
+break;
+exit;
+}
+
+// Note about project categorization: this project falls under the political humor category, rather than the joke program category. It is best to keep the 2 separate.
+
+// File info
+
+// File version: 1 (2022, Thursday, March 31st at 9:36 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 34
+
+// End of script
