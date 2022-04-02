895ce28613ab740721a45d4a3fce9e6eabfd4b56
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-02 16:02:53 -0700

    PROJECT_LANG_3.sh [V1]
    The current version [version 1] of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Research_Projects/Green-Star-OS/Shell/PROJECT_LANG_3.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Start of script
+# Project language file 3
+# For: Green Star OS
+# About:
+# I have chosen Shell as the third project language file for this project (seanpm2001/Green-Star-OS) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it.
+function projectLanguageFileThree() {
+  echo ("Project language file #3");
+  wait 1;
+  echo ("For: Green Star OS");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the third project language file for this project (seanpm2001/Green-Star-OS) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileThree();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Saturday, 2022, April 2nd at 4:02 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 31
+
+# End of script
