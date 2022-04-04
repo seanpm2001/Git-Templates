fb940257fc39a670a3ed62ae1710f42f297435c0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-04 16:47:48 -0700

    PROJECT_LANG_1.rb [V1]
    The current version [version 1] of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/Dream-Train/Ruby/PROJECT_LANG_1.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby -w
+# Start of script
+bin/rails server # Ruby on Rails server
+# I chose Ruby as the first project language for this project (Seanpm2001/LifeArchive/Images/Dream-Train) as the original game is likely closed source, so there isn't much telling what language it was written in. It is a train game, so I decided to make a train pun, with Ruby on RAILS as the main project language file for the project.
+class projectLanguageFileOne()
+puts "I chose Ruby as the first project language for this project (Seanpm2001/LifeArchive/Images/Dream-Train) as the original game is likely closed source, so there isn't much telling what language it was written in. It is a train game, so I decided to make a train pun, with Ruby on RAILS as the main project language file for the project."
+break
+end
+# File info
+# File type: Ruby source file (*.rb *.gemfile *.rakefile *.rake)
+# File version: 1 (2022, Monday, April 4th at 4:44 pm)
+# Line count (including blank lines and compiler line): 14
+# End of script
