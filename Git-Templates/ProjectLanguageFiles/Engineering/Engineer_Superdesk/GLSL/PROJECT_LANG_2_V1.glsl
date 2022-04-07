288997de6e1e8458772181e6761c50b84e660833
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-06 18:16:23 -0700

    PROJECT_LANG_2_V1.glsl
    An archive of the first version of the 2nd project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Engineering/Engineer_Superdesk/GLSL/PROJECT_LANG_2_V1.glsl
@@ -0,0 +1,17 @@
+// Start of script
+// Sample file usage: https://www.lighthouse3d.com/tutorials/glsl-tutorial/hello-world/ (warning: insecure connection)
+#define Project_Language_File_Two
+// Project language file 2
+// For: Engineer Superdesk
+// I decided to make GLSL (OpenGL Shading Language) the second project language file for this project (Engineer Superdesk) as GLSL is the main shading language used on this project, and it needs to be represented with a project language file.
+void main()
+{
+    outputF = vec4(1.0, 0.0, 0.0, 1.0);
+}
+#endif
+/* File info
+* File type: GLSL source file (*.glsl)
+* File version: 1 (2022, Wednesday, April 6th at 6:15 pm)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
