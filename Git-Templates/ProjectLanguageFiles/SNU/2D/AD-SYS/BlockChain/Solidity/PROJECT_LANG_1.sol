2ab79c5e51a946d2608bf8d9e7ea84cd842e7164
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-07 15:18:26 -0700

    PROJECT_LANG_1.sol [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/AD-SYS/BlockChain/Solidity/PROJECT_LANG_1.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+// Start of scrript
+contract projectLanguageFile1 {   
+	constructor() public{
+	}
+	// Project language file 1
+	// For: SNU/2D/Ad-System/BlockChain/
+	// About:
+	// I chose Solidity as the 1st project language for this project (SNU / 2D / Ad System / BlockChain ) as Solidity is the main blockchain language used for this project, and since it is the first one to be used, it is getting its own project language file, starting here.
+	function print () public pure returns (string) {       
+		return "Project language file 1\nFor: SNU/2D/Ad-System/BlockChain/\nAbout:\nI chose Solidity as the 1st project language for this project (SNU / 2D / Ad System / BlockChain ) as Solidity is the main blockchain language used for this project, and since it is the first one to be used, it is getting its own project language file, starting here.";             
+	} 
+}
+// File info
+// File type: Solidity source file (*.sol)
+// File version: 1 (2022, Thursday, April 7th at 3:17 pm PST)
+// Line count (including blank lines and compiler line): 21
+
+// End of script
