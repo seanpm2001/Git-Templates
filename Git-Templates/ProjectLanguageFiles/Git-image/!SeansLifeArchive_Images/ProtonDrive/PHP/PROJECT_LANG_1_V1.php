068f81480813bab747307a0495a83b98f7e62b2a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-09 15:27:12 -0700

    PROJECT_LANG_1_V1.php
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/ProtonDrive/PHP/PROJECT_LANG_1_V1.php
@@ -0,0 +1,23 @@
+<?php
+// Start of script
+// Project language file 1
+// For: SeansLifeArchive/Images/ProtonDrive
+// About:
+// I decided to make PHP the main project language file for this project (Seanpm2001/SeansLifeArchive/Images/ProtonDrive) as PHP has the protonmail color (purple) in it, and is most likely used in some way with ProtonMail. I originally was going to use Haskell, but decided not to, as it doesn't fit as well.
+$PROJECT_LANGUAGE_ONE; {
+  echo ("Project language 1");
+  echo ("For: Seanpm2001/SeansLifeArchive/Images/ProtonDrive");
+  echo ("About:");
+  echo ("I decided to make PHP the main project language file for this project (Seanpm2001/SeansLifeArchive/Images/ProtonDrive) as PHP has the protonmail color (purple) in it, and is most likely used in some way with ProtonMail. I originally was going to use Haskell, but decided not to, as it doesn't fit as well.");
+  break;
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return $PROJECT_LANGUAGE_ONE;
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (2022, Saturday, April 9th at 3:25 pm PST)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
+?>
