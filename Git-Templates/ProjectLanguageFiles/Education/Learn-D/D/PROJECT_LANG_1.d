43513f180639325c612bd7a7f0fe7182789e18f9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-11 19:22:45 -0700

    PROJECT_LANG_1.d [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-D/D/PROJECT_LANG_1.d
@@ -0,0 +1,24 @@
+#!/usr/bin/rdmd
+// Start of script
+import std.stdio : writeln;
+import std.range : iota;
+import std.parallelism : parallel;
+/* DIVIDER */
+void main()
+{
+  writeln("Project language file 1");
+  writeln("For: seanpm2001/Learn-D");
+  writeln("About:");
+  writeln("I chose D as the first project language file for this project (Seanpm2001/Learn-D) as this project is about learning the D programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.");
+  break;
+}
+return main();
+return 0;
+break;
+// I chose D as the first project language file for this project (Seanpm2001/Learn-D) as this project is about learning the D programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here. this purpose.
+/* DIVIDER */
+// File info
+// File version: 1 (2022, Monday, April 11th at 7:22 pm PST)
+// File type: D programming language source file (*.d)
+// Line count (including blank lines and compiler line): 25
+// End of script
