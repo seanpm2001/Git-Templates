a22c0dcebe0afb55d8ab34dbca13e24459e4ab85
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-13 16:46:29 -0700

    PROJECT_LANG_1_V1.hs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-Haskell/Haskell/PROJECT_LANG_1_V1.hs
@@ -0,0 +1,15 @@
+-- Start of script
+module Main (main) where          -- not needed in interpreter, is the default in a module file
+
+main :: IO ()                     -- the compiler can infer this type definition
+main = putStrLn "Project language file 1\nFor: seanpm2001/Learn-Haskell\nAbout:\nI chose Haskell as the first project language file for this project (Seanpm2001/Learn-Haskell) as this project is about learning the Haskell programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here."
+-- Output
+-- Project language file 1
+-- For: seanpm2001/Learn-Haskell
+-- About:
+-- I chose Haskell as the first project language file for this project (Seanpm2001/Learn-Haskell) as this project is about learning the Haskell programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+
+-- File version: 1 (2022M Wednesday, April 13th at 4:45 pm PST)
+-- File type: Haskell script file (*.hs)
+-- Line count (including blank lines and compiler line): 16
+-- End of script
