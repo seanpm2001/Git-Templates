5ba27d1018436ecc960341162fcf58aedd5f05b2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-13 14:30:23 -0700

    PROJECT_LANG_1.rs [V1]
    The current version [version 1] of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Web_Browser_Tools/3D-WebPage-Inspector/Rust/PROJECT_LANG_1.rs
@@ -0,0 +1,21 @@
+#!/usr/bin/env run-cargo-script
+// Start of script
+fn main() {
+  println!("Project language file 1 (Rust)");
+  println!("For: 3D WebPage Inspector");
+  println!("About:");
+  println!("I decided to make Rust the first project language file for this project (Seanpm2001/3D-WebPage-Inspector) as Rust is the main language that Firefox is written in, and I wanted to use Rust for this reason, also because of software diversity. Other languages will be used as well, but I am unsure on which I will use at the moment.");
+  break;
+}
+// Output:
+// Project language file 1 (Rust)
+// For: 3D WebPage Inspector
+// About:
+// I decided to make Rust the first project language file for this project (Seanpm2001/3D-WebPage-Inspector) as Rust is the main language that Firefox is written in, and I wanted to use Rust for this reason, also because of software diversity. Other languages will be used as well, but I am unsure on which I will use at the moment.
+return main();
+break;
+// File info
+// File version: 1 (2022, Wednesday, April 13th at 2:27 pm PST)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 22
+// End of script
