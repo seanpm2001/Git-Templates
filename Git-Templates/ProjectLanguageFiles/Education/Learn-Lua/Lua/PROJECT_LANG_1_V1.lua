101969bd73e5931aa44bd925d74374d3db26c408
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-12 17:11:25 -0700

    PROJECT_LANG_1_V1.lua
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-Lua/Lua/PROJECT_LANG_1_V1.lua
@@ -0,0 +1,22 @@
+-- Start of script
+-- Project language file 1 for Learn-Lua
+-- Written in Lua 5.4
+
+local projectLanguageFile1
+print "Project language file 1"
+print "For: seanpm2001/Learn-Lua"
+print "About:"
+print "I chose Lua as the first project language file for this project (Seanpm2001/Learn-Lua) as this project is about learning the Lua programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here."
+break
+
+-- Output:
+-- Project language file 1
+-- For: seanpm2001/Learn-Lua
+-- About:
+-- I chose Lua as the first project language file for this project (Seanpm2001/Learn-Lua) as this project is about learning the Lua programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+
+-- File info:
+-- File type: Lua source file (*.lua)
+-- File version: 1 (2022, Tuesday, April 12th at 5:09 pm PST)
+-- Line count (including blank lines and compiler line): 23
+-- End of script
