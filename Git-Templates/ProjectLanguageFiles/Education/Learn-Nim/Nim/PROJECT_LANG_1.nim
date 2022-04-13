48eb8f7a23c2a1951a7639633d38f049742f0972
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-13 16:10:58 -0700

    PROJECT_LANG_1.nim [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/Learn-Nim/Nim/PROJECT_LANG_1.nim
@@ -0,0 +1,17 @@
+# Start of script
+# Project language file 1
+# For: Seanpm2001/Learn-Nim
+# About:
+# I chose Nim as the first project language file for this project (Seanpm2001/Learn-Nim) as this project is about learning the Nim programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+
+class projectLanguageFileOne()
+echo ("Project language file 1")
+echo ("For: Seanpm2001/Learn-Nim")
+echo ("About:")
+echo ("I chose Nim as the first project language file for this project (Seanpm2001/Learn-Nim) as this project is about learning the Nim programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.")
+break
+
+# File version: 1 (2022, Wednesday, April 13th at 4:10 pm PST)
+# File type: Nim source file (*.nim)
+# Line count (including blank lines and compiler line): 18
+# End of script
