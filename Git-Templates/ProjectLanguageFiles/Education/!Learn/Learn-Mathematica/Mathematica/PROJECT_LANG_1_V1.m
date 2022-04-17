204727ee04058f56e1638bc210f6ab2d1d85b4d3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-17 16:21:42 -0700

    PROJECT_LANG_1_V1.m
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Mathematica/Mathematica/PROJECT_LANG_1_V1.m
@@ -0,0 +1,40 @@
+(* ::Package:: *)
+(* Start of script *)
+(* Paclet Info File *)
+
+(* created Sept. 14 2018 *)
+
+(* Modified Jul. 22 2021 from https://github.com/WolframResearch/WolframLanguageForJupyter/blob/master/WolframLanguageForJupyter/PacletInfo.m *)
+
+(* Modified again on 2022, Sunday, April 17th at 4:20 pm PST *)
+
+Paclet[
+    Name -> "WolframLanguageForJupyter",
+    Version -> "0.9.2",
+    MathematicaVersion -> "11.2+",
+    Extensions -> {
+        {"Kernel", Context -> {"WolframLanguageForJupyter`"}},
+        {"Documentation", Language -> "English"}
+    }
+]
+
+(* Print it out *)
+Print[ OutputForm["Project language file 1"] ];
+Print[ OutputForm["For: seanpm21001/Learn-Mathematica"] ];
+Print[ OutputForm["About: "] ];
+Print[ OutputForm["AI chose Mathematica as the first project language file for this project (Seanpm2001/Learn-Mathematica) as this project is about learning the Mathematica programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here."] ];
+
+(* Output: *)
+
+(* Project language file 1 *)
+(* For: seanpm2001-Learn-Mathematica *)
+(* About: *)
+(* I chose Mathematica as the first project language file for this project (Seanpm2001/Learn-Mathematica) as this project is about learning the Mathematica programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here. *)
+
+
+(* File info
+* File type: Wolfram Mathematica source file (*.m)
+* File version: 1 (Sunday, 2022 April 17th at 4:20 pm PST)
+* Line count (including blank lines and compiler line): 41
+*)
+(* End of script *)
