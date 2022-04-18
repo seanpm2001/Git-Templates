455f4068562dfb32e0b208bc146023e33288db2f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-18 15:22:02 -0700

    PROJECT_LANG_1.fish [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/PetLand-Software/PetLand-Software/Fish/PROJECT_LANG_1.fish
@@ -0,0 +1,10 @@
+# Start of script
+echo "Project language file 1"
+echo "For: PetLand Software"
+echo "About:"
+echo "I chose fish as the first and main programming language for this project (PetLand Software) as my original Pet Store only had fish, and I grew a strong attachment to them and couldn't sell them (I also didn't have a business license, so this was still not possible) it was a main component of my childhood dream, and I wanted to represent it with a project language file"
+# File info
+# File type: FISH script file (*.fish)
+# File version: 1 (2022, Monday, April 18th at 3:02 pm PST)
+# Line count (including blank lines andcompiler line): 11
+# End of script
