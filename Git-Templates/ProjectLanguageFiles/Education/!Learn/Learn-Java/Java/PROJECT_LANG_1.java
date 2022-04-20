e9316524d6d4eadd9e47ea3878cf1cd0e11a2e99
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-20 14:41:40 -0700

    PROJECT_LANG_1.java [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Java/Java/PROJECT_LANG_1.java
@@ -0,0 +1,29 @@
+// Start of script
+// Project language file 1
+// For: seanpm2001/Learn-Java
+// About:
+// I chose Java as the first project language file for this project (Seanpm2001/Learn-Java) as this project is about learning the Java programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+// Import section
+import java.util.*
+// Main function
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: seanpm2001/Learn-Java");
+		System.out.println("About:");
+		System.out.println("I chose Java as the first project language file for this project (Seanpm2001/Learn-Java) as this project is about learning the Java programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.");
+		break;
+}
+// Main section
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break;
+} // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Wednesday, April 20th at 2:39 pm PST)
+* File type: Java 8 Source file (*.java)
+* Line count (including blank lines and compiler line): 30
+*/
+// End of script
