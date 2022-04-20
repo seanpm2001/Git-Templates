7bb00afeea1e8446b01a44d67fb97c27d037c4a4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-20 14:29:49 -0700

    PROJECT_LANG_1_V1.sol
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Solidity/Solidity/PROJECT_LANG_1_V1.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+// Start of script
+contract projectLanguageFile1 {   
+	constructor() public{
+	}
+	// Project language file 1
+	// For: seanpm2001/Learn-Solidity
+	// About:
+	// I chose Solidity as the first project language file for this project (Seanpm2001/Learn-Solidity) as this project is about learning the Solidity programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+	function print () public pure returns (string) {       
+		return "Project language file 1\nFor: seanpm2001/Learn-Solidity\nAbout:\nI chose Solidity as the first project language file for this project (Seanpm2001/Learn-Solidity) as this project is about learning the Solidity programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.";             
+	} 
+}
+// File info
+// File type: Solidity source file (*.sol)
+// File version: 1 (2022, Wednesday, April 20th at 2:28 pm PST)
+// Line count (including blank lines and compiler line): 21
+
+// End of script
