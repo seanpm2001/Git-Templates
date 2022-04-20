64b01a9bf8d5bf556a2a2d11a55bb167cdb2def9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-20 14:36:55 -0700

    PROJECT_LANG_1.js [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-JavaScript/JavaScript/PROJECT_LANG_1.js
@@ -0,0 +1,31 @@
+// Start of script
+
+// Project language file 1
+// For: seanpm2001/Learn-JavaScript
+// About:
+// I chose JavaScript as the first project language file for this project (Seanpm2001/Learn-JavaScript) as this project is about learning the JavaScript programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+
+void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 1");
+    alert("Project language file 1");
+    console.log ("For: seanpm2001/Learn-JavaScript");
+    alert("For: seanpm2001/Learn-JavaScript");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the first project language file for this project (Seanpm2001/Learn-JavaScript) as this project is about learning the JavaScript programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.");
+    alert("I chose JavaScript as the first project language file for this project (Seanpm2001/Learn-JavaScript) as this project is about learning the JavaScript programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+
+return main();
+break;
+
+}
+
+/* File info
+* File version: 1 (2022, Wednesday, April 20th at 2:35 pm PST)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
