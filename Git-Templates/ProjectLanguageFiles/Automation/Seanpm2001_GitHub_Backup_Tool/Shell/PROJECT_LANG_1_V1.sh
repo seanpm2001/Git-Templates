eb4900573fb0f7e3d5b79d91984f6501bb91bdcf
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-22 00:29:04 -0700

    PROJECT_LANG_1_V1.sh
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Automation/Seanpm2001_GitHub_Backup_Tool/Shell/PROJECT_LANG_1_V1.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Start of script
+# Project language file 1
+# For: Seanpm2001 GitHub Backup Tool
+# About:
+# I have chosen Shell as the main project language file for this project (seanpm2001/Seanpm2001_GitHub_Backup_Tool) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it.
+function projectLanguageFileOne() {
+  echo ("Project language file #1");
+  wait 1;
+  echo ("For: seanpm2001/Seanpm2001_GitHub_Backup_Tool");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the main project language file for this project (seanpm2001/Seanpm2001_GitHub_Backup_Tool) because Shell is the go-to language for debug scripting on UNIX, and is native to several parts of functionality on this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it.");
+  wait 12;
+  return 0;
+  break;
+}
+return projectLanguageFileOne();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (Friday, 2022, April 22nd at 12:26 am)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 31
+
+# End of script
