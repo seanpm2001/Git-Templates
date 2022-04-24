86e103b6752815dd8cb142c02b4f402a7364ba3c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-23 20:18:28 -0700

    PROJECT_LANG_1.py [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Software_LIBraries/AcroSlideXLIBrary/Python/PROJECT_LANG_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: Seanpm2001/AcroSlideXLIBrary
+# About
+# I have decided to have the main language for this project (Seanpm2001/AcroSlideXLIB) be Python, as Python is the first programming language that this library was written in, and its main implementation will stay in the Python language. Ports to other languages will be made as well in the future.
+''' '''
+class projectLanguageFileOne()
+def plang1():
+  print("Project Language File 1\n")
+  print("For: Seanpm2001/AcroSlideXLIBrary)
+  print("About:")
+  print("I have decided to have the main language for this project (Seanpm2001/AcroSlideXLIB) be Python, as Python is the first programming language that this library was written in, and its main implementation will stay in the Python language. Ports to other languages will be made as well in the future.")
+  break
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+""" """
+# File info
+# File version: 1 (2022, Saturday, April 23rd at 8:16 pm PST)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 26
+# End of script
