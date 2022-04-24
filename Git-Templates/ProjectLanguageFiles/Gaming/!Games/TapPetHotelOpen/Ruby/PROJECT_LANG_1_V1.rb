fb2c6356cd8fec15d66dbbc5ffc32f652e3c8326
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-23 20:14:01 -0700

    PROJECT_LANG_1_V1.rb
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/TapPetHotelOpen/Ruby/PROJECT_LANG_1_V1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+# Start of script
+# I chose Ruby as the main project language file for this project (Tap Pet Hotel Open) as for software diversity reasons, I can't write everything purely in Python. I don't have very many Ruby projects, and since the original creators of this game were Pocket Gems, and since Ruby distributes through Gemfiles, it fits here. Also, Ruby works well both in desktop and web applications, and seems to fit my needs here. It may be supplemented with other languages in the future.
+class project_language_file_1():
+  def projectLanguageFile1():
+    puts "Project language file 1\n"
+    puts "For: Tap Pet Hotel Open\n"
+    puts "About: \n"
+    puts "I chose Ruby as the main project language file for this project (Tap Pet Hotel Open) as for software diversity reasons, I can't write everything purely in Python. I don't have very many Ruby projects, and since the original creators of this game were Pocket Gems, and since Ruby distributes through Gemfiles, it fits here. \nAlso, Ruby works well both in desktop and web applications, and seems to fit my needs here. It may be supplemented with other languages in the future."
+    break
+  return projectLanguageFile1()
+  return 0
+  break
+end # end class
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (2022, Saturday, April 23rd at 8:12 pm PST)
+# Line count (including blank lines and compiler line): 20
+# End of script
