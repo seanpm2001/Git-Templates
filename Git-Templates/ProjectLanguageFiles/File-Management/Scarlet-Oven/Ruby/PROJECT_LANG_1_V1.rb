ad04b986351eadec27f311d6e3d270ada64cb60b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-30 18:45:40 -0700

    PROJECT_LANG_1_V1.rb
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/File-Management/Scarlet-Oven/Ruby/PROJECT_LANG_1_V1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+# Start of script
+# I chose Ruby as the main project language file for this project (Scarlet Oven) as for software diversity reasons, I can't write everything purely in Python. I don't have very many Ruby projects at the moment, and I feel like it would be good to write this program in Ruby. Rubys are typically red, and the Ruby logo will likely always be red, so it can partially support the color theme for this project as well.
+class project_language_file_1():
+  def projectLanguageFile1():
+    puts "Project language file 1\n"
+    puts "For: Scarlet Oven\n"
+    puts "About: \n"
+    puts "I chose Ruby as the main project language file for this project (Scarlet Oven) as for software diversity reasons, I can't write everything purely in Python. I don't have very many Ruby projects at the moment, and I feel like it would be good to write this program in Ruby. Rubys are typically red, and the Ruby logo will likely always be red, so it can partially support the color theme for this project as well."
+    break
+  return projectLanguageFile1()
+  return 0
+  break
+end # end class
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (2022, Saturday, April 30th at 6:44 pm PST)
+# Line count (including blank lines and compiler line): 20
+# End of script
