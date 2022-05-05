74f3ef36c84eaf8cf05f8656741f4f1df155d0ce
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-04 22:03:16 -0700

    PROJECT_LANG_1_V1.js
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/SnapchatMode/JavaScript/PROJECT_LANG_1_V1.js
@@ -0,0 +1,31 @@
+// Start of script
+
+// Project language file 1
+// For: seanpm2001/SNU_2D_SnapchatMode
+// About:
+// I chose JavaScript as the first project language file for this project (Seanpm2001/SNU_2D_SnapchatMode) as this project is a website project, the color of JavaScript is yellow, and matches the themes of those it is trying to emulate. JavaScript is a reasonable language to use here, so it is in use.
+
+void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 1");
+    alert("Project language file 1");
+    console.log ("For: seanpm2001/SNU_2D_SnapchatMode");
+    alert("For: seanpm2001/SNU_2D_SnapchatMode");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the first project language file for this project (Seanpm2001/SNU_2D_SnapchatMode) as this project is a website project, the color of JavaScript is yellow, and matches the themes of those it is trying to emulate. JavaScript is a reasonable language to use here, so it is in use.");
+    alert("I chose JavaScript as the first project language file for this project (Seanpm2001/SNU_2D_SnapchatMode) as this project is a website project, the color of JavaScript is yellow, and matches the themes of those it is trying to emulate. JavaScript is a reasonable language to use here, so it is in use..");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+
+return main();
+break;
+
+}
+
+/* File info
+* File version: 1 (2022, Wednesday, May 4th at 10:01 pm PST)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
