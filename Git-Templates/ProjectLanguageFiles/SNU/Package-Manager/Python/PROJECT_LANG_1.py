c23c55bc8fa4fa93d2df8ff702759b18bfee2334
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-04 23:01:04 -0700

    PROJECT_LANG_1.py [V1]
    The current version [version 1] of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/Package-Manager/Python/PROJECT_LANG_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: Seanpm2001/SNU-Package-Manager
+# About
+# I have decided to have the main language for this project (Seanpm2001/SNU-Package-Manager) be Python, as Python is the language that most SNU non-web programs are written in. Python is what is suited for this job.
+''' '''
+class projectLanguageFileOne():
+def plang1():
+  print("Project Language File 1\n")
+  print("For: Seanpm2001/SNU-Package-Manager")
+  print("About:")
+  print("I have decided to have the main language for this project (Seanpm2001/SNU-Package-Manager) be Python, as Python is the language that most SNU non-web programs are written in. Python is what is suited for this job.")
+  break
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+""" """
+# File info
+# File version: 1 (2022, Wednesday, May 4th at 11:00 pm PST)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 26
+# End of script
