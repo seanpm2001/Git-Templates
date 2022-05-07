d2b6575afda6c810f47d2c077f1e83b4322bc6f3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-07 15:46:33 -0700

    PROJECT_LANG_1.c [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/wOS/6/C/PROJECT_LANG_1.c
@@ -0,0 +1,20 @@
+// Start of script
+// I decided to make the main project language for this project (WacOS/wOS 6) the C programming language, as C is a stable system language that is preferred for the core of this project. It may be supplemented with another language in the future.
+#include <stdio.h>
+union projectLanguageFileOne() {
+int main(void) {
+    printf("Project language file 1: C\n");
+    printf("For: WacOS/wOS 6 (subsystem for iOS 6)\n");
+    printf("I decided to make the main project language for this project (WacOS/wOS 6) the C programming language, as C is a stable system language that is preferred for the core of this project. It may be supplemented with another language in the future.\n");
+    break;
+  }
+}
+return main();
+return 0;
+break;
+exit;
+// File info
+// File version: 1 (2022, Saturday, May 7th at 3:45 pm PST)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 21
+// End of script
