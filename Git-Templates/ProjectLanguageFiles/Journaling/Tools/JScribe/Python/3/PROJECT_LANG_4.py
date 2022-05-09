31e9894bb9ccf668bee0ff307a8796e25cfda74e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-09 16:27:45 -0700

    PROJECT_LANG_4.py [V1]
    The current version [version 1] of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Journaling/Tools/JScribe/Python/3/PROJECT_LANG_4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 4
+# For: Seanpm2001/JScribe
+# About
+# I decided to make the fourth project language file for this project (JScribe) to be Python, as Python is a good application level language to use. It is going to be used alongside C in the application layer, and alongside Objective-J and Inform 7 in the scope of the program. C will be used for more complicated tasks, while Python will be used for simpler tasks.
+''' '''
+class projectLanguageFileFour():
+def plang1():
+  print("Project Language File 4\n")
+  print("For: Seanpm2001/JScribe")
+  print("About:")
+  print("I decided to make the fourth project language file for this project (JScribe) to be Python, as Python is a good application level language to use. It is going to be used alongside C in the application layer, and alongside Objective-J and Inform 7 in the scope of the program. C will be used for more complicated tasks, while Python will be used for simpler tasks.")
+  break
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+""" """
+# File info
+# File version: 1 (2022, Monday, May 9th at 4:26 pm PST)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 26
+# End of script
