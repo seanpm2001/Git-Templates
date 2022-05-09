deda931c01532f3635cf7cf20a4bc0aa13d954e4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-09 16:22:52 -0700

    PROJECT_LANG_3_V1.c
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Journaling/Tools/JScribe/C/PROJECT_LANG_3_V1.c
@@ -0,0 +1,20 @@
+// Start of script
+// I decided to make the third project language file for this project (JScribe) to be C, as C is a good application level language to use. It is going to be used alongside Python in the application layer, and alongside Objective-J and Inform 7 in the scope of the program. C will be used for more complicated tasks, while Python will be used for simpler tasks.
+#include <stdio.h>
+union projectLanguageFileOne() {
+int main(void) {
+    printf("Project language file 3: C\n");
+    printf("For: seanpm2001/JScribe");
+    printf("I decided to make the third project language file for this project (JScribe) to be C, as C is a good application level language to use. It is going to be used alongside Python in the application layer, and alongside Objective-J and Inform 7 in the scope of the program. C will be used for more complicated tasks, while Python will be used for simpler tasks.\n");
+    break;
+  }
+}
+return main();
+return 0;
+break;
+exit;
+// File info
+// File version: 1 (2022, Monday, May 9th at 4:21 pm PST)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 21
+// End of script
