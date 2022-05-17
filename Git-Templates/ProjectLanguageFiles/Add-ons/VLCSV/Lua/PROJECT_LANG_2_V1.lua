9178ab6aea8cb45ab3a8819024a9d02687b09228
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-16 20:01:37 -0700

    PROJECT_LANG_2_V1.lua
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Add-ons/VLCSV/Lua/PROJECT_LANG_2_V1.lua
@@ -0,0 +1,22 @@
+-- Start of script
+-- Project language file 2 for VLCSV
+-- Written in Lua 5.4
+
+local projectLanguageFile2
+print "Project language file 2"
+print "For: VLSCV"
+print "About:"
+print "I decided to make Lua the second project language file for this project (VLCSV) as I chose Lua as the primary language for this project for software diversity reasons, and because it is a good language for this project. I chose between Lua and Rust, because those seem to be the 2 languages supported for VLC Media Player addon development. It is getting its own project language file, starting here."
+break
+
+-- Output:
+-- Project language file 2
+-- For: VLCSV
+-- About:
+-- I decided to make Lua the second project language file for this project (VLCSV) as I chose Lua as the primary language for this project for software diversity reasons, and because it is a good language for this project. I chose between Lua and Rust, because those seem to be the 2 languages supported for VLC Media Player addon development. It is getting its own project language file, starting here.
+
+-- File info:
+-- File type: Lua source file (*.lua)
+-- File version: 1 (2022, Monday, May 16th at 8:00 pm PST)
+-- Line count (including blank lines and compiler line): 23
+-- End of script
