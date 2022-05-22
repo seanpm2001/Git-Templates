7f68df0dffd72a399e32348fa32172fb8b9396fa
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-22 14:37:11 -0700

    PROJECT_LANG_17_V1.sh
    An archive of the first version of the 17th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/GitHub_Organization_Info/Shell/PROJECT_LANG_17_V1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 17th project language file for this project.
+
+function projectLanguageFileSeventeen() {
+  echo ("Project language file #17");
+  wait 1;
+  echo ("For: seanpm2001/GitHub_Organization_Info");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the 17th project language file for this project (seanpm2001/GitHub_Organization_Info) because Shell is the go-to language for debug scripting on UNIX, and is native to some build functionality for this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it. It is getting its own project language file, starting here.");
+  wait 12;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #17
+# For: Seanpm2001/GitHub_Organization_Info
+# About:
+# I have chosen Shell as the 17th project language file for this project (seanpm2001/GitHub_Organization_Info) because Shell is the go-to language for debug scripting on UNIX, and is native to some build functionality for this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it. It is getting its own project language file, starting here. 
+
+return projectLanguageFileSeventeen();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Friday, May 22nd at 2:35 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
