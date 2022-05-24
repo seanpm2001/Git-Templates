87ecd6f4b395d4ca8491910424de60ebb995c032
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-24 16:28:05 -0700

    PROJECT_LANG_3.py [V1]
    The current version [version 1] of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Meadows/qMeadows/Python/PROJECT_LANG_3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Start of script
+
+class projectLanguageFileThree():
+
+def plang3():
+	print("Project Language File 3\n")
+	print("For: Mesdows/qMeadows")
+	print("About:")
+	print("I decided to make the third project language file for this project (Meadows/qMeadows) to be Python, as Python is a good application level language to use, and one of the few languages I know works well with Quantum computing. It is going to be used alongside Q# (and other languages) in the application layer. C will be used for more complicated tasks, while Python will be used for simpler tasks, and will be used more than other application layer languages. It is getting its own project language file, starting here.")
+	break
+
+return plang3()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+''' Output '''
+
+# Project language file 3 
+# For: Meadows/qMeadows
+# About
+# I decided to make the third project language file for this project (Meadows/qMeadows) to be Python, as Python is a good application level language to use, and one of the few languages I know works well with Quantum computing. It is going to be used alongside Q# (and other languages) in the application layer. C will be used for more complicated tasks, while Python will be used for simpler tasks, and will be used more than other application layer languages. It is getting its own project language file, starting here.
+
+""" """
+
+# File info
+# File version: 1 (2022, Tuesday, May 24th at 4:27 pm PST)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 33
+# End of script
