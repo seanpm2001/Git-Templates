02ef687c60c75ecaae3fe4ea7787906c201efa88
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-25 19:35:40 -0700

    PROJECT_LANG_4_V1.ex
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/SPM_FoodIndex/Elixir/PROJECT_LANG_4_V1.ex
@@ -0,0 +1,20 @@
+# Start of script
+# I chose Elixir as the fourth language for this project (seanpm2001/SPM_FoodIndex) as Elixir is a language related to food and drink, and I decided to have it as a sample language for this project.
+# Definitions
+def projectLangFour_IEX {
+  iex> IO.puts("I chose Elixir as the fourth language for this project (seanpm2001/SPM_FoodIndex) as Elixir is a language related to food and drink, and I decided to have it as a sample language for this project.")
+}
+def projectLangFour_Standard {
+  print ("I chose Elixir as the fourth language for this project (seanpm2001/SPM_FoodIndex) as Elixir is a language related to food and drink, and I decided to have it as a sample language for this project.")
+}
+# Output
+try projectLangFour_Standard()
+else: {
+  try projectLangFour_IEX() # Not all systems may support the standard, so IEX is used as a backup.
+}
+# I don't know how to write in Elixir, so this is a demo script and likely doesn't work.
+# File info
+# File type: Elixir source file (*.ex)
+# File version: 1 (2022, Wednesday, May 25th at 7:34 pm PST)
+# Line count (including blank lines and compiler line): 21
+# End of script
