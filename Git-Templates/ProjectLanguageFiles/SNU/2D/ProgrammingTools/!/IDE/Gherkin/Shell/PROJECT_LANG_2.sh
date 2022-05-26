d9b59194df35a27f7a7798253ebb62f043b00df3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-26 13:20:23 -0700

    PROJECT_LANG_2.sh [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Gherkin/Shell/PROJECT_LANG_2.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 2nd project language file for this project.
+
+function projectLanguageFileTwo() {
+  echo ("Project language file #2");
+  wait 1;
+  echo ("For: /SNU/2D/ProgrammingTools/IDE/Gherkin/");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the 2nd project language file for this project (/SNU/2D/ProgrammingTools/IDE/Gherkin/) because Shell is a secondary language already in use on this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it. It is getting its own project language file, starting here.");
+  wait 12;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #2
+# For: /SNU/2D/ProgrammingTools/IDE/Gherkin/
+# About:
+# I have chosen Shell as the 2nd project language file for this project (/SNU/2D/ProgrammingTools/IDE/Gherkin/) because Shell is a secondary language already in use on this project. I am making it a project language, as Shell is just beginning to be used on this project, and it needed a project language file to represent it. It is getting its own project language file, starting here.
+
+return projectLanguageFileTwo();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Thursday, May 26th at 1:19 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
