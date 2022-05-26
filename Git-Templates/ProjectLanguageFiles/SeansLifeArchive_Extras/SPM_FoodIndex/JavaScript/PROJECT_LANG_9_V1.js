20ee950d427f4746c94b3c5aaf41efdc25e19a71
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-26 12:57:00 -0700

    PROJECT_LANG_9_V1.js
    An archive of the first version of the 9th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/SPM_FoodIndex/JavaScript/PROJECT_LANG_9_V1.js
@@ -0,0 +1,32 @@
+// Start of script
+
+class projectLanguageFileNine() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 9");
+    alert("Project language file 9");
+    console.log ("For: seanpm2001/SPM_FoodIndex");
+    alert("For: Wseanpm2001/SPM_FoodIndex");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the ninth project language file for this project (seanpm2001/SPM_FoodIndex) as JavaScript has the word Java in it, and the same terminology as project language 8 applies here: I don't drink coffee, but I associated it with this project to match the food and drink theme. It also fits well with web development. It is getting its own project language file, starting here.");
+    alert("I chose JavaScript as the ninth project language file for this project (seanpm2001/SPM_FoodIndex) as JavaScript has the word Java in it, and the same terminology as project language 8 applies here: I don't drink coffee, but I associated it with this project to match the food and drink theme. It also fits well with web development. It is getting its own project language file, starting here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+}
+
+// Output:
+// Project language file 6
+// For: seanpm2001/SPM_FoodIndex
+// About:
+// I chose JavaScript as the ninth project language file for this project (seanpm2001/SPM_FoodIndex) as JavaScript has the word Java in it, and the same terminology as project language 8 applies here: I don't drink coffee, but I associated it with this project to match the food and drink theme. It also fits well with web development. It is getting its own project language file, starting here.
+
+/* File info
+* File version: 1 (2022, Thursday, May 26th at 12:55 pm PST)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 33
+*/
+
+// End of script
