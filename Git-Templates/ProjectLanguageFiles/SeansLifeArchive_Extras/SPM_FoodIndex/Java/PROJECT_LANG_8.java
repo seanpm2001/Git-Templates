ad9de55e8b5141a357be11ee903781ebfbca80e2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-25 20:12:12 -0700

    PROJECT_LANG_8.java [V1]
    The current version [version 1] of the 8th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/SPM_FoodIndex/Java/PROJECT_LANG_8.java
@@ -0,0 +1,29 @@
+// Start of script
+// Project language file 8
+// For: seanpm2001/SPM_FoodIndex
+// About:
+// I chose Java as the eighth project language file for this project (Seanpm2001/SPM_FoodIndex) as Java is a term for Coffee (and also an island in Indonesia) it was included to fit the food and drink theme of this project. It is getting its own project language file, starting here.
+// Import section
+import java.util.*
+// Main function
+public class projectLanguageFileEight() {
+	public static void main(String args) {
+		System.out.println("Project language file 8");
+		System.out.println("For: seanpm2001/SPM_FoodIndex");
+		System.out.println("About:");
+		System.out.println("I chose Java as the eighth project language file for this project (Seanpm2001/SPM_FoodIndex) as Java is a term for Coffee (and also an island in Indonesia) it was included to fit the food and drink theme of this project. It is getting its own project language file, starting here.");
+		break;
+}
+// Main section
+return main();
+int y = 8;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break;
+} // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Wednesday, May 25th at 8:11 pm PST)
+* File type: Java 8 Source file (*.java)
+* Line count (including blank lines and compiler line): 30
+*/
+// End of script
