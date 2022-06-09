74f7442a5ea7264477a030c6eb50e409f366b04e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-08 19:17:54 -0700

    PROJECT_LANG_1.m [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/!Software/WOAHS-X/Calculator/Objective-C/PROJECT_LANG_1.m
@@ -0,0 +1,29 @@
+@interface classname : projectLanguageFileOne {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @WOAHS_X_Calculator_ObjectiveC classname : superclassname {
+    // Output:
+    // Project language file 1
+    // For: WacOS/WOAHS-X/Calculator
+    // About:
+    // I chose Objective-C as the main project language file for this project (WOAHS-X Calculator) as I intend to write this software piece in Objective-C to match the subsystem that is based on the system that this is based on. I could have chosen AppleScript, but I decided to leave that for another version. Objective-C is getting its own project language file, starting here.
+    NSLog(@"Project language file 1");
+    NSLog(@"For: WacOS/WOAHS-X/Calculator");
+    NSLog(@"About:");
+    NSLog(@"I chose Objective-C as the main project language file for this project (WOAHS-X Calculator) as I intend to write this software piece in Objective-C to match the subsystem that is based on the system that this is based on. I could have chosen AppleScript, but I decided to leave that for another version. Objective-C is getting its own project language file, starting here.");
+    break;
+}
+@end
+/* File info
+* File version: 1 (2022, Wednesday, June 8th at 7:17 pm PST)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 30
+* This script is incomplete, and needs lots of work */
+// End of script
