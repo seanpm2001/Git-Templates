41d66a93dffee16e13eba7a0ae7081efb9eb8a93
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-09 15:48:01 -0700

    PROJECT_LANG_1.mm [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/!Software/Modern/Calculator/Objective-C++/PROJECT_LANG_1.mm
@@ -0,0 +1,55 @@
+#!/bin/objective-cpp -x
+// Start of script
+
+// Import section
+
+#import </CoreFoundation/CFAttributedString.h>
+#import </Foundation/NSAttributedString.h>
+#import </Foundation/NSDictionary.h>
+#import </Foundation/NSMutableAttributedString.h>
+#import </UIKit/UIKit.h>
+
+// using System.DotNet;
+// using System.ObjectiveCSharp;
+
+@namespace objectivecsharp-projectLanguage {
+
+void projectLanguageFileOne(); {
+	@interface classMain : superclassMain {
+  	// instance variables
+	}
++ classMethod1;
++ (return_type)classMethod2;
++ (return_type)classMethod3:(param1_type)param1_varName;
+
+- (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+- (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+                              param2_callName:(param2_type)param2_varName;
+    	NSLog.println("Project language file 1");
+    	NSLog.println("For: WacOS/Modern/Calculator");
+    	NSLog.println("About:"); 	
+    	NSLog.println("I decided to make Objective-C++ the first project language file for this project (WacOS/Modern/Calculator) as I want to write each calculator iteration in a different language. I have already done Objective-C. It would be pointless to go with the original plan (the WacOS Calculator++) so it was merged with the modern WacOS calculator. It is getting its own project language file, starting here.");
+    	break;
+    	@end
+	}
+return projectLanguageFileOne();
+break;
+@end.
+}
+break;
+@end
+
+// Output:
+
+// Project language file 1
+// For: WacOS/Modern/Calculator
+// About:
+// I decided to make Objective-C++ the first project language file for this project (WacOS/Modern/Calculator) as I want to write each calculator iteration in a different language. I have already done Objective-C. It would be pointless to go with the original plan (the WacOS Calculator++) so it was merged with the modern WacOS calculator. It is getting its own project language file, starting here.
+
+/* File info
+* File type: Objective-C++ source file (*.mm)
+* File version: 1 (2022, Thursday, June 9th at 3:47 pm PST)
+* Line count (including blank lines and compiler line): 56
+*/
+
+// End of script
