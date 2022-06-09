3445e3a392e7c3defca0237ed257cf6b4aeeda54
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-08 19:28:11 -0700

    PROJECT_LANG_1_V1.pas
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/!Software/BaSYS/Calculator/Pascal/PROJECT_LANG_1_V1.pas
@@ -0,0 +1,26 @@
+// Start of script
+
+program LanguageFileFileOne(output);
+begin
+    Write('Project language file 1')
+    Write('For: WacOS/BaSYS/Calculator')
+    Write('About:')
+    Write('I have decided to have the first project language file for this project (WacOS/BaSYS/Calculator) to be Pascal, as Pascal is a language used commonly in Classic Mac OS applications. Since this calculator is designed to be ran similarly, I needed to use Pascal a little more, as I have been heavily using C, and I normally use Pascal as the application level language for BaSYS. Pascal is getting its own project language file, starting here.')
+    break
+    {No ";" is required after the last statement of a block -
+        adding one adds a "null statement" to the program, which is ignored by the compiler.}
+end.
+
+// Output:
+
+// Project language file 1
+// For: WacOS/BaSYS/Calculator
+// About:
+// I have decided to have the first project language file for this project (WacOS/BaSYS/Calculator) to be Pascal, as Pascal is a language used commonly in Classic Mac OS applications. Since this calculator is designed to be ran similarly, I needed to use Pascal a little more, as I have been heavily using C, and I normally use Pascal as the application level language for BaSYS. Pascal is getting its own project language file, starting here.
+
+// File info
+// File version: 1 (2022, Wednesday, June 8th at 7:25 pm PST)
+// File type: Pascal source file (*.pas)
+// Line count (including blank lines and compiler line): 27
+
+// End of script
