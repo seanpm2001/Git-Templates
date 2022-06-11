7303f372f701d8dc20f39ffdae46b8645db2aec9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-10 23:05:33 -0700

    PROJECT_LANG_1.m (V1)
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/!Software/whyPhoneOS/Objective-C/PROJECT_LANG_1.m
@@ -0,0 +1,29 @@
+@interface classname : projectLanguageFileOne {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @whyPhoneOS_Software_Calculator_ObjectiveC classname : superclassname {
+    // Output:
+    // Project language file 1
+    // For: WacOS/whyPhoneOS/Calculator
+    // About:
+    // I chose Objective-C as the main project language file for this project (WhPhoneOS Calculator) as I intend to write this software piece in Objective-C to match the subsystem that is based on the system that this is based on. Swift wasn't an option, as it wasn't introduced into the base environment until later on. Objective-C is getting its own project language file, starting here.
+    NSLog(@"Project language file 1");
+    NSLog(@"For: WacOS/whyPhoneOS/Calculator");
+    NSLog(@"About:");
+    NSLog(@"I chose Objective-C as the main project language file for this project (WhPhoneOS Calculator) as I intend to write this software piece in Objective-C to match the subsystem that is based on the system that this is based on. Swift wasn't an option, as it wasn't introduced into the base environment until later on. Objective-C is getting its own project language file, starting here.");
+    break;
+}
+@end
+/* File info
+* File version: 1 (2022, Friday, June 10th at 11:04 pm PST)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 30
+* This script is incomplete, and needs lots of work */
+// End of script
