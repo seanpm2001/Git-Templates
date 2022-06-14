0953347f57f5d7659caaba57e3f2aa272c33cc70
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-13 19:36:00 -0700

    PROJECT_LANG_1.mm [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/!Software/WacTVOS/Calculator/Objective-C++/PROJECT_LANG_1.mm
@@ -0,0 +1,55 @@
+#!/bin/objective-cpp -x
+// Start of script
+
+// Import section
+
+#import </CoreFoundation/CFAttributedString.h>
+#import </Foundation/NSAttributedString.h>
+#import </Foundation/NSDictionary.h>
+#import </Foundation/NSMutableAttributedString.h>
+#import </UIKit/UIKit.h>
+
+// using System.DotNet;
+// using System.ObjectiveCSharp;
+
+@namespace objectivecsharp-projectLanguage {
+
+void projectLanguageFileOne(); {
+	@interface classMain : superclassMain {
+  	// instance variables
+	}
++ classMethod1;
++ (return_type)classMethod2;
++ (return_type)classMethod3:(param1_type)param1_varName;
+
+- (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+- (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+                              param2_callName:(param2_type)param2_varName;
+    	NSLog.println("Project language file 1");
+    	NSLog.println("For: WacOS/WacTVOS/Calculator");
+    	NSLog.println("About:"); 	
+    	NSLog.println("I decided to make Objective-C++ the first project language file for this project (WacOS/WacTVOS/Calculator) as I had to choose between Objective-C, Objective-C++ and Swift for this project, and I decided that Objective-C++ would be a good source for this platform. I don't have any tvOS devices, but I assume they don't come with a calculator. I am making one anyway, as any existing computer deserves a calculator. This project may need to be omitted from databases that aim to be a true recreation. It is getting its own project language file, starting here.");
+    	break;
+    	@end
+	}
+return projectLanguageFileOne();
+break;
+@end.
+}
+break;
+@end
+
+// Output:
+
+// Project language file 1
+// For: WacOS/WacTVOS/Calculator
+// About:
+// I decided to make Objective-C++ the first project language file for this project (WacOS/WacTVOS/Calculator) as I had to choose between Objective-C, Objective-C++ and Swift for this project, and I decided that Objective-C++ would be a good source for this platform. I don't have any tvOS devices, but I assume they don't come with a calculator. I am making one anyway, as any existing computer deserves a calculator. This project may need to be omitted from databases that aim to be a true recreation. It is getting its own project language file, starting here.
+
+/* File info
+* File type: Objective-C++ source file (*.mm)
+* File version: 1 (2022, Monday, June 13th at 7:35 pm PST)
+* Line count (including blank lines and compiler line): 56
+*/
+
+// End of script
