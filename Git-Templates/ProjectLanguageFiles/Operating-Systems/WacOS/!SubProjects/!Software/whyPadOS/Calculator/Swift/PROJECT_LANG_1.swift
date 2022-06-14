807f4496eeca69d90c9b43cfa777b7ce0bf6acdb
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-13 20:12:03 -0700

    PROJECT_LANG_1.swift [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/!Software/whyPadOS/Calculator/Swift/PROJECT_LANG_1.swift
@@ -0,0 +1,27 @@
+// Start of script
+
+class projectLanguageFileOne {
+
+var str = "Project language file 1\nFor: WhyPadOS/Software/Calculator\nAbout:\nI have chosen Swift as the first project language file for this project (WacOS/wOS/Software/Calculator) as Swift is a common Apple language to use, and is needed with the more ''modern'' core of iPadOS (the base system) along with cross-compatibility, and software diversity reasons. It is getting its own project language file, starting here.";
+myValue = @String
+let myValue = str();
+return myValue;
+break;
+}
+
+// Output
+
+// Project language file 1
+// For: WhyPadOS/Software/Calculator
+// About:
+// I have chosen Swift as the first project language file for this project (WacOS/wOS/Software/Calculator) as Swift is a common Apple language to use, and is needed with the more ''modern'' core of iPadOS (the base system) along with cross-compatibility, and software diversity reasons. It is getting its own project language file, starting here.
+
+// I don't know how to make working strings in Swift
+
+// File info
+
+// File version: 1 (2022, Monday, June 13th at 8:11 pm PST)
+// File type: Swift 5.3 Source file (*.swift)
+// Line count (including blank lines and compiler line): 28
+
+// End of script
