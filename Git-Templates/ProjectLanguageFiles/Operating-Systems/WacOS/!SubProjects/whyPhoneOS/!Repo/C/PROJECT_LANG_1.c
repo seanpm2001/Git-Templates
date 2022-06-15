cc19f3f77381bf83e447392297f3de3143648621
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-14 21:40:57 -0700

    PROJECT_LANG_1.c [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/WacOS/!SubProjects/whyPhoneOS/!Repo/C/PROJECT_LANG_1.c
@@ -0,0 +1,40 @@
+
+/* Start of script */
+
+/* Import section */
+
+#include <stdio.h>
+
+/* Program section */
+
+union projectLanguageFileOne() {
+
+int main(void) {
+    printf("Project language file 1: C\n");
+    printf("For: WacOS/whyPhoneOS\n");
+    printf("About:\n");
+    printf("I decided to make C the main project language for this project (WacOS project/WhyPhoneOS/) be the C programming language, as C is a stable system language that is being used for the core of all 3 major versions of whyPhoneOS. It is supplemented by 2 other languages, including x86 Assembly, and Objective-C. It is getting its own project language file, starting here.\n");
+    break;
+}
+
+return main();
+return 0;
+break;
+exit;
+
+}
+
+/* Output */
+
+// Project language file 1
+// For: WacOS/whyPhoneOS
+// About:
+// I decided to make C the main project language for this project (WacOS project/WhyPhoneOS/) be the C programming language, as C is a stable system language that is being used for the core of all 3 major versions of whyPhoneOS. It is supplemented by 2 other languages, including x86 Assembly, and Objective-C. It is getting its own project language file, starting here.
+
+/* File info */
+
+// File version: 1 (2022, Tuesday, June 14th at 9:40 pm PST)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 41
+
+/* End of script */
