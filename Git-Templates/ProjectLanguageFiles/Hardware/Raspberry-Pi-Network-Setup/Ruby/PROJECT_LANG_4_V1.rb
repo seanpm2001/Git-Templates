14f38e17b491c8dec4fbad67d2e97d2b3c470b7c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-22 16:19:36 -0700

    PROJECT_LANG_4_V1.rb
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Hardware/Raspberry-Pi-Network-Setup/Ruby/PROJECT_LANG_4_V1.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+# Start of script
+
+# Project language file 4
+# For: seanpm2001/Raspberry-Pi-Network-Setup
+# About: 
+# I chose Ruby as the fourth project language file for this project (seanpm2001/Raspberry-Pi-Network-Setup) as for software diversity reasons, I can't write everything purely in Python. I don't have very many Ruby projects at the moment, and I feel like it would be good to write some Raspberry Pi programs in Ruby (such as Scarlet Oven) Rubys are typically red, and the Ruby logo will likely always be red, so it can partially support the color theme for the projects as well.
+class project_language_file_4():
+  def projectLanguageFile4():
+    puts "Project language file 4\n"
+    puts "For: seanpm2001/Raspberry-Pi-Network-Setup\n"
+    puts "About: \n"
+    puts "I chose Ruby as the fourth project language file for this project (seanpm2001/Raspberry-Pi-Network-Setup) as for software diversity reasons, I can't write everything purely in Python. I don't have very many Ruby projects at the moment, and I feel like it would be good to write some Raspberry Pi programs in Ruby (such as Scarlet Oven) Rubys are typically red, and the Ruby logo will likely always be red, so it can partially support the color theme for the projects as well."
+    break
+  return projectLanguageFile4()
+  return 0
+  break
+end # end class
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (2022, Wednesday, June 22nd at 4:18 pm PST)
+# Line count (including blank lines and compiler line): 24
+# End of script
