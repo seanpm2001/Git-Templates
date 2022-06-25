62c0eb4880ceb34030eaf4ebc69e455112b5d4a3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-25 16:42:21 -0700

    PROJECT_LANG_1_V1.purs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Notchware/FloweredNotch/PureScript/PROJECT_LANG_1_V1.purs
@@ -0,0 +1,20 @@
+-- Start of script
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+
+-- Project language 1
+-- For: FloweredNotch
+-- About:
+-- I decided to make PureScript the first main project language file for this project (FloweredNotch) as I wanted to have a little challenge. I am sure Notch decorations can be done in PureScript/JavaScript. I had to distinguish this projects functionality from MalNotch, as so the two are separate and safe. It is getting its own project language file, starting here.
+main :: Effect Unit
+main = log "Project language 1\nFor: FloweredNotch\nAbout:\nI decided to make PureScript the first main project language file for this project (FloweredNotch) as I wanted to have a little challenge. I am sure Notch decorations can be done in PureScript/JavaScript. I had to distinguish this projects functionality from MalNotch, as so the two are separate and safe. It is getting its own project language file, starting here."
+{- File info
+File type: PureScript source file (*.purs)
+File version: 1 (2022, Saturday, June 25th at 4:41 pm PST)
+Line count (including blank lines and compiler line): 21
+}-
+-- End of script
