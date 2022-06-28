c37dc215b6014542ea7f50412a930143d097a0ee
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-28 15:43:26 -0700

    PROJECT_LANG_1.kt [V1]
    The current version [version 1] of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Linting/Kotlint/Kotlin/PROJECT_LANG_1.kt
@@ -0,0 +1,26 @@
+// Start of script
+
+fun main() {
+    val scope = "Project Language File 1\nFor: seanpm2001/Kotlint\nAbout:\n\n"
+    println("I chose Kotlin as the first project language file for this project (Seanpm2001/Kotlint) as this project is based solely around Kotlin linters, and Kotlin is the language that needs to be represented here. It is getting its own project language file, starting here.\n, $scope")
+}
+fun main(args: Array<String>) {
+    for (arg in args) {
+        println(arg)
+    }
+}
+return main();
+break;
+// Output:
+
+// Project language file 1
+// For: seanpm2001/Kotlint
+// About:
+// I chose Kotlin as the first project language file for this project (Seanpm2001/Kotlint) as this project is based solely around Kotlin linters, and Kotlin is the language that needs to be represented here. It is getting its own project language file, starting here.
+
+/* File info
+* File version: 1 (2022, Tuesday, June 28th at 3:42 pm PST)
+* File type: Kotlin Source file (*.kt, *.kts, *.ktm)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
