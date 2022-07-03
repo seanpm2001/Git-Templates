98a185a7f91383a83609f17612ed096e4c7ef597
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-02 19:54:23 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Macros/The-Graeyt-Macro/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Start of script
+
+class projectLanguageFileOne():
+
+def plang1():
+	print("Project Language File 1\n")
+	print("For: seanpm2001/The-Graeyt-Macro")
+	print("About:")
+	print("I decided to make the first project language file for this project (The Græy(t) Macro) to be Python, as Python is the first language to be used for macro writing on this project, and gets elevated privilege among the many macro languages used for this project. is perfect for the job, as the job is pretty simple. It will be supplemented with other languages that macros are to be written in. It is getting its own project language file, starting here.")
+	break
+
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+''' Output '''
+
+# Project language file 1
+# For: seanpm2001/The-Graeyt-Macro
+# About
+# I decided to make the first project language file for this project (The Græy(t) Macro) to be Python, as Python is the first language to be used for macro writing on this project, and gets elevated privilege among the many macro languages used for this project. is perfect for the job, as the job is pretty simple. It will be supplemented with other languages that macros are to be written in. It is getting its own project language file, starting here.
+
+""" """
+
+# File info
+# File version: 1 (2022, Saturday, July 2nd at 7:53 pm PST)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 33
+# End of script
