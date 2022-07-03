94315b8c9d2a4cc0e5a5f1440a1525c96567488a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-02 20:11:11 -0700

    PROJECT_LANG_2.nim [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/TinyDeathStar/Nim/PROJECT_LANG_2.nim
@@ -0,0 +1,19 @@
+# Start of script
+
+echo ("Project language file 2")
+echo ("For: seanpm2001/SeansLifeArchive_Images_TinyDeathStar/")
+echo ("About:")
+echo ("I chose Nim as the second project language file for this project (SeansLifeArchive/Images/TinyDeathStar/) as this game is by NimbleBit, so the official (but secondary) project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. The original language for this project (Eiffel) will stay as the first language, but will no longer be the primary language.")
+
+# Output:
+
+# Project language file 2
+# For: seanpm2001/SeansLifeArchive_Images_TinyDeathStar/
+# About:
+# I chose Nim as the second project language file for this project (SeansLifeArchive/Images/TinyDeathStar/) as this game is by NimbleBit, so the official (but secondary) project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. The original language for this project (Eiffel) will stay as the first language, but will no longer be the primary language.
+
+# File info
+# File version: 1 (2022, Saturday, July 2nd at 8:10 pm PST)
+# File type: Nim source file (*.nim)
+# Line count (including blank lines and compiler line): 20
+# End of script
