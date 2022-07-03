8f9581da0e5660a4ed39f7b1ccc1d07db71ec069
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-03 16:42:55 -0700

    PROJECT_LANG_2.asm [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/MotorWorld_CarFactory/Assembly/PROJECT_LANG_2.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 2
+; For: seanpm2001/SeansLifeArchive_Images_MotorWorld_CarFactory
+; About:
+; I have decided to make the Assembly language the second official language for this project, as the game in question is a game that involves heavy usage of an ASSEMBLY line, and aside from the rust, Assembly was chosen as the second project language file for this reason. It is getting its own project language file, starting here.
+
+; File info
+; File version: 1 (2022, Sunday, July 3rd at 4:42 pm PST)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
