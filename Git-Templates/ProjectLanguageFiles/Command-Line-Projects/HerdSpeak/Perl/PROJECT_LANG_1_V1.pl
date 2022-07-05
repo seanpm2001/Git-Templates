e8cd0b54d4e056e7e7c8be74c43fdcfa45450a53
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-04 21:01:30 -0700

    PROJECT_LANG_1_V1.pl
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Command-Line-Projects/HerdSpeak/Perl/PROJECT_LANG_1_V1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+# Start of script
+# Project laguage file 1
+# For: HerdSpeak
+# About:
+# I chose Perl as the first project language file for this project (HerdSpeak) as the project this was forked from (Cowsay) is written in Perl, and I didn't feel like changing it, as Perl is a good language to use. It is getting its own project language file, starting here.
+print "Project laguage file 1";
+print "For: HerdSpeak";
+print "About:";
+print "I chose Perl as the first project language file for this project (HerdSpeak) as the project this was forked from (Cowsay) is written in Perl, and I didn't feel like changing it, as Perl is a good language to use. It is getting its own project language file, starting here.";
+# File info
+# File type: Perl source file (*.pl)
+# File version: 1 (2022, Monday, July 4th at 8:58 pm PST)
+# Line count (including blank lines and compiler line): 16
+# End of script
