64fb4b6aefe7be334f1dfecbe7377d511416c55b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-05 18:53:35 -0700

    PROJECT_LANG_1_V1.js
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/pCloud/JavaScript/PROJECT_LANG_1_V1.js
@@ -0,0 +1,32 @@
+// Start of script
+
+class projectLanguageFileOne() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 1");
+    alert("Project language file 1");
+    console.log ("For: seanpm2001/SeansLifeArchive_Images_pCloud");
+    alert("For: Wseanpm2001/SeansLifeArchive_Images_pCloud");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the first (main) project language for this project (SeansLifeArchive/Images/pCloud) as I can't entirely see the pCloud source code, but I am noticing strong JavaScript influences. Since this project is about pCloud, I decided to choose JavaScript for this reason. It is getting its own project language file, starting here.");
+    alert("I chose JavaScript as the first (main) project language for this project (SeansLifeArchive/Images/pCloud) as I can't entirely see the pCloud source code, but I am noticing strong JavaScript influences. Since this project is about pCloud, I decided to choose JavaScript for this reason. It is getting its own project language file, starting here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+}
+
+// Output:
+// Project language file 1
+// For: seanpm2001/SeansLifeArchive_Images_pCloud
+// About:
+// I chose JavaScript as the first (main) project language for this project (SeansLifeArchive/Images/pCloud) as I can't entirely see the pCloud source code, but I am noticing strong JavaScript influences. Since this project is about pCloud, I decided to choose JavaScript for this reason. It is getting its own project language file, starting here.
+
+/* File info
+* File version: 1 (2022, Tuesday, July 5th at 6:52 pm PST)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 33
+*/
+
+// End of script
