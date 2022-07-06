cd0e3616892053a644831e91a67fd97e0bdc34a4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-05 18:21:56 -0700

    PROJECT_LANG_2.nim [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Bit-City/Nim/PROJECT_LANG_2.nim
@@ -0,0 +1,19 @@
+# Start of script
+
+echo ("Project language file 2")
+echo ("For: seanpm2001/SeansLifeArchive_Images_Bit_City_-NimbleBit_Game-/")
+echo ("About:")
+echo ("I chose Nim as the second project language file for this project (SeansLifeArchive/Images/Bit City (NimbleBit Game)) as this game is by NimbleBit, so the official (but secondary) project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. The original language for this project (Rust) will stay as the first language, but will no longer be the primary language.")
+
+# Output:
+
+# Project language file 2
+# For: seanpm2001/SeansLifeArchive_Images_Bit_City_-NimbleBit_Game-/
+# About:
+# I chose Nim as the second project language file for this project (SeansLifeArchive/Images/Bit City (NimbleBit Game)) as this game is by NimbleBit, so the official (but secondary) project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. The original language for this project (Rust) will stay as the first language, but will no longer be the primary language.
+
+# File info
+# File version: 1 (2022, Tuesday, July 5th at 6:21 pm PST)
+# File type: Nim source file (*.nim)
+# Line count (including blank lines and compiler line): 20
+# End of script
