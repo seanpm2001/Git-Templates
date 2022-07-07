1f641a8c8fa000220ed1b0e2da5c39780714a6ab
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-06 18:11:08 -0700

    PROJECT_LANG_2.nim [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Lego-Tower/Nim/PROJECT_LANG_2.nim
@@ -0,0 +1,19 @@
+# Start of script
+
+echo ("Project language file 2")
+echo ("For: seanpm2001/SeansLifeArchive_Images/LegoTower/")
+echo ("About:")
+echo ("I chose Nim as the second project language file for this project (SeansLifeArchive/Images/Lego Tower) as this game is by NimbleBit, so the official (but secondary) project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. The original language for this project (Boo) will stay as the first language, but will no longer be the primary language.")
+
+# Output:
+
+# Project language file 2
+# For: seanpm2001/SeansLifeArchive_Images_LegoTower/
+# About:
+# I chose Nim as the second project language file for this project (SeansLifeArchive/Images/Lego Tower) as this game is by NimbleBit, so the official (but secondary) project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. The original language for this project (Boo) will stay as the first language, but will no longer be the primary language.
+
+# File info
+# File version: 1 (2022, Wednesday, July 6th at 6:10 pm PST)
+# File type: Nim source file (*.nim)
+# Line count (including blank lines and compiler line): 20
+# End of script
