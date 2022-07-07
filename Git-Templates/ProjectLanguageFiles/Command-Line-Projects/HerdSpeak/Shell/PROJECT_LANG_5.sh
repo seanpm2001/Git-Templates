0c747102826ba7515dd520512098853b423c7188
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-06 21:01:18 -0700

    PROJECT_LANG_5.sh [V1]
    The current version [version 1] of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Command-Line-Projects/HerdSpeak/Shell/PROJECT_LANG_5.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 5th project language file for this project.
+
+function projectLanguageFileFive() {
+  echo ("Project language file #5");
+  wait 1;
+  echo ("For: /seanpm2001/HerdSpeak/");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I have chosen Shell as the 5th project language file for this project (HerdSpeak) because Shell is a primary configuration language already in use on this project. I am making it a project language, as Shell needed a project language file to represent it. It is getting its own project language file, starting here.");
+  wait 12;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #5
+# For: /seanpm2001/HerdSpeak/
+# About:
+# I have chosen Shell as the 5th project language file for this project (HerdSpeak) because Shell is a primary configuration language already in use on this project. I am making it a project language, as Shell needed a project language file to represent it. It is getting its own project language file, starting here.
+
+return projectLanguageFileFive();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Wednesday, July 6th at 9:00 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
