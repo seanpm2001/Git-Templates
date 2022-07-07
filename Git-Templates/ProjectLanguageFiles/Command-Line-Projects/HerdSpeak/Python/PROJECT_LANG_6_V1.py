16fa0bc4f3ef338cb2280c9e9215fbd1e2274ff9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-06 21:22:26 -0700

    PROJECT_LANG_6_V1.py
    An archive of the first version of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Command-Line-Projects/HerdSpeak/Python/PROJECT_LANG_6_V1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Start of script
+
+class projectLanguageFileSix():
+
+def plang1():
+	print("Project Language File 6\n")
+	print("For: seanpm2001/HerdSpeak")
+	print("About:")
+	print("I decided to make the sixth project language file for this project (HerdSpeak) to be Python, as Python is being used as an alternative language for source programming on some parts of this project (where Perl isn't used) It is getting its own project language file, starting here.")
+	break
+
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+''' Output '''
+
+# Project language file 6
+# For: seanpm2001/HerdSpeak
+# About
+# I decided to make the sixth project language file for this project (HerdSpeak) to be Python, as Python is being used as an alternative language for source programming on some parts of this project (where Perl isn't used) It is getting its own project language file, starting here.
+
+""" """
+
+# File info
+# File version: 1 (2022, Wednesday, July 6th at 9:20 pm PST)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 33
+# End of script
