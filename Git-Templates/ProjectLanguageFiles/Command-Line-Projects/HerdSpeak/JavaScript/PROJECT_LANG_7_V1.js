0e0a0ea08ca6550050476e1e7c5dde75880f6472
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-06 21:29:44 -0700

    PROJECT_LANG_7_V1.js
    An archive of the first version of the 7th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Command-Line-Projects/HerdSpeak/JavaScript/PROJECT_LANG_7_V1.js
@@ -0,0 +1,32 @@
+// Start of script
+
+class projectLanguageFileSeven() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 7");
+    alert("Project language file 7");
+    console.log ("For: seanpm2001/HerdSpeak");
+    alert("For: Wseanpm2001/HerdSpeak");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the seventh project language for this project (HerdSpeak) as JavaScript is being used for the web portion of this project, and already has a functional Node.js implementation ready. It is getting its own project language file, starting here.");
+    alert("I chose JavaScript as the seventh project language for this project (HerdSpeak) as JavaScript is being used for the web portion of this project, and already has a functional Node.js implementation ready. It is getting its own project language file, starting here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+}
+
+// Output:
+// Project language file 7
+// For: seanpm2001/HerdSpeak
+// About:
+// I chose JavaScript as the seventh project language for this project (HerdSpeak) as JavaScript is being used for the web portion of this project, and already has a functional Node.js implementation ready. It is getting its own project language file, starting here.
+
+/* File info
+* File version: 1 (2022, Wednesday, July 6th at 9:26 pm PST)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 33
+*/
+
+// End of script
