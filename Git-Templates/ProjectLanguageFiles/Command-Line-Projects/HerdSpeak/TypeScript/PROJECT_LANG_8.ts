03f50d3a698d51483a49ee9a7d021abddcf59a81
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-07 15:11:22 -0700

    PROJECT_LANG_8.ts [V1]
    The current version [version 1] of the 8th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Command-Line-Projects/HerdSpeak/TypeScript/PROJECT_LANG_8.ts
@@ -0,0 +1,18 @@
+// Start of script
+/* I chose TypeScript as the 8th project language for this project (HerdSpeak) as TypeScript was already in use on this project. Before creating this project language file, its usage was so low that I hardly noticed it. It is getting its own project language file, starting here. */
+// WARNING: I am not very experienced with TypeScript. This program may not execute properly. /
+namespace ts {
+	public void projectLanguageFileEight() {
+	let message: string = 'Project language file 8\nFor: HerdSpeak\nAbout:\nI chose TypeScript as the 8th project language for this project (HerdSpeak) as TypeScript was already in use on this project. Before creating this project language file, its usage was so low that I hardly noticed it. It is getting its own project language file, starting here.\n';	
+	console.log(message);
+	break;
+	}
+	return projectLanguageFileEight();
+	break;
+}
+/* File info
+* File type: TypeScript source file (*.ts)
+* File version: 1 (2022, Thursday, July 7th at 3:10 pm PST) 
+* Line count (including blank lines and compiler line): 19
+*/
+// End of script
