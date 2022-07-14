c3b3c63324097cff33824be474944ceac3fcb815
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-14 14:36:05 -0700

    PROJECT_LANG_2.v [V1]
    The current version [version 1] of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/PoliticalProjects/OpenGovernment-Voter/V/PROJECT_LANG_2.v
@@ -0,0 +1,9 @@
+// Start of script
+struct App {
+	print "I chose V as the second project language for this project (OpenGovernment Voter) as V is being used for the user interface and front-end of this project for uniqueness, and to support the language. It is getting its own project language file, starting here.";
+}
+// File info
+// File type: V source file (*.v)
+// File version: 1 (2022, Thursday, July 14th at 2:35 pm PST)
+// (Including blank lines and compiler line): 10
+// End of script
