2d450618445d40b9644959a44d46e3b5e121dee7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-14 14:31:25 -0700

    PROJECT_LANG_1.py [V1]
    The current version [version 1] of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/PoliticalProjects/OpenGovernment-Voter/Python/PROJECT_LANG_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Start of script
+
+class projectLanguageFileOne():
+
+def plang1():
+	print("Project Language File 1\n")
+	print("For: OpenGovernment Voter")
+	print("About:")
+	print("I decided to make the first project language file for this project (OpenGovernment Voter) Python, as Python is being used for general programming on this project in both the backend and frontend, while the user interface will be written in V. It is getting its own project language file, starting here.")
+	break
+
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+''' Output '''
+
+# Project language file 1
+# For: OpenGovernment Voter
+# About
+# I decided to make the first project language file for this project (OpenGovernment Voter) Python, as Python is being used for general programming on this project in both the backend and frontend, while the user interface will be written in V. It is getting its own project language file, starting here.
+
+""" """
+
+# File info
+# File version: 1 (2022, Thursday, July 14th at 2:30 pm PST)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 33
+# End of script
