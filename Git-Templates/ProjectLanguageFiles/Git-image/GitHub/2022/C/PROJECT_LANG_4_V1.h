2320b1f7dc4bfc0e76d4823a11634bb70a0068ff
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-18 19:00:06 -0700

    PROJECT_LANG_4_V1.h
    An archive of the first version of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/GitHub/2022/C/PROJECT_LANG_4_V1.h
@@ -0,0 +1,25 @@
+// Start of script
+
+union projectLanguageFileFour() {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 4");
+    printf("For: SeansLifeArchive/Images/GitHub/2022")
+    break;
+}
+
+// I chose C as the fourth project language for this project (Seanpm2001/LifeArchive/Images/GitHub/2022) as C is one of the 4 languages (Ruby, Go, JavaScript, C) used in the development of GitHub.
+return main();
+printf("I chose C as the fourth project language for this project (Seanpm2001/LifeArchive/Images/GitHub/2022) as C is one of the 4 languages (Ruby, Go, JavaScript, C) used in the development of GitHub.");
+wait 10;
+break;
+exit;
+}
+
+// File info
+// File version: 1 (Sunday, 2021 December 12th at 7:21 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 26
+
+// End of script
