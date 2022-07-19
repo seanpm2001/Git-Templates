f3c9b8430365333cee3a1a5bd29ecaba19ce9c73
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-18 17:08:34 -0700

    PROJECT_LANG_1_V1.rb
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/GitHub_Organization_Info/Ruby/PROJECT_LANG_1_V1.rb
@@ -0,0 +1,21 @@
+# Start of script
+# GitHub is written mostly in Ruby, not Go (2009 Google language) and I want this project (GitHub_Organization_Info) to have a language assigned to it that isn't Go this time, so I created this Ruby source file (I don't want it to be Go, as I hate Google, and I am not going to repeat the mistake of having a key project (SeansLifeArchive_Images_GitHub) having a bad language associated with it.
+
+# Old Go file kept for reference
+
+# package main
+# import "fmt"
+# func main() {
+    #// Project language file
+    #fmt.Println("GitHub is written mostly in Go (2009 Google language) and I want this project to have a language assigned to it, so I created this Go (2009 Google language) file")
+# }
+#// End of script
+
+puts "GitHub is written mostly in Ruby, not Go (2009 Google language) and I want this project (GitHub_Organization_Info) to have a language assigned to it that isn't Go this time, so I created this Ruby source file (I don't want it to be Go, as I hate Google, and I am not going to repeat the mistake of having a key project (SeansLifeArchive_Images_GitHub) having a bad language associated with it."
+
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (Friday, May 7th 2021 at 5:36 ppm)
+# Line count (including blank lines and compiler line): 22
+
+# End of script
