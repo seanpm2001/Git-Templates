e16e2fedcfaebfe030fe392fe0478f2e7a082c57
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-20 17:35:00 -0700

    PROJECT_LANG_1.mk [V1]
    Version 1 of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/Daily-desktop-screenshots/Makefile/PROJECT_LANG_1.mk
@@ -0,0 +1,14 @@
+# Start of script
+# Project language file 1
+# For seanpm2001/Daily-desktop-screenshots
+# About
+# I decided to make GNU Make (Makefile) the main project language file for this project (seanpm2001/Daily-desktop-screenshots) as it is currently the only functional programming/scripting language for this project. It could have been markdown, but then that would apply to every project, it could have been YAML, but YAML doesn't relate to this project, so GNU Make was chosen.
+std::cout << "Project language file 1\n ";
+std::cout << "For seanpm2001/Daily-desktop-screenshots\n";
+std::cout << "About:\n";
+std::cout << "I decided to make GNU Make (Makefile) the main project language file for this project (seanpm2001/Daily-desktop-screenshots) as it is currently the only functional programming/scripting language for this project. It could have been markdown, but then that would apply to every project, it could have been YAML, but YAML doesn't relate to this project, so GNU Make was chosen.\n";
+# File info
+# File type: GNU Makefile (*.mk *.mak *.make)
+# File version: 1 (2021, Tuesday, December 14th at 3:28 pm)
+# Line count (including blank lines and compiler line): 15
+# End of script
