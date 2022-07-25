8f7d3e16fbd0523b9c7fe38b9517e685ddd741ff
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-25 13:31:27 -0700

    PROJECT_LANG_1_V1.svelte
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Svelte/Svelte/PROJECT_LANG_1_V1.svelte
@@ -0,0 +1,13 @@
+<script>
+	let desc = 'I chose Svelte as the first project language file for this project (Seanpm2001/Learn-Svelte) as this project is about learning the Svelte programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.';
+</script>
+
+<h1>Project language file 1</h1>
+<h2>For: seanpm2001/Learn-Svelte</h1>
+<h3>About:</h3>
+<p> {desc}!</p>
+<!-- File info
+File type: Svelte source file (*.svelte)
+File version: 1 (2022, Monday, July 25th at 1:30 pm PST)
+Line count (including blank lines and compiler line): 14
+!-->
