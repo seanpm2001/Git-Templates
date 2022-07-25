b451f3de48fa8196ccd51ed850f7a0bca0e6ae5e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-24 19:40:36 -0700

    PROJECT_LANG_1.v [V1]
    The current version [version 1] of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-V/V/PROJECT_LANG_1.v
@@ -0,0 +1,10 @@
+// Start of script
+struct App {
+	print "I chose V as the first project language file for this project (Seanpm2001/Learn-V) as this project is about learning the V programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.";
+}
+// I chose V as the first project language file for this project (Seanpm2001/Learn-V) as this project is about learning the V programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+// File info
+// File type: V source file (*.v)
+// File version: 1 (2022, Sunday, July 24th at 7:39 pm PST)
+// (Including blank lines and compiler line): 11
+// End of script
