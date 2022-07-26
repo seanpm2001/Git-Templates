f6d1190f51e85196fe382072b2fdb359116fed22
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-25 22:21:17 -0700

    PROJECT_LANG_1_V1.dart
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Dart/Dart/PROJECT_LANG_1_V1.dart
@@ -0,0 +1,18 @@
+// Start of script
+// Project language file 1
+// For: seanpm2001/Learn-Dart
+// About:
+// I chose Dart as the first project language file for this project (Seanpm2001/Learn-Dart) as this project is about learning the Dart programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+main() {
+  print('Project language file 1');
+  print('For: seanpm2001/Learn-Dart');
+  print('About:')
+  print('I chose Dart as the first project language file for this project (Seanpm2001/Learn-Dart) as this project is about learning the Dart programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.');
+  break;
+}
+return main();
+// File info
+// File type: Dart source file (*.dart)
+// File version: 1 (2022, Monday, July 25th at 10:20 pm PST)
+// Line count (including blank lines and compiler line): 19
+// End of script
