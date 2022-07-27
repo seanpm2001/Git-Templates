8bf36584f94d831749b22561dc9fad7080814f37
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-26 20:21:17 -0700

    PROJECT_LANG_1_V1.cr
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Crystal/Crystal/PROJECT_LANG_1_V1.cr
@@ -0,0 +1,15 @@
+#!/usr/bin/env crystal
+# Start of script
+# Project language file 1
+# For: seanpm2001/Learn-Crystal
+# About: 
+# I chose Crystal as the first project language file for this project (Seanpm2001/Learn-Crystal) as this project is about learning the Crystal programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+puts "Project language file 1";
+puts "For: seanpm2001/Learn-Crystal";
+puts "About: ";
+puts "I chose Crystal as the first project language file for this project (Seanpm2001/Learn-Crystal) as this project is about learning the Crystal programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.";
+# File info
+# File type: Crystal source file (*.cr)
+# File version: 1 (2022, Tuesday, July 26th at 8:20 pm PST)
+# Line count (including blank lines and compiler line): 16
+# End of script
