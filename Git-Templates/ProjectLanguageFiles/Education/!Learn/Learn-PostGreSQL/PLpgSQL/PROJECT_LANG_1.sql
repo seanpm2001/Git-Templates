5953cfb944ce8c8f23e00da8815bd257dc0e2e16
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-26 20:45:21 -0700

    PROJECT_LANG_1.sql [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-PostGreSQL/PLpgSQL/PROJECT_LANG_1.sql
@@ -0,0 +1,16 @@
+-- Start of script
+-- Project language file 1
+-- For: seanpm2001/Learn-PLpgSQL
+-- About: 
+-- I chose PLpgSQL as the first project language file for this project (Seanpm2001/Learn-PostGreSQL) as this project is about learning the PLpgSQL programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+BEGIN
+'Project language file 1'
+'For: seanpm2001/Learn-PLpgSQL'
+'About:'
+'I chose PLpgSQL as the first project language file for this project (Seanpm2001/Learn-PostGreSQL) as this project is about learning the PLpgSQL programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.'
+END;
+-- File info
+-- File type: PLpgSQL source file (*.sql *.psql)
+-- File version: 1 (2022, Tuesday, July 26th at 8:43 pm PST)
+-- Line count (including blank lines and compiler line): 17
+-- End of script
