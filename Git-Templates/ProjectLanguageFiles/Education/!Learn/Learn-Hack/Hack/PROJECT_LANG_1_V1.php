9577e88be4411cd3506d3289180fafd01eb87f2b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-26 20:05:07 -0700

    PROJECT_LANG_1_V1.php
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Hack/Hack/PROJECT_LANG_1_V1.php
@@ -0,0 +1,15 @@
+<<__EntryPoint>>
+// Start of script
+function projectLanguageOne(): void {
+  echo 'Project language file 1';
+  echo 'For: seanpm2001/Learn-Hack';
+  echo 'About: ';
+  echo 'I chose Hack as the first project language file for this project (Seanpm2001/Learn-Hack) as this project is about learning the Hack programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.';
+}
+return projectLanguageOne();
+/* File info
+* File type: Hack source file (*.hh *.hck *.php *.hack)
+* File version: 1 (2022, Tuesday, July 26th at 7:58 pm PST)
+* Line count (including blank lines and compiler line): 16
+*/
+// End of script
