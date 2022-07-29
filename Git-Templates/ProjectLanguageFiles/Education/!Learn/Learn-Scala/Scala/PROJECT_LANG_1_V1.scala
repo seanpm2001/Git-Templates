f37b3724c45723c0f8018b9e98aaca9af7fe461a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 20:38:13 -0700

    PROJECT_LANG_1_V1.scala
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Scala/Scala/PROJECT_LANG_1_V1.scala
@@ -0,0 +1,16 @@
+// Start of script
+// Project language file 1
+// For: seanpm2001/Learn-Scala
+// About:
+// I chose Scala as the first project language file for this project (Seanpm2001/Learn-Scala) as this project is about learning the Scala programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here
+import users // Not necessary, just getting used to the language
+package users // Not necessary, just getting used to the language
+object ProjectLanguageOne extends App {
+   println("Project language file 1\nFor: seanpm2001/Learn-Scala\nAbout:\nI chose Scala as the first project language file for this project (Seanpm2001/Learn-Scala) as this project is about learning the Scala programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.")
+ }
+return object(ProjectLanguageOne)
+// Note: I am not very good at Scala, so this script may not work.
+// File version: 1 (2022, Thursday, July 28th at 8:36 pm PST)
+// File type: Scala 2.1 Source file (*.scala *.sc)
+// Line count (including blank lines and compiler line): 17
+// End of script
