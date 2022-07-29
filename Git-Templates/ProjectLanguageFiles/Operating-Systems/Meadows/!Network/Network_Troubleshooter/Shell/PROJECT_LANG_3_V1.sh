c745c79679d5c5abb80edbe927eea0791b118a03
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 17:42:21 -0700

    PROJECT_LANG_3_V1.sh
    An archive of the 1st version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Meadows/!Network/Network_Troubleshooter/Shell/PROJECT_LANG_3_V1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Start of script
+# I decided to make Shell the third project language file for this project (Meadows/Network Troubleshooter) as Shell is being used to run system commands on this project. It is the default scripting language here. It is getting its own project language file, starting here.
+function projectLanguageFileThree() {
+  echo ("Project language file #3");
+  wait 1;
+  echo ("For: Meadows/Network Troubleshooter");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I decided to make Shell the third project language file for this project (Meadows/Network Troubleshooter) as Shell is being used to run system commands on this project. It is the default scripting language here. It is getting its own project language file, starting here.");
+  wait 15;
+  return 0;
+  break;
+}
+return projectLanguageFileThree();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Thursday, July 28th at 5:41 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 28
+
+# End of script
