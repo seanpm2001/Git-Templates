d631aa80cc9f73f0ab9d3594b6e43add803b73b5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 17:36:04 -0700

    PROJECT_LANG_2.cr [V1]
    Version 1 of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Meadows/!Network/Network_Troubleshooter/Crystal/PROJECT_LANG_2.cr
@@ -0,0 +1,15 @@
+#!/usr/bin/env crystal
+# Start of script
+# Project language file 2
+# For: Meadows/Network Troubleshooter
+# About: 
+# I chose Crystal as the second project language file for this project (Meadows/Network Troubleshooter) as I wanted to improve software diversity in Meadows, and also increase speed and readability. Crystal claims to be a mix of all the good of C (readability) and all the good of Ruby (speed). It is going to be used in many places where C can be alternated from here. It is getting its own project language file, starting here.
+puts "Project language file 2";
+puts "For: Meadows/Network Troubleshooter";
+puts "About: ";
+puts "I chose Crystal as the second project language file for this project (Meadows/Network Troubleshooter) as I wanted to improve software diversity in Meadows, and also increase speed and readability. Crystal claims to be a mix of all the good of C (readability) and all the good of Ruby (speed). It is going to be used in many places where C can be alternated from here. It is getting its own project language file, starting here.";
+# File info
+# File type: Crystal source file (*.cr)
+# File version: 1 (2022, Thursday, July 28th at 5:35 pm PST)
+# Line count (including blank lines and compiler line): 16
+# End of script
