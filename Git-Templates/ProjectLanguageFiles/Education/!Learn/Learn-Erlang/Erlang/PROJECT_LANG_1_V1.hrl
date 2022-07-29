826e3dead9414e96557f35e365c0d1f940f76439
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 20:31:26 -0700

    PROJECT_LANG_1_V1.hrl
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Erlang/Erlang/PROJECT_LANG_1_V1.hrl
@@ -0,0 +1,14 @@
+%% Start of script
+%% Project language file 1
+%% For: seanpm2001/Learn-Erlang
+%% About:
+%% I chose Erlang as the first project language file for this project (Seanpm2001/Learn-Erlang) as this project is about learning the Erlang programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+io:format("Project language file 1").
+io:format("For: seanpm2001/Learn-Erlang").
+io:format("About:").
+io:format("I chose Erlang as the first project language file for this project (Seanpm2001/Learn-Erlang) as this project is about learning the Erlang programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.~n").
+%% File info
+%% File type: Erlang source file (*.erl *.hrl)
+%% File version: 1 (2022, Thursday, July 28th at 8:28 pm PST)
+%% Line count (including blank lines and compiler line): 15
+%% End of script
