05e3c53012e36d272b02e4fbcfa116acbb57d63d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 20:44:20 -0700

    PROJECT_LANG_1_V1.clj
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Clojure/Clojure/PROJECT_LANG_1_V1.clj
@@ -0,0 +1,11 @@
+; Start of script
+; Project language file 1
+; For: seanpm2001/Learn-Clojure
+; About:
+; I chose Clojure as the first project language file for this project (Seanpm2001/Learn-Clojure) as this project is about learning the Clojure programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+(println "Project language file 1\nFor: seanpm2001/Learn-Clojure\nAbout:\nI chose Clojure as the first project language file for this project (Seanpm2001/Learn-Clojure) as this project is about learning the Clojure programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.")
+; File info
+; File type: Clojure source file (*.clj)
+; File version: 1 (2022, Thursday, July 28th at 8:43 pm PST)
+; Line count (including blank lines and compiler line): 12
+; End of script
