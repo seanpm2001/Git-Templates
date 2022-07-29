a90e9abc24b5436b89195f09b7f3f8ed28191331
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 20:16:55 -0700

    PROJECT_LANG_1.exs [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Elixir/Elixir/PROJECT_LANG_1.exs
@@ -0,0 +1,23 @@
+# Start of script
+# Project language file 1
+# For: seanpm2001/Learn-Elixir
+# About:
+# I chose Elixir as the first project language file for this project (Seanpm2001/Learn-Elixir) as this project is about learning the Elixir programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+# Definitions
+def projectLangOne_IEX {
+  iex> IO.puts("Project language file 1\nFor: seanpm2001/Learn-Elixir\nAbout:\nI chose Elixir as the first project language file for this project (Seanpm2001/Learn-Elixir) as this project is about learning the Elixir programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.")
+}
+def projectLangOne_Standard {
+  print ("Project language file 1\nFor: seanpm2001/Learn-Elixir\nAbout:\nI chose Elixir as the first project language file for this project (Seanpm2001/Learn-Elixir) as this project is about learning the Elixir programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.")
+}
+# Output
+try projectLangOne_Standard()
+else: {
+  try projectLangOne_IEX() # Not all systems may support the standard, so IEX is used as a backup.
+}
+# I don't know how to write in Elixir, so this is a demo script and likely doesn't work.
+# File info
+# File type: Elixir source file (*.ex *.exs)
+# File version: 1 (2022, Thursday, July 28th at 8:14 pm PST)
+# Line count (including blank lines and compiler line): 24
+# End of script
