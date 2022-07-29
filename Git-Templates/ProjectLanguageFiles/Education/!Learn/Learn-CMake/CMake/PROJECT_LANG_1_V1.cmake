ab405a02330701db5df5416c2642f1e6fcab58c8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 20:49:14 -0700

    PROJECT_LANG_1_V1.cmake
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-CMake/CMake/PROJECT_LANG_1_V1.cmake
@@ -0,0 +1,10 @@
+# Start of script
+# Project language file 1
+# For: seanpm2001/Learn-CMake
+# About:
+# I chose CMake as the first project language file for this project (Seanpm2001/Learn-CMake) as this project is about learning the CMake programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+# File info
+# File type: CMake source file (*.cmake)
+# File version: 1 (2022, Thursday, July 28th at 8:48 pm PST)
+# Line count (including blank lines and compiler line): 11
+# End of script
