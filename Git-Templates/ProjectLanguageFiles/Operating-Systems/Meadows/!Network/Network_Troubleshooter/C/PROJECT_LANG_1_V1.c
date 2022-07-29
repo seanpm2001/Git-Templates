e22f5e25d040e83a85b6188d277719728acda28f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-28 17:27:41 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Meadows/!Network/Network_Troubleshooter/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,40 @@
+
+/* Start of script */
+
+/* Import section */
+
+#include <stdio.h>
+
+/* Program section */
+
+union projectLanguageFileOne() {
+
+int main(void) {
+    printf("Project language file 1: C\n");
+    printf("For: Meadows/Network Troubleshooter\n");
+    printf("About:\n");
+    printf("I decided to make the main project language for this project (Meadows/Network Troubleshooter) be the C programming language, as C is a stable system language that I rely on for the core of a system. It has been proven through time to be stable, and fit for the job. It won't be the only language in use, however. Crystal is going to be used heavily in place of C, but not replacing it entirely. C is getting its own project language file, starting here.\n");
+    break;
+}
+
+return main();
+return 0;
+break;
+exit;
+
+}
+
+/* Output */
+
+// Project language file 1
+// For: Meadows/Network Troubleshooter
+// About:
+// I decided to make the main project language for this project (Meadows/Network Troubleshooter) be the C programming language, as C is a stable system language that I rely on for the core of a system. It has been proven through time to be stable, and fit for the job. It won't be the only language in use, however. Crystal is going to be used heavily in place of C, but not replacing it entirely. C is getting its own project language file, starting here.
+
+/* File info */
+
+// File version: 1 (2022, Thursday, July 28th at 5:25 pm PST)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 41
+
+/* End of script */
