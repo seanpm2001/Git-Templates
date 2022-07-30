933ce910b95b3750c9b423647bfd17daab7f0f84
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-30 16:13:25 -0700

    PROJECT_LANG_1_V1.hs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Articles/Firefox-version-number-fix/Haskell/PROJECT_LANG_1_V1.hs
@@ -0,0 +1,18 @@
+-- Start of script
+
+module Main (main) where          -- not needed in interpreter, is the default in a module file
+
+main :: IO ()                     -- the compiler can infer this type definition
+main = putStrLn "Project language file 1\nFor: seanpm2001/Firefox-version-number-fix\nAbout:\nI chose Haskell as the first project language file for this project (seanpm2001/Firefox-version-number-fix) as I haven't used Haskell in a while, and due to its pointers and syntax, I thought it would be a good fit for this project. It is getting its own project language file, starting here"
+
+-- Output:
+-- Project language file 1
+-- For: seanpm2001/Firefox-version-number-fix
+-- About:
+-- I chose Haskell as the first project language file for this project (seanpm2001/Firefox-version-number-fix) as I haven't used Haskell in a while, and due to its pointers and syntax, I thought it would be a good fit for this project. It is getting its own project language file, starting here
+
+-- File info
+-- File version: 1 (2022, Saturday, July 30th at 4:12 pm PST)
+-- File type: Haskell source file (*.hs)
+-- Line count (including blank lines and compiler line): 19
+-- End of script
