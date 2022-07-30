f1ac69c8d51f5db5f6426d13f16f006aacfad4c3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-29 17:53:38 -0700

    PROJECT_LANG_1_V1.h
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/AdVenture_Communist/Part3/C/PROJECT_LANG_1_V1.h
@@ -0,0 +1,21 @@
+// Start of script
+union projectLanguageFileOne() {
+# include <stdio.h>
+int main(void) {
+    printf("Project language file 1\n");
+    printf("For: SeansLifeArchive/Images/AdVenture Communist/Part3\n");
+    printf("About:\n");
+    printf("AdVenture Communist is closed source, and I don't know what language it is written in, so after a lot of thought, I chose the C prorgramming language, as it has no classes, just like Communism. I chose C++ for the opposite game to match completely.");
+}
+return main();
+return 0;
+break;
+exit;
+}
+// Output:
+// AdVenture Communist is closed source, and I don't know what language it is written in, so after a lot of thought, I chose the C prorgramming language, as it has no classes, just like Communism. I chose C++ for the opposite game to match completely.
+// File info
+// File version: 1 (2022, Saturday, April 23rd at 8:27 pm PST)
+// File type: C programming language script file (*.c)
+// Line count (including blank lines and compiler line): 22
+// End of script
