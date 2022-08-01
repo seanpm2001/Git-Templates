6dc2c23fbce3c78cfac99b6c7ed03c7e2055b82e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-31 17:01:14 -0700

    PROJECT_LANG_1.scm [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/Boom-Beach/Scheme/PROJECT_LANG_1.scm
@@ -0,0 +1,12 @@
+;; Start of script
+;; Boom Beach is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of something commonly done (scheming) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Scheme
+
+(let ((message1 (lambda (p) (display "Boom Beach is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of something commonly done (scheming) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Scheme" p) (newline p))))
+  (call-with-output-file "message1outputfile" message1))
+  
+  
+;; I would have done another language, but I couldn't find any good languages with war in its name theme
+;; File version: 1 (Sunday, October 25th 2020 at 11:14 am)
+;; File type: Scheme (*.ss)
+;; Line count (including blank lines and compiler line): 13
+;; End of script
