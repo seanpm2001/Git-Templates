49dd7915e9ba6ed9f868b1e3e59461f555a14aea
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-01 20:26:15 -0700

    PROJECT_LANG_17.java [V1]
    Version 1 of the 17th project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/SeansLifeArchive_Extras/Seanpm2001-seanpm2001/Java/PROJECT_LANG_17.java
@@ -0,0 +1,23 @@
+// Start of script
+// Project language file 17
+// For: seanpm2001/seanpm2001
+// About:
+// I chose Java as the 17th project language file for this project (seanpm2001/seanpm2001) as it is a language I have history with, it is the second programming language I took an in-person class on, and I want to keep it up at the top. A project language file in my core repository can indicate this. It is getting its own project language file, starting here.
+public class projectLanguageFileSeventeen {
+	public static void main(String args) {
+	    System.out.println("Project language file 17");
+	    System.out.println("For: seanpm2001/seanpm2001");
+	    System.out.println("About:");
+		System.out.println("I chose Java as the 17th project language file for this project (seanpm2001/seanpm2001) as it is a language I have history with, it is the second programming language I took an in-person class on, and I want to keep it up at the top. A project language file in my core repository can indicate this. It is getting its own project language file, starting here.");
+		break;
+}
+main();
+int y = 17;
+System.out.println("Current project language file: " + int(y));
+break; } // No exit command exists
+/* File info
+* File version: 1 (2022, Monday, August 1st at 8:25 pm PST)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
