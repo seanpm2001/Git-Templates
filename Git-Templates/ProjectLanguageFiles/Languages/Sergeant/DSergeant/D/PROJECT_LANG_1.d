835ceeef1be69e9f76036d21830265ca12199eba
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-01 20:34:10 -0700

    PROJECT_LANG_1.d [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Languages/Sergeant/DSergeant/D/PROJECT_LANG_1.d
@@ -0,0 +1,27 @@
+#!/usr/bin/rdmd
+// Start of script
+import std.stdio : writeln;
+import std.range : iota;
+import std.parallelism : parallel;
+/* DIVIDER */
+void main()
+{
+  writeln("Project language file 1");
+  writeln("For: DSergeant (Command Language)");
+  writeln("About:");
+	writeln ("I chose D as the first project language file for this project (DSergeant) as I wanted to use the D language for this project, and I feel it also works well for this purpose. I wanted to implement this command language in D, not because of the naming scheme (D rill Sergeant) but because of the language features of D, including type safety mode.");
+}
+return main();
+return 0;
+break;
+// Output:
+// Project language file 1
+// For: DSergeant (Command Language)
+// About:
+// I chose D as the first project language file for this project (DSergeant) as I wanted to use the D language for this project, and I feel it also works well for this purpose. I wanted to implement this command language in D, not because of the naming scheme (D rill Sergeant) but because of the language features of D, including type safety mode.
+/* DIVIDER */
+// File info
+// File version: 1 (2022, Monday, August 1st at 8:33 pm PST)
+// File type: D programming language source file (*.d)
+// Line count (including blank lines and compiler line): 28
+// End of script
