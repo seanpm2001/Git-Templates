03c1f29b2f1bd5429c6a5152effaa97335362419
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-03 16:22:54 -0700

    PROJECT_LANG_1.f90 [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Languages/AGRAN/GRANNY/FORTRAN/PROJECT_LANG_1.f90
@@ -0,0 +1,37 @@
+C Start of script
+
+C I chose FORTRAN as the first project language file for this project (Seanpm2001/AGRAN-Programming language)(Seanpm2001/GRANNY-Programming-language) as FORTRAN is the most significant influence on the projects language, and is ordered 1st out of 4 in its implementation. It is getting its own project language file, starting here.
+
+program projectLangOne
+
+     print *, "Project language file 1"
+     print *, "For: seanpm2001/AGRAN-Programming language"
+     print *, "or: seanpm2001/GRANNY-Programming-language"
+     print *, "About:"
+     print *, "I chose FORTRAN as the first project language file for this project (Seanpm2001/AGRAN-Programming language)(Seanpm2001/GRANNY-Programming-language) as FORTRAN is the most significant influence on the projects language, and is ordered 1st out of 4 in its implementation. It is getting its own project language file, starting here."
+     pause
+     
+end program prolangone
+
+return projectLangOne
+return 0
+pause
+STOP
+END
+
+C output:
+
+C Project language file 1
+C For: seanpm2001/AGRAN-Programming language"
+C or: seanpm2001/GRANNY-Programming-language"
+C About:
+C I chose FORTRAN as the first project language file for this project (Seanpm2001/AGRAN-Programming language)(Seanpm2001/GRANNY-Programming0language) as FORTRAN is the most significant influence on the projects language, and is ordered 1st out of 4 in its implementation. It is getting its own project language file, starting here.
+
+
+C File info
+
+C File type: FORTRAN 90 source file (*.f90, *.f, *.for)
+C File version: 1 (2022, Wednesday, August 3rd at 4:18 pm PST)
+C Line count (including blank lines and compiler line): 38
+
+C End of script
