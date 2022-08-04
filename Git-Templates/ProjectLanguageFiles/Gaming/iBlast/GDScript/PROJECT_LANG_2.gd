e3535930010b254d7638f8b7d2b6ad9611a0944b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-03 20:01:10 -0700

    PROJECT_LANG_2.gd [V1]
    Version 1 of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/iBlast/GDScript/PROJECT_LANG_2.gd
@@ -0,0 +1,25 @@
+# Start of script
+
+# Project language file output
+func _plang2():
+	print("Project language file 2")
+	print("For: seanpm2001/iBlast")
+	print("About")
+	print("I chose GDScript (GoDot Script) as the second project language file for this project (seanpm2001/iBlast) as GoDot is used for better cross platform support until the other engines are ready, and also just to have a GoDot version of the program. It is officially in use as of 2022, Wednesday, August 3rd. It is getting its own project language file, starting here.")
+	break
+return _plang2()
+break
+
+# Output:
+
+# Project language file 2
+# For: seanpm2001/iBlast
+# About:
+# I chose GDScript (GoDot Script) as the second project language file for this project (seanpm2001/iBlast) as GoDot is used for better cross platform support until the other engines are ready, and also just to have a GoDot version of the program. It is officially in use as of 2022, Wednesday, August 3rd. It is getting its own project language file, starting here
+
+# File info
+# File type: GDscript (GoDot Script) source file (*.gd)
+# File version: 1 (2022, Wednesday, August 3rd at 8:01 pm PST)
+# Line count (including blank lines and compiler line): 26
+
+# End of script
