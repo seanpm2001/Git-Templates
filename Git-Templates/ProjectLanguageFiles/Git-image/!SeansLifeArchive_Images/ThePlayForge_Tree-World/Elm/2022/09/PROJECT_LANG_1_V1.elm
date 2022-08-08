3082713d0a9e3447cac09d46422ea9d46f40e49f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-07 19:51:33 -0700

    PROJECT_LANG_1_V1.elm
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/ThePlayForge_Tree-World/Elm/2022/09/PROJECT_LANG_1_V1.elm
@@ -0,0 +1,16 @@
+-- Start of script
+-- Project language file 1
+-- For: seanpm2001/SeansLifeArchive_Images_ThePlayForge_TreeWorld_2022_V9
+-- About:
+-- I decided to make Elm the main (first) project language for this project (Seanpm2001/SeansLifeArchive/Images/ThePlayForge/Tree-World/2022/September) as this project is about a game that is about trees, and it only makes sense to dedicate a language like Elm to it (since Elm is also named after a tree) this project needs a language file, so it works.
+-- Start
+projectLanguageOne : String
+projectLanguageOne =
+    "Project language file 1\nFor: seanpm2001/SeansLifeArchive_Images_ThePlayForge_TreeWorld_2022_V9\nAbout:\nI decided to make Elm the main (first) project language for this project (Seanpm2001/SeansLifeArchive/Images/ThePlayForge/Tree-World/2022/September) as this project is about a game that is about trees, and it only makes sense to dedicate a language like Elm to it (since Elm is also named after a tree) this project needs a language file, so it works."
+-- End
+{- File info
+File type: Elm source file (*.elm)
+File version: 1 (2022, Sunday, August 7th at 7:50 pm PST)
+Line count (including blank lines and compiler line): 17
+-}
+-- End of script
