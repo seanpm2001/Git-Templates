dbe798d489b2559fe919c33db0db45e0f2641667
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-16 18:15:55 -0700

    PROJECT_LANG_6_V1.php
    An archive of the first version of the 6th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/!Seanpm2001-GitHub-Pages-Collection/PHP/PROJECT_LANG_6_V1.php
@@ -0,0 +1,23 @@
+<?php
+// Start of script
+
+// Project language file 6
+// For: seanpm2001/Seanpm2001-GitHub-Pages-Collection
+// About:
+// I chose PHP as the sixth project language for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as PHP is one of the primary scripting languages used on this project, and I am representing all project languages with project language files on this project. It is getting its own project language file, starting here.
+$PROJECT_LANGUAGE_SIX; {
+  echo ("Project language 6");
+  echo ("For: seanpm2001/Seanpm2001-GitHub-Pages-Collection");
+  echo ("I chose PHP as the sixth project language for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as PHP is one of the primary scripting languages used on this project, and I am representing all project languages with project language files on this project. It is getting its own project language file, starting here.");
+  break;
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return $PROJECT_LANGUAGE_SIX;
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (2022, Tuesday, August 16th at 6:14 pm PST)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
+?>
