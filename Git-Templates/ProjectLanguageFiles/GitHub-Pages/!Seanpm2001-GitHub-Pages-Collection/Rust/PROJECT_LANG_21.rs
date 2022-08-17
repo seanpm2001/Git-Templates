7e853d1df2e65a74d377598d0556732cdbbb69ba
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-16 20:54:00 -0700

    PROJECT_LANG_21.rs [V1]
    Version 1 of the 21st project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/!Seanpm2001-GitHub-Pages-Collection/Rust/PROJECT_LANG_21.rs
@@ -0,0 +1,22 @@
+// Start of script
+
+// Project language file 21
+// For: seanpm2001/Seanpm2001-GitHub-Pages-Collection
+// About:
+// I decided to make Rust the 21st project language file for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Rust is one of the languages briefly used in some GitHub pages portions of this project, and I am representing all project languages today via project language files (2022, Tuesday, August 16th)
+
+fn main() {
+  println!("Project language file 21 (Rust)");
+  println!("For: seanpm2001/Seanpm2001-GitHub-Pages-Collection");
+  println!("About:");
+  println!("I decided to make Rust the 21st project language file for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Rust is one of the languages briefly used in some GitHub pages portions of this project, and I am representing all project languages today via project language files (2022, Tuesday, August 16th)");
+  break;
+}
+return main();
+break;
+
+// File info
+// File version: 1 (2022, Tuesday, August 16th at 8:53 pm PST)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 23
+// End of script
