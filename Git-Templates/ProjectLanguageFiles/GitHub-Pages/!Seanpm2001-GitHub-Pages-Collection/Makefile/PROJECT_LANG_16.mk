1dfec68cf2ad8edd767c92490cea8f2e51c5f5cf
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-16 19:35:02 -0700

    PROJECT_LANG_16.mk [V1]
    Version 1 of the 16th project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/!Seanpm2001-GitHub-Pages-Collection/Makefile/PROJECT_LANG_16.mk
@@ -0,0 +1,9 @@
+# Start of script
+# Project language file 16
+# For: seanpm2001/Seanpm2001-GitHub-Pages-Collecton
+# I decided to make GNU Make the 16th project language file for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Makefile is one of the languages used for configuration of this project, and I am representing all project languages today via project language files (2022, Tuesday, August 16th)
+# File info
+# File type: Makefile (MAKEFILE, *.mk *.mak *.make)
+# File version: 1 (2022, Tuesday, August 16th at 7:32 pm PST)
+# Line count (including blank lines and compiler line): 10
+# End of script
