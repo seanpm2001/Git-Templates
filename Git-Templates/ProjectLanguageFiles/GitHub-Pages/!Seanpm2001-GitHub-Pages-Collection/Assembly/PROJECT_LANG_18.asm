5f4c4fe6f22b05281426c175f87ad9c8c3b0f2f5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-16 20:30:58 -0700

    PROJECT_LANG_18.asm [V1]
    Version 1 of the 18th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/!Seanpm2001-GitHub-Pages-Collection/Assembly/PROJECT_LANG_18.asm
@@ -0,0 +1,12 @@
+; Start of script
+
+; Project language file 18
+; For: seanpm2001/Seanpm2001-GitHub-Pages-Collection
+; I decided to make Assembly the 18th project language file for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Assembly is one of the languages briefly used in some GitHub pages portions of this project, and I am representing all project languages today via project language files (2022, Tuesday, August 16th)
+
+; File info
+; File version: 1 (2022, Tuesday, August 16th at 8:30 pm PST)
+; File type: x86 Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 13
+
+; End of script
