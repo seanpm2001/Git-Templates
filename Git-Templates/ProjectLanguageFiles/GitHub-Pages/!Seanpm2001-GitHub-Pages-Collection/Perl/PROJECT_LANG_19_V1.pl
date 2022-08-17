ebbd1c7f949c0f9666c08ce16eec2b5e22d430b5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-16 20:39:48 -0700

    PROJECT_LANG_19_V1.pl
    An archive of the first version of the 19th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/!Seanpm2001-GitHub-Pages-Collection/Perl/PROJECT_LANG_19_V1.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+# Start of script
+# Project language file 19
+# For: seanpm2001/Seanpm2001-GitHub-Pages-Collection
+# About:
+# I decided to make Perl the 19th project language file for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Perl is one of the languages briefly used in some GitHub pages portions of this project, and I am representing all project languages today via project language files (2022, Tuesday, August 16th)
+
+print "Project language file 19";
+print "For: seanpm2001/Seanpm2001-GitHub-Pages-Collection";
+print "About:";
+print "I decided to make Perl the 19th project language file for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Perl is one of the languages briefly used in some GitHub pages portions of this project, and I am representing all project languages today via project language files (2022, Tuesday, August 16th)";
+break;
+
+# File info
+# File type: Perl source file (*pl)
+# File version: 1 (2022, Tuesday, August 16th at 8:38 pm PST)
+# Line count (including blank lines and compiler line): 19
+# End of script
