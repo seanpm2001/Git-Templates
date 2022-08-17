3925cbb83ce555ccc313d450b2f0fa41c908ec5f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-16 18:39:15 -0700

    PROJECT_LANG_8.py [V1]
    Version 1 of the 8th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/!Seanpm2001-GitHub-Pages-Collection/Python/PROJECT_LANG_8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Start of script
+
+class projectLanguageFileEight():
+
+def plang8():
+	print("Project Language File 8\n")
+	print("For: seanpm2001/Seanpm2001-GitHub-Pages-Collection")
+	print("About:")
+	print("I chose Python as the eighth project language for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Python is one of the primary scripting languages used on this project, and I am representing all project languages with project language files on this project. It is getting its own project language file, starting here.")
+	break
+
+return plang8()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+''' Output '''
+
+# Project language file 8
+# For: seanpm2001/Seanpm2001-GitHub-Pages-Collection
+# About
+# I chose Python as the eighth project language for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as Python is one of the primary scripting languages used on this project, and I am representing all project languages with project language files on this project. It is getting its own project language file, starting here.
+
+""" """
+
+# File info
+# File version: 1 (2022, Tuesday, August 16th at 6:35 pm PST)
+# File type: Python 3 source file (*.py *.py3 *.pyw *.pyt)
+# Line count (including blank lines and compiler line): 33
+# End of script
