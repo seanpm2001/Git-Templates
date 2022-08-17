30caa94488f9f945876977a5798c95e94ad067ec
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-16 17:46:23 -0700

    PROJECT_LANG_5_V1.mjs
    An archive of the first version of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/GitHub-Pages/!Seanpm2001-GitHub-Pages-Collection/JavaScript/PROJECT_LANG_5_V1.mjs
@@ -0,0 +1,28 @@
+// Start of script
+
+// Project language file 5
+// For: seanpm2001/Seanpm2001-GitHub-Pages-Collection
+// About:
+// I chose JavaScript as the third project language for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as JavaScript is one of the top scripting languages used on this project, and I am representing all project languages with project language files on this project. It is getting its own project language file, starting here.
+class projectLanguageFileFive() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 5");
+    alert("Project language file 5");
+    console.log ("For: seanpm2001/Seanpm2001-GitHub-Pages-Collection");
+    alert("For: seanpm2001/Seanpm2001-GitHub-Pages-Collection");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the third project language for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as JavaScript is one of the top scripting languages used on this project, and I am representing all project languages with project language files on this project. It is getting its own project language file, starting here.");
+    alert("I chose JavaScript as the third project language for this project (seanpm2001/Seanpm2001-GitHub-Pages-Collection) as JavaScript is one of the top scripting languages used on this project, and I am representing all project languages with project language files on this project. It is getting its own project language file, starting here.");
+    break;
+	}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+/* File info
+* File version: 1 (2022, Tuesday, August 16th at 5:40 pm PST)
+* File type: JavaScript 1.8 source file (*.js *.cjs *.,mjs)
+* Line count (including blank lines and compiler line): 29
+*/
+// End of script
