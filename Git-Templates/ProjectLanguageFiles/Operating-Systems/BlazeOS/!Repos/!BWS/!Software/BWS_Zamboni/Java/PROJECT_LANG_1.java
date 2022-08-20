5dd6fdde241693f8b4dfe16a2b1bcbc89790709b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-19 18:48:54 -0700

    PROJECT_LANG_1.java [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/!Repos/!BWS/!Software/BWS_Zamboni/Java/PROJECT_LANG_1.java
@@ -0,0 +1,24 @@
+// Start of script
+// Project language file 1
+// For: BWS_Zamboni
+// About:
+// I decided to make Java the 1st project language file for this project (BlazeOS/BWS/Zamboni) as the default language for BlazeOS projects is Java. I wanted to implement it as a client here for this reason, and because Java is good at making client software. It is getting its own project language file, starting here.
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: BWS_Zamboni");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (BlazeOS/BWS/Zamboni) as the default language for BlazeOS projects is Java. I wanted to implement it as a client here for this reason, and because Java is good at making client software. It is getting its own project language file, starting here.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 10)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Friday, August 19th at 6:48 pm PST)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 25
+*/
+// End of script
