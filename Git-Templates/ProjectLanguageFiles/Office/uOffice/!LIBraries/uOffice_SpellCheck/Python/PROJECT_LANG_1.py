564f20a055908df083a8fb286628994725fcd268
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-19 18:58:57 -0700

    PROJECT_LANG_1.py [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Office/uOffice/!LIBraries/uOffice_SpellCheck/Python/PROJECT_LANG_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: uOffice_SpellCheck
+# About
+# I decided to make Python the first project language file for this project (uOffice/SpellCheck) as Python is the default language for writing uOffice components, as I chose it as the default language. It is getting its own project language file, starting here.
+class projectLanguageFileOne():
+	print("Project language file 1")
+	print("For: uOffice_SpellCheck")
+	print("About:")
+	print("I decided to make Python the first project language file for this project (uOffice/SpellCheck) as Python is the default language for writing uOffice components, as I chose it as the default language. It is getting its own project language file, starting here.")
+	noMore = input("Press [ENTER] key to quit")
+	print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+	break
+return projectLanguageFileOne()
+break
+return 0
+"""
+File info
+# File type: Python 3 source file (*.py *.py3 *.pyw *.pyt)
+# File version: 1 (2022, Friday, August 19th at 6:53 pm PST)
+# Line count (including blank lines and compiler line): 25
+"""
+# End of script
