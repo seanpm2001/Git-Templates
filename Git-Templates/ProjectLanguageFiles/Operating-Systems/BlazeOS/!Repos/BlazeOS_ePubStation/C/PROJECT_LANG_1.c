e34e6578fcf1df533fb8a8e59368b66cbe01d6a9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-21 15:36:21 -0700

    PROJECT_LANG_1.c [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/!Repos/BlazeOS_ePubStation/C/PROJECT_LANG_1.c
@@ -0,0 +1,27 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1");
+    printf("For: BlazeOS/ePubStation/")
+    break;
+}
+
+// I decided to make C the 1st project language file for this project (BlazeOS/ePubStation/) as this software library for BlazeOS is written mostly in C, and is the first language used on the sub-project. It is being represented here in its own repository.
+
+return main();
+printf("I decided to make C the 1st project language file for this project (BlazeOS/ePubStation) as this software library for BlazeOS is written mostly in C, and is the first language used on the sub-project. It is being represented here in its own repository.\n");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Monday, January 10th at 3:54 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 28
+
+// End of script
