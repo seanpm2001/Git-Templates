6af2f92d8193617a59301f5b46b90b70bcd21e8a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-21 15:16:55 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/!Repos/Mobi_Dick/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,27 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1");
+    printf("For: BlazeOS/Mobi_Dick/")
+    break;
+}
+
+// I decided to make C the 1st project language file for this project (BlazeOS/Mobi_Dick/) as this software library for BlazeOS is written mostly in C, and is the first language used on the sub-project. It is being represented here in its own repository.
+
+return main();
+printf("I decided to make C the 1st project language file for this project (BlazeOS/Mobi_Dick) as this software library for BlazeOS is written mostly in C, and is the first language used on the sub-project. It is being represented here in its own repository.\n");
+wait 15;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Sunday, January 9th at 5:38 pm)
+// File type: C programming language source file (*.c)
+// Line count (including blank lines and compiler line): 28
+
+// End of script
