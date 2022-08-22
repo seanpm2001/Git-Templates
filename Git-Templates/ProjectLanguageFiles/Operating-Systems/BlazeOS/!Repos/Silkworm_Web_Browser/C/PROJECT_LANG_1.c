fcd0c886c4e211e10ec7d3411939c4e7bd3dce10
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-21 19:59:58 -0700

    PROJECT_LANG_1.c [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/!Repos/Silkworm_Web_Browser/C/PROJECT_LANG_1.c
@@ -0,0 +1,28 @@
+// Start of script
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: BlazeOS/Silkworm-web-browser/\n");
+    printf("About:\n");
+    break;
+}
+return main();
+printf("I decided to make C the 1st project language file for this project (BlazeOS/Silkworm-web-browser/) as this web browser project should not be written in Java, and I didn't feel like using Rust, Python, or C++. Due to C being the default system language, I decided to extend it to the application ecosystem, where it will be used in this browsers development. It is getting its own project language file, starting here.\n");
+wait 15;
+break;
+exit;
+}
+/* Output */
+// Project language file 1
+// For: BlazeOS/Silkworm-web-browser/
+// About:
+// I decided to make C the 1st project language file for this project (BlazeOS/Silkworm-web-browser/) as this web browser project should not be written in Java, and I didn't feel like using Rust, Python, or C++. Due to C being the default system language, I decided to extend it to the application ecosystem, where it will be used in this browsers development. It is getting its own project language file, starting here.
+
+// File info
+// File version: 1 (2022, Sunday, August 21st at 7:56 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
