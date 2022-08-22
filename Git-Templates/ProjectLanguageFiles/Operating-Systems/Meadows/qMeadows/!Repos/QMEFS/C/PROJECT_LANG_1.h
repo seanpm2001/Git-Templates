06ce5cd6330f2d4898a6f26b97adf53689533052
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-22 14:31:45 -0700

    PROJECT_LANG_1.h [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Meadows/qMeadows/!Repos/QMEFS/C/PROJECT_LANG_1.h
@@ -0,0 +1,40 @@
+
+/* Start of script */
+
+/* Import section */
+
+#include <stdio.h>
+
+/* Program section */
+
+union projectLanguageFileOne() {
+
+int main(void) {
+    printf("Project language file 1: C\n");
+    printf("For: Meadows/QMEFS\n");
+    printf("About:\n");
+    printf("I decided to make the main project language for this project (Meadows/QMEFS) be the C programming language, as C is the most stable language to write a file system in, and probably one of the best ones. It will be supplemented with at least 1 other programming language. It is getting its own project language file, starting here.\n");
+    break;
+}
+
+return main();
+return 0;
+break;
+exit;
+
+}
+
+/* Output */
+
+// Project language file 1
+// For: Meadows/QMEFS
+// About:
+// I decided to make the main project language for this project (Meadows/QMEFS) be the C programming language, as C is the most stable language to write a file system in, and probably one of the best ones. It will be supplemented with at least 1 other programming language. It is getting its own project language file, starting here.
+
+/* File info */
+
+// File version: 1 (2022, Monday, August 22nd at 2:28 pm PST)
+// File type: C Source file (*.c *.h)
+// Line count (including blank lines and compiler line): 41
+
+/* End of script */
