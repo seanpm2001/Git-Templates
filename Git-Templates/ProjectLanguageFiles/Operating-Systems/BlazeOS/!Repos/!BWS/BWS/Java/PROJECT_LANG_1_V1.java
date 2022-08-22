2c1fbbddfa8415b73f68d82fa6d71d3ff0a41f6b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-21 19:49:19 -0700

    PROJECT_LANG_1_V1.java
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/!Repos/!BWS/BWS/Java/PROJECT_LANG_1_V1.java
@@ -0,0 +1,25 @@
+// Start of script
+// Project language file 1
+// For: BlazeOS/BWS
+// About:
+// I decided to make Java the 1st project language file for this project (BlazeOS/BWS/) as Java is already working on becoming the dominant program language on the BlazeOS platform (the dominant system language is still C) and I am learning from my past mistake, but with this attempt, I will use Java instead of JavaScript. It is possible, and it is a powerful language to use. I also don't want to get it confused with other web services and how they program things. It is most likely going to be supplemented with another language. It is getting its own project language file, starting here.
+
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: BlazeOS/BWS");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (BlazeOS/BWS/) as Java is already working on becoming the dominant program language on the BlazeOS platform (the dominant system language is still C) and I am learning from my past mistake, but with this attempt, I will use Java instead of JavaScript. It is possible, and it is a powerful language to use. I also don't want to get it confused with other web services and how they program things. It is most likely going to be supplemented with another language. It is getting its own project language file, starting here.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 10)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Sunday, August 21st at 7:48 pm PST)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 26
+*/
+// End of script
