20823ac1475d58aac6d4e5e5375d434374a2865f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-23 18:10:43 -0700

    PROJECT_LANG_1.java [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/BlazeOS/!Repos/!Peak/Peak_Video/Java/PROJECT_LANG_1.java
@@ -0,0 +1,25 @@
+// Start of script
+// Project language file 1
+// For: BlazeOS/Peak_Video
+// About:
+// I decided to make Java the 1st project language file for this project (BlazeOS/Peak_Video/) as Java is already working on becoming the dominant program language on the BlazeOS platform (the dominant system language is still C) and I am learning from my past mistake, but with this attempt, I will use Java instead of JavaScript. It is possible, and it is a powerful language to use. It is most likely going to be supplemented with another language. A video platform written in Java is perfectly doable. For this specific project and architecture, it also has benefits. It is getting its own project language file, starting here.
+
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: BlazeOS/Peak_Video");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (BlazeOS/Peak_Video/) as Java is already working on becoming the dominant program language on the BlazeOS platform (the dominant system language is still C) and I am learning from my past mistake, but with this attempt, I will use Java instead of JavaScript. It is possible, and it is a powerful language to use. It is most likely going to be supplemented with another language. A video platform written in Java is perfectly doable. For this specific project and architecture, it also has benefits. It is getting its own project language file, starting here.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 10)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Tuesday, August 23rd at 6:09 pm PST)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 26
+*/
+// End of script
