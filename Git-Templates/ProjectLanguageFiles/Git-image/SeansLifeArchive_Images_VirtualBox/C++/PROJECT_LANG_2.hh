601d830f340e01b383fe947b319012ba6b506c40
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-24 23:16:58 -0700

    PROJECT_LANG_2.hh [V1]
    Version 1 of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/SeansLifeArchive_Images_VirtualBox/C++/PROJECT_LANG_2.hh
@@ -0,0 +1,16 @@
+#include <iostream>
+// Start of script
+// I decided to make C++ the second project language for this project (SeansLifeArchive_Images_VirtualBox) as C++ is the second language VirtualBox is written in, and this project is related to VirtualBox, so it fits.
+class projectLanguageFile {
+	int main() {
+    		std::cout << "I decided to make C++ the second project language for this project (SeansLifeArchive_Images_VirtualBox) as C++ is the second language VirtualBox is written in, and this project is related to VirtualBox, so it fits.\n";
+    		break;
+	}
+return main();
+break;
+}
+// File info
+// File version: 1 (Thursday, April 8th 2021 at 6:44 pm)
+// File type: C++ Source file (*.cpp)
+// Line count (including blank lines and compiler line): 17
+// End of script
