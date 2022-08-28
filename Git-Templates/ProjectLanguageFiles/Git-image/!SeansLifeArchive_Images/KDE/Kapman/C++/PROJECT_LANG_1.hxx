153503adb8997a68eebf11ac765506f18578bbf8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-27 17:47:39 -0700

    PROJECT_LANG_1.hxx [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/KDE/Kapman/C++/PROJECT_LANG_1.hxx
@@ -0,0 +1,31 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// Project language file 1
+// For: SeansLifeArchive/Images/KDE_KapMan
+// About:
+// I chose C++ as the first project language file for this project (SeansLifeArchive/Images/KDE_KapMan) as the source game for this project (Kapman, by KDE) is written mostly in C++ and I felt it should be represented here. It is getting its own project language file, starting here.
+// Start of code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: SeansLifeArchive/Images/KDE_KapMan\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I chose C++ as the first project language file for this project (SeansLifeArchive/Images/KDE_KapMan) as the source game for this project (Kapman, by KDE) is written mostly in C++ and I felt it should be represented here. It is getting its own project language file, starting here.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}}
+return main();
+return 0;
+break;
+// End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.hh *.hxx *.c++ *.h++)
+* File version: 1 (2022, Saturday, August 27th at 5:40 pm PST)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
