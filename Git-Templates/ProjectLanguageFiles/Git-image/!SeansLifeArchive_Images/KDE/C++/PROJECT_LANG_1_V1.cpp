4ebba52fd9f0c0bf1df4efb7d979d248824ddf4f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-28 18:31:29 -0700

    PROJECT_LANG_1_V1.cpp
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/KDE/C++/PROJECT_LANG_1_V1.cpp
@@ -0,0 +1,31 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// Project language file 1
+// For: SeansLifeArchive/Images/KDE/
+// About:
+// I chose C++ as the first project language file for this project (SeansLifeArchive/Images/KDE) as C++ is the #1 most used language by KDE for their projects, and I wanted to represent it here with a project language file, as this is a project directly about KDE. I will also make project language files for QML (2) Python (3) CMake (4) and Shell (5) It is getting its own project language file, starting here.
+// Start of code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: SeansLifeArchive/Images/KDE/\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I chose C++ as the first project language file for this project (SeansLifeArchive/Images/KDE) as C++ is the #1 most used language by KDE for their projects, and I wanted to represent it here with a project language file, as this is a project directly about KDE. I will also make project language files for QML (2) Python (3) CMake (4) and Shell (5) It is getting its own project language file, starting here.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}}
+return main();
+return 0;
+break;
+// End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.hh *.hxx *.c++ *.h++)
+* File version: 1 (2022, Sunday, August 28th at 6:25 pm PST)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
