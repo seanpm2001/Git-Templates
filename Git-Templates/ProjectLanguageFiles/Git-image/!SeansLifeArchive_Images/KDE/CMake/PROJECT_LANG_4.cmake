fffcc2060e3c724fe3903e2214afce129d66d257
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-28 18:56:02 -0700

    PROJECT_LANG_4.cmake [V1]
    Version 1 of the 4th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/KDE/CMake/PROJECT_LANG_4.cmake
@@ -0,0 +1,10 @@
+# Start of script
+# Project language file 4
+# For: seanpm2001//SeansLifeArchive/Images/KDE/
+# About:
+# I chose CMake as the fourth project language file for this project (SeansLifeArchive/Images/KDE) as CMake is the #4 most used language by KDE for their projects, and I wanted to represent it here with a project language file, as this is a project directly about KDE. I will also make project language files for C++ (1) QML (2) Python (3) and Shell (5) It is getting its own project language file, starting here.
+# File info
+# File type: CMake source file (*.cmake)
+# File version: 1 (2022, Sunday, August 28th at 6:55 pm PST)
+# Line count (including blank lines and compiler line): 11
+# End of script
