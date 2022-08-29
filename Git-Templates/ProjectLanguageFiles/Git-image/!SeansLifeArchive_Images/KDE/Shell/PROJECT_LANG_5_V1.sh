423bce9ebca36955a99fb14804831785c5a0c7ae
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-28 19:01:20 -0700

    PROJECT_LANG_5_V1.sh
    An archive of the first version of the 5th project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/KDE/Shell/PROJECT_LANG_5_V1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 5th project language file for this project.
+
+function projectLanguageFileFive() {
+  echo ("Project language file #5");
+  wait 1;
+  echo ("For: seanpm2001//SeansLifeArchive/Images/KDE/");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I chose Shell as the fifth project language file for this project (SeansLifeArchive/Images/KDE) as Shell is the #5 most used language by KDE for their projects, and I wanted to represent it here with a project language file, as this is a project directly about KDE. I will also make project language files for C++ (1) QML (2) Python (3) and CMake (4) It is getting its own project language file, starting here.");
+  wait 12;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #5
+# For: seanpm2001//SeansLifeArchive/Images/KDE/
+# About:
+# I chose Shell as the fifth project language file for this project (SeansLifeArchive/Images/KDE) as Shell is the #5 most used language by KDE for their projects, and I wanted to represent it here with a project language file, as this is a project directly about KDE. I will also make project language files for C++ (1) QML (2) Python (3) and CMake (4) It is getting its own project language file, starting here.
+
+return projectLanguageFileFive();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Sunday, August 28th at 7:00 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
