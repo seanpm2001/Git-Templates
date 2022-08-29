8d2e7822cb6acdb6a9a31a548dcc0c9607973833
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-28 22:02:37 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/GNOME/GIMP/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,26 @@
+// Start of script
+union projectLanguageFileOne() {
+#include <stdio.h>
+int main(void) {
+    printf("Project language file 1\n");
+    printf("For: Seanpm2001//SeansLifeArchive/Images/GNOME/GIMP\n");
+    printf("About:\n");
+    printf("I decided to make C the main project language file for this project (Seanpm2001//SeansLifeArchive/Images/GNOME/GIMP) as C is the language that GIMP (GNU Image Manipulation Program) is written in, and this project is about GIMP, so it fits. It is getting its own project language file, starting here.\n");
+}
+return main();
+return 0;
+break;
+exit;
+}
+// Output:
+
+// Project language file 1
+// For: Seanpm2001//SeansLifeArchive/Images/GNOME/GIMP
+// About:
+// I decided to make C the main project language file for this project (Seanpm2001//SeansLifeArchive/Images/GNOME/GIMP) as C is the language that GIMP (GNU Image Manipulation Program) is written in, and this project is about GIMP, so it fits. It is getting its own project language file, starting here.
+
+// File info
+// File version: 1 (2022, Sunday, August 28th at 10:00 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 27
+// End of script
