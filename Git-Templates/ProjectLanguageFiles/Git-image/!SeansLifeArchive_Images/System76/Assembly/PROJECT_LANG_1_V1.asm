55965ac6ae82e7291588ad639062a0ecfd48edb3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-28 19:15:06 -0700

    PROJECT_LANG_1_V1.asm
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/System76/Assembly/PROJECT_LANG_1_V1.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 1
+; For: seanpm2001//SeansLifeArchive/Images/System76
+; About:
+; I have decided to make the Assembly language the official language for this project (SeansLifeArchive/Images/System76) as laptops and other computers require assembly, and this project is about a hardware company (System76) so the name fits. It is getting its own project language file, starting here.
+
+; File info
+; File version: 1 (2022, Sunday, August 28th at 7:11 pm PST)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
