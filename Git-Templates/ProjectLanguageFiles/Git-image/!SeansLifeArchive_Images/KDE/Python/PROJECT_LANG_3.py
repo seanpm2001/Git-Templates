86e8c4fbc177d4dc5413db79a1dce4f421db4e78
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-28 18:48:32 -0700

    PROJECT_LANG_3.py [V1]
    Version 1 of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/KDE/Python/PROJECT_LANG_3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Start of script
+
+class projectLanguageFileThree():
+
+def plang3():
+	print("Project Language File 3\n")
+	print("For: seanpm2001//SeansLifeArchive/Images/KDE/\n")
+	print("About:\n")
+	print("I chose Python as the third project language file for this project (SeansLifeArchive/Images/KDE) as Python is the #3 most used language by KDE for their projects, and I wanted to represent it here with a project language file, as this is a project directly about KDE. I will also make project language files for C++ (1) QML (2) CMake (4) and Shell (5) It is getting its own project language file, starting here.\n")
+	break
+
+return plang3()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+''' Output '''
+
+# Project language file 3
+# For: For: seanpm2001//SeansLifeArchive/Images/KDE/
+# About
+# I chose Python as the third project language file for this project (SeansLifeArchive/Images/KDE) as Python is the #3 most used language by KDE for their projects, and I wanted to represent it here with a project language file, as this is a project directly about KDE. I will also make project language files for C++ (1) QML (2) CMake (4) and Shell (5) It is getting its own project language file, starting here.
+
+""" """
+
+# File info
+# File version: 1 (2022, Sunday, August 28th at 6:45 pm PST)
+# File type: Python 3 source file (*.py *.py3 *.pyw *.pyt)
+# Line count (including blank lines and compiler line): 33
+# End of script
