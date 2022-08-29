f44178a6d5d729a99f24826d33ab7a99051980e6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-28 19:07:02 -0700

    PROJECT_LANG_1.sh [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Virtualization/VirtualBox_PlusPlus/Shell/PROJECT_LANG_1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 1st project language file for this project.
+
+function projectLanguageFileOne() {
+  echo ("Project language file #1");
+  wait 1;
+  echo ("For: seanpm2001/VirtualBox++");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I chose Shell as the first project language file for this project (VirtualBox_PlusPlus) as Shell is the main scripting language needed for this project. It will likely be supplemented with another language, but Shell scripting will remain common with this project. It is getting its own project language file, starting here.");
+  wait 12;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #1
+# For: seanpm2001/VirtualBox++
+# About:
+# I chose Shell as the first project language file for this project (VirtualBox_PlusPlus) as Shell is the main scripting language needed for this project. It will likely be supplemented with another language, but Shell scripting will remain common with this project. It is getting its own project language file, starting here.
+
+return projectLanguageFileOne();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Sunday, August 28th at 7:06 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
