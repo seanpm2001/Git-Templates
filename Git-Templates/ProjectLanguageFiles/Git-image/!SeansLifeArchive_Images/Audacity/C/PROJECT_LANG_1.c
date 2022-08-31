f7510a0c73e943a27e5baa50c1f8a95e3832e548
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-30 19:38:51 -0700

    PROJECT_LANG_1.c [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/Audacity/C/PROJECT_LANG_1.c
@@ -0,0 +1,31 @@
+// Start of script
+
+union projectLanguageFileOne() {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: seanpm2001/SeansLifeArchive_Images_Audacity\n");
+    printf("About:\n");
+    break;
+}
+
+printf("I decided to make C the first and main project language file for this project (eanpm2001/SeansLifeArchive_Images_Audacity) as C is the main language Audacity is written in, and this project is purely about the Audacity software, so it fits. It is getting its own project language file, starting here.\n");
+}
+return 0;
+exit;
+
+// Output:
+
+// Project language file 1
+// For: seanpm2001/SeansLifeArchive_Images_Audacity
+// About:
+// I decided to make C the first and main project language file for this project (eanpm2001/SeansLifeArchive_Images_Audacity) as C is the main language Audacity is written in, and this project is purely about the Audacity software, so it fits. It is getting its own project language file, starting here.
+
+// File info
+
+// File version: 1 (2022, Tuesday, August 30th at 7:37 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 32
+
+// End of script
