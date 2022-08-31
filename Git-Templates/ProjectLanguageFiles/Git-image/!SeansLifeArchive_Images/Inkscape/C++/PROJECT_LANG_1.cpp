744052af7df5d34cd11fd79abdd75b1a850489d0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-30 19:50:27 -0700

    PROJECT_LANG_1.cpp [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/Inkscape/C++/PROJECT_LANG_1.cpp
@@ -0,0 +1,31 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// Project language file 1
+// For: SeansLifeArchive/Images/InkScape
+// About:
+// I decided to make C++ the first and main project language file for this project (seanpm2001/SeansLifeArchive_Images_Inkscape) as C++ is the main language Inkscape is written in, and this project is purely about the Inkscape software, so it fits. It is getting its own project language file, starting here
+// Start of code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: SeansLifeArchive/Images/InkScape\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I decided to make C++ the first and main project language file for this project (seanpm2001/SeansLifeArchive_Images_Inkscape) as C++ is the main language Inkscape is written in, and this project is purely about the Inkscape software, so it fits. It is getting its own project language file, starting here\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}}
+return main();
+return 0;
+break;
+// End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.hh *.hxx *.c++ *.h++)
+* File version: 1 (2022, Tuesday, August 30th at 7:45 pm PST)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
