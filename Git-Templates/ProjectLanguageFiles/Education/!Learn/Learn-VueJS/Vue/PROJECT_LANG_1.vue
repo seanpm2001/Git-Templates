a6dc58102d83d304848ea0aafd2b7ffcef1fb39c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-02 16:57:51 -0700

    PROJECT_LANG_1.vue [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-VueJS/Vue/PROJECT_LANG_1.vue
@@ -0,0 +1,24 @@
+<div id="plang1">
+  <p>{{ message }}</p>
+</div>
+<script>
+new Vue({
+  el: '#plang1',
+  data: {
+    message: 'Project language file 1\nFor: seanpm2001/Learn-VueJS\nAbout:\nI decided to make Vue.js the main project language file for this project (Seanpm2001/Learn-VueJS) as Vue.js is the language this project is dedicated to, because this project is about learning the Vue.js programming language. It only makes sense to Vue.js the official language for this project. It is getting its own project language file, starting here.\n'
+  }
+})
+</script>
+// Output:
+// Project language file 1
+// For: seanpm2001/Learn-VueJS
+// About:
+// I decided to make Vue.js the main project language file for this project (Seanpm2001/Learn-VueJS) as Vue.js is the language this project is dedicated to, because this project is about learning the Vue.js programming language. It only makes sense to Vue.js the official language for this project. It is getting its own project language file, starting here.
+
+/* File info
+* File type: Vue.js source file (*.vue *.js)
+* File version: 1 (2022, Friday, September 2nd at 4:56 pm PST)
+* Line count (including blank lines and compiler line): 25
+*/
+
+// End of script
