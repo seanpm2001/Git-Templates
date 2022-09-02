fed7c38534fb9c3e1ce5c9726788db60fe48de67
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-01 19:26:35 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/VLC_Media_Player/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,31 @@
+// Start of script
+
+union projectLanguageFileOne() {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: seanpm2001/SeansLifeArchive_Images_VLC_Media_Player\n");
+    printf("About:\n");
+    break;
+}
+
+printf("I decided to make C the first and main project language file for this project (eanpm2001/SeansLifeArchive_Images_VLC_Media_Player) as C is the main language VLC Media Player is written in, and this project is purely about the VLC Media Player software, so it fits. It is getting its own project language file, starting here.\n");
+}
+return 0;
+exit;
+
+// Output:
+
+// Project language file 1
+// For: seanpm2001/SeansLifeArchive_Images_VLC_Media_Player
+// About:
+// I decided to make C the first and main project language file for this project (eanpm2001/SeansLifeArchive_Images_VLC_Media_Player) as C is the main language VLC Media Player is written in, and this project is purely about the VLC Media Player software, so it fits. It is getting its own project language file, starting here.
+
+// File info
+
+// File version: 1 (2022, Thursday, September 1st at 7:24 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 32
+
+// End of script
