e99b26b3f9365eb302f8171f1aaca8dd2ed2b05c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-03 16:13:38 -0700

    PROJECT_LANG_1.hxx [V1]
    Version 1 of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/KDE/Konqueror/C++/PROJECT_LANG_1.hxx
@@ -0,0 +1,31 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// Project language file 1
+// For: SeansLifeArchive/Images/Konqueror
+// About:
+// I chose C++ as the first project language file for this project (SeansLifeArchive/Images/Konqueror) as C++ is the #1 most used language by KDE for their projects, and specifically the Konquerer web browser/file manager, and I wanted to represent it here with a project language file, as this is a project directly about the Konqueror software. It is getting its own project language file, starting here.
+// Start of code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: SeansLifeArchive/Images/Konqueror/\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I chose C++ as the first project language file for this project (SeansLifeArchive/Images/Konqueror) as C++ is the #1 most used language by KDE for their projects, and specifically the Konquerer web browser/file manager, and I wanted to represent it here with a project language file, as this is a project directly about the Konqueror software. It is getting its own project language file, starting here.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}}
+return main();
+return 0;
+break;
+// End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.hh *.hxx *.c++ *.h++)
+* File version: 1 (2022, Saturday, September 3rd at 4:05 pm PST)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
