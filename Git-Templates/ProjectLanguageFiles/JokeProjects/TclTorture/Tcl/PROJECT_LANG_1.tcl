0b97ed61b6662a622754dec79ee6a6d5e7571102
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-03 17:42:39 -0700

    PROJECT_LANG_1.tcl [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/JokeProjects/TclTorture/Tcl/PROJECT_LANG_1.tcl
@@ -0,0 +1,23 @@
+# Start of script
+oo::class create plang1 {
+    puts "Project language file 1"
+    puts "For: seanpm2001/TclTorture"
+    puts "About:"
+    puts "I decided to make Tcl the main project language file for this project (Seanpm2001/TclTorture) as Tcl is the language this project is dedicated to, because this project is about making a series of mind-numbing, 'torturous' Tcl scripts with the Tcl programming language. It only makes sense to Tcl the official language for this project. It is getting its own project language file, starting here."
+}
+return plang1()
+break
+
+# Output:
+
+# Project language file 1
+# For: seanpm2001/TclTorture
+# About:
+# I decided to make Tcl the main project language file for this project (Seanpm2001/TclTorture) as Tcl is the language this project is dedicated to, because this project is about making a series of mind-numbing, 'torturous' Tcl scripts with the Tcl programming language. It only makes sense to Tcl the official language for this project. It is getting its own project language file, starting here.
+
+# File info
+# File type: Tcl source file (*.tcl *.tbc)
+# File version: 1 (2022, Saturday, September 3rd at 5:40 pm PST)
+# Line count (including blank lines and compiler line): 24
+
+# End of script
