d553a635d389698833bcc2e192ef41faa3ec3539
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-04 19:02:10 -0700

    PROJECT_LANG_1_V1.sh
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Compression/SmartZIP/Shell/PROJECT_LANG_1_V1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 1st project language file for this project.
+
+function projectLanguageFileOne() {
+  echo ("Project language file #1");
+  wait 1;
+  echo ("For: seanpm2001/SmartZIP");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I chose Shell as the first project language file for this project (SmartZIP) as Shell is a recommended command language to use with this project. It will be used inside the ZIP cipher block to give instructions on how to compress the file more efficiently. It is getting its own project language file, starting here.");
+  wait 12;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #1
+# For: seanpm2001/SmartZIP
+# About:
+# I chose Shell as the first project language file for this project (SmartZIP) as Shell is a recommended command language to use with this project. It will be used inside the ZIP cipher block to give instructions on how to compress the file more efficiently. It is getting its own project language file, starting here.
+
+return projectLanguageFileOne();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Sunday, September 4th at 7:01 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
