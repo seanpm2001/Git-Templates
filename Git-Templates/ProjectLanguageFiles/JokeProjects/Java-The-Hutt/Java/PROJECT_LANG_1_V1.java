59d1942cd8cedba2a55766d2d270527ceee9a1f1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-04 19:15:24 -0700

    PROJECT_LANG_1_V1.java
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/JokeProjects/Java-The-Hutt/Java/PROJECT_LANG_1_V1.java
@@ -0,0 +1,25 @@
+// Start of script
+// Project language file 1
+// For: Java-The-Hutt
+// About:
+// I decided to make Java the 1st project language file for this project (Java-The-Hutt) as Java is the main language of this project, because of the pun. I am stll deciding what I am going to use this project for. It is getting its own project language file, starting here.
+
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: Java-the-Hutt");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (Java-The-Hutt) as Java is the main language of this project, because of the pun. I am stll deciding what I am going to use this project for. It is getting its own project language file, starting here.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 10)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Sunday, September 4th at 7:14 pm PST)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 26
+*/
+// End of script
