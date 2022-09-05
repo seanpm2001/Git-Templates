9a3881c94e3a984eb11f6bf0a416b88c61dc636d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-04 19:10:20 -0700

    PROJECT_LANG_1_V1.h
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Culinary/CMilk/C/PROJECT_LANG_1_V1.h
@@ -0,0 +1,28 @@
+// Start of script
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: seanpm2001/CMilk/\n");
+    printf("About:\n");
+    break;
+}
+return main();
+printf("I decided to make C the 1st project language file for this project (seanpm2001/CMilk) as this indexing and engine software is written in C due to the pun, and also for its functionality. I wanted to try making a software of this time in C, rather than JavaScript, Perl, Python. or another language. It is getting its own project language file, starting here.\n");
+wait 15;
+break;
+exit;
+}
+/* Output */
+// Project language file 1
+// For: seanpm2001/CMilk
+// About:
+// I decided to make C the 1st project language file for this project (seanpm2001/CMilk) as this indexing and engine software is written in C due to the pun, and also for its functionality. I wanted to try making a software of this time in C, rather than JavaScript, Perl, Python. or another language. It is getting its own project language file, starting here.
+
+// File info
+// File version: 1 (2022, Sunday, September 4th at 7:07 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
