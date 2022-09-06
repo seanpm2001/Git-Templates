434e255cfdb7a7bf0b21cd5efe932f7543573858
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-05 19:58:48 -0700

    PROJECT_LANG_2.f90 [V1]
    Version 1 of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Chemistry/FireDiamondSuite/Fortran/PROJECT_LANG_2.f90
@@ -0,0 +1,34 @@
+C Start of script
+
+C I chose FORTRAN as the second project language file for this project (Seanpm2001/FireDiamondSuite) as FORTRAN was a language I decided to use alongside Forth, for software diversity reasons, and because Fortran goes well with the field of chemistry. I wanted to choose 4 notable languages for this project, this was the second in the order of 4 (shuffled) It is getting its own project language file, starting here.
+
+program prolangtwo
+
+     print *, "Project language file 2"
+     print *, "For: seanpm2001/FireDiamondSuite"
+     print *, "About:"
+     print *, "I chose FORTRAN as the second project language file for this project (Seanpm2001/FireDiamondSuite) as FORTRAN was a language I decided to use alongside Forth, for software diversity reasons, and because Fortran goes well with the field of chemistry. I wanted to choose 4 notable languages for this project, this was the second in the order of 4 (shuffled) It is getting its own project language file, starting here."
+     pause
+     
+end program prolangtwo
+
+return prolangtwo
+return 0
+pause
+STOP
+END
+
+C output:
+
+C Project language file 2
+C For: seanpm2001/FireDiamondSuite
+C About:
+C I chose FORTRAN as the second project language file for this project (Seanpm2001/FireDiamondSuite) as FORTRAN was a language I decided to use alongside Forth, for software diversity reasons, and because Fortran goes well with the field of chemistry. I wanted to choose 4 notable languages for this project, this was the second in the order of 4 (shuffled) It is getting its own project language file, starting here.
+
+C File info
+
+C File type: FORTRAN 90 source file (*.f90, *.f, *.for)
+C File version: 1 (2022, Monday, September 5th at 7:55 pm PST)
+C Line count (including blank lines and compiler line): 35
+
+C End of script
