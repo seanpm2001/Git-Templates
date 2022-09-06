4a066eabb03d5a3e05c40884f94fe25f12d3a580
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-05 19:01:26 -0700

    PROJECT_LANG_1_V1.m
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/M/Mumps/PROJECT_LANG_1_V1.m
@@ -0,0 +1,20 @@
+; Start of script
+
+w "Project language file 1",!
+w "For: seanpm2001/SNU_2D_ProgrammingTools_IDE_M",!
+w "About:",!
+w "I decided to make M the main project language file for this project (SNU / 2D / Programming Tools / IDE / M) as this is a M IDE, and it needs its main language to be represented here.",!
+
+; Output
+
+; Project language file 1
+; For: seanpm2001/SNU_2D_ProgrammingTools_IDE_M
+; About:
+; I decided to make M the main project language file for this project (SNU / 2D / Programming Tools / IDE / M) as this is a M IDE, and it needs its main language to be represented here.
+
+; File info
+; File type: M (MUMPS) source file (*.m)
+; File version: 1 (2022, Monday, September 5th at 7:00 pm PST)
+; Line count (including blank lines and compiler line): 21
+
+; End of script
