4395f3e251ef2f89afb16f08a4e2bb9bece40ca5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-05 20:07:59 -0700

    PROJECT_LANG_3_V1.h
    An archive of the first version of the 3rd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Chemistry/FireDiamondSuite/C/PROJECT_LANG_3_V1.h
@@ -0,0 +1,28 @@
+// Start of script
+union projectLanguageFileThree {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 3\n");
+    printf("For: seanpm2001/FireDiamondSuite/\n");
+    printf("About:\n");
+    break;
+}
+return main();
+printf("I decided to make C the 3rd project language file for this project (seanpm2001/FireDiamondSuite) as this project needs to use C in some areas for functionality, and also for software diversity reasons. It is getting its own project language file, starting here.\n");
+wait 15;
+break;
+exit;
+}
+/* Output */
+// Project language file 3
+// For: seanpm2001/FireDiamondSuite/
+// About:
+// I decided to make C the 3rd project language file for this project (seanpm2001/FireDiamondSuite) as this project needs to use C in some areas for functionality, and also for software diversity reasons. It is getting its own project language file, starting here.
+
+// File info
+// File version: 1 (2022, Monday, September 5th at 8:03 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
