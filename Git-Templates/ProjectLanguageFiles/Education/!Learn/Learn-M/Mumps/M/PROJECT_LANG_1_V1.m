b90269803e061b32d6f84496b044c146316a3f64
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-05 18:34:35 -0700

    PROJECT_LANG_1_V1.m
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-M/Mumps/M/PROJECT_LANG_1_V1.m
@@ -0,0 +1,20 @@
+; Start of script
+
+w "Project language file 1",!
+w "For: seanpm2001/Learn-M",!
+w "About:",!
+w "I decided to make M the main project language file for this project (Seanpm2001/Learn-M) as M is the language this project is dedicated to, because this project is about learning the M programming language. It only makes sense to M the official language for this project. It is getting its own project language file, starting here.",!
+
+; Output
+
+; Project language file 1
+; For: seanpm2001/Learn-M
+; About:
+; I decided to make M the main project language file for this project (Seanpm2001/Learn-M) as M is the language this project is dedicated to, because this project is about learning the M programming language. It only makes sense to M the official language for this project. It is getting its own project language file, starting here.
+
+; File info
+; File type: M (MUMPS) source file (*.m)
+; File version: 1 (2022, Monday, September 5th at 6:33 pm PST)
+; Line count (including blank lines and compiler line): 21
+
+; End of script
