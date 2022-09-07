3e8ad7360d451c1cac8fdda272e902e17f9065a4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-06 21:12:05 -0700

    PROJECT_LANG_1.d [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Audio_Projects/Seanpm2001-Albums/D/PROJECT_LANG_1.d
@@ -0,0 +1,27 @@
+#!/usr/bin/rdmd
+// Start of script
+import std.stdio : writeln;
+import std.range : iota;
+import std.parallelism : parallel;
+/* DIVIDER */
+void main()
+{
+    writeln("Project language file 1");
+    writeln("For: seanpm2001/Seanpm2001-Albums");
+    writeln("About:");
+    writeln ("I chose D as the first project language file for this project (seanpm2001/Seanpm2001-Albums) as I wanted to use it for software diversity reasons. I would have chosen C, CSound, or PureData, but they are used too commonly. I could have made the pun C/'D/' but chose not to. D is getting its own project language file, starting here");
+}
+return main();
+return 0;
+break;
+// Output:
+// Project language file 1
+// For: seanpm2001/Seanpm2001-Albums
+// About:
+// I chose D as the first project language file for this project (DIFFicult) as I wanted to use the D language for this project, and I feel it also works well for this purpose.
+/* DIVIDER */
+// File info
+// File version: 1 (2022, Tuesday, September 6th at 9:12 pm PST)
+// File type: D programming language source file (*.d)
+// Line count (including blank lines and compiler line): 28
+// End of script
