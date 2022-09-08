196a8de63632f1297bd7da752b8e59eb9c2c65bd
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-07 19:54:59 -0700

    PROJECT_LANG_1.el [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-EmacsLisp/EmacsLisp/PROJECT_LANG_1.el
@@ -0,0 +1,17 @@
+;; Start of script
+(message "Project language file 1")
+(message "For: seanpm2001/Learn-EmacsLisp")
+(message "About: ")
+(message "I decided to make Emacs Lisp the main project language file for this project (Seanpm2001/Learn-EmacsLisp) as Emacs Lisp is the language this project is dedicated to, because this project is about learning the Emacs Lisp programming language. It only makes sense to Emacs Lisp the official language for this project. It is getting its own project language file, starting here.")
+;; Output
+;; Project language file 1
+;; For: seanpm2001/Learn-EmacsLisp
+;; About:
+;; I decided to make Emacs Lisp the main project language file for this project (Seanpm2001/Learn-EmacsLisp) as Emacs Lisp is the language this project is dedicated to, because this project is about learning the Emacs Lisp programming language. It only makes sense to Emacs Lisp the official language for this project. It is getting its own project language file, starting here.
+
+;; File info
+;; File type: Emacs Lisp source file (*.el *.elc *.eln)
+;; File version 1 (2022, Wednesday, September 7th at 7:52 pm PST) 
+;; Line count (including blank lines and compiler line): 18
+
+;; End of script
