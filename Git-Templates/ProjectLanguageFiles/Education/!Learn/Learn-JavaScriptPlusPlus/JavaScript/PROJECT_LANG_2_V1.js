f7c64fef7dd64226ba8fc5a7e4c601bda5cef9ca
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-07 18:23:38 -0700

    PROJECT_LANG_2_V1.js
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-JavaScriptPlusPlus/JavaScript/PROJECT_LANG_2_V1.js
@@ -0,0 +1,35 @@
+// Start of script
+class projectLanguageFileTwo() {
+  void main() {
+    console.log ("Project language file 2");
+    console.log ("For: seanpm2001/Learn-JavaScriptPlusPlus");
+    console.log ("About:");
+    console.log ("I chose JavaScript as the second project language for this project (seanpm2001/Learn-JavaScriptPlusPus) as JavaScript is the language that extends JS++ is JavaScript, and JavaScript++ is the language this project aims to compile. Due to GitHub not officially recognizing JS++ as a language yet, I made a second project language file to represent JavaScript. It is getting its own project language file, starting here.");
+    /* Garbage syntax, so I can test out 2 new keywords I recently learned. Uncomment to highlight
+    x = bool("true");
+    y = bool("false");
+    if (x == "false"); {
+      try y
+    } else: {
+      catch x;
+    }
+    */
+    break;
+    return 0;
+  }
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+// Output:
+// Project language file 2
+// For: seanpm2001/Learn-JavaScriptPlusPlus");
+// About:
+// I chose JavaScript as the second project language for this project (seanpm2001/Learn-JavaScriptPlusPus) as JavaScript is the language that extends JS++ is JavaScript, and JavaScript++ is the language this project aims to compile. Due to GitHub not officially recognizing JS++ as a language yet, I made a second project language file to represent JavaScript. It is getting its own project language file, starting here.
+
+/* File info
+* File version: 1 (2022, Wednesday, September 7th at 6:20 pm PST)
+* File type: JavaScript 1.8 source file (*.js *.cjs *.mhs)
+* Line count (including blank lines and compiler line): 36
+*/
+// End of script
