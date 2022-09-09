7bdb1af93b7897b439df2e56fcbc2f2c874119e7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-08 18:42:29 -0700

    PROJECT_LANG_1_V1.mli
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/OCaml/OCaml/PROJECT_LANG_1_V1.mli
@@ -0,0 +1,19 @@
+(* Start of script *)
+(* Project language file 1 *)
+(* For: SNU/2D/ProgrammingTools/IDE/OCaml *)
+(* About: *)
+(* I decided to make OCaml the main project language file for this project (SNU / 2D / Programming Tools / IDE / OCaml) as this is a OCaml IDE, and it needs its main language to be represented here. *)
+
+begin
+class plangone()
+print_endline "Project language file 1"
+print_endline "For: SNU/2D/ProgrammingTools/IDE/OCaml"
+print_endline "About: "
+print_endline "I decided to make OCaml the main project language file for this project (SNU / 2D / Programming Tools / IDE / OCaml) as this is a OCaml IDE, and it needs its main language to be represented here."
+end
+
+(* File info
+* File type: OCaml source file (*.ml *.mli) *)
+* File version: 1 (2022, Thursday, September 8th at 6:38 pm PST) *)
+(* Line count (including blank lines and compiler line): 20 *)
+(* End of script *)
