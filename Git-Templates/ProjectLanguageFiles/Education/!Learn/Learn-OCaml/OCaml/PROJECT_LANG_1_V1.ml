f8964ab4aebeff1e527612e54fb1ea91eacc2cee
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-08 18:34:38 -0700

    PROJECT_LANG_1_V1.ml
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-OCaml/OCaml/PROJECT_LANG_1_V1.ml
@@ -0,0 +1,19 @@
+(* Start of script *)
+(* Project language file 1 *)
+(* For: seanpm2001/Learn-OCaml *)
+(* About: *)
+(* I decided to make OCaml the project language file for this project (SeansLifeArchive_Images_AndrOpen-Office) as it is a unique language to use and be associated with a piece of office software. *)
+
+begin
+class plangone()
+print_endline "Project language file 1"
+print_endline "For: seanpm2001/Learn-OCaml"
+print_endline "About: "
+print_endline "I decided to make OCaml the main project language file for this project (Seanpm2001/Learn-OCaml) as OCaml is the language this project is dedicated to, because this project is about learning the OCaml programming language. It only makes sense to OCaml the official language for this project. It is getting its own project language file, starting here."
+end
+
+(* File info
+* File type: OCaml source file (*.ml *.mli) *)
+* File version: 1 (2022, Thursday, September 8th at 6:32 pm PST) *)
+(* Line count (including blank lines and compiler line): 20 *)
+(* End of script *)
