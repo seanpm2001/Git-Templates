a4bd9ccac50c6cc21a128c30801019471bcae98f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-09 20:10:04 -0700

    PROJECT_LANG_1.hx [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Haxe/Haxe/PROJECT_LANG_1.hx
@@ -0,0 +1,23 @@
+// Start of script
+class Haxe {
+    static function main() {
+        trace("Project language file 1");
+        trace("For: SNU/2D/ProgrammingTools/IDE/Haxe");
+        trace("About:");
+        trace("I decided to make Haxe the main project language file for this project (SNU / 2D / Programming Tools / IDE / Haxe) as this is a Haxe IDE, and it needs its main language to be represented here.");
+        break;
+    }
+}
+/* Output */
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Haxe
+// About:
+// I decided to make Haxe the main project language file for this project (SNU / 2D / Programming Tools / IDE / Haxe) as this is a Haxe IDE, and it needs its main language to be represented here.
+
+/* File info
+* File type: Haxe source file (*.hx)
+* File version: 1 (2022, Friday, September 9th at 8:09 pm PST)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
