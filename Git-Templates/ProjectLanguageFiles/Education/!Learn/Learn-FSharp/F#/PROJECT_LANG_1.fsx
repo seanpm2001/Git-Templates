b51d43f2b6e3841e0921eb36178db8e03932c397
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-11 18:49:38 -0700

    PROJECT_LANG_1.fsx [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-FSharp/F#/PROJECT_LANG_1.fsx
@@ -0,0 +1,30 @@
+// Start of script
+
+// Notes:
+// Note: I am not very experienced with F# and currently have no way of testing this program. There is a chance it may not run, or run correctly/.
+// Note: the # in the repository name was substituted with the word Sharp due to ASCII to ANSI limitations
+
+begin
+class projectLanguageFile1() {
+  printfn "Project language file 1"
+  printfn "For: seanpm2001/Learn-FSharp"
+  printfn "About:"
+  printfn "I decided to make F# the main project language file for this project (Seanpm2001/Learn-FSharp) as F# is the language this project is dedicated to, because this project is about learning the F# programming language. It only makes sense to F# the official language for this project. It is getting its own project language file, starting here."
+  break
+}
+return 0
+end
+
+// Output
+
+// Project language file 1
+// For: seanpm2001/Learn-FSharp
+// About:
+// I decided to make F# the main project language file for this project (Seanpm2001/Learn-FSharp) as F# is the language this project is dedicated to, because this project is about learning the F# programming language. It only makes sense to F# the official language for this project. It is getting its own project language file, starting here.
+
+// File info
+// File type: F# source file (*.fs *.fsi *.fsx *.fsscript)
+// File version: 1 (2022, Sunday, September 11th at 6:44 pm PST)
+// Line count (including blank lines and compiler line): 31
+
+// End of script
