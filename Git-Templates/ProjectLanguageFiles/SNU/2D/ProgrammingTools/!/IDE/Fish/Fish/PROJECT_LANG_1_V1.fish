f632afbddae3a4489938bfe2809790a9c0eb6a18
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-16 20:42:25 -0700

    PROJECT_LANG_1_V1.fish
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Fish/Fish/PROJECT_LANG_1_V1.fish
@@ -0,0 +1,14 @@
+# Start of script
+# Project language file 1
+# For: seanpm2001/Learn-Fish
+# About:
+# I chose to make Fish the project language for this project (SNU/2D/Programming Tools/IDE/Fish) as this is a Fish IDE, and Fish should be the official language for the project.
+echo("Project language file 1");
+echo("For: seanpm2001/Learn-Fish");
+echo("About:");
+echo("I chose to make Fish the project language for this project (SNU/2D/Programming Tools/IDE/Fish) as this is a Fish IDE, and Fish should be the official language for the project.");
+# File info
+# File type: Fish source file (*.fish)
+# File version: 1 (2022, Friday, September 16th at 8:41 pm PST)
+# Line count (including blank lines and compiler line): 15
+# End of script
