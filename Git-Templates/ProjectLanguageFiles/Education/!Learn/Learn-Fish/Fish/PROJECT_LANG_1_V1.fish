3ed61be44fe7cfd2809444c64d48c49da3df8d7c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-16 20:38:29 -0700

    PROJECT_LANG_1_V1.fish
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Fish/Fish/PROJECT_LANG_1_V1.fish
@@ -0,0 +1,14 @@
+# Start of script
+# Project language file 1
+# For: seanpm2001/Learn-Fish
+# About:
+# I decided to make Fish the main project language file for this project (Seanpm2001/Learn-Fish) as Fish is the language this project is dedicated to, because this project is about learning the Fish programming language. It only makes sense to Fish the official language for this project. It is getting its own project language file, starting here.
+echo("Project language file 1");
+echo("For: seanpm2001/Learn-Fish");
+echo("About:");
+echo("I decided to make Fish the main project language file for this project (Seanpm2001/Learn-Fish) as Fish is the language this project is dedicated to, because this project is about learning the Fish programming language. It only makes sense to Fish the official language for this project. It is getting its own project language file, starting here.");
+# File info
+# File type: Fish source file (*.fish)
+# File version: 1 (2022, Friday, September 16th at 8:37 pm PST)
+# Line count (including blank lines and compiler line): 15
+# End of script
