0ff52f4180eee0d540710fd30e63e8dd4f6ca419
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-18 17:40:24 -0700

    PROJECT_LANG_1_V1.lean
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Lean/Lean/PROJECT_LANG_1_V1.lean
@@ -0,0 +1,15 @@
+#print "\n" -- Start of script
+#print "Project language file 1"
+#print "For: SNU/2D/ProgrammingTools/IDE/Lean"
+#print "About: "
+#print "I decided to make Lean the main project language file for this project (SNU / 2D / Programming Tools / IDE / Lean) as this is a Lean IDE, and it needs its main language to be represented here."
+#print "\n" -- Output
+#print "\n" -- Project language file 1
+#print "\n" -- For: SNU/2D/ProgrammingTools/IDE/Lean
+#print "\n" -- About:
+#print "\n" -- I decided to make Lean the main project language file for this project (SNU / 2D / Programming Tools / IDE / Lean) as this is a Lean IDE, and it needs its main language to be represented here.
+#print "\n" -- File info
+#print "\n" -- File type: Lean source file (*.lean)
+#print "\n" -- File version: 1 (2022, Sunday, September 18th at 5:39 pm PST)
+#print "\n" -- Line count (including blank lines and compiler line): 16
+#print "\n" -- End of script
