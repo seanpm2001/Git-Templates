eadf66a50edc44b072291a1f6a04616d083dba02
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-21 20:07:54 -0700

    PROJECT_LANG_1.asm [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Assembly/Assembly/PROJECT_LANG_1.asm
@@ -0,0 +1,16 @@
+; Start of script
+
+; Notes
+; I don't know how to make strings in BitBake, so comments will do for now
+
+; Project language file 1
+; For: SNU/2D/ProgrammingTools/IDE/Assembly
+; About:
+; I decided to make Assembly the main project language file for this project (SNU / 2D / Programming Tools / IDE / Assembly) as this is a Assembly IDE, and it needs its main language to be represented here.
+
+; File info
+; File version: 1 (2022, Wednesday, September 21st at 8:07 pm PST)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 17
+
+; End of script
