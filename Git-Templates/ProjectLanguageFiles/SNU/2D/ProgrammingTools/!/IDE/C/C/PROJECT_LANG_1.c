d584aa63430e3678482a6c227e2d1e421fb7f456
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-23 22:04:23 -0700

    PROJECT_LANG_1.c [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/C/C/PROJECT_LANG_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+// Start of script
+
+// Warning: contains bad code, may not compile
+
+// I decided to make C the main project language file for this project (SNU / 2D / Programming Tools / IDE / C) as this is a C IDE, and it needs its main language to be represented here..
+union projectLanguageFileOne {
+int main(void) {
+    printf("Project language file 1: C\n");
+    printf("For: SNU/2D/ProgrammingTools/IDE/C\n");
+    printf("About:");
+    printf("I decided to make C the main project language file for this project (SNU / 2D / Programming Tools / IDE / C) as this is a C IDE, and it needs its main language to be represented here.\n");
+    break;
+    return 0;
+  }
+return main();
+printf("The program is going to close in 15 seconds");
+wait 15;
+return 0;
+break;
+exit;
+}
+// File info
+
+// File version: 1 (2022, Friday, September 23rd at 10:02 pm PST)
+// File type: C Source file (*.c *.h)
+// Line count (including blank lines and compiler line): 30
+
+// End of script
