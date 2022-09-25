61b27d37171fe8c4feca66e64159017ee07f2a7b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-24 21:06:26 -0700

    PROJECT_LANG_1_V1.hlsl
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/HLSL/HLSL/PROJECT_LANG_1_V1.hlsl
@@ -0,0 +1,20 @@
+// Start of script
+
+// Notes
+// I don't know how to make strings in HLSL, so comments will do for now
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/HLSL
+// About:
+// I decided to make HLSL the main project language file for this project (SNU / 2D / Programming Tools / IDE / HLSL) as this is a HLSL IDE, and it needs its main language to be represented here.
+
+SUMMARY = "Project language file 1 for seanpm2001/SNU_2D_ProgrammingTools_IDE_HLSL"
+DESCRIPTION = "I decided to make HLSL the main project language file for this project (SNU / 2D / Programming Tools / IDE / HLSL) as this is a HLSL IDE, and it needs its main language to be represented here."
+HOMEPAGE = "https://github.com/seanpm2001/SNU_2D_ProgrammingTools_IDE_HLSL/"
+
+// File info
+// File type: HLSL source file (*.hlsl)
+// File version: 1 (2022, Saturday, September 24th at 9:05 pm PST)
+// Line count (including blank lines and compiler line): 21
+
+// End of script
