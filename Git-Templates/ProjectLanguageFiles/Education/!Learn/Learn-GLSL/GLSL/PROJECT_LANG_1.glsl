bf862bad97f1e54cd5d9c8fdd94399f58a5267d4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-24 20:51:01 -0700

    PROJECT_LANG_1.glsl [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-GLSL/GLSL/PROJECT_LANG_1.glsl
@@ -0,0 +1,17 @@
+// Start of script
+// Sample file usage: https://www.lighthouse3d.com/tutorials/glsl-tutorial/hello-world/ (warning: insecure connection)
+#define Project_Language_File_One
+// Project language file 1
+// For: seanpm2001/Learn-GLSL
+// I decided to make GLSL the main project language file for this project (Seanpm2001/Learn-GLSL) as GLSL is the language this project is dedicated to, because this project is about learning the GLSL programming language. It only makes sense to GLSL the official language for this project. It is getting its own project language file, starting here.
+void main()
+{
+    outputF = vec4(1.0, 0.0, 0.0, 1.0);
+}
+#endif
+/* File info
+* File type: GLSL source file (*.glsl)
+* File version: 1 (2022, Saturday, September 24th at 8:50 pm PST)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
