39656b526452744800e3d9569351ba9e8c444cb6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-24 20:42:17 -0700

    PROJECT_LANG_1.cr [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Crystal/Crystal/PROJECT_LANG_1.cr
@@ -0,0 +1,15 @@
+#!/usr/bin/env crystal
+# Start of script
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Crystal
+# About: 
+# I decided to make Crystal the main project language file for this project (SNU / 2D / Programming Tools / IDE / Crystal) as this is a Crystal IDE, and it needs its main language to be represented here.
+puts "Project language file 1";
+puts "For: SNU/2D/ProgrammingTools/IDE/Crystal";
+puts "About: ";
+puts "I decided to make Crystal the main project language file for this project (SNU / 2D / Programming Tools / IDE / Crystal) as this is a Crystal IDE, and it needs its main language to be represented here.";
+# File info
+# File type: Crystal source file (*.cr)
+# File version: 1 (2022, Saturday, September 24th at 8:41 pm PST)
+# Line count (including blank lines and compiler line): 16
+# End of script
