d158ec2a7884d0e81456db17a742501ee2bd52f9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-24 19:56:27 -0700

    PROJECT_LANG_1_V1.glsl
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/GLSL/GLSL/PROJECT_LANG_1_V1.glsl
@@ -0,0 +1,17 @@
+// Start of script
+// Sample file usage: https://www.lighthouse3d.com/tutorials/glsl-tutorial/hello-world/ (warning: insecure connection)
+#define Project_Language_File_One
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/GLSL
+// I decided to make GLSL the main project language file for this project (SNU / 2D / Programming Tools / IDE / GLSL) as this is a GLSL IDE, and it needs its main language to be represented here.
+void main()
+{
+    outputF = vec4(1.0, 0.0, 0.0, 1.0);
+}
+#endif
+/* File info
+* File type: GLSL source file (*.glsl)
+* File version: 1 (2022, Saturday, September 24th at 7:48 pm PST)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
