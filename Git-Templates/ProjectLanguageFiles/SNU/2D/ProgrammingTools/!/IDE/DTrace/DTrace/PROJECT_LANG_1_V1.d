0b4f142df8553cd48edfb1530fd7c272500cd89d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-24 21:22:37 -0700

    PROJECT_LANG_1_V1.d
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/DTrace/DTrace/PROJECT_LANG_1_V1.d
@@ -0,0 +1,17 @@
+#!/usr/sbin/dtrace -s
+/* Start of script */
+/* Project language file 1
+* For: SNU/2D/ProgrammingTools/IDE/DTrace
+* About:
+* I decided to make DTrace the main project language file for this project (SNU / 2D / Programming Tools / IDE / DTrace) as this is a DTrace IDE, and it needs its main language to be represented here.
+*/
+printf("Project language file 1\n");
+printf("For: SNU/2D/ProgrammingTools/IDE/DTrace\n");
+printf("About:\n");
+printf("I decided to make DTrace the main project language file for this project (SNU / 2D / Programming Tools / IDE / DTrace) as this is a DTrace IDE, and it needs its main language to be represented here.\n");
+/* File info
+* File type: DTrace source file (*.d)
+* File version: 1 (2022, Saturday, September 24th at 9:21 pm PST)
+* Line count (including blank lines and compiler line): 18
+*/
+/* End of script */
