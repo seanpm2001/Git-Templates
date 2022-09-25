ab3272237483727b8cac00781d265ee351c861b7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-24 21:09:57 -0700

    PROJECT_LANG_1.hlsl [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-HLSL/HLSL/PROJECT_LANG_1.hlsl
@@ -0,0 +1,20 @@
+// Start of script
+
+// Notes
+// I don't know how to make strings in HLSL, so comments will do for now
+
+// Project language file 1
+// For: seanpm2001/Learn-HLSL
+// About:
+// I decided to make HLSL the main project language file for this project (Seanpm2001/Learn-HLSL) as HLSL is the language this project is dedicated to, because this project is about learning the HLSL programming language. It only makes sense to HLSL the official language for this project. It is getting its own project language file, starting here.
+
+SUMMARY = "Project language file 1 for seanpm2001/Learn-HLSL"
+DESCRIPTION = "I decided to make HLSL the main project language file for this project (Seanpm2001/Learn-HLSL) as HLSL is the language this project is dedicated to, because this project is about learning the HLSL programming language. It only makes sense to HLSL the official language for this project. It is getting its own project language file, starting here."
+HOMEPAGE = "https://github.com/seanpm2001/Learn-HLSL/"
+
+// File info
+// File type: HLSL source file (*.hlsl)
+// File version: 1 (2022, Saturday, September 24th at 9:09 PST)
+// Line count (including blank lines and compiler line): 21
+
+// End of script
