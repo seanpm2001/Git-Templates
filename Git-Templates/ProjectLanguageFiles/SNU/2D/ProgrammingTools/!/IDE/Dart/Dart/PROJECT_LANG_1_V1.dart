c4eb096389dc7a87b03a2764434f840ff3801f4f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-25 16:43:38 -0700

    PROJECT_LANG_1_V1.dart
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Dart/Dart/PROJECT_LANG_1_V1.dart
@@ -0,0 +1,18 @@
+// Start of script
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Dart
+// About:
+// I decided to make Dart the main project language file for this project (SNU / 2D / Programming Tools / IDE / Dart) as this is a Dart IDE, and it needs its main language to be represented here.
+main() {
+  print('Project language file 1');
+  print('For: SNU/2D/ProgrammingTools/IDE/Dart');
+  print('About:')
+  print('I decided to make Dart the main project language file for this project (SNU / 2D / Programming Tools / IDE / Dart) as this is a Dart IDE, and it needs its main language to be represented here.');
+  break;
+}
+return main();
+// File info
+// File type: Dart source file (*.dart)
+// File version: 1 (2022, Sunday, September 25th at 4:42 pm PST)
+// Line count (including blank lines and compiler line): 19
+// End of script
