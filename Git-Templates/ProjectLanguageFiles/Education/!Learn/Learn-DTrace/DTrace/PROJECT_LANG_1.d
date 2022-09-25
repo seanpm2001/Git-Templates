31b6db7d368c526b574c5116e740a8e180b348ca
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-24 21:17:37 -0700

    PROJECT_LANG_1.d [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-DTrace/DTrace/PROJECT_LANG_1.d
@@ -0,0 +1,17 @@
+#!/usr/sbin/dtrace -s
+/* Start of script */
+/* Project language file 1
+* For: seanpnm2001/Learn-DTrace
+* About:
+* I decided to make DTrace the main project language file for this project (Seanpm2001/Learn-DTrace) as DTrace is the language this project is dedicated to, because this project is about learning the DTrace programming language. It only makes sense to DTrace the official language for this project. It is getting its own project language file, starting here.
+*/
+printf("Project language file 1\n");
+printf("For: seanpm2001/Learn-DTrace\n");
+printf("About:\n");
+printf("I decided to make DTrace the main project language file for this project (Seanpm2001/Learn-DTrace) as DTrace is the language this project is dedicated to, because this project is about learning the DTrace programming language. It only makes sense to DTrace the official language for this project. It is getting its own project language file, starting here.\n");
+/* File info
+* File type: DTrace source file (*.d)
+* File version: 1 (2022, Saturday, September 24th at 9:16 pm PST)
+* Line count (including blank lines and compiler line): 18
+*/
+/* End of script */
