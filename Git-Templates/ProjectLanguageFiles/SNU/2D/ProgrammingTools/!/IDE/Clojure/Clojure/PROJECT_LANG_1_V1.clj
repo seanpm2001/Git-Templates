083ffcfa67ee5624f89de6c05a2cbb4fb79dc94a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-25 17:17:14 -0700

    PROJECT_LANG_1_V1.clj
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Clojure/Clojure/PROJECT_LANG_1_V1.clj
@@ -0,0 +1,22 @@
+; Start of script
+
+; Project language file 1
+(println "Project language file 1")
+(println "For: SNU/2D/ProgrammingTools/IDE/Clojure")
+(println "About:")
+(println "I decided to make Clojure the main project language file for this project (SNU / 2D / Programming Tools / IDE / Clojure) as this is a Clojure IDE, and it needs its main language to be represented here.")
+
+; Output:
+
+; Project language file 1
+; For: seanpm2001/Learn-Clojure
+; About:
+; I decided to make Clojure the main project language file for this project (SNU / 2D / Programming Tools / IDE / Clojure) as this is a Clojure IDE, and it needs its main language to be represented here.
+
+; File info
+
+; File type: Clojure source file (*.clj *.edn)
+; File version: 1 (2022, Sunday, September 25th at 5:14 pm PST)
+; Line count (including blank lines and compiler line): 23
+
+; End of script
