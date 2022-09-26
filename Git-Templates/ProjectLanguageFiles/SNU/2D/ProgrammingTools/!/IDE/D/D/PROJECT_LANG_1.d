2efb20d7849f77d23f6d632203e312def66487ed
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-26 14:25:16 -0700

    PROJECT_LANG_1.d [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/D/D/PROJECT_LANG_1.d
@@ -0,0 +1,28 @@
+#!/usr/bin/rdmd
+// Start of script
+import std.stdio : writeln;
+import std.range : iota;
+import std.parallelism : parallel;
+/* DIVIDER */
+void main()
+{
+  writeln("Project language file 1");
+  writeln("For: SNU/2D/ProgrammingTools/IDE/D");
+  writeln("About:");
+  writeln("I decided to make D the main project language file for this project (SNU / 2D / Programming Tools / IDE / D) as this is a D IDE, and it needs its main language to be represented here.
+");
+  break;
+}
+return main();
+return 0;
+break;
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/D
+// About:
+// I decided to make D the main project language file for this project (SNU / 2D / Programming Tools / IDE / D) as this is a D IDE, and it needs its main language to be represented here.
+/* DIVIDER */
+// File info
+// File version: 1 (2022, Monday, September 26th at 2:24 pm PST)
+// File type: D programming language source file (*.d)
+// Line count (including blank lines and compiler line): 29
+// End of script
