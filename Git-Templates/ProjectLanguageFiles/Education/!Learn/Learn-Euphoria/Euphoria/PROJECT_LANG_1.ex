b143d94c11f22a0e4186f9071bbf997b02a6d4ed
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-25 22:37:43 -0700

    PROJECT_LANG_1.ex
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Euphoria/Euphoria/PROJECT_LANG_1.ex
@@ -0,0 +1,14 @@
+-- Start of script
+-- Project language file 1
+-- For: seanpm2001/Learn-Euphoria
+-- About:
+-- I decided to make Euphoria the main project language file for this project (Seanpm2001/Learn-Euphoria) as Euphoria is the language this project is dedicated to, because this project is about learning the Euphoria programming language. It only makes sense to Euphoria the official language for this project. It is getting its own project language file, starting here.
+puts (1, "Project language file 1\n")
+puts (2, "For: seanpm2001/Learn-Euphoria\n")
+puts (3, "About:\n")
+puts (4, "I decided to make Euphoria the main project language file for this project (Seanpm2001/Learn-Euphoria) as Euphoria is the language this project is dedicated to, because this project is about learning the Euphoria programming language. It only makes sense to Euphoria the official language for this project. It is getting its own project language file, starting here.\n")
+-- File info
+-- File type: Euphoria source file (*.e *.ex *.exw)
+-- File version: 1 (2022, Sunday, September 25th at 10:33 pm PST)
+-- Line count (including blank lines and compiler line): 15
+-- End of script
