88984bee6bf8745e860a2fadc0fb3efc6eba13d4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-28 15:16:55 -0700

    PROJECT_LANG_1_V1.ex
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Elixir/Elixir/PROJECT_LANG_1_V1.ex
@@ -0,0 +1,23 @@
+# Start of script
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Elixir
+# About:
+# I decided to make Elixir the main project language file for this project (SNU / 2D / Programming Tools / IDE / Elixir) as this is a Elixir IDE, and it needs its main language to be represented here.
+# Definitions
+def projectLangOne_IEX {
+  iex> IO.puts("Project language file 1\nFor: SNU/2D/ProgrammingTools/IDE/Elixir\nAbout:\nI decided to make Elixir the main project language file for this project (SNU / 2D / Programming Tools / IDE / Elixir) as this is a Elixir IDE, and it needs its main language to be represented here.")
+}
+def projectLangOne_Standard {
+  print ("Project language file 1\nFor: SNU/2D/ProgrammingTools/IDE/Elxiir\nAbout:\nI decided to make Elixir the main project language file for this project (SNU / 2D / Programming Tools / IDE / Elixir) as this is a Elixir IDE, and it needs its main language to be represented here.")
+}
+# Output
+try projectLangOne_Standard()
+else: {
+  try projectLangOne_IEX() # Not all systems may support the standard, so IEX is used as a backup.
+}
+# I don't know how to write in Elixir, so this is a demo script and likely doesn't work.
+# File info
+# File type: Elixir source file (*.ex *.exs)
+# File version: 1 (2022, Wednesday, September 28th at 3:14 pm PST)
+# Line count (including blank lines and compiler line): 24
+# End of script
