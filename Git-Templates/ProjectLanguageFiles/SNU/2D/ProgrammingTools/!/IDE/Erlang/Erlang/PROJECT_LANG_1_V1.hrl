02abc0174221b49c03eb277461f3a33beed8d30a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-28 16:05:10 -0700

    PROJECT_LANG_1_V1.hrl
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Erlang/Erlang/PROJECT_LANG_1_V1.hrl
@@ -0,0 +1,14 @@
+%% Start of script
+%% Project language file 1
+%% For: SNU/2D/ProgrammingTools/IDE/Erlang
+%% About:
+%% I decided to make Erlang the main project language file for this project (SNU / 2D / Programming Tools / IDE / Erlang) as this is a Erlang IDE, and it needs its main language to be represented here.
+io:format("Project language file 1~n").
+io:format("For: SNU/2D/ProgrammingTools/IDE/Erlang~n").
+io:format("About:~n").
+io:format("I decided to make Erlang the main project language file for this project (SNU / 2D / Programming Tools / IDE / Erlang) as this is a Erlang IDE, and it needs its main language to be represented here.~n").
+%% File info
+%% File type: Erlang source file (*.erl *.hrl)
+%% File version: 1 (2022, Wednesday, September 28th at 4:01 pm PST)
+%% Line count (including blank lines and compiler line): 15
+%% End of script
