d4aa3c5af5f4bbd739fc08b6250c87153021f759
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-28 21:49:43 -0700

    PROJECT_LANG_1_V1.thrift
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Thrift/Thrift/PROJECT_LANG_1_V1.thrift
@@ -0,0 +1,18 @@
+// Start of script
+
+// Notes
+// I don't know how to make strings very well in Thrift, so comments will do for now
+
+// Project language file 1
+// For: seanpm2001/Learn-Thrift
+// About:
+// I decided to make Thrift the main project language file for this project (SNU / 2D / Programming Tools / IDE / Thrift) as this is a Thrift IDE, and it needs its main language to be represented here.
+
+/* string message(1:string) */
+
+// File info
+// File type: aApache Thrift source file (*.thrift)
+// File version: 1 (2022, Wednesday, Septemnber 28th at 9:48 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
