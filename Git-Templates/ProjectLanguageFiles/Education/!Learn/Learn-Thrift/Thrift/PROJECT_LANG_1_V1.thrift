74f8dcbae97891235b618681de6095eeaa626343
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-28 21:45:48 -0700

    PROJECT_LANG_1_V1.thrift
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Thrift/Thrift/PROJECT_LANG_1_V1.thrift
@@ -0,0 +1,18 @@
+// Start of script
+
+// Notes
+// I don't know how to make strings very well in Thrift, so comments will do for now
+
+// Project language file 1
+// For: seanpm2001/Learn-Thrift
+// About:
+// I decided to make Thrift the main project language file for this project (Seanpm2001/Learn-Thrift) as Thrift is the language this project is dedicated to, because this project is about learning the Thrift programming language. It only makes sense to Thrift the official language for this project. It is getting its own project language file, starting here.
+
+/* string message(1:string) */
+
+// File info
+// File type: aApache Thrift source file (*.thrift)
+// File version: 1 (2022, Wednesday, Septemnber 28th at 9:44 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
