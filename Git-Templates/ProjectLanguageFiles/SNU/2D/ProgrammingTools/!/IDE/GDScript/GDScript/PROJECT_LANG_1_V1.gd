f7f39652e2d6c14a33df28ed1cedda58b9f91156
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-29 16:15:34 -0700

    PROJECT_LANG_1_V1.gd
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/GDScript/GDScript/PROJECT_LANG_1_V1.gd
@@ -0,0 +1,23 @@
+# Start of script
+
+# Project language file output
+func _ready():
+	print("Project language file 1\n")
+	print("For: SNU/2D/ProgrammingTools/IDE/GDScript\n")
+	print("About:\n")
+	print("I decided to make GDScript (GoDOt Script) the main project language file for this project (SNU / 2D / Programming Tools / IDE / GDScript) as this is a GDScript IDE, and it needs its main language to be represented here.\n")
+	# Output
+	# Project language file 1
+	# For: SNU/2D/ProgrammingTools/IDE/GDScript
+	# About:
+	# I decided to make GDScript (GoDOt Script) the main project language file for this project (SNU / 2D / Programming Tools / IDE / GDScript) as this is a GDScript IDE, and it needs its main language to be represented here.
+	break
+return _ready()
+break
+
+# File info
+# File type: GDscript (GoDot Script) source file (*.gd)
+# File version: 1 (2022, Thursday, September 29th at 4:14 pm PST)
+# Line count (including blank lines and compiler line): 24
+
+# End of script
