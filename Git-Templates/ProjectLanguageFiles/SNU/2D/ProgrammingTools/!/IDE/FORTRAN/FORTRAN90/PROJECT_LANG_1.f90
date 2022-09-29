88327b3ea59b367b10d8478bdf5ecd264a8630e6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-29 16:05:21 -0700

    PROJECT_LANG_1.f90
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/FORTRAN/FORTRAN90/PROJECT_LANG_1.f90
@@ -0,0 +1,24 @@
+C Start of script
+
+C I decided to make Fortran the main project language file for this project (SNU / 2D / Programming Tools / IDE / Fortran) as this is a Fortran IDE, and it needs its main language to be represented here.
+
+program prolangone
+     print *, "Project language file 1\n"
+     print *, "For: SNU/2D/ProgrammingTools/IDE/Fortran\n"
+     print *, "About:\n"
+     print *, "I decided to make Fortran the main project language file for this project (SNU / 2D / Programming Tools / IDE / Fortran) as this is a Fortran IDE, and it needs its main language to be represented here.\n"
+C output:
+C Project language file 1
+C For: SNU/2D/ProgrammingTools/IDE/Fortran
+C About:
+C I decided to make Fortran the main project language file for this project (SNU / 2D / Programming Tools / IDE / Fortran) as this is a Fortran IDE, and it needs its main language to be represented here.
+end program prolangone
+STOP
+END
+
+C File info
+C File type: FORTRAN 90 source file (*.f90, *.f, *.for) | Fortran source file (*.f *.f77 *.f90 *.f95 *.for other)
+C File version: 1 (2022, Thursday, September 29th at 3:58 pm PST)
+C Line count (including blank lines and compiler line): 25
+
+C End of script
