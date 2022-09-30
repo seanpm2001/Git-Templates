17495280135c151a373570b179916908473b09bd
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-09-30 16:39:33 -0700

    PROJECT_LANG_1_V1.hs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Haskell/Haskell/PROJECT_LANG_1_V1.hs
@@ -0,0 +1,20 @@
+-- Start of script
+
+module Main (main) where          -- not needed in interpreter, is the default in a module file
+
+main :: IO ()                     -- the compiler can infer this type definition
+main = putStrLn "Project language file 1\nFor: SNU/2D/ProgrammingTools/IDE/Haskell\nAbout:\nI decided to make Haskell the main project language file for this project (SNU / 2D / Programming Tools / IDE / Haskell) as this is a Haskell IDE, and it needs its main language to be represented here."
+
+-- Output
+
+-- Project language file 1
+-- For: SNU/2D/ProgrammingTools/IDE/Haskell
+-- About:
+-- I decided to make Haskell the main project language file for this project (SNU / 2D / Programming Tools / IDE / Haskell) as this is a Haskell IDE, and it needs its main language to be represented here.
+
+-- File info
+-- File version: 1 (2022, Friday, September 30th at 4:37 pm PST)
+-- File type: Haskell script file (*.hs)
+-- Line count (including blank lines and compiler line): 21
+
+-- End of script
