0b700780a0d78a91a82247ab3a520f8c1a56fa50
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-03 22:42:42 -0700

    PROJECT_LANG_1.pp [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Puppet/Puppet/PROJECT_LANG_1.pp
@@ -0,0 +1,19 @@
+# Start of script
+
+# Notes
+# I don't know how to make strings in Puppet, so comments will do for now
+
+# Project language file 1
+# For: seanpm2001/Learn-Puppet
+# About:
+# I decided to make Puppet the main project language file for this project (Seanpm2001/Learn-Puppet) as Puppet is the language this project is dedicated to, because this project is about learning the Puppet programming language. It only makes sense to Puppet the official language for this project. It is getting its own project language file, starting here.
+
+attribute => title("Project language file 1 for seanpm2001/Puppet")
+attribute => description("I decided to make Puppet the main project language file for this project (Seanpm2001/Learn-Puppet) as Puppet is the language this project is dedicated to, because this project is about learning the Puppet programming language. It only makes sense to Puppet the official language for this project. It is getting its own project language file, starting here.")
+
+# File info
+# File type: Puppet source file (*.pp)
+# File version: 1 (2022, Monday, October 3rd at 10:41 pm PST)
+# Line count (including blank lines and compiler line): 20
+
+# End of script
