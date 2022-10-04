5b1a2f14162574adfce4cf8f5dd0d17b7a38ef60
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-03 19:39:51 -0700

    PROJECT_LANG_1_V1.jl
    An archive of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Julia/Julia/PROJECT_LANG_1_V1.jl
@@ -0,0 +1,17 @@
+# Start of script
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Julia
+# About:
+# I decided to make Julia the main project language file for this project (SNU / 2D / Programming Tools / IDE / Julia) as this is a Julia IDE, and it needs its main language to be represented here.
+
+"Project language file 1"
+"For: SNU/2D/ProgrammingTools/IDE/Julia"
+"About:"
+"I decided to make Julia the main project language file for this project (SNU / 2D / Programming Tools / IDE / Julia) as this is a Julia IDE, and it needs its main language to be represented here."
+break
+
+# File info
+# File version: 1 (2022, Monday, October 3rd at 7:38 pm PST)
+# File type: Julia source file (*.jl)
+# Line count (including blank lines and compiler line): 18
+# End of script
