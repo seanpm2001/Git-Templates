bd55561c980d563685028a7faaa4d7e23e757179
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-03 22:47:22 -0700

    PROJECT_LANG_1.pp [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Puppet/Puppet/PROJECT_LANG_1.pp
@@ -0,0 +1,19 @@
+# Start of script
+
+# Notes
+# I don't know how to make strings in Puppet, so comments will do for now
+
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Puppet
+# About:
+# I decided to make Puppet the main project language file for this project (SNU / 2D / Programming Tools / IDE / Puppet) as this is a Puppet IDE, and it needs its main language to be represented here.
+
+attribute => title("Project language file 1 for SNU/2D/ProgrammingTools/IDE/Puppet")
+attribute => description("I decided to make Puppet the main project language file for this project (SNU / 2D / Programming Tools / IDE / Puppet) as this is a Puppet IDE, and it needs its main language to be represented here..")
+
+# File info
+# File type: Puppet source file (*.pp)
+# File version: 1 (2022, Monday, October 3rd at 10:46 pm PST)
+# Line count (including blank lines and compiler line): 20
+
+# End of script
