a066df9e554969f404c492fd782328110328a0cd
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-05 15:09:04 -0700

    PROJECT_LANG_1.lua [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Lua/Lua/PROJECT_LANG_1.lua
@@ -0,0 +1,22 @@
+-- Start of script
+-- Project language file 1 for seanpm2001/SNU_2D_ProgrammingTools_IDE_Lua
+-- Written in Lua 5.4
+
+local projectLanguageFile1
+print "Project language file 1"
+print "For: SNU/2D/ProgrammingTools/IDE/Lua"
+print "About:"
+print "I decided to make Lua the main project language file for this project (SNU / 2D / Programming Tools / IDE / Lua) as this is a Lua IDE, and it needs its main language to be represented here."
+break
+
+-- Output:
+-- Project language file 1
+-- For: SNU/2D/ProgrammingTools/IDE/Lua
+-- About:
+-- I decided to make Lua the main project language file for this project (SNU / 2D / Programming Tools / IDE / Lua) as this is a Lua IDE, and it needs its main language to be represented here.
+
+-- File info:
+-- File type: Lua source file (*.lua)
+-- File version: 1 (2022, Wednesday, October 5th at 3:08 pm PST)
+-- Line count (including blank lines and compiler line): 23
+-- End of script
