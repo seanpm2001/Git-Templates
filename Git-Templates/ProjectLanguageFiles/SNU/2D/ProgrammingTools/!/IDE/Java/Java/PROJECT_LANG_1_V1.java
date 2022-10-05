6e679623bf729222b87d575895ca41d64b470d81
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-04 17:40:19 -0700

    PROJECT_LANG_1_V1.java
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Java/Java/PROJECT_LANG_1_V1.java
@@ -0,0 +1,29 @@
+// Start of script
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Java
+// About:
+// I decided to make Java the main project language file for this project (SNU / 2D / Programming Tools / IDE / Java) as this is a Java IDE, and it needs its main language to be represented here.
+// Import section
+import java.util.*
+// Main function
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: SNU/2D/ProrgrammingTools/IDE/Java");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the main project language file for this project (SNU / 2D / Programming Tools / IDE / Java) as this is a Java IDE, and it needs its main language to be represented here.");
+		break;
+}
+// Main section
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break;
+} // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Tuesday, October 4th at 5:39 pm PST)
+* File type: Java 8 Source file (*.java)
+* Line count (including blank lines and compiler line): 30
+*/
+// End of script
