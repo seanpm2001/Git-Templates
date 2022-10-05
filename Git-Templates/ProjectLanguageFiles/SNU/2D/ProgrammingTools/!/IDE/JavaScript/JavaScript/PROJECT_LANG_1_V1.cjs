1903e9064089856d9f3a0fcdf0dfc3bf9de8f34a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-04 17:36:38 -0700

    PROJECT_LANG_1_V1.cjs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/JavaScript/JavaScript/PROJECT_LANG_1_V1.cjs
@@ -0,0 +1,32 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/JavaScript
+// About:
+// I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / JavaScript) as this is a JavaScript IDE, and it needs its main language to be represented here.
+
+void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 1");
+    alert("Project language file 1");
+    console.log ("For: SNU/2D/ProgrammingTools/IDE/JavaScript");
+    alert("For: SNU/2D/ProgrammingTools/IDE/JavaScript");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / JavaScript) as this is a JavaScript IDE, and it needs its main language to be represented here.");
+    alert("I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / JavaScript) as this is a JavaScript IDE, and it needs its main language to be represented here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+
+return main();
+break;
+
+}
+
+/* File info
+*| File version: 1 (2022, Tuesday, October 4th at 5:30 pm PST)
+*| File type: JavaScript 1.8 source file (*.js *.cjs *.mjs)
+*| Line count (including blank lines and compiler line): 33
+*/
+
+// End of script
