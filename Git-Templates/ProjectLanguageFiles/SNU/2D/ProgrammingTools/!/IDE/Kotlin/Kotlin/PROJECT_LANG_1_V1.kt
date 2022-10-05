06a1f8a27d7585af59e5c0cf4cd8841e05d463d4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-05 15:34:44 -0700

    PROJECT_LANG_1_V1.kt
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Kotlin/Kotlin/PROJECT_LANG_1_V1.kt
@@ -0,0 +1,23 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Kotlin
+// About:
+// I decided to make Kotlin the main project language file for this project (SNU / 2D / Programming Tools / IDE / Kotlin) as this is a Kotlin IDE, and it needs its main language to be represented here.
+fun main() {
+    val scope = "Project Language File 1\nFor: SNU/2D/ProgrammingTools/IDE/Kotlin\nAbout:\n\n"
+    println("I decided to make Kotlin the main project language file for this project (SNU / 2D / Programming Tools / IDE / Kotlin) as this is a Kotlin IDE, and it needs its main language to be represented here." \n, $scope")
+}
+fun main(args: Array<String>) {
+    for (arg in args) {
+        println(arg)
+    }
+}
+return main();
+break;
+/* File info
+* File version: 1 (2022, Wednesday, October 5th at 3:30 pm PST)
+* File type: Kotlin Source file (*.kt, *.kts, *.ktm)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
