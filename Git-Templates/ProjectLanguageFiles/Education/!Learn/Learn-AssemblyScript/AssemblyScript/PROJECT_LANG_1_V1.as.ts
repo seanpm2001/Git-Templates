5be610c0d930faab1921f33bbb677bb72efc50cb
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-05 18:34:43 -0700

    PROJECT_LANG_1_V1.as.ts
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-AssemblyScript/AssemblyScript/PROJECT_LANG_1_V1.as.ts
@@ -0,0 +1,17 @@
+// Start of script
+
+// Notes
+// I don't know how to make strings in AssemblyScript, so comments will do for now
+// Also, I don't want to mess with the TypeScript-AssemblyScript cross compatibility at the moment, so coments will be the only part of this file until I can find good AssemblyScript documentation.
+
+// Project language file 1
+// For: seanpm2001/Learn-AssemblyScript
+// About:
+// I decided to make AssemblyScript the main project language file for this project (Seanpm2001/Learn-AssemblyScript) as AssemblyScript is the language this project is dedicated to, because this project is about learning the AssemblyScript programming language. It only makes sense to AssemblyScript the official language for this project. It is getting its own project language file, starting here.
+
+// File info
+// File type: AssemblyScript source file (*.as *.ts) (I am just guessing, there isn't very much good documentation on this language yet)
+// File version: 1 (2022, Wednesday, October 5th at 6:33 pm PST)
+// Line count (including blank lines and compiler line): 18
+
+// End of script
