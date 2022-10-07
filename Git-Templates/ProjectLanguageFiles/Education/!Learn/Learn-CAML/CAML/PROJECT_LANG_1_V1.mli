504b157d2fd6d59e52d287cc5d9afc80a7ad91bd
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-06 19:31:33 -0700

    PROJECT_LANG_1_V1.mli
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-CAML/CAML/PROJECT_LANG_1_V1.mli
@@ -0,0 +1,18 @@
+# Start of script
+
+# Project language file 1
+# For: seanpm2001/Learn-CAML
+# About:
+# I decided to make CAML the main project language file for this project (Seanpm2001/Learn-CAML) as CAML is the language this project is dedicated to, because this project is about learning the CAML programming language. It only makes sense to CAML the official language for this project. It is getting its own project language file, starting here.
+
+print_endline "Project language file 1";;
+print_endline "For seanpm2001/Learn-CAML";;
+print_endline "About:";;
+print_endline "I decided to make CAML the main project language file for this project (Seanpm2001/Learn-CAML) as CAML is the language this project is dedicated to, because this project is about learning the CAML programming language. It only makes sense to CAML the official language for this project. It is getting its own project language file, starting here.";;
+
+# File info
+# File type: CAML source file (*.mli)
+# File version: 1 (2022, Thursday, October 6th at 7:30 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
