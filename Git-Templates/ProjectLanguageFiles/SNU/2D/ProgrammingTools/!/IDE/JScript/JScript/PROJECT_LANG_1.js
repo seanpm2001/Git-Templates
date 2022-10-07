d8b727f04278ec4fc12c5e75f15f2bfc84afdb92
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-06 19:45:21 -0700

    PROJECT_LANG_1.js [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/JScript/JScript/PROJECT_LANG_1.js
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/JScript
+// About:
+// I decided to make JScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / JScript) as this is a JScript IDE, and it needs its main language to be represented here.
+
+console.log ("Project language file 1");
+console.log ("For: SNU/2D/ProgrammingTools/IDE/JScript");
+console.log ("About:");
+console.log ("I decided to make JScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / JScript) as this is a JScript IDE, and it needs its main language to be represented here.");
+
+/* File info
+* File version: 1 (2022, Thursday, October 6th at 7:44 pm PST)
+* File type: JScript source file (*.js)
+* Line count (including blank lines and compiler line): 19
+*/
+// End of script
