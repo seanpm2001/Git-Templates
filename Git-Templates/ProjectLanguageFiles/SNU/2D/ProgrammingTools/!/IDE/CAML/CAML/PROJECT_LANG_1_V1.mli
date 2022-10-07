3b25b42bf4b98dfd4753c31887ea089daadea6e2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-06 19:36:52 -0700

    PROJECT_LANG_1_V1.mli
    An archive of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/CAML/CAML/PROJECT_LANG_1_V1.mli
@@ -0,0 +1,18 @@
+# Start of script
+
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/CAML
+# About:
+# I decided to make CAML the main project language file for this project (SNU / 2D / Programming Tools / IDE / CAML) as this is a CAML IDE, and it needs its main language to be represented here.
+
+print_endline "Project language file 1";;
+print_endline "For SNU/2D/ProgrammingTools/IDE/CAML";;
+print_endline "About:";;
+print_endline "I decided to make CAML the main project language file for this project (SNU / 2D / Programming Tools / IDE / CAML) as this is a CAML IDE, and it needs its main language to be represented here.";;
+
+# File info
+# File type: CAML source file (*.mli)
+# File version: 1 (2022, Thursday, October 6th at 7:35 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
