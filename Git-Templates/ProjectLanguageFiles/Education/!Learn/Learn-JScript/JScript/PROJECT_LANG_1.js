3ed183ed0ea87e86d38996d20a18276daa89f952
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-06 19:41:10 -0700

    PROJECT_LANG_1.js [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-JScript/JScript/PROJECT_LANG_1.js
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: seanpm2001/Learn-JScript
+// About:
+// I chose JScript as the first project language file for this project (Seanpm2001/Learn-JScript) as this project is about learning the JScript programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.
+
+console.log ("Project language file 1");
+console.log ("For: seanpm2001/Learn-JScript");
+console.log ("About:");
+console.log ("I chose JScript as the first project language file for this project (Seanpm2001/Learn-JScript) as this project is about learning the JScript programming language, and showing my knowledge for the language. Its project language file should represent what language is being showcased and studied here.");
+
+/* File info
+* File version: 1 (2022, Thursday, October 6th at 7:40 pm PST)
+* File type: JScript source file (*.js)
+* Line count (including blank lines and compiler line): 19
+*/
+// End of script
