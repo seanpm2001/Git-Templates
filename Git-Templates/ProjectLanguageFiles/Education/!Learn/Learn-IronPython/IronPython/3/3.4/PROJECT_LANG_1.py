5832328a62da1a0d38b6e9842739722eda8cdae2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-07 21:20:12 -0700

    PROJECT_LANG_1.py [V1]
    Version 1 of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-IronPython/IronPython/3/3.4/PROJECT_LANG_1.py
@@ -0,0 +1,19 @@
+# Start of script
+import clr
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import MessageBox, MessageBoxButtons
+
+MessageBox.Show("I decided to make IronPython the main project language file for this project (Seanpm2001/Learn-IronPython) as IronPython is the language this project is dedicated to, because this project is about learning the IronPython programming language. It only makes sense to IronPython the official language for this project. It is getting its own project language file, starting here.", "Project language file 1 - For: seanpm2001/Learn-IronPython", MessageBoxButtons.OKCancel)
+
+# Output
+
+# Project language file 1
+# For: seanpm2001/Learn-IronPython
+# I decided to make IronPython the main project language file for this project (Seanpm2001/Learn-IronPython) as IronPython is the language this project is dedicated to, because this project is about learning the IronPython programming language. It only makes sense to IronPython the official language for this project. It is getting its own project language file, starting here.
+
+# File info
+# File type: IronPython 3 source file (*.py)
+# File version: 1 (2022, Friday, October 7th at 9:19 pm PST)
+# Line count (including blank lines and compiler line): 20
+
+# End of script
