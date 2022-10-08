f1d6a91f0fc1d9f3a763321f54061ab306d6c466
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-08 15:32:02 -0700

    PROJECT_LANG_1.mk [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Makefile/Makefile/PROJECT_LANG_1.mk
@@ -0,0 +1,18 @@
+# Start of script
+
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Makefile
+# About:
+# I decided to make GNU Make (Makefile) the main project language file for this project (SNU / 2D / Programming Tools / IDE / Makefile) as this is a Makefile IDE, and it needs its main language to be represented here.
+
+std::cout << "Project language file 1"
+std::cout << "For: SNU/2D/ProgrammingTools/IDE/Makefile"
+std::cout << "About:"
+std::cout << "I decided to make GNU Make (Makefile) the main project language file for this project (SNU / 2D / Programming Tools / IDE / Makefile) as this is a Makefile IDE, and it needs its main language to be represented here."
+
+# File info
+# File type: GNU Make (Makefile) source file (*.mk *.mak *.make *Makefile)
+# File version: 1 (2022, Saturday, October 8th at 3:27 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
