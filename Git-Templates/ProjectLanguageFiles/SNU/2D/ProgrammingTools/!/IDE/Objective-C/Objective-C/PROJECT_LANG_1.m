c67b5c85e7bc684230a12442627c0d311fa124d1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-08 16:58:15 -0700

    PROJECT_LANG_1.m [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Objective-C/Objective-C/PROJECT_LANG_1.m
@@ -0,0 +1,22 @@
+@interface classname : projectLanguageFileOne {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @MacOS_10_1_ObjectiveC classname : superclassname {
+    // # I decided to make Objective-C the main project language file for this project (SNU / 2D / Programming Tools / IDE / Objective-C) as this is a Objective-C IDE, and it needs its main language to be represented here.
+	NSLog(@"I decided to make Objective-C the main project language file for this project (SNU / 2D / Programming Tools / IDE / Objective-C) as this is a Objective-C IDE, and it needs its main language to be represented here.");
+	break;
+}
+@end
+/* File info
+* File version: 2022, Saturday, October 8th at 4:57 pm PST
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 23
+* This script is incomplete, and needs lots of work */
+// End of script
