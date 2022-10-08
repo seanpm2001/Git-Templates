b9199073c9264b22e5fbdc3e06fddbc4f4341370
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-07 21:24:16 -0700

    PROJECT_LANG_1.py [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/IronPython/IronPython/3/3.4/PROJECT_LANG_1.py
@@ -0,0 +1,19 @@
+# Start of script
+import clr
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import MessageBox, MessageBoxButtons
+
+MessageBox.Show("I decided to make IronPython the main project language file for this project (SNU / 2D / Programming Tools / IDE / IronPython) as this is a IronPython IDE, and it needs its main language to be represented here.", "Project language file 1 - For: SNU/2D/ProgrammingTools/IDE/IronPython", MessageBoxButtons.OKCancel)
+
+# Output
+
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/IronPython
+# # I decided to make IronPython the main project language file for this project (SNU / 2D / Programming Tools / IDE / IronPython) as this is a IronPython IDE, and it needs its main language to be represented here.
+
+# File info
+# File type: IronPython 3 source file (*.py)
+# File version: 1 (2022, Friday, October 7th at 9:23 pm PST)
+# Line count (including blank lines and compiler line): 20
+
+# End of script
