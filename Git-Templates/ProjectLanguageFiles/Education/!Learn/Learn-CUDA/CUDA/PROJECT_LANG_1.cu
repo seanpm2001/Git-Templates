161ceb20b3fa127607c16936166cf4f2beec3b93
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-08 23:43:51 -0700

    PROJECT_LANG_1.cu [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-CUDA/CUDA/PROJECT_LANG_1.cu
@@ -0,0 +1,20 @@
+// Start of script
+
+/* Project language file 1
+* For: seanpm2001/Learn-CUDA
+* About:
+* I decided to make CUDA the main project language file for this project (Seanpm2001/Learn-CUDA) as CUDA is the language this project is dedicated to, because this project is about learning the CUDA programming language. It only makes sense to CUDA the official language for this project. It is getting its own project language file, starting here.
+*/
+
+printf("Project language file 1");
+printf("For: seanpm2001/Learn-CUDA");
+printf("About:");
+printf("I decided to make CUDA the main project language file for this project (Seanpm2001/Learn-CUDA) as CUDA is the language this project is dedicated to, because this project is about learning the CUDA programming language. It only makes sense to CUDA the official language for this project. It is getting its own project language file, starting here.");
+
+/* File info
+* File type: CUDA source file (*.cu *.cuda)
+* File version: 1 (2022, Saturday, October 8th at 11:42 pm PST)
+* Line count (including blank lines and compiler line): 21
+*/
+
+// End of script
