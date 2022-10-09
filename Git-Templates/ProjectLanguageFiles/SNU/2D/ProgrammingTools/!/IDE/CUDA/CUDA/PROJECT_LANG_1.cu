d56ecdbab10d9332c714ebb1bee2b91a9df4f1c2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-08 23:47:36 -0700

    PROJECT_LANG_1.cu [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/CUDA/CUDA/PROJECT_LANG_1.cu
@@ -0,0 +1,20 @@
+// Start of script
+
+/* Project language file 1
+* For: SNU/2D/ProgrammingTools/IDE/CUDA
+* About:
+* I decided to make CUDA the main project language file for this project (SNU / 2D / Programming Tools / IDE / CUDA) as this is a CUDA IDE, and it needs its main language to be represented here.
+*/
+
+printf("Project language file 1");
+printf("For: SNU/2D/ProgrammingTools/IDE/CUDA");
+printf("About:");
+printf("I decided to make CUDA the main project language file for this project (SNU / 2D / Programming Tools / IDE / CUDA) as this is a CUDA IDE, and it needs its main language to be represented here.");
+
+/* File info
+* File type: CUDA source file (*.cu *.cuda)
+* File version: 1 (2022, Saturday, October 8th at 11:46 pm PST)
+* Line count (including blank lines and compiler line): 21
+*/
+
+// End of script
