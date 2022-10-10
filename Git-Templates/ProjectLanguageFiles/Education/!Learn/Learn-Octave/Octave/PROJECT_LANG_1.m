c9bcaf4cac23acc69d45d22fcb90798f115fd665
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-09 20:50:57 -0700

    PROJECT_LANG_1.m [V1]
    Version 1 of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Octave/Octave/PROJECT_LANG_1.m
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: seanpm2001/Learn-Octave
+// About:
+// I decided to make Octave the main project language file for this project (Seanpm2001/Learn-Octave) as Octave is the language this project is dedicated to, because this project is about learning the Octave programming language. It only makes sense to Octave the official language for this project. It is getting its own project language file, starting here.
+
+disp('Project language file 1');
+disp('For: seanpm2001/Learn-Octave');
+disp('About:');
+disp('I decided to make Octave the main project language file for this project (Seanpm2001/Learn-Octave) as Octave is the language this project is dedicated to, because this project is about learning the Octave programming language. It only makes sense to Octave the official language for this project. It is getting its own project language file, starting here.');
+
+// File info
+// File type: Octave source file (*.m)
+// File version: 1 (2022, Sunday, October 9th at 8:49 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
