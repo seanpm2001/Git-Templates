fbffe0f290f6e8a0bcbfb58cd5834e9643aa912a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-09 20:57:14 -0700

    PROJECT_LANG_1_V1.m
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Octave/Octave/PROJECT_LANG_1_V1.m
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Octave
+// About:
+// I decided to make Octave the main project language file for this project (SNU / 2D / Programming Tools / IDE / Octave) as this is a Octave IDE, and it needs its main language to be represented here.
+
+disp('Project language file 1');
+disp('For: SNU/2D/ProgrammingTools/IDE/Octave');
+disp('About:');
+disp('I decided to make Octave the main project language file for this project (SNU / 2D / Programming Tools / IDE / Octave) as this is a Octave IDE, and it needs its main language to be represented here.');
+
+// File info
+// File type: Octave source file (*.m)
+// File version: 1 (2022, Sunday, October 9th at 8:55 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
