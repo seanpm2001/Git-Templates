2b1fb92f04d48f845cf1d2e50a0339fa09405bd6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-10 15:59:34 -0700

    PROJECT_LANG_1_V1.pl
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Perl/Perl/PROJECT_LANG_1_V1.pl
@@ -0,0 +1,22 @@
+# Start of script
+
+print "Project language file 1";
+print "For: SNU/2D/ProgrammingTools/IDE/Perl";
+print "About:"
+print "I decided to make Perl the main project language file for this project (SNU / 2D / Programming Tools / IDE / Perl) as this is a Perl IDE, and it needs its main language to be represented here.";
+break;
+
+# Output
+
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Perl
+# About:
+# I decided to make Perl the main project language file for this project (SNU / 2D / Programming Tools / IDE / Perl) as this is a Perl IDE, and it needs its main language to be represented here.
+
+# File info
+
+# File type: Perl source file (*.pl)
+# File version: 1 (2022, Monday, October 10th at 3:58 pm PST)
+# Line count (including blank lines and compiler line): 23
+
+# End of script
