8cdc0d8e32a8768905f92788d3a01bbfdc6fb069
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-09 19:44:15 -0700

    PROJECT_LANG_1.mm [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Objective-C++/Objective-C++/PROJECT_LANG_1.mm
@@ -0,0 +1,41 @@
+#!/bin/objective-cpp -x
+// Start of script
+#import </CoreFoundation/CFAttributedString.h>
+#import </Foundation/NSAttributedString.h>
+#import </Foundation/NSDictionary.h>
+#import </Foundation/NSMutableAttributedString.h>
+#import </UIKit/UIKit.h>
+using System.DotNet;
+using System.ObjectiveCSharp;
+
+@namespace objectivecsharp-projectLanguage {
+
+void projectLanguageFileOne(); {
+	@interface classMain : superclassMain {
+  	// instance variables
+	}
++ classMethod1;
++ (return_type)classMethod2;
++ (return_type)classMethod3:(param1_type)param1_varName;
+
+- (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+- (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+                              param2_callName:(param2_type)param2_varName;
+        // Project language file 1
+        // For: SNU/2D/ProgrammingTools/IDE/Objective-C++
+        // About:
+        // I decided to make Objective-C++ the main project language file for this project (SNU / 2D / Programming Tools / IDE / Objective-C++) as this is a Objective-C++ IDE, and it needs its main language to be represented here.'
+    	NSLog.println("Project language file 1");
+    	NSLog.println("For: SNU/2D/ProgrammingTools/IDE/Objective-C++");
+    	NSLog.println("About:");
+    	NSLog.println("I decided to make Objective-C++ the main project language file for this project (SNU / 2D / Programming Tools / IDE / Objective-C++) as this is a Objective-C++ IDE, and it needs its main language to be represented here.");
+    	break;
+    	@end
+	}
+}
+/* File info
+* File type: Objective-C++ source file (*.mm)
+* File version: 1 (2022, Sunday, October 9th at 7:43 pm PST)
+* Line count (including blank lines and compiler line): 42
+*/
+// End of script
