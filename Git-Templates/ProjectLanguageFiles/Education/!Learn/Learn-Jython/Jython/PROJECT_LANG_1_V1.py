594ec552dd5cb757d307d706154002bd4ee81cb4
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-10 20:27:08 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Jython/Jython/PROJECT_LANG_1_V1.py
@@ -0,0 +1,18 @@
+# Start of script
+
+# Project language file 1
+# For: seanpm2001/Learn-Jython
+# About:
+# I decided to make Jython the main project language file for this project (Seanpm2001/Learn-Jython) as Jython is the language this project is dedicated to, because this project is about learning the Jython programming language. It only makes sense to Jython the official language for this project. It is getting its own project language file, starting here.
+
+print "Project language file 1\n"
+print "For: seanpm2001/Learn-Jython\n"
+print "About:\n"
+print "I decided to make Jython the main project language file for this project (Seanpm2001/Learn-Jython) as Jython is the language this project is dedicated to, because this project is about learning the Jython programming language. It only makes sense to Jython the official language for this project. It is getting its own project language file, starting here.\n"
+
+# File info
+# File type: Jython source file (*.py)
+# File version: 1 (2022, Monday, October 10th at 8:25 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
