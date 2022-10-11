ba9f38add101cb677ad294ae7f632bfa6522196a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-10 20:59:43 -0700

    PROJECT_LANG_1.js [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-JScript.NET/JScript.NET/PROJECT_LANG_1.js
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: seanpm2001/Learn-JScript.NET
+// About:
+// I decided to make JScript.NET the main project language file for this project (Seanpm2001/Learn-JScript.NET) as JScript.NET is the language this project is dedicated to, because this project is about learning the JScript.NET programming language. It only makes sense to JScript.NET the official language for this project. It is getting its own project language file, starting here.
+
+Console.WriteLine("Project language file 1");
+Console.WriteLine("For: seanpm2001/Learn-JScript.NET");
+Console.WriteLine("About:");
+Console.WriteLine("I decided to make JScript.NET the main project language file for this project (Seanpm2001/Learn-JScript.NET) as JScript.NET is the language this project is dedicated to, because this project is about learning the JScript.NET programming language. It only makes sense to JScript.NET the official language for this project. It is getting its own project language file, starting here.");
+
+// File info
+// File type: JScript.NET source file (*.js)
+// File version: 1 (2022, Monday, October 10th at 8:58 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
