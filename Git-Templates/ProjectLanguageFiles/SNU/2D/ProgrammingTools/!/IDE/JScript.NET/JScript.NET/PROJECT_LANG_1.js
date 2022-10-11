3401ceca14edb70d2d5bc5e747a9c0c34ad908f5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-10 21:05:49 -0700

    PROJECT_LANG_1.js
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/JScript.NET/JScript.NET/PROJECT_LANG_1.js
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Learn-JScript.NET
+// About:
+// I decided to make JScript.NET the main project language file for this project (SNU / 2D / Programming Tools / IDE / JScript.NET) as this is a JScript.NET IDE, and it needs its main language to be represented here.
+
+Console.WriteLine("Project language file 1");
+Console.WriteLine("For: SNU/2D/ProgrammingTools/IDE/Learn-JScript.NET");
+Console.WriteLine("About:");
+Console.WriteLine("I decided to make JScript.NET the main project language file for this project (SNU / 2D / Programming Tools / IDE / JScript.NET) as this is a JScript.NET IDE, and it needs its main language to be represented here.");
+
+// File info
+// File type: JScript.NET source file (*.js)
+// File version: 1 (2022, Monday, October 10th at 9:03 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
