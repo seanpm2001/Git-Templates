1b07ceb2d439f21144a6e0e437260dd75d4ebce9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-10 20:30:58 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Jython/Jython/PROJECT_LANG_1_V1.py
@@ -0,0 +1,18 @@
+# Start of script
+
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Jython
+# About:
+# I decided to make Jython the main project language file for this project (SNU / 2D / Programming Tools / IDE / Jython) as this is a Jython IDE, and it needs its main language to be represented here.
+
+print "Project language file 1\n"
+print "For: SNU/2D/ProgrammingTools/IDE/Jython\n"
+print "About:\n"
+print "I decided to make Jython the main project language file for this project (SNU / 2D / Programming Tools / IDE / Jython) as this is a Jython IDE, and it needs its main language to be represented here.\n"
+
+# File info
+# File type: Jython source file (*.py)
+# File version: 1 (2022, Monday, October 10th at 8:29 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
