43fd00aefae89d34f8f13439fa93f5d29b251bb2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-10 20:19:05 -0700

    PROJECT_LANG_1_V1.ino
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Arduino/Arduino/PROJECT_LANG_1_V1.ino
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Arduino
+// About:
+// I decided to make Arduino the main project language file for this project (SNU / 2D / Programming Tools / IDE / Arduino) as this is a Arduino IDE, and it needs its main language to be represented here.
+
+const char check[] PROGMEM = "Project language file 1\n";
+const char check[] PROGMEM = "For: SNU/2D/ProgrammingTools/IDE/Arduino\n";
+const char check[] PROGMEM = "About:\n";
+const char check[] PROGMEM = "I decided to make Arduino the main project language file for this project (SNU / 2D / Programming Tools / IDE / Arduino) as this is a Arduino IDE, and it needs its main language to be represented here.\n";
+
+// File info
+// File type: Arduino source file (*.ino)
+// File version: 1 (2022, Monday, October 10th at 8:18 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
