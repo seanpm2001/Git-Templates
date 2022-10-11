fdb9efa6503716a77f44f14769212ef911b522a5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-10 20:15:24 -0700

    PROJECT_LANG_1_V1.ino
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Arduino/Arduino/PROJECT_LANG_1_V1.ino
@@ -0,0 +1,18 @@
+// Start of script
+
+// Project language file 1
+// For: seanpm2001/Learn-Arduino
+// About:
+// I decided to make Arduino the main project language file for this project (Seanpm2001/Learn-Arduino) as Arduino is the language this project is dedicated to, because this project is about learning the Arduino programming language. It only makes sense to Arduino the official language for this project. It is getting its own project language file, starting here.
+
+const char check[] PROGMEM = "Project language file 1\n";
+const char check[] PROGMEM = "For: seanpm2001/Learn-Arduino\n";
+const char check[] PROGMEM = "About:\n";
+const char check[] PROGMEM = "I decided to make Arduino the main project language file for this project (Seanpm2001/Learn-Arduino) as Arduino is the language this project is dedicated to, because this project is about learning the Arduino programming language. It only makes sense to Arduino the official language for this project. It is getting its own project language file, starting here.\n";
+
+// File info
+// File type: Arduino source file (*.ino)
+// File version: 1 (2022, Monday, October 10th at 8:13 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
