4a2d4916e2b5fe24063becf2090f9005ac94c349
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-12 21:32:41 -0700

    PROJECT_LANG_1.lisp [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-CommonLisp/CommonLisp/PROJECT_LANG_1.lisp
@@ -0,0 +1,20 @@
+; Start of script
+
+; Project language file 1
+; For: seanpm2001/Learn-CommonLisp
+; About:
+; I decided to make Common Lisp the main project language file for this project (Seanpm2001/Learn-CommonLisp) as Common Lisp is the language this project is dedicated to, because this project is about learning the Common Lisp programming language. It only makes sense to Common Lisp the official language for this project. It is getting its own project language file, starting here.
+
+(defun plangone ()
+	(format t "Project language file 1")
+	(format t "For: seanpm2001/Learn-CommonLisp")
+	(format t "About:")
+	(format t "I decided to make Common Lisp the main project language file for this project (Seanpm2001/Learn-CommonLisp) as Common Lisp is the language this project is dedicated to, because this project is about learning the Common Lisp programming language. It only makes sense to Common Lisp the official language for this project. It is getting its own project language file, starting here.")
+)
+
+; File info
+; File type: CommonLisp source file (*.lisp)
+; File version: 1 (2022, Wednesday, October 12th at 9:30 pm PST)
+; Line count (including blank lines and compiler line): 21
+
+; End of script
