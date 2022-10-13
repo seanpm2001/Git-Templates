462e57e37097add73f8e53afc3f7a99eda67b078
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-12 18:31:00 -0700

    PROJECT_LANG_1_V1.rs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Rust/Rust/PROJECT_LANG_1_V1.rs
@@ -0,0 +1,27 @@
+// Start of script
+
+/* Project language file 1 */
+fn main() {
+  println!("Project language file 1 (Rust)");
+  println!("For: SNU/2D/ProgrammingTools/IDE/Rust");
+  println!("About:");
+  println!("I decided to make Rust the main project language file for this project (SNU / 2D / Programming Tools / IDE / Rust) as this is a Rust IDE, and it needs its main language to be represented here.");
+  break;
+}
+return main();
+break;
+
+/* Output */
+
+// Project language file 1 (Rust)
+// For: SNU/2D/ProgrammingTools/IDE/Rust
+// About:
+// I decided to make Rust the main project language file for this project (SNU / 2D / Programming Tools / IDE / Rust) as this is a Rust IDE, and it needs its main language to be represented here.
+
+/* File info */
+
+// File version: 1 (2022, Wednesday, October 12th at 6:29 pm PST)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 28
+
+// End of script
