0db9f469e5553fd5cb1eef4ee8b56a884b422391
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-12 21:39:43 -0700

    PROJECT_LANG_1_V1.lisp
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/CommonLisp/CommonLisp/PROJECT_LANG_1_V1.lisp
@@ -0,0 +1,20 @@
+; Start of script
+
+; Project language file 1
+; For: SNU/2D/ProgrammingTools/IDE/CommonLisp
+; About:
+; I decided to make Common Lisp the main project language file for this project (SNU / 2D / Programming Tools / IDE / CommonLisp) as this is a Common Lisp IDE, and it needs its main language to be represented here.
+
+(defun plangone ()
+	(format t "Project language file 1")
+	(format t "For: SNU/2D/ProgrammingTools/IDE/CommonLisp")
+	(format t "About:")
+	(format t "I decided to make Common Lisp the main project language file for this project (SNU / 2D / Programming Tools / IDE / CommonLisp) as this is a Common Lisp IDE, and it needs its main language to be represented here.")
+)
+
+; File info
+; File type: CommonLisp source file (*.lisp)
+; File version: 1 (2022, Wednesday, October 12th at 9:38 pm PST)
+; Line count (including blank lines and compiler line): 21
+
+; End of script
