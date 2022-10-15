ef4fab5a4aae15bf909f392d232c950b1af5c134
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-14 17:42:45 -0700

    PROJECT_LANG_1.rb
    Version 1 of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Ruby/Ruby/PROJECT_LANG_1.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+# Start of script
+
+class project_language_file_1():
+  def projectLanguageFile1():
+    puts "Project language file 1"
+    puts "For: SNU/2D/ProgrammingTools/IDE/Ruby"
+    puts "About: "
+    puts " I decided to make Ruby the main project language file for this project (SNU / 2D / Programming Tools / IDE / Ruby) as this is a Ruby IDE, and it needs its main language to be represented here."
+    break
+  return projectLanguageFile1()
+  break
+end # end class
+
+# Output
+
+# project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/Ruby
+# About:
+# I decided to make Ruby the main project language file for this project (SNU / 2D / Programming Tools / IDE / Ruby) as this is a Ruby IDE, and it needs its main language to be represented here.
+ 
+# File info
+
+# File type: Ruby source file (*.rb *.rbx *.ruby *.gem *.rake *.gemspec)
+# File version: 1 (2022, Friday, October 14th at 5:35 pm PST)
+# Line count (including blank lines and compiler line): 29
+
+# End of script
