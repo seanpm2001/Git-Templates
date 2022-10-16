fcf0e4fdb0bb7d270396557aa4d2da42022fc165
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-15 17:24:37 -0700

    PROJECT_LANG_1_V1.ts
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/TypeScript/TypeScript/PROJECT_LANG_1_V1.ts
@@ -0,0 +1,23 @@
+// Start of script
+// WARNING: I am not very experienced with TypeScript. This program may not execute properly. /
+namespace ts {
+	public void projectLanguageFileOne() {
+	let message: string = 'Project language file 1\nFor: /SNU/2D/ProgrammingTools/IDE/TypeScript/\nAbout:\nI decided to make TypeScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / TypeScript) as this is a TypeScript IDE, and it needs its main language to be represented here.';	
+	console.log(message);
+	break;
+	}
+	return projectLanguageFileOne();
+	break;
+}
+/* Output:
+Project language file 1
+For: /SNU/2D/ProgrammingTools/IDE/TypeScript/
+About:
+I decided to make TypeScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / TypeScript) as this is a TypeScript IDE, and it needs its main language to be represented here.
+*/
+/* File info
+* File type: TypeScript source file (*.ts *.tsx)
+* File version: 1 (2022, Saturday, October 15th at 5:22 pm PST) 
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
