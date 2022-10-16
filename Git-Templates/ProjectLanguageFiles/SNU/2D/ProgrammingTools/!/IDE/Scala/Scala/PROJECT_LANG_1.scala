b8dde9453d5650967b4d3cf4d4a4368e7f808bb2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-15 17:18:11 -0700

    PROJECT_LANG_1.scala
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Scala/Scala/PROJECT_LANG_1.scala
@@ -0,0 +1,32 @@
+// Start of script
+
+/*  Import section */
+
+import users // Not necessary, just getting used to the language
+package users // Not necessary, just getting used to the language
+
+/* Project language file 1 */
+
+object ProjectLanguageOne extends App {
+   println("Project language file 1\nFor: /SNU/2D/ProgrammingTools/IDE/Scala/\nAbout:\nI decided to make Scala the main project language file for this project (SNU / 2D / Programming Tools / IDE / Scala) as this is a Scala IDE, and it needs its main language to be represented here.")
+ }
+return object(ProjectLanguageOne)
+
+/* Output */
+
+// Project language file 1
+// For: /SNU/2D/ProgrammingTools/IDE/Scala/
+// About:
+// I decided to make Scala the main project language file for this project (SNU / 2D / Programming Tools / IDE / Scala) as this is a Scala IDE, and it needs its main language to be represented here.
+
+/* Notes */
+
+// Note: I am not very good at Scala, so this script may not work.
+
+/* File info */
+
+// File version: 1 (2022, Saturday, October 15th at 5:15 pm PST)
+// File type: Scala 2.1 Source file (*.scala *.sc)
+// Line count (including blank lines and compiler line): 33
+
+// End of script
