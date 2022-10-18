cfcaa02f4e5f269497922713984b847553ea86fc
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-17 18:15:58 -0700

    PROJECT_LANG_1_V1.swift
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Swift/Swift/PROJECT_LANG_1_V1.swift
@@ -0,0 +1,28 @@
+// Start of script
+
+/* Project language file 1 */
+
+class projectLanguageFileOne {
+	print("Project language file 1");
+	print("For: /SNU/2D/ProgrammingTools/IDE/Swift/");
+	print("About:");
+	print("I decided to make Swift the main project language file for this project (SNU / 2D / Programming Tools / IDE / Swift) as this is a Swift IDE, and it needs its main language to be represented here.")
+	break;
+}
+return projectLanguageFileOne();
+return 0;
+
+/* Output: */
+
+// Project language file 1
+// For: /SNU/2D/ProgrammingTools/IDE/Swift/
+// ABout:
+// I decided to make Swift the main project language file for this project (SNU / 2D / Programming Tools / IDE / Swift) as this is a Swift IDE, and it needs its main language to be represented here.
+
+/* File info */
+
+// File version: 1 (2022, Monday, October 17th at 6:14 pm PST)
+// File type: Swift 5.3 Source file (*.swift)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
