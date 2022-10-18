62a622a1a995d5421c061fa68ddd252b8bf37fff
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-17 18:25:26 -0700

    PROJECT_LANG_1_V1.vue.js
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Vue.js/JavaScript/PROJECT_LANG_1_V1.vue.js
@@ -0,0 +1,24 @@
+<div id="plang1">
+  <p>{{ message }}</p>
+</div>
+<script>
+new Vue({
+  el: '#plang1',
+  data: {
+    message: 'Project language file 1\nFor: /SNU/2D/ProgrammingTools/IDE/Vue.js/\nAbout:\nI decided to make Vue.js the main project language file for this project (SNU / 2D / Programming Tools / IDE / Vue.js) as this is a Vue.js IDE, and it needs its main language to be represented here.\n'
+  }
+})
+</script>
+// Output:
+// Project language file 1
+// For: /SNU/2D/ProgrammingTools/IDE/Vue.js/
+// About:
+// I decided to make Vue.js the main project language file for this project (SNU / 2D / Programming Tools / IDE / Vue.js) as this is a Vue.js IDE, and it needs its main language to be represented here.
+
+/* File info
+* File type: Vue.js source file (*.vue *.js)
+* File version: 1 (2022, Monday, October 17th at 6:20 pm PST)
+* Line count (including blank lines and compiler line): 25
+*/
+
+// End of script
