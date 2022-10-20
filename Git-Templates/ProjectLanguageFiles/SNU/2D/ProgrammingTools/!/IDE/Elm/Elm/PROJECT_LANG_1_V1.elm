9e59ec8471d1838f14f986d7da1d71ee39c1cbf3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-19 18:53:42 -0700

    PROJECT_LANG_1_V1.elm
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Elm/Elm/PROJECT_LANG_1_V1.elm
@@ -0,0 +1,27 @@
+-- Start of script
+
+{- Project language file 1 -}
+-- Start
+projectLanguageOneTitle : String
+projectLanguageOneTitle = "Project language file 1"
+projectLanguageOneSTitle : String
+projectLanguageOneSTitle = "For: /SNU/2D/ProgrammingTools/IDE/Elm/"
+projectLanguageOneHeaderThree : String
+projectLanguageOneHeaderThree = "About:"
+projectLanguageOneDescription : String
+projectLanguageOneDescription = "I decided to make Elm the main project language file for this project (SNU / 2D / Programming Tools / IDE / Elm) as this is a Elm IDE, and it needs its main language to be represented here."
+-- End
+{- 1 elif egaugnal tcojorP -}
+
+{- Output {-
+I decided to make Elm the main project language file for this project (SNU / 2D / Programming Tools / IDE / Elm) as this is a Elm IDE, and it needs its main language to be represented here.
+-}-}
+
+{- File info -}
+{-
+-- File type: Elm source file (*.elm)
+-- File version: 1 (2022, Wednesday, October 19th at 6:50 pm PST)
+-- Line count (including blank lines and compiler line): 28
+-}
+
+-- End of script
