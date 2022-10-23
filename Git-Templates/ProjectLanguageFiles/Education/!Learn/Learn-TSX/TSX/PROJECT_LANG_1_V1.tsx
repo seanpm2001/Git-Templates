c7a2650cf2fbcfc07cb0629abba87699822f6628
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-22 22:51:00 -0700

    PROJECT_LANG_1_V1.tsx
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-TSX/TSX/PROJECT_LANG_1_V1.tsx
@@ -0,0 +1,19 @@
+// Start of script
+
+// Notes
+// This language is a part/subsystem of TypeScript, I don't know how much it deviates from normal TypeScript
+
+// Project language file 1
+// For: seanpm2001/Learn-TSX
+// About:
+// I decided to make TSX the main project language file for this project (Seanpm2001/Learn-TSX) as TSX is the language this project is dedicated to, because this project is about learning the TSX programming language. It only makes sense to TSX the official language for this project. It is getting its own project language file, starting here.
+
+let message: string = 'Project language file 1\nFor: seanpm2001/Learn-TSX\nAbout:\nI decided to make TSX the main project language file for this project (Seanpm2001/Learn-TSX) as TSX is the language this project is dedicated to, because this project is about learning the TSX programming language. It only makes sense to TSX the official language for this project. It is getting its own project language file, starting here.';
+console.log(message);
+
+// File info
+// File type: TSX source file (*.tsx)
+// File version: 1 (2022, Saturday, October 22nd at 10:49 pm PST)
+// Line count (including blank lines and compiler line): 20
+
+// End of script
