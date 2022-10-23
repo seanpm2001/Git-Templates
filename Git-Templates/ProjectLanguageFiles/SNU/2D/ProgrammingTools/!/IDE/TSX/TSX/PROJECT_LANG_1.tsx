64b7788f3ce9c2d7b0eb297b496397d7ce14c075
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-22 22:54:33 -0700

    PROJECT_LANG_1.tsx [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/TSX/TSX/PROJECT_LANG_1.tsx
@@ -0,0 +1,19 @@
+// Start of script
+
+// Notes
+// This language is a part/subsystem of TypeScript, I don't know how much it deviates from normal TypeScript
+
+// Project language file 1
+// For: /SNU/2D/ProgrammingTools/IDE/TSX/
+// About:
+// I decided to make TSX the main project language file for this project (SNU / 2D / Programming Tools / IDE / TSX) as this is a TSX IDE, and it needs its main language to be represented here.
+
+let message: string = 'Project language file 1\nFor: /SNU/2D/ProgrammingTools/IDE/TSX/\nAbout:\nI decided to make TSX the main project language file for this project (SNU / 2D / Programming Tools / IDE / TSX) as this is a TSX IDE, and it needs its main language to be represented here.';
+console.log(message);
+
+// File info
+// File type: TSX source file (*.tsx)
+// File version: 1 (2022, Saturday, October 22nd at 10:53 pm PST)
+// Line count (including blank lines and compiler line): 20
+
+// End of script
