656c09bcb3de4f8792f75f59dee1bd1a387d1948
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-23 23:13:06 -0700

    PROJECT_LANG_1.s [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/SASL/SASL/PROJECT_LANG_1.s
@@ -0,0 +1,18 @@
+|| Start of script
+
+|| Project language file 1
+|| For: /SNU/2D/ProgrammingTools/IDE/SASL/
+|| About:
+|| I decided to make SASL the main project language file for this project (SNU / 2D / Programming Tools / IDE / SASL) as this is a SASL IDE, and it needs its main language to be represented here.
+
+%p: 'roject language file 1''
+%f: 'or: /SNU/2D/ProgrammingTools/IDE/SASL/''
+%a: 'bout: ''
+%i: ' decided to make SASL the main project language file for this project (SNU / 2D / Programming Tools / IDE / SASL) as this is a SASL IDE, and it needs its main language to be represented here."
+
+|| File info
+|| File type: SASL source file (*.s) (this may not be the correct file extension, it is a guess based on extensions of the time (1976))
+|| File version: 1 (2022, Sunday, October 23rd at 11:12 pm PST)
+|| Line count (including blank lines and compiler line): 19
+
+|| End of script
