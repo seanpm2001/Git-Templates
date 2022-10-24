539a7c30d373633018834eaed6f408935c65c37e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-23 23:28:59 -0700

    PROJECT_LANG_1_V1.ml
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Amulet/Amulet/PROJECT_LANG_1_V1.ml
@@ -0,0 +1,20 @@
+(* Start of script *)
+
+(* Project language file 1
+* For: seanpm2001/Learn-Amulet
+* About:
+* I decided to make Amulet the main project language file for this project (Seanpm2001/Learn-Amulet) as Amulet is the language this project is dedicated to, because this project is about learning the Amulet programming language. It only makes sense to Amulet the official language for this project. It is getting its own project language file, starting here.
+*)
+
+put_line "Project language file 1"
+put_line "For: seanpm2001/Learn-Amulet"
+put_line "About:"
+put_line "I decided to make Amulet the main project language file for this project (Seanpm2001/Learn-Amulet) as Amulet is the language this project is dedicated to, because this project is about learning the Amulet programming language. It only makes sense to Amulet the official language for this project. It is getting its own project language file, starting here."
+
+(* File info
+* File type: Amulet source file (*.ml)
+* File version: 1 (2022, Sunday, October 23rd at 11:25 pm PST)
+* Line count (including blank lines and compiler line): 21
+*)
+
+(* End of script *)
