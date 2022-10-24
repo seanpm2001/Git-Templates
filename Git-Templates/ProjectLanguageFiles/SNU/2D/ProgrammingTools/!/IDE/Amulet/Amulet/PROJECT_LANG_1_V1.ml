ed51a90a113641bbf3220eb169282962625a48f1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-23 23:35:44 -0700

    PROJECT_LANG_1_V1.ml
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Amulet/Amulet/PROJECT_LANG_1_V1.ml
@@ -0,0 +1,20 @@
+(* Start of script *)
+
+(* Project language file 1
+* For: /SNU/2D/ProgrammingTools/IDE/Amulet/
+* About:
+* I decided to make Amulet the main project language file for this project (SNU / 2D / Programming Tools / IDE / Amulet) as this is a Amulet IDE, and it needs its main language to be represented here.
+*)
+
+put_line "Project language file 1"
+put_line "For: /SNU/2D/ProgrammingTools/IDE/Amulet/"
+put_line "About:"
+put_line "I decided to make Amulet the main project language file for this project (SNU / 2D / Programming Tools / IDE / Amulet) as this is a Amulet IDE, and it needs its main language to be represented here."
+
+(* File info
+* File type: Amulet source file (*.ml)
+* File version: 1 (2022, Sunday, October 23rd at 11:34 pm PST)
+* Line count (including blank lines and compiler line): 21
+*)
+
+(* End of script *)
