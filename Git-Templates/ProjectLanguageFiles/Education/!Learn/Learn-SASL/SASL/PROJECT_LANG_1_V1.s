0a0a6f2b8c185f3f43b92ea16e033ea4cf259330
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-23 23:09:31 -0700

    PROJECT_LANG_1_V1.s
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-SASL/SASL/PROJECT_LANG_1_V1.s
@@ -0,0 +1,18 @@
+|| Start of script
+
+|| Project language file 1
+|| For: seanpm2001/Learn-SASL
+|| About:
+|| I decided to make SASL the main project language file for this project (Seanpm2001/Learn-SASL) as SASL is the language this project is dedicated to, because this project is about learning the SASL programming language. It only makes sense to SASL the official language for this project. It is getting its own project language file, starting here.
+
+%p: 'roject language file 1''
+%f: 'or: seanpm2001/Learn-SASL''
+%a: 'bout: ''
+%i: ' decided to make SASL the main project language file for this project (Seanpm2001/Learn-SASL) as SASL is the language this project is dedicated to, because this project is about learning the SASL programming language. It only makes sense to SASL the official language for this project. It is getting its own project language file, starting here."
+
+|| File info
+|| File type: SASL source file (*.s) (this may not be the correct file extension, it is a guess based on extensions of the time (1976))
+|| File version: 1 (2022, Sunday, October 23rd at 11:08 pm PST)
+|| Line count (including blank lines and compiler line): 19
+
+|| End of script
