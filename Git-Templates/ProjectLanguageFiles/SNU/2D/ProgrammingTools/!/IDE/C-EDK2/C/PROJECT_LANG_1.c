ce833c03cbc1cd467f440118fbccf847870993a0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-25 13:10:38 -0700

    PROJECT_LANG_1.c [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/C-EDK2/C/PROJECT_LANG_1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+// Start of script
+
+// Notes and warnings
+// Warning: contains bad code, may not compile
+
+/* Project language file 1 */
+union projectLanguageFileOne {
+int main(void) {
+    printf("Project language file 1: C\n");
+    printf("For: /SNU/2D/ProgrammingTools/IDE/C/EDK2/\n");
+    printf("About:\n");
+    printf("I decided to make C-EDK2 the main project language file for this project (SNU / 2D / Programming Tools / IDE / C-EDK2) as this is a C-EDK2 IDE, and it needs its main language to be represented here..\n");
+    break;
+    return 0;
+  }
+return main();
+printf("The program is going to close in 15 seconds\n");
+wait 15;
+return 0;
+break;
+exit;
+}
+/* Output */
+
+/*
+
+Project language file 1
+For: SNU/2D/ProgrammingTools/IDE/C/EDK2/
+About:
+I decided to make C-EDK2 the main project language file for this project (SNU / 2D / Programming Tools / IDE / C-EDK2) as this is a C-EDK2 IDE, and it needs its main language to be represented here.
+
+*/
+
+/* File info */
+
+// File version: 1 (2022, Tuesday, October 25th at 1:08 pm PST)
+// File type: C Source file (*.c *.h)
+// Line count (including blank lines and compiler line): 42
+
+// End of script
