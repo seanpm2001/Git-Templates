4b12b85e6929a0dcdd7debfdd8e5b360c42c6398
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-27 22:45:10 -0700

    PROJECT_LANG_1.pl [V]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Audio_Projects/ChargeFX/Perl/PROJECT_LANG_1.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+# Start of script
+
+# Project language file 1
+print "Project language file 1";
+print "For: ChargeFX";
+print "About:";
+print "I chose Perl as the first project language file for this project (seanpm2001/ChargeFX) as I wanted to write this project in Perl due to a lack of Perl programs being created by me. It also seems like a fitting language. I would have used Python, but I wanted to promote Perl. All of the programs functions can be done with Perl efficiently, so it was merely a choice between Perl and Python. It is getting its own project language file, starting here.";
+break;
+wait "60";
+exit;
+
+# Output
+
+# Project language file 1
+# For: ChargeFX
+# About:
+# I chose Perl as the first project language file for this project (seanpm2001/ChargeFX) as I wanted to write this project in Perl due to a lack of Perl programs being created by me. It also seems like a fitting language. I would have used Python, but I wanted to promote Perl. All of the programs functions can be done with Perl efficiently, so it was merely a choice between Perl and Python. It is getting its own project language file, starting here.
+
+# File info
+# File type: Perl source file (*.pl)
+# File version: 1 (2022, Thursday, October 27th at 10:44 pm PST)
+# Line count (including blank lines and compiler line): 26
+
+# End of script
