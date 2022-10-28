3582511ab88a8be5b3dcda472e87caace72b0f9e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-27 22:19:43 -0700

    PROJECT_LANG_1_V1.nim
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/Lego-Tower/!2023/Nim/PROJECT_LANG_1_V1.nim
@@ -0,0 +1,21 @@
+# Start of script
+
+echo ("Project language file 1")
+echo ("For: seanpm2001/SeansLifeArchive_Images/LegoTower/Year2023/")
+echo ("About:")
+echo ("I chose Nim as the first project language file for this project (SeansLifeArchive/Images/Lego Tower/Year2023) as this game is by NimbleBit, so the official project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. If you count file extensions (nimble) only the last 3 characters are missing. It is getting its own project language file, starting here. This is mostly a Git-image project, so languages can be chosen easily. I normally go for puns or related subjects.")
+break
+
+# Output:
+
+# Project language file 1
+# For: seanpm2001/SeansLifeArchive_Images_LegoTower/Year2023/
+# About:
+# I chose Nim as the first project language file for this project (SeansLifeArchive/Images/Lego Tower/Year2023) as this game is by NimbleBit, so the official project language is Nim. This was chosen, as Nim contains the first 3 letters (out of 9) for the NimbleBit company name. If you count file extensions (nimble) only the last 3 characters are missing. It is getting its own project language file, starting here. This is mostly a Git-image project, so languages can be chosen easily. I normally go for puns or related subjects.
+
+# File info
+# File version: 1 (2022, Thursday, October 27th at 10:10 pm PST)
+# File type: Nim source file (*.nim *.nimble *.nimrod)
+# Line count (including blank lines and compiler line): 22
+
+# End of script
