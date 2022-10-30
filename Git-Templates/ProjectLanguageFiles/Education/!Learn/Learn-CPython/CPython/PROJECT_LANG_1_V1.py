4c1e1ea2cd0a2339fe9b43d4f5fea2ff96d477e2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-29 22:42:22 -0700

    PROJECT_LANG_1_V1.py
    An archive of the first version project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-CPython/CPython/PROJECT_LANG_1_V1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Start of script
+
+''' Project language file 1 '''
+class projectLanguageFileOne():
+print("Project Language File 1\n")
+print("For: seanpm2001/Learn-CPython")
+print("About:\n")
+print("I decided to make Python the main project language file for this project (Seanpm2001/Learn-CPython) as Python is the language this project is dedicated to, because this project is about learning the Python programming language. It only makes sense to Python the official language for this project. It is getting its own project language file, starting here. This project uses the default CPython implementation of Python.")
+noMore = input("\nPress [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+
+""" Output """
+
+# Project language file 1
+# For: seanpm2001/Learn-CPython
+# About:
+# I decided to make Python the main project language file for this project (Seanpm2001/Learn-CPython) as Python is the language this project is dedicated to, because this project is about learning the Python programming language. It only makes sense to Python the official language for this project. It is getting its own project language file, starting here. This project uses the default CPython implementation of Python.
+
+break
+
+''' File info '''
+
+# File version: 1 (2022, Saturday, October 29th at 10:40 pm PST)
+# File type: Python 3 source file (*.py *.py3 *.pyt *.pyw)
+# Line count (including blank lines and compiler line): 29
+
+# End of script
