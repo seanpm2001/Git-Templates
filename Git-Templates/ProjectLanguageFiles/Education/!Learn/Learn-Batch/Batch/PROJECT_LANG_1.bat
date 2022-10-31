0136ad6522f3bbb826775d5295124bcfc9f8cb3c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-30 23:06:02 -0700

    PROJECT_LANG_1.bat
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Batch/Batch/PROJECT_LANG_1.bat
@@ -0,0 +1,18 @@
+:: Start of script
+
+:: Project language file 1
+:: For: seanpm2001/Learn-Batch
+:: About:
+:: I decided to make Batch the main project language file for this project (Seanpm2001/Learn-Batch) as Batch is the language this project is dedicated to, because this project is about learning the Batch programming language. It only makes sense to Batch the official language for this project. It is getting its own project language file, starting here.
+
+echo "Project language file 1"
+echo "For: seanpm2001/Learn-Batch"
+echo "About:"
+echo "I decided to make Batch the main project language file for this project (Seanpm2001/Learn-Batch) as Batch is the language this project is dedicated to, because this project is about learning the Batch programming language. It only makes sense to Batch the official language for this project. It is getting its own project language file, starting here."
+
+:: File info
+:: File type: Batch source file (*.bat)
+:: File version: 1 (2022, Sunday, October 30th at 11:05 pm PST)
+:: Line count (including blank lines and compiler line): 19
+
+:: End of script
