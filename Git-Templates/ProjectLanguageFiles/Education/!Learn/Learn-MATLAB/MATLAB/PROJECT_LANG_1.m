a1dfc37f621cf03516035ee61b66ecd1e9144f13
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-10-30 23:10:28 -0700

    PROJECT_LANG_1.m [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-MATLAB/MATLAB/PROJECT_LANG_1.m
@@ -0,0 +1,18 @@
+% Start of script
+
+% Project language file 1
+% For: seanpm2001/Learn-MATLAB
+% About:
+% I decided to make MATLAB the main project language file for this project (Seanpm2001/Learn-MATLAB) as MATLAB is the language this project is dedicated to, because this project is about learning the MATLAB programming language. It only makes sense to MATLAB the official language for this project. It is getting its own project language file, starting here.
+
+disp('Project language file 1')
+disp('For: seanpm2001/Learn-MATLAB')
+disp('About:')
+disp('I decided to make MATLAB the main project language file for this project (Seanpm2001/Learn-MATLAB) as MATLAB is the language this project is dedicated to, because this project is about learning the MATLAB programming language. It only makes sense to MATLAB the official language for this project. It is getting its own project language file, starting here.')
+
+% File info
+% File type: MATLAB source file (*.m)
+% File version: 1 (2022, Sunday, October 30th at 11:09 pm PST)
+% Line count (including blank lines and compiler line): 19
+
+% End of script
