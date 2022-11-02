e316c172306ed419f509ca8d7c48f351f840bff0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-01 23:33:29 -0700

    PROJECT_LANG_1_V1.v
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Coq/Coq/PROJECT_LANG_1_V1.v
@@ -0,0 +1,18 @@
+(* Start of script *)
+
+(* Project language file 1
+* For: /SNU/2D/ProgrammingTools/IDE/Coq/
+* About:
+* I decided to make Coq the main project language file for this project (SNU / 2D / Programming Tools / IDE / Coq) as this is a Coq IDE, and it needs its main language to be represented here.
+*)
+
+System.log (LString.s "Project language file 1")
+System.log (LString.s "For: /SNU/2D/ProgrammingTools/IDE/Coq/")
+System.log (LString.s "About:")
+System.log (LString.s "I decided to make Coq the main project language file for this project (SNU / 2D / Programming Tools / IDE / Coq) as this is a Coq IDE, and it needs its main language to be represented here.")
+
+(* File info
+* File type: Coq source file (*.v)
+* File version: 1 (2022, Tuesday, November 1st at 11:32 pm PST)
+* Line count (including blank lines and compiler line): 19
+*)
