417cde1b279f8c0dbdb94b092b1f8b18deb8b2c2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-01 23:28:11 -0700

    PROJECT_LANG_1.v
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Coq/Coq/PROJECT_LANG_1.v
@@ -0,0 +1,18 @@
+(* Start of script *)
+
+(* Project language file 1
+* For: seanpm2001/Learn-Coq
+* About:
+* I decided to make Coq the main project language file for this project (Seanpm2001/Learn-Coq) as Coq is the language this project is dedicated to, because this project is about learning the Coq programming language. It only makes sense to Coq the official language for this project. It is getting its own project language file, starting here.
+*)
+
+System.log (LString.s "Project language file 1")
+System.log (LString.s "For: seanpm2001/Learn-Coq")
+System.log (LString.s "About:")
+System.log (LString.s "I decided to make Coq the main project language file for this project (Seanpm2001/Learn-Coq) as Coq is the language this project is dedicated to, because this project is about learning the Coq programming language. It only makes sense to Coq the official language for this project. It is getting its own project language file, starting here.")
+
+(* File info
+* File type: Coq source file (*.v)
+* File version: 1 (2022, Tuesday, November 1st at 11:27 pm PST)
+* Line count (including blank lines and compiler line): 19
+*)
