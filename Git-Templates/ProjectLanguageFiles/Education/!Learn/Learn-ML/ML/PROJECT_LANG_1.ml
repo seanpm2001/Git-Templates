84f9f14f6f17b4e803a7418d17264615ab3a49f9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-04 22:18:59 -0700

    PROJECT_LANG_1.ml [V1
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-ML/ML/PROJECT_LANG_1.ml
@@ -0,0 +1,20 @@
+(* Start of script *)
+
+(* Project language file 1
+* For: seanpm2001/Learn-ML
+* About:
+* I decided to make ML the main project language file for this project (Seanpm2001/Learn-ML) as ML is the language this project is dedicated to, because this project is about learning the ML programming language. It only makes sense to ML the official language for this project. It is getting its own project language file, starting here.
+*)
+
+print "Project language file 1\n"
+print "For: seanpm2001/Learn-ML\n"
+print "About:\n"
+print "I decided to make ML the main project language file for this project (Seanpm2001/Learn-ML) as ML is the language this project is dedicated to, because this project is about learning the ML programming language. It only makes sense to ML the official language for this project. It is getting its own project language file, starting here.\n"
+
+(* File info
+* File type: ML source file (*.ml)
+* File version: 1 (2022, Friday, November 4th at 10:17 pm PST)
+* Line count (including blank lines and compiler line): 21
+*)
+
+(* End of script *)
