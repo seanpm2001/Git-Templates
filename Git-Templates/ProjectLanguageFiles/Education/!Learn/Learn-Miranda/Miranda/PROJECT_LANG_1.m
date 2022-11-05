c800a4f9f7d2df60f77c474a2d8c99823848facf
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-04 22:06:09 -0700

    PROJECT_LANG_1.m [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Miranda/Miranda/PROJECT_LANG_1.m
@@ -0,0 +1,18 @@
+|| Start of script
+
+|| Project language file 1
+|| For: seanpm2001/Learn-Miranda
+|| About:
+|| I decided to make Miranda the main project language file for this project (Seanpm2001/Learn-Miranda) as Miranda is the language this project is dedicated to, because this project is about learning the Miranda programming language. It only makes sense to Miranda the official language for this project. It is getting its own project language file, starting here.
+
+lay ["String", "Project language file 1"] = "String\Project language file 1\n"
+lay ["String", "For: seanpm2001/Learn-Miranda"] = "String\seanpm2001/Learn-Miranda\n"
+lay ["String", "About:"] = "String\About:\n"
+lay ["String", "I decided to make Miranda the main project language file for this project (Seanpm2001/Learn-Miranda) as Miranda is the language this project is dedicated to, because this project is about learning the Miranda programming language. It only makes sense to Miranda the official language for this project. It is getting its own project language file, starting here."] = "String\I decided to make Miranda the main project language file for this project (Seanpm2001/Learn-Miranda) as Miranda is the language this project is dedicated to, because this project is about learning the Miranda programming language. It only makes sense to Miranda the official language for this project. It is getting its own project language file, starting here.\n"
+
+|| File info
+|| File type: Miranda source file (*.m)
+|| File version: 1 (2022, Friday, November 4th at 10:05 pm PST)
+|| Line count (including blank lines and compiler line): 19
+
+|| End of script
