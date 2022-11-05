9d22f305c918c17494d4cf42b6f27c5a5ad10352
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-04 22:12:59 -0700

    PROJECT_LANG_1.m [V1]
    Version 1 of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Miranda/Miranda/PROJECT_LANG_1.m
@@ -0,0 +1,18 @@
+|| Start of script
+
+|| Project language file 1
+|| For: /SNU/2D/ProgrammingTools/IDE/Miranda/
+|| About:
+|| I decided to make Miranda the main project language file for this project (SNU / 2D / Programming Tools / IDE / Miranda) as this is a Miranda IDE, and it needs its main language to be represented here.
+
+lay ["String", "Project language file 1"] = "String\Project language file 1\n"
+lay ["String", "For: /SNU/2D/ProgrammingTools/IDE/Miranda/"] = "String\seanpm2001/Learn-Miranda\n"
+lay ["String", "About:"] = "String\About:\n"
+lay ["String", "I decided to make Miranda the main project language file for this project (SNU / 2D / Programming Tools / IDE / Miranda) as this is a Miranda IDE, and it needs its main language to be represented here."] = "String\I decided to make Miranda the main project language file for this project (SNU / 2D / Programming Tools / IDE / Miranda) as this is a Miranda IDE, and it needs its main language to be represented here.\n"
+
+|| File info
+|| File type: Miranda source file (*.m)
+|| File version: 1 (2022, Friday, November 4th at 10:12 pm PST)
+|| Line count (including blank lines and compiler line): 19
+
+|| End of script
