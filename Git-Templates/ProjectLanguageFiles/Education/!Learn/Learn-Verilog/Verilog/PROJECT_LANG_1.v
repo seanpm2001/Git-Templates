53b8b05894fa9375180d6e936ae7fcaf4c7ca739
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-05 22:55:37 -0700

    PROJECT_LANG_1.v [V1]
    Version 1 of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Verilog/Verilog/PROJECT_LANG_1.v
@@ -0,0 +1,24 @@
+// Start of script */
+
+// Project language file 1
+// For: seanpm2001/Learn-Verilog
+// About:
+// I decided to make Verilog the main project language file for this project (Seanpm2001/Learn-Verilog) as Verilog is the language this project is dedicated to, because this project is about learning the Verilog programming language. It only makes sense to Verilog the official language for this project. It is getting its own project language file, starting here.
+
+module main;
+  initial
+    begin
+      $display("Project language file 1");
+      $display("For: seanpm2001/Learn-Verilog");
+      $display("About:");
+      $display("I decided to make Verilog the main project language file for this project (Seanpm2001/Learn-Verilog) as Verilog is the language this project is dedicated to, because this project is about learning the Verilog programming language. It only makes sense to Verilog the official language for this project. It is getting its own project language file, starting here.");
+      $finish;
+    end
+endmodule
+
+// File info
+// File type: Verilog source file (*.v *.vh)
+// File version: 1 (2022, Saturday, November 5th at 10:52 pm PST)
+// Line count (including blank lines and compiler line): 25
+
+// End of script */
