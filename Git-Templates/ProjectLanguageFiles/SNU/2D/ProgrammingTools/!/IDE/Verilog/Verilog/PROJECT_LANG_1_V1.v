f2d2fc62a2c13ec93805c9173136243f970cc582
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-05 23:03:46 -0700

    PROJECT_LANG_1_V1.v
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Verilog/Verilog/PROJECT_LANG_1_V1.v
@@ -0,0 +1,24 @@
+// Start of script */
+
+// Project language file 1
+// For: /SNU/2D/ProgrammingTools/IDE/Verilog/
+// About:
+// I decided to make Verilog the main project language file for this project (SNU / 2D / Programming Tools / IDE / Verilog) as this is a Verilog IDE, and it needs its main language to be represented here.
+
+module main;
+  initial
+    begin
+      $display("Project language file 1");
+      $display("For: /SNU/2D/ProgrammingTools/IDE/Verilog/");
+      $display("About:");
+      $display("I decided to make Verilog the main project language file for this project (SNU / 2D / Programming Tools / IDE / Verilog) as this is a Verilog IDE, and it needs its main language to be represented here.");
+      $finish;
+    end
+endmodule
+
+// File info
+// File type: Verilog source file (*.v *.vh)
+// File version: 1 (2022, Saturday, November 5th at 11:01 pm PST)
+// Line count (including blank lines and compiler line): 25
+
+// End of script */
