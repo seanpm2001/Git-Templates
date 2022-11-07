e68f0add0ec83b393feed09ec3ada7b095b20a68
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-06 23:29:49 -0800

    PROJECT_LANG_1_V1.sql
    An archive of the first version of the first proejct language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-TSQL/TSQL/PROJECT_LANG_1_V1.sql
@@ -0,0 +1,26 @@
+-- Start of script
+
+-- Project language file 1
+-- For: seanpm2001/Learn-TSQL
+-- About:
+-- I decided to make TSQL the main project language file for this project (Seanpm2001/Learn-TSQL) as TSQL is the language this project is dedicated to, because this project is about learning the TSQL programming language. It only makes sense to TSQL the official language for this project. It is getting its own project language file, starting here.
+
+DECLARE @title varchar(128)
+SELECT  @title = 'Project language file 1'
+PRINT   @title
+DECLARE @subtitle varchar(128)
+SELECT  @subtitle = 'For: seanpm2001/Learn-TTSQL'
+PRINT   @subtitle
+DECLARE @abt varchar(128)
+SELECT  @abt = 'About:'
+PRINT   @abt
+DECLARE @description varchar(128)
+SELECT  @description = 'I decided to make TSQL the main project language file for this project (Seanpm2001/Learn-TSQL) as TSQL is the language this project is dedicated to, because this project is about learning the TSQL programming language. It only makes sense to TSQL the official language for this project. It is getting its own project language file, starting here.'
+PRINT   @description
+
+-- File info
+-- File type: TSQL source file (*.sql *.tsql)
+-- File version: 1 (2022, Sunday, November 6th at 11:27 pm PST)
+-- Line count (including blank lines and compiler line): 27
+
+-- End of script
