0d7efdc71fd12133c933fd22c0f1c7a64234e92f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-06 22:57:25 -0800

    PROJECT_LANG_1_V1.sql
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-SQL/SQL/PROJECT_LANG_1_V1.sql
@@ -0,0 +1,18 @@
+-- Start of script
+
+-- Project language file 1
+-- For: seanpm2001/Learn-SQL
+-- About:
+-- I decided to make SQL the main project language file for this project (Seanpm2001/Learn-SQL) as SQL is the language this project is dedicated to, because this project is about learning the SQL programming language. It only makes sense to SQL the official language for this project. It is getting its own project language file, starting here.
+
+SELECT 'Project language file 1'
+SELECT 'For: seanpm2001/Learn-SQL'
+SELECT 'About:'
+SELECT 'I decided to make SQL the main project language file for this project (Seanpm2001/Learn-SQL) as SQL is the language this project is dedicated to, because this project is about learning the SQL programming language. It only makes sense to SQL the official language for this project. It is getting its own project language file, starting here.'
+
+-- File info
+-- File type: SQL source file (*.sql)
+-- File version: 1 (2022, Sunday, November 6th at 10:56 pm PST)
+-- Line count (including blank lines and compiler line): 19
+
+-- End of script
