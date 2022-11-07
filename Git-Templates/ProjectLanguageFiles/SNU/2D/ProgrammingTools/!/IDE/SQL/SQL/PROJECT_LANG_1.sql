ecf00f4fcb74d3cb10aa1e48cedfb4d44df9de04
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-06 23:03:47 -0800

    PROJECT_LANG_1.sql [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/SQL/SQL/PROJECT_LANG_1.sql
@@ -0,0 +1,18 @@
+-- Start of script
+
+-- Project language file 1
+-- For: /SNU/2D/ProgrammingTools/IDE/SQL/
+-- About:
+-- I decided to make SQL the main project language file for this project (SNU / 2D / Programming Tools / IDE / SQL) as this is a SQL IDE, and it needs its main language to be represented here.
+
+SELECT 'Project language file 1'
+SELECT 'For: /SNU/2D/ProgrammingTools/IDE/SQL/'
+SELECT 'About:'
+SELECT 'I decided to make SQL the main project language file for this project (SNU / 2D / Programming Tools / IDE / SQL) as this is a SQL IDE, and it needs its main language to be represented here.'
+
+-- File info
+-- File type: SQL source file (*.sql)
+-- File version: 1 (2022, Sunday, November 6th at 11:03 pm PST)
+-- Line count (including blank lines and compiler line): 19
+
+-- End of script
