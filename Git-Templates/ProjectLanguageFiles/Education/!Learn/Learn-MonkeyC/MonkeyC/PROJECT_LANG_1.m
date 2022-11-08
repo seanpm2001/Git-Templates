42c0b942e4ddab54fdc846d5d56d898386879956
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-07 23:29:01 -0800

    PROJECT_LANG_1.m [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-MonkeyC/MonkeyC/PROJECT_LANG_1.m
@@ -0,0 +1,18 @@
+# Start of script
+
+# Project language file 1
+# For: seanpm2001/Learn-Monkey-C
+# About:
+# I decided to make Monkey-C the main project language file for this project (Seanpm2001/Learn-Monkey-C) as Monkey-C is the language this project is dedicated to, because this project is about learning the Monkey-C programming language. It only makes sense to Monkey-C the official language for this project. It is getting its own project language file, starting here.
+
+puts("Project language file 1")
+puts("For: seanpm2001/Learn-Monkey-C")
+puts("About:")
+puts("I decided to make Monkey-C the main project language file for this project (Seanpm2001/Learn-Monkey-C) as Monkey-C is the language this project is dedicated to, because this project is about learning the Monkey-C programming language. It only makes sense to Monkey-C the official language for this project. It is getting its own project language file, starting here.")
+
+# File info
+# File type: Monkey-C source file (*.m)
+# File version: 1 (2022, Monday, November 7th at 11:28 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
