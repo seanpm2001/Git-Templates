78050faa23df4ea512975413321c70d259c10c14
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-07 23:32:34 -0800

    PROJECT_LANG_1.m
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/MonkeyC/MonkeyC/PROJECT_LANG_1.m
@@ -0,0 +1,18 @@
+# Start of script
+
+# Project language file 1
+# For: /SNU/2D/ProgrammingTools/IDE/Monkey-C/
+# About:
+# I decided to make Monkey-C the main project language file for this project (SNU / 2D / Programming Tools / IDE / Monkey-C) as this is a Monkey-C IDE, and it needs its main language to be represented here.
+
+puts("Project language file 1")
+puts("For: /SNU/2D/ProgrammingTools/IDE/Monkey-C/")
+puts("About:")
+puts("I decided to make Monkey-C the main project language file for this project (SNU / 2D / Programming Tools / IDE / Monkey-C) as this is a Monkey-C IDE, and it needs its main language to be represented here.")
+
+# File info
+# File type: Monkey-C source file (*.m)
+# File version: 1 (2022, Monday, November 7th at 11:31 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
