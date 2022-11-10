6b64b614a90fc926cc22f9bd47deacab44338866
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-09 23:42:42 -0800

    PROJECT_LANG_1_V1.vb
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/VisualBasic.NET/VisualBasic.NET/PROJECT_LANG_1_V1.vb
@@ -0,0 +1,18 @@
+' Start of script
+
+' Project language file 1
+' For: /SNU/2D/ProgrammingTools/IDE/VisualBasic.NET/
+' About:
+' I decided to make VisualBasic.NET the main project language file for this project (SNU / 2D / Programming Tools / IDE / VisualBasic.NET) as this is a VisualBasic.NET IDE, and it needs its main language to be represented here.
+
+Console.WriteLine("Project language file 1")
+Console.WriteLine("For: /SNU/2D/ProgrammingTools/IDE/VisualBasic.NET/")
+Console.WriteLine("About:")
+Console.WriteLine("I decided to make VisualBasic.NET the main project language file for this project (SNU / 2D / Programming Tools / IDE / VisualBasic.NET) as this is a VisualBasic.NET IDE, and it needs its main language to be represented here.")
+
+' File info
+' File type: VisualBasic.NET source file (*.vb)
+' File version: 1 (2022, Wednesday, November 9th at 11:41 pm PST)
+' Line count (including blank lines and compiler line): 19
+
+' End of script
