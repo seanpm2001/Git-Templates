a0770dd2494c55e80782ed0cfdb08b8570509361
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-09 23:15:16 -0800

    PROJECT_LANG_1.cmd
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Batchfile/Batchfile/PROJECT_LANG_1.cmd
@@ -0,0 +1,19 @@
+:: Start of script
+
+:: Project language file 1
+:: For: seanpm2001/Learn-Batchfile
+:: About:
+:: I decided to make Batchfile the main project language file for this project (Seanpm2001/Learn-Batchfile) as Batchfile is the language this project is dedicated to, because this project is about learning the Batchfile programming language. It only makes sense to make Batchfile the official language for this project. It is getting its own project language file, starting here.
+
+@echo off
+echo "Project language file 1"
+echo "For: seanpm2001/Learn-Batchfile"
+echo "About:"
+echo "I decided to make Batchfile the main project language file for this project (Seanpm2001/Learn-Batchfile) as Batchfile is the language this project is dedicated to, because this project is about learning the Batchfile programming language. It only makes sense to make Batchfile the official language for this project. It is getting its own project language file, starting here."
+
+:: File info
+:: File type: Batchfile source file (*.bat *.btm *.cmd)
+:: File version: 1 (2022, Wednesday, November 9th at 11:08 pm PST)
+:: Line count (including blank lines and compiler line): 20
+
+:: End of script
