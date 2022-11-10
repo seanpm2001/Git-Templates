37312cec05321a8d43a1031ec4fc9a913d08f450
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-09 23:38:00 -0800

    PROJECT_LANG_1_V1.vb
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-VisualBasic.NET/VisualBasic.NET/PROJECT_LANG_1_V1.vb
@@ -0,0 +1,18 @@
+' Start of script
+
+' Project language file 1
+' For: seanpm2001/Learn-VisualBasic.NET
+' About:
+' I decided to make VisualBasic.NET the main project language file for this project (Seanpm2001/Learn-VisualBasic.NET) as VisualBasic.NET is the language this project is dedicated to, because this project is about learning the VisualBasic.NET programming language. It only makes sense to make VisualBasic.NET the official language for this project. It is getting its own project language file, starting here.
+
+Console.WriteLine("Project language file 1")
+Console.WriteLine("For: seanpm2001/Learn-VisualBasic.NET")
+Console.WriteLine("About:")
+Console.WriteLine("I decided to make VisualBasic.NET the main project language file for this project (Seanpm2001/Learn-VisualBasic.NET) as VisualBasic.NET is the language this project is dedicated to, because this project is about learning the VisualBasic.NET programming language. It only makes sense to make VisualBasic.NET the official language for this project. It is getting its own project language file, starting here.")
+
+' File info
+' File type: VisualBasic.NET source file (*.vb)
+' File version: 1 (2022, Wednesday, November 9th at 11:36 pm PST)
+' Line count (including blank lines and compiler line): 19
+
+' End of script
