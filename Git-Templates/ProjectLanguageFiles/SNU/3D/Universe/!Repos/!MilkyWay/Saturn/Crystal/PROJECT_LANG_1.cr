b2a3f4fa2f607c0b4c5a41867c36bb9b81e340c8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-10 14:31:23 -0800

    PROJECT_LANG_1.cr [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/3D/Universe/!Repos/!MilkyWay/Saturn/Crystal/PROJECT_LANG_1.cr
@@ -0,0 +1,15 @@
+#!/usr/bin/env crystal
+# Start of script
+# Project language file 1
+# For: /SNU/3D/Universe/MilkyWay/Pluto/
+# About: 
+# I chose Crystal as the first project language file for this project (SNU/3D/Universe/MilkyWay/Pluto) as Pluto is (as far as we know) made up mostly of ice and ice crystals. I chose Crystal for this reason. It is getting its own project language file, starting here.
+puts "Project language file 1";
+puts "For: /SNU/3D/Universe/MilkyWay/Pluto/";
+puts "About: ";
+puts "I chose Crystal as the first project language file for this project (SNU/3D/Universe/MilkyWay/Pluto) as Pluto is (as far as we know) made up mostly of ice and ice crystals. I chose Crystal for this reason. It is getting its own project language file, starting here.";
+# File info
+# File type: Crystal source file (*.cr)
+# File version: 1 (2022, Thursday, November 10th at 2:30 pm PST)
+# Line count (including blank lines and compiler line): 16
+# End of script
