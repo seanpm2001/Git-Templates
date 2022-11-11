52d51d1b74ea404d6ede1ec135115d9d20a5c593
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-10 23:45:41 -0800

    PROJECT_LANG_1_V1.rs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-RenderScript/RenderScript/PROJECT_LANG_1_V1.rs
@@ -0,0 +1,16 @@
+// Start of script
+
+// Notes
+// I don't know how to make strings in RenderScript, so comments will do for now
+
+// Project language file 1
+// For: seanpm2001/Learn-RenderScript
+// About:
+// I decided to make RenderScript the main project language file for this project (Seanpm2001/Learn-RenderScript) as RenderScript is the language this project is dedicated to, because this project is about learning the RenderScript programming language. It only makes sense to make RenderScript the official language for this project. It is getting its own project language file, starting here.
+
+// File info
+// File type: RenderScript source file (*.rs)
+// File version: 1 (2022, Thursday, November 10th at 11:44 pm PST)
+// Line count (including blank lines and compiler line): 17
+
+// End of script
