15302322c1400f569bedad5928417309756beeb6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-10 23:50:50 -0800

    PROJECT_LANG_1_V1.rs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/RenderScript/RenderScript/PROJECT_LANG_1_V1.rs
@@ -0,0 +1,16 @@
+// Start of script
+
+// Notes
+// I don't know how to make strings in RenderScript, so comments will do for now
+
+// Project language file 1
+// For: /SNU/2D/ProgrammingTools/IDE/RenderScript/
+// About:
+// I decided to make RenderScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / RenderScript) as this is a RenderScript IDE, and it needs its main language to be represented here.
+
+// File info
+// File type: RenderScript source file (*.rs)
+// File version: 1 (2022, Thursday, November 10th at 11:48 pm PST)
+// Line count (including blank lines and compiler line): 17
+
+// End of script
