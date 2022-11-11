8a78a9697b95df6fe63b45f72cb881b0b6eebe2c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-10 23:21:22 -0800

    PROJECT_LANG_1.purs [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/PureScript/PureScript/PROJECT_LANG_1.purs
@@ -0,0 +1,20 @@
+-- Start of script
+
+-- Project language file 1
+-- For: /SNU/2D/ProgrammingTools/IDE/PureScript/
+-- About:
+-- I decided to make PureScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / PureScript) as this is a PureScript IDE, and it needs its main language to be represented here.
+
+module Main where
+
+import Debug.Trace
+
+main1 = trace "Project language file 1"
+main2 = trace "For: /SNU/2D/ProgrammingTools/IDE/PureScript/"
+main3 = trace "About:"
+main4 = trace "I decided to make PureScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / PureScript) as this is a PureScript IDE, and it needs its main language to be represented here."
+
+-- File info
+-- File type: PureScript source file (*.purs)
+-- File version: 1 (2022, Thursday, November 10th at 11:20 pm PST)
+-- Line count (including blank lines and compiler line): 21
