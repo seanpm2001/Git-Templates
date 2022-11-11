73b623585a5b9b0bf738493443e6ed14e13c3fcd
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-10 23:17:46 -0800

    PROJECT_LANG_1_V1.purs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-PureScript/PureScript/PROJECT_LANG_1_V1.purs
@@ -0,0 +1,20 @@
+-- Start of script
+
+-- Project language file 1
+-- For: seanpm2001/Learn-PureScript
+-- About:
+-- I decided to make PureScript the main project language file for this project (Seanpm2001/Learn-PureScript) as PureScript is the language this project is dedicated to, because this project is about learning the PureScript programming language. It only makes sense to make PureScript the official language for this project. It is getting its own project language file, starting here.
+
+module Main where
+
+import Debug.Trace
+
+main1 = trace "Project language file 1"
+main2 = trace "For: seanpm2001/Learn-PureScript"
+main3 = trace "About:"
+main4 = trace "I decided to make PureScript the main project language file for this project (Seanpm2001/Learn-PureScript) as PureScript is the language this project is dedicated to, because this project is about learning the PureScript programming language. It only makes sense to make PureScript the official language for this project. It is getting its own project language file, starting here."
+
+-- File info
+-- File type: PureScript source file (*.purs)
+-- File version: 1 (2022, Thursday, November 10th at 11:16 pm PST)
+-- Line count (including blank lines and compiler line): 21
