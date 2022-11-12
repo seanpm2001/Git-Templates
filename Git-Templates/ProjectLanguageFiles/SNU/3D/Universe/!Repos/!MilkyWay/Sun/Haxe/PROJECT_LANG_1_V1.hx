3eb37e35461ff69714c73d8f88db899be42cb135
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-11 16:36:17 -0800

    PROJECT_LANG_1_V1.hx
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/3D/Universe/!Repos/!MilkyWay/Sun/Haxe/PROJECT_LANG_1_V1.hx
@@ -0,0 +1,23 @@
+// Start of script
+class Haxe {
+    static function main() {
+        trace("Project language file 1");
+        trace("For: /SNU/3D/Universe/MilkyWay/Sun/");
+        trace("About:");
+        trace("I decided to make Haxe the main project language file for this project (/SNU/3D/Universe/MilkyWay/Sun/) as Haxe is the closest language I can find that has a similar color scheme to the sun, and that can be used for web development. It is not the only language that will be used for this project. It is getting its own project language file, starting here.");
+        break;
+    }
+}
+/* Output */
+
+// Project language file 1
+// For: /SNU/3D/Universe/MilkyWay/Sun/
+// About:
+// I decided to make Haxe the main project language file for this project (/SNU/3D/Universe/MilkyWay/Sun/) as Haxe is the closest language I can find that has a similar color scheme to the sun, and that can be used for web development. It is not the only language that will be used for this project. It is getting its own project language file, starting here.
+
+/* File info
+* File type: Haxe source file (*.hx)
+* File version: 1 (2022, Friday, November 11th at 4:35 pm PST)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
