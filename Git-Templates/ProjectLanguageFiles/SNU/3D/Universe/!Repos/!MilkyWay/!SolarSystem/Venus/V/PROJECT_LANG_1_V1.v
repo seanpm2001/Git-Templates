2631341231ed244f19a24e472020e1c648baedc0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-12 20:21:20 -0800

    PROJECT_LANG_1_V1.v
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/3D/Universe/!Repos/!MilkyWay/!SolarSystem/Venus/V/PROJECT_LANG_1_V1.v
@@ -0,0 +1,23 @@
+// Start of script
+
+struct App {
+    print "Project language file 1";
+    print "For: /SNU/3D/Universe/MilkyWay/SolarSystem/Venus/";
+    print "About:";
+    print "I chose V as the main project language file for this project (SNU/3D/Universe/MilkyWay/SolarSystem/Venus) as I couldn't find a suitable unique language to represent the planet, and thus chose V, as it is the first letter in the name of the planet. It is getting its own project language file, starting here.";
+}
+
+// Output
+
+// Project language file 1
+// For: /SNU/3D/Universe/MilkyWay/SolarSystem/Venus/
+// About:
+// I chose V as the main project language file for this project (SNU/3D/Universe/MilkyWay/SolarSystem/Venus) as I couldn't find a suitable unique language to represent the planet, and thus chose V, as it is the first letter in the name of the planet. It is getting its own project language file, starting here.
+
+// File info
+
+// File type: V source file (*.v)
+// File version: 1 (2022, Saturday, November 12th at 8:20 pm PST)
+// (Including blank lines and compiler line): 24
+
+// End of script
