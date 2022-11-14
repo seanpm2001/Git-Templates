7623df543b204d38e22bbce956bb924a2fc928e1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-13 23:36:54 -0800

    PROJECT_LANG_1.sh [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/BourneAgainShell_BASH/Shell/PROJECT_LANG_1.sh
@@ -0,0 +1,20 @@
+# Start of script
+
+# Project language file 1
+# For: SNU/2D/ProgrammingTools/IDE/BourneAgainShell_BASH
+# About:
+# I decided to make Shell the main project language file for this project (SNU / 2D / Programming Tools / IDE / BourneAgainShell_BASH) as this is a Shell IDE, and it needs its main language to be represented here.
+
+echo ("Project language file 1");
+echo ("For: SNU/2D/ProgrammingTools/IDE/BourneAgainShell_BASH");
+echo ("About:");
+echo ("I decided to make Shell the main project language file for this project (SNU / 2D / Programming Tools / IDE / BourneAgainShell_BASH) as this is a Shell IDE, and it needs its main language to be represented here.");
+break;
+exit;
+
+# File info
+# File type: BourneAgainShell_BASH source file (*.sh)
+# File version: 1 (2022, Sunday, November 13th at 11:36 pm PST)
+# Line count (including blank lines and compiler line): 21
+
+# End of script
