0cf1a8d924bf8588b15198d713a870d0f6935456
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-14 13:10:20 -0800

    PROJECT_LANG_1.cmd
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/BATCH/Batchfile/PROJECT_LANG_1.cmd
@@ -0,0 +1,28 @@
+REM Start of script
+
+REM Project language file 1
+@ECHO OFF
+START
+ECHO "Project language file 1"
+ECHO "For: /SNU/2D/ProgrammingTools/IDE/Batch/"
+ECHO "About:"
+ECHO "I have decided to have the first and main project language file for this project (SNU / 2D / ProgrammingTools / IDE / BATCH) to be Batchfile, as it is the main language used for this IDE, and may need to be supplemented with another language, as I am not sure how effective bootstrapping batchfile is."
+PAUSE
+:: BREAK
+EXIT
+@ECHO ON
+
+REM Output:
+
+:: Project language file 1
+:: For: /SNU/2D/ProgrammingTools/IDE/Batch/
+:: About:
+:: I have decided to have the first and main project language file for this project (SNU / 2D / ProgrammingTools / IDE / BATCH) to be Batchfile, as it is the main language used for this IDE, and may need to be supplemented with another language, as I am not sure how effective bootstrapping batchfile is.
+
+REM File info
+
+:: File version: 1 (2022, Monday, 2022, November 14th at 1:04 pm PST)
+:: File type: MS-DOS Batch file (*.bat *.btm *.cmd)
+:: Line count (including blank lines and compiler line): 29
+
+REM End of script
