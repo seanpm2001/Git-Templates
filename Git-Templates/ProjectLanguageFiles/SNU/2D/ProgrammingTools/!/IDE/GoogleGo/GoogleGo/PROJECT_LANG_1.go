c26122a29a93fcf1cf18ddefbc49394478839712
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-13 23:42:43 -0800

    PROJECT_LANG_1.go [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/GoogleGo/GoogleGo/PROJECT_LANG_1.go
@@ -0,0 +1,24 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/GoogleGo
+// About:
+// I decided to make Google Go the main project language file for this project (SNU / 2D / Programming Tools / IDE / GoogleGo) as this is a Google Go IDE, and it needs its main language to be represented here.
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Project language file 1")
+  fmt.Println("For: SNU/2D/ProgrammingTools/IDE/GoogleGo")
+  fmt.Println("About:")
+  fmt.Println("I decided to make Google Go the main project language file for this project (SNU / 2D / Programming Tools / IDE / GoogleGo) as this is a Google Go IDE, and it needs its main language to be represented here.")
+}
+
+// File info
+// File type: Google Go source file (*.go)
+// File version: 1 (2022, Sunday, November 13th at 11:41 pm PST)
+// Line count (including blank lines and compiler line): 25
+
+// End of script
