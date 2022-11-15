5dfa1c2f5a28da28235cccb7e0f80ec7fc3b3937
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-14 18:09:53 -0800

    PROJECT_LANG_1.c [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/3D/Universe/!Repos/!MilkyWay/!SolarSystem/!Kuiper_Belt/!Kuiper_Belt/C/PROJECT_LANG_1.c
@@ -0,0 +1,28 @@
+// Start of script
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: /SNU/3D/Universe/MilkyWay/Kuiper_Belt/\n");
+    printf("About:\n");
+    break;
+}
+return main();
+printf("I decided to make C the 1st project language file for this project (SNU/3D/Universe/MilkyWay/Kuiper_Belt) as C seems like a pretty universal language, and would fit for mapping out a solar belt. It may be supplemented with another language if things get too difficult. It is getting its own project language file, starting here.\n");
+wait 15;
+break;
+exit;
+}
+/* Output */
+// Project language file 1
+// For: /SNU/3D/Universe/MilkyWay/Kuiper_Belt/
+// About:
+// I decided to make C the 1st project language file for this project (SNU/3D/Universe/MilkyWay/Kuiper_Belt) as C seems like a pretty universal language, and would fit for mapping out a solar belt. It may be supplemented with another language if things get too difficult. It is getting its own project language file, starting here
+
+// File info
+// File version: 1 (2022, Monday, November 14th at 6:08 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
