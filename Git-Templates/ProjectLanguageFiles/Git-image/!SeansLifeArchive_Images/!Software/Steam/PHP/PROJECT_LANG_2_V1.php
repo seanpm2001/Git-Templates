45e8207482d3f54132efbb4e97dba17acafccc0d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-15 17:09:22 -0800

    PROJECT_LANG_2_V1.php
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/!Software/Steam/PHP/PROJECT_LANG_2_V1.php
@@ -0,0 +1,23 @@
+<?php
+// Start of script
+// Project language file 2
+// For: /seanpm2001/SeansLifeArchive/Images/Software/Steam/
+// About:
+// I decided to make PHP the second project language file for this project (Seanpm2001/SeansLifeArchive/Images/Software/Steam) as PHP appears to be one of the languages used with the Steam software client, due to its appearance in search queries. It fits to include it here, as this project is about the Steam software client. It is getting its own project language file, starting here.
+$PROJECT_LANGUAGE_TWO; {
+  echo ("Project language 2");
+  echo ("For: /seanpm2001/SeansLifeArchive/Images/Software/Steam/");
+  echo ("About:");
+  echo ("I decided to make PHP the second project language file for this project (Seanpm2001/SeansLifeArchive/Images/Software/Steam) as PHP appears to be one of the languages used with the Steam software client, due to its appearance in search queries. It fits to include it here, as this project is about the Steam software client. It is getting its own project language file, starting here.");
+  break;
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return $PROJECT_LANGUAGE_TWO;
+break;
+/* File info
+* File type: PHP Source file (*.php *.phar *.phtml *.pht *.phps)
+* File version: 1 (2022, Tuesday, November 15th at 5:02 pm PST)
+* Line count (including blank lines and compiler line): 24
+*/
+// End of script
+?>
