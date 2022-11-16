44e5faa536a08d6ae13adf7694c3b3f883f92029
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-15 16:53:34 -0800

    PROJECT_LANG_1_V1.hpp
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/!Software/Krita/C++/PROJECT_LANG_1_V1.hpp
@@ -0,0 +1,31 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// Project language file 1
+// For: /seanpm2001/SeansLifeArchive/Images/Software/Krita/
+// About:
+// I decided to make C++ the first project language file for this project (seanpm2001/SeansLifeArchive_Images_Software_Krita) as C++ is the main language that the Krita graphic design software is written in. This project is about the Krita graphic design software, so it makes sense to use C++ to represent this. It is getting its own project language file, starting here.
+// Start of code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: /seanpm2001/SeansLifeArchive/Images/Software/Krita/\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I decided to make C++ the first project language file for this project (seanpm2001/SeansLifeArchive_Images_Software_Krita) as C++ is the main language that the Krita graphic design software is written in. This project is about the Krita graphic design software, so it makes sense to use C++ to represent this. It is getting its own project language file, starting here.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}}
+return main();
+return 0;
+break;
+// End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.hh *.hxx *.c++ *.h++)
+* File version: 1 (2022, Tuesday, November 15th at 4:35 pm PST)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
