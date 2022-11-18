5def7284073a8efafc5e502b889fc587554a6349
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-17 22:34:26 -0800

    PROJECT_LANG_1_V1.cjs
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Blockly/JavaScript/PROJECT_LANG_1_V1.cjs
@@ -0,0 +1,32 @@
+// Start of script
+
+// Project language file 1
+// For: SNU/2D/ProgrammingTools/IDE/Blockly
+// About:
+// I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / Blockly) as this is a Blockly IDE, and it needs its main languages to be represented here. Blockly is written in mostly JavaScript, while its projects are mostly written in JSON. It is getting its own project language file, starting here.
+
+void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 1");
+    alert("Project language file 1");
+    console.log ("For: SNU/2D/ProgrammingTools/IDE/Blockly");
+    alert("For: SNU/2D/ProgrammingTools/IDE/Blockly");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / Blockly) as this is a Blockly IDE, and it needs its main languages to be represented here. Blockly is written in mostly JavaScript, while its projects are mostly written in JSON. It is getting its own project language file, starting here.");
+    alert("I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / Blockly) as this is a Blockly IDE, and it needs its main languages to be represented here. Blockly is written in mostly JavaScript, while its projects are mostly written in JSON. It is getting its own project language file, starting here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+
+return main();
+break;
+
+}
+
+/* File info
+*| File version: 1 (2022, Thursday, November 17th at 10:29 pm PST)
+*| File type: JavaScript 1.8 source file (*.js *.cjs *.mjs)
+*| Line count (including blank lines and compiler line): 33
+*/
+
+// End of script
