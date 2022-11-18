e57e62ed9c1ecab66019acab36e9c2d8dd61ef28
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-17 21:29:31 -0800

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/SNU/3D/Universe/!Repos/!MilkyWay/!SolarSystem/!SolarSystem/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,32 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: SNU/3D/Universe/Galaxy:Milky_Way/SolarSystem/\n")
+    printf("About:\n\n")
+    break;
+}
+
+return main();
+printf("I decided to make C the 1st project language file for this project (SNU/3D/Universe/Galaxy:Milky_Way/SolarSystem) as what language would you use to represent our Galaxy? For now, I think C is the best representation. It is basically universal in the computing world. I don't think Python would be a good choice, as our galaxy isn't a reptile dominated area, and Python isn't as universal as C. C is getting its own project language, starting here.\n");
+wait 15;
+break;
+exit;
+}
+/* Output */
+/*
+Project language file 1
+For: SNU/3D/Universe/Galaxy:Milky_Way/SolarSystem/
+I decided to make C the 1st project language file for this project (SNU/3D/Universe/Galaxy:Milky_Way/SolarSystem) as what language would you use to represent our Galaxy? For now, I think C is the best representation. It is basically universal in the computing world. I don't think Python would be a good choice, as our galaxy isn't a reptile dominated area, and Python isn't as universal as C. C is getting its own project language, starting here.
+*/
+
+// File info
+
+// File version: 1 (2022, Thursday, November 17th at 9:26 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 33
+
+// End of script
