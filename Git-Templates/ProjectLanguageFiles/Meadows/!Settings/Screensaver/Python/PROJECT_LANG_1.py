c0ae1563b96b0833b3de5b8fa33bd3961a900e4f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-18 22:35:55 -0800

    PROJECT_LANG_1,py [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Meadows/!Settings/Screensaver/Python/PROJECT_LANG_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Start of script
+# Project language file 1
+# For: Meadows/Settings/Screensaver/
+# About
+# I decided to make Python the first project language file for this project (Meadows/Settings/Screensavers) as Python is the default language for writing Meadows components, as I chose it as the default language. It is best suited here, but will be accompanied by other languages. It is getting its own project language file, starting here.
+class projectLanguageFileOne():
+	print("Project language file 1")
+	print("For: Meadows/Settings/Screensaver/")
+	print("About:")
+	print("I decided to make Python the first project language file for this project (Meadows/Settings/Screensavers) as Python is the default language for writing Meadows components, as I chose it as the default language. It is best suited here, but will be accompanied by other languages. It is getting its own project language file, starting here.")
+	noMore = input("Press [ENTER] key to quit")
+	print("The program should now be closed. If the program is still running, try clicking/tapping the close button. If the program is still not responding, try ending the process with your preferred process/task manager.")
+	break
+return projectLanguageFileOne()
+break
+"""
+File info
+# File type: Python 3 source file (*.py *.py3 *.pyt *.pyw)
+# File version: 1 (2022, Friday, November 18th at 10:31 pm PST)
+# Line count (including blank lines and compiler line): 24
+"""
+# End of script
