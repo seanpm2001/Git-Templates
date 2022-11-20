0c87310708b84d7d6f330b5e0333945813dbd44c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-19 22:59:46 -0800

    PROJECT_LANG_1_V1.sh
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Add-ons/Bash.sh/European-Union/Shell/PROJECT_LANG_1_V1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 1st project language file for this project.
+
+function projectLanguageFileOne() {
+  echo ("Project language file #1");
+  wait 1;
+  echo ("For: seanpm2001/BASH.sh/EuropeanUnion");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I chose Shell as the first project language file for this project (Bash.sh, 🇪🇺️ variant) as this project is merely an extension of the BASH (sh/Shell) language. It needs to be represented by the language it is working with. It is getting its own project language file, starting here.");
+  wait 19;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #1
+# For: seanpm2001/BASH.sh/EuropeanUnion
+# About:
+# I chose Shell as the first project language file for this project (Bash.sh, 🇪🇺️ variant) as this project is merely an extension of the BASH (sh/Shell) language. It needs to be represented by the language it is working with. It is getting its own project language file, starting here.
+
+return projectLanguageFileOne();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Saturday, November 19th at 10:57 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
