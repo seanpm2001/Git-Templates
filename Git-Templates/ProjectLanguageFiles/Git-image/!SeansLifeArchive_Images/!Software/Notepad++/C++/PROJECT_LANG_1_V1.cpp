2916233599233b2a999a885e7f1b0a7f29719de7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-20 20:37:14 -0800

    PROJECT_LANG_1_V1.cpp
    An archive of the first version of the first project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/!Software/Notepad++/C++/PROJECT_LANG_1_V1.cpp
@@ -0,0 +1,31 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+// Project language file 1
+// For: /seanpm2001/SeansLifeArchive/Images/Software/Notepad-Plus-Plus/
+// About:
+// I decided to make C++ the first project language file for this project (seanpm2001/SeansLifeArchive_Images_Software_Notepad-Plus-Plus) as C++ is the main language that the Notepad++ text editor/IDE is written in. This project is about the Notepad++ text editor software, so it makes sense to use C++ to represent this. It is getting its own project language file, starting here.
+// Start of code
+class projectLanguageFileOne {
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: /seanpm2001/SeansLifeArchive/Images/Software/Notepad-Plus-Plus/\n";
+    // Neck
+    std::cout << "About:\n";
+    // Body
+    std::cout << "I decided to make C++ the first project language file for this project (seanpm2001/SeansLifeArchive_Images_Software_Notepad-Plus-Plus) as C++ is the main language that the Notepad++ text editor/IDE is written in. This project is about the Notepad++ text editor software, so it makes sense to use C++ to represent this. It is getting its own project language file, starting here.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+}}
+return main();
+return 0;
+break;
+// End of code
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx *.C *.hh *.hxx *.c++ *.h++)
+* File version: 1 (2022, Sunday, November 20th at 8:26 pm PST)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
