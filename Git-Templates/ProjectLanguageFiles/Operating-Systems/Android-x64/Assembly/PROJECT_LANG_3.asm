140b117c24ebe91ffb6972fe5dd98f63ab52fc52
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-22 22:58:48 -0800

    PROJECT_LANG_3.asm [V1]
    Version 1 of the 3rd project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Android-x64/Assembly/PROJECT_LANG_3.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 3
+; For: Android-x64
+; About:
+; I have decided to make Assembly the third project language for this project (Android-x64) as it is needed for core programming alongside C, notably for usage in the bootloader. It is getting its own project language file, starting here.
+
+; File info
+; File version: 1 (2022, Tuesday, November 22nd at 10:48 pm PST)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
