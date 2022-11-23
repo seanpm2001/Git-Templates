2f200ce17811b68a41874af7a9a28b372bb2ce50
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-22 23:14:52 -0800

    PROJECT_LANG_2_V1.java
    An archive of the first version of the 2nd project language file for this project.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Android-x32/Java/PROJECT_LANG_2_V1.java
@@ -0,0 +1,27 @@
+// Start of script
+/* Divider */
+// Prject language file 2
+// For: Android x32
+// About:
+// I decided to make Java the 2nd project language file for this project (Android-x32) as the model operating system (Android) heavily uses Java. I feel it is a viable platform. Due to legacy roots, Kotlin has been eliminated from Android x32, but may be available in Android x64. Java is to be used for application programming and user interfaces. It is getting its own project language file, starting here.
+/* Divider */
+public class projectLanguageFileTwo() {
+	public static void main(String args) {
+		System.out.println("Project language file 2");
+		System.out.println("For: Android x32");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 2nd project language file for this project (Android-x32) as the model operating system (Android) heavily uses Java. I feel it is a viable platform. Due to legacy roots, Kotlin has been eliminated from Android x32, but may be available in Android x64. Java is to be used for application programming and user interfaces. It is getting its own project language file, starting here.");
+		break;
+}
+return main();
+int y = 2;
+System.out.println("Current project language file: " + int(y) + " (Java 10)");
+break; } // No exit command exists
+/* Divider */
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Tuesday, November 22nd at 11:10 pm PST)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 28
+*/
+// End of script
