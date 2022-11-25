50abd9ec5fad71697855bd641ed944774ae9855a
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-24 17:08:34 -0800

    PROJECT_LANG_2_V1.php
    An archive of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Education/PhD_In_PHP/Hack/PROJECT_LANG_2_V1.php
@@ -0,0 +1,15 @@
+<<__EntryPoint>>
+// Start of script
+function projectLanguageTwo(): void {
+  echo 'Project language file 2';
+  echo 'For: seanpm2001/PhD-In-PHP';
+  echo 'About: ';
+  echo 'I chose Hack as the second project language file for this project (seanpm2001/PhD-in-PHP) as despite not being purely PHP, it is heavily influenced by PHP. On 2022, November 23rd, I expanded this projects scope by including languages that are influenced by PHP as extra credit courses and assignments. Hack was the first language to come up after PHP. It is getting its own project language file, starting here.';
+}
+return projectLanguageTwo();
+/* File info
+* File type: Hack source file (*.hh *.hck *.php *.hack)
+* File version: 1 (2022, Thursday, November 24th at 5:00 pm PST)
+* Line count (including blank lines and compiler line): 16
+*/
+// End of script
