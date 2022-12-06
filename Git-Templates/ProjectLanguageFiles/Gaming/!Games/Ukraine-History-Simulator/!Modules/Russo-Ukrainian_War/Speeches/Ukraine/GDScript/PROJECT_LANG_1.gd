8b518c19a51a073f2f9b4d3a4e5831ea13dd0169
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-12-05 23:01:48 -0800

    PROJECT_LANG_1.gd [V2]
    Version 2 of the first project language file for this directory set.

--- Git-Templates/ProjectLanguageFiles/Gaming/!Games/Ukraine-History-Simulator/!Modules/Russo-Ukrainian_War/Speeches/Ukraine/GDScript/PROJECT_LANG_1.gd
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Ukraine-History-Simulator/!Modules/Russo-Ukrainian_War/Speeches/Ukraine/GDScript/PROJECT_LANG_1.gd
@@ -3,9 +3,9 @@
 # Project language file output
 func _plang1():
 	print("Project language file 1")
-	print("For: Ukraine History Simulator/Modules/Russo-UkrainianWar/Ukrainian-Speeches-Map")
+	print("For: Ukraine History Simulator/Modules/Russo-UkrainianWar/Ukrainian-Speeches")
 	print("About")
-	print("I chose GDScript (GoDot Script) as the first and main project language file for this project (Seanpm2001/Ukraine-History-Simulator/Modules/Russo-UkrainianWar/Ukrainian-Speeches-Map) as GDScript is the language that the main project is written in. Initially, I thought Python would be a good interoperating language, but I later decided that GDScript worked best for consistency. It is getting its own project language file, starting here.")
+	print("I chose GDScript (GoDot Script) as the first and main project language file for this project (Seanpm2001/Ukraine-History-Simulator/Modules/Russo-UkrainianWar/Ukrainian-Speeches) as GDScript is the language that the main project is written in. Initially, I thought Python would be a good interoperating language, but I later decided that GDScript worked best for consistency. It is getting its own project language file, starting here.")
 	break
 return _plang1()
 break
@@ -13,13 +13,13 @@ break
 # Output:
 
 # Project language file 1
-# For: Ukraine History Simulator / Modules / Russo-Ukrainian War / Ukrainian Speeches Map
+# For: Ukraine History Simulator / Modules / Russo-Ukrainian War / Ukrainian Speeches
 # About:
-# I chose GDScript (GoDot Script) as the first and main project language file for this project (Seanpm2001/Ukraine-History-Simulator/Modules/Russo-UkrainianWar/Ukrainian-Speeches-Map) as GDScript is the language that the main project is written in. Initially, I thought Python would be a good interoperating language, but I later decided that GDScript worked best for consistency. It is getting its own project language file, starting here.
+# I chose GDScript (GoDot Script) as the first and main project language file for this project (Seanpm2001/Ukraine-History-Simulator/Modules/Russo-UkrainianWar/Ukrainian-Speeches) as GDScript is the language that the main project is written in. Initially, I thought Python would be a good interoperating language, but I later decided that GDScript worked best for consistency. It is getting its own project language file, starting here.
 
 # File info
 # File type: GDscript (GoDot Script) source file (*.gd)
-# File version: 1 (2022, Monday, December 5th at 10:55 pm PST)
+# File version: 2 (2022, Monday, December 5th at 11:01 pm PST)
 # Line count (including blank lines and compiler line): 26
 
 # End of script
