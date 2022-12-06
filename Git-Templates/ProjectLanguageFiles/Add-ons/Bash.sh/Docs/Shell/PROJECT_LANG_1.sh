964fdae368e4abfffd27148a78b9d063639de9b0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-12-05 23:16:08 -0800

    PROJECT_LANG_1.sh [V1]
    Version 1 of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Add-ons/Bash.sh/Docs/Shell/PROJECT_LANG_1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Start of script
+# The 1st project language file for this project.
+
+function projectLanguageFileOne() {
+  echo ("Project language file #1");
+  wait 1;
+  echo ("For: seanpm2001/BASH.sh_Docs/");
+  wait 1;
+  echo ("About:");
+  wait 0.2;
+  echo ("I chose Shell as the first project language file for this project (BASH.sh/Docs) as this project is about a set of extensions for the BASH (sh/Shell) language. It needs to be represented by the language it is working with. It is getting its own project language file, starting here.");
+  wait 19;
+  return 0;
+  break;
+}
+
+# Output:
+
+# Project language file #1
+# For: seanpm2001/BASH.sh/Docs/
+# About:
+# I chose Shell as the first project language file for this project (BASH.sh/Docs) as this project is about a set of extensions for the BASH (sh/Shell) language. It needs to be represented by the language it is working with. It is getting its own project language file, starting here.
+
+return projectLanguageFileOne();
+break;
+return 0;
+exit;
+
+# File info
+
+# File version: 1 (2022, Monday, December 5th at 11:15 pm PST)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 37
+
+# End of script
