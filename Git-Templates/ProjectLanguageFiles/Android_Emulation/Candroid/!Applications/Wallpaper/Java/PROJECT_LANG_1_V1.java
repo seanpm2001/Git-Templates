2836fcf530ff26d6a43e9adea600ab213fb235b0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-12-07 17:55:28 -0800

    PROJECT_LANG_1_V1.java
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Android_Emulation/Candroid/!Applications/Wallpaper/Java/PROJECT_LANG_1_V1.java
@@ -0,0 +1,24 @@
+// Start of script
+// Project language file 1
+// For: Candroid-Wallpaper
+// About:
+// I decided to make Java the 1st project language file for this project (Candroid-Wallpaper) as the native application langugae for the framework this program runs on is Java. Java is the best-suited language here for this reason. It is getting its own project language file, starting here.
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: Candroid-Wallpaper");
+		System.out.println("About:");
+		System.out.println("I decided to make Java the 1st project language file for this project (Candroid-Wallpaper) as the native application langugae for the framework this program runs on is Java. Java is the best-suited language here for this reason. It is getting its own project language file, starting here.");
+		break;
+}
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 11)");
+break; } // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Wednesday, December 7th at 5:54 pm PST)
+* File type: Java 11 Source file (*.java)
+* Line count (including blank lines and compiler line): 25
+*/
+// End of script
