60199bb14d0006bdc518f9f02e6136c63f770cc9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-12-10 19:31:25 -0800

    PROJECT_LANG_1_V2.pl
    An archive of the 2nd version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Memorial-Projects/Perl-Harbor/Perl/PROJECT_LANG_1_V2.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+# Start of script
+
+# Project language file 1
+print "Project language file 1";
+print "For: Perl Harbor";
+print "About:";
+print "Perl Harbor is written in Perl, due to its 1 letter difference with Pearl, and is used to remember and commemorate the attacks on Pearl Harbor on 1941, Sunday December 7th, with the 2399 people killed (2335 when not counting the attacking Kamikaze pilots) it is accompanied with Perl6, and Pod, but not Raku. It is getting its own project language file, starting here.";
+break;
+wait "95";
+exit;
+
+# Output
+
+# Project language file 1
+# For: Perl Harbor
+# About:
+# Perl Harbor is written in Perl, due to its 1 letter difference with Pearl, and is used to remember and commemorate the attacks on Pearl Harbor on 1941, Sunday December 7th, with the 2399 people killed (2335 when not counting the attacking Kamikaze pilots) it is accompanied with Perl6, and Pod, but not Raku. It is getting its own project language file, starting here.
+
+# File info
+# File type: Perl source file (*.pl)
+# File version: 2 (2022, Saturday, December 10th at 7:30 pm PST)
+# Line count (including blank lines and compiler line): 26
+
+# End of script
