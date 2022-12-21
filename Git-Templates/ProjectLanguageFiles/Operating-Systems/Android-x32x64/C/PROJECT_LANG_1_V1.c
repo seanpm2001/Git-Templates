c40bccbcdc51d319042f7b9854852c73ec34aec0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-12-20 23:21:43 -0800

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Android-x32x64/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: Android x32x64\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the first project language file for this project (Android-x32x64) as the core of the system needs to be written in C for functionality. It will be supplemented with Assembly language, Java, and other languages. It is in use for core system functions and programming. It is not to be used for user interfaces. It is getting its own project language file, starting here.
+
+return main();
+printf(" chose C as the first project language file for this project (Android-x32x64) as the core of the system needs to be written in C for functionality. It will be supplemented with Assembly language, Java, and other languages. It is in use for core system functions and programming. It is not to be used for user interfaces. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2022, Tuesday, December 20th at 11:19 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
