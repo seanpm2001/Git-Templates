ebada0638a7d4dc5fb3b95c50e20a967237b3f64
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-12-28 23:43:20 -0800

    PROJECT_LANG_2.asm [V1]
    Version 1 of the first version of the 2nd project language file for this directory set.

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Android-x32/!Android/LiveCD/4.1A/Assembly/PROJECT_LANG_2.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 2
+; For: Android-x32/LiveCD_4.1A
+; About:
+; I have decided to make Assembly the second project language for this project (Android-x32/LiveCD_4.1A) as it is needed for core programming alongside C, notably for usage in the bootloader. It is getting its own project language file, starting here.
+
+; File info
+; File version: 1 (2022, Wednesday, December 28th at 11:42 pm PST)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
