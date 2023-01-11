b0a5b97bcf1e78f9ae024b44c9d3619de6a7835e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-10 16:26:46 -0800

    PROJECT_LANG_1.cjs
    PROJECT_LANG_1.cjs

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/ProtonCalendar/JavaScript/PROJECT_LANG_1.cjs
@@ -0,0 +1,32 @@
+// Start of script
+
+class projectLanguageFileOne() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 1");
+    alert("Project language file 1");
+    console.log ("For: seanpm2001/SeansLifeArchive_Images_ProtonCalendar");
+    alert("For: seanpm2001/SeansLifeArchive_Images_ProtonCalendar");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the first (main) project language for this project (seanpm2001/SeansLifeArchive_Images_ProtonCalendar) as the only source repository I can find for ProtonCalendar is written mostly in TypeScript, and mainly JavaScript, so I am assuming that is the language ProtonCalendar is written in. This project is solely about ProtonCalendar, so it fits. It is getting its own project language file, starting here.");
+    alert("I chose JavaScript as the first (main) project language for this project (seanpm2001/SeansLifeArchive_Images_ProtonCalendar) as the only source repository I can find for ProtonCalendar is written mostly in TypeScript, and mainly JavaScript, so I am assuming that is the language ProtonCalendar is written in. This project is solely about ProtonCalendar, so it fits. It is getting its own project language file, starting here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+}
+
+// Output: (2x)
+// Project language file 1
+// For: seanpm2001/SeansLifeArchive_Images_ProtonCalendar
+// About:
+// I chose JavaScript as the first (main) project language for this project (seanpm2001/SeansLifeArchive_Images_ProtonCalendar) as the only source repository I can find for ProtonCalendar is written mostly in TypeScript, and mainly JavaScript, so I am assuming that is the language ProtonCalendar is written in. This project is solely about ProtonCalendar, so it fits. It is getting its own project language file, starting here.
+
+/* File info
+* File version: 1 (2023, Tuesday, January 10th at 4:25 pm PST)
+* File type: JavaScript 1.8 source file (*.js *.cjs *.mjs)
+* Line count (including blank lines and compiler line): 33
+*/
+
+// End of script
