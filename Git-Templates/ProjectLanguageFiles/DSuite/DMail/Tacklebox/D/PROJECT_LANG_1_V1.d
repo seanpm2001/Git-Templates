e2f6b7bc5d073084035b0afcb5122d8be809fe87
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-13 18:54:06 -0800

    PROJECT_LANG_1_V1.d
    PROJECT_LANG_1_V1.d

---
+++ Git-Templates/ProjectLanguageFiles/DSuite/DMail/Tacklebox/D/PROJECT_LANG_1_V1.d
@@ -0,0 +1,33 @@
+#!/usr/bin/rdmd
+// Start of script
+// Import section
+import std.stdio : writeln;
+import std.range : iota;
+import std.parallelism : parallel;
+/* DIVIDER */
+// Main section
+void main()
+{
+  writeln("Project language file 1");
+  writeln("For: DMail Tacklebox");
+  writeln("About:");
+	writeln ("I chose D as the first project language file for this project (DMail Tacklebox) as I wanted to use the D language for this project in for general software development and backend development. It is getting its own project language file, starting here.");
+}
+return main();
+return 0;
+break;
+// End of functionality sections
+
+// Project language file 1
+// For: DMail Tacklebox
+// About:
+// I chose D as the first project language file for this project (DMail Tacklebox) as I wanted to use the D language for this project in for general software development and backend development. It is getting its own project language file, starting here.
+
+/* DIVIDER */
+
+// File info
+// File version: 1 (2023, Friday, January 13th at 6:53 pm PST)
+// File type: D programming language source file (*.d)
+// Line count (including blank lines and compiler line): 34
+
+// End of script
