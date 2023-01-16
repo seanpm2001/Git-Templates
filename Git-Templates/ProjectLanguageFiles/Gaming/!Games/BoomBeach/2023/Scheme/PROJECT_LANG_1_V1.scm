5c57ee939b2843442ef6d53a41796be810faabe6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-15 17:46:01 -0800

    PROJECT_LANG_1_V1.scm
    PROJECT_LANG_1_V1.scm

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/BoomBeach/2023/Scheme/PROJECT_LANG_1_V1.scm
@@ -0,0 +1,19 @@
+;; Start of script
+
+;; Project language file 1
+;; For: seanpm2001/SeansLifeArchive_Images_Boom-Beach_Y2023
+;; About:
+;; Boom Beach is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of something commonly done (scheming) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Scheme. It is getting its own project language file, starting here.
+
+(let ((message1 (lambda (p) (display "Project language file 1\nFor: seanpm2001/SeansLifeArchive_Images_Boom-Beach_Y2023\nAbout:\nBoom Beach is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of something commonly done (scheming) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Scheme. It is getting its own project language file, starting here." p) (newline p))))
+  (call-with-output-file "message1outputfile" message1))
+
+;; I would have gone with Ceylon, but I want to continue the Scheme naming scheme
+
+;; File info
+
+;; File version: 1 (2023, Sunday, January 15th at 5:43 pm PST)
+;; File type: Scheme source file (*.scm *.sld *.sls  *.sps *.ss)
+;; Line count (including blank lines and compiler line): 20
+
+;; End of script
