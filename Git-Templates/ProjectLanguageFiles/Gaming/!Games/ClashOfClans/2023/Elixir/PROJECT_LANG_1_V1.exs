bd8b39f7dae0439407d41e502585328058b809ff
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-15 17:33:40 -0800

    PROJECT_LANG_1_V1.exs
    PROJECT_LANG_1_V1.exs

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/ClashOfClans/2023/Elixir/PROJECT_LANG_1_V1.exs
@@ -0,0 +1,19 @@
+# Start of script
+
+# Project language file 1
+# For: seanpm2001/SeansLifeArchive_Images_Clash-of-Clans_Y2023/
+# About:
+# Clash of Clans is closed source, and I don't know what language it is written in (my closest guess is ActionScript, as it was reported to originally be written in that language) s so I chose the one programming language with the name of an in-game currency (elixir) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Elixir. It is getting its own project language file, starting here.
+
+print ("Project language file 1")
+print ("For: seanpm2001/SeansLifeArchive_Images_Clash-of-Clans_Y2023/")
+print ("About: ")
+print ("Clash of Clans is closed source, and I don't know what language it is written in (my closest guess is ActionScript, as it was reported to originally be written in that language) so I chose the one programming language with the name of an in-game currency (elixir) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Elixir")
+
+# File info
+
+# File type: Elixir source file (*.ex *.exs)
+# File version: 1 (2023, Sunday, January 15th at 5:31 pm PST)
+# Line count (including blank lines and compiler line): 20
+
+# End of script
