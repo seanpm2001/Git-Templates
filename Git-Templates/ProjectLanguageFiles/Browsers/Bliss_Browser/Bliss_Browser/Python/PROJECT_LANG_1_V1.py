ab5aeffddb6ea7ea494740ba1368f02cb9345424
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-17 18:35:54 -0800

    PROJECT_LANG_1_V1.py
    PROJECT_LANG_1_V1.py

---
+++ Git-Templates/ProjectLanguageFiles/Browsers/Bliss_Browser/Bliss_Browser/Python/PROJECT_LANG_1_V1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Start of script
+
+class projectLanguageFileOne():
+
+def plang1():
+	print("Project Language File 1\n")
+	print("For: seanpm2001/Bliss_Browser")
+	print("About:")
+	print("I decided to make Python the first project language file for this project (seanpm2001/Bliss_Browser) as the browser is written mostly in Python, as part of a plan to write the browser mainly in Python instead of another language like Rust or C++. It is getting its own project language file, starting here.")
+	break
+
+return plang1()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/process manager")
+break
+return 0
+
+''' Output '''
+
+# Project language file 1
+# For: seanpm2001/Bliss_Browser
+# About
+# I decided to make Python the first project language file for this project (seanpm2001/Bliss_Browser) as the browser is written mostly in Python, as part of a plan to write the browser mainly in Python instead of another language like Rust or C++. It is getting its own project language file, starting here.
+
+""" """
+
+# File info
+# File version: 1 (2023, Tuesday, January 17th at 6:33 pm PST)
+# File type: Python 3 source file (*.py *.pyw *.py3)
+# Line count (including blank lines and compiler line): 33
+# End of script
