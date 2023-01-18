77b31901cdde6f9a49296c90d64c8768ba782a5c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-17 18:57:53 -0800

    PROJECT_LANG_1_V1.c
    PROJECT_LANG_1_V1.c

---
+++ Git-Templates/ProjectLanguageFiles/Browsers/Bliss_Browser/!OSMode/OSMode/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: Bliss Browser OSMode\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the first project language file for this project (Bliss_Browser_OSMode) as the main configuration for OSMode is planned to be written in C. The shell will be placed on top of this, C is used to connect different variants of the shell to the operating system. It is getting its own project language file, starting here.
+
+return main();
+printf("I chose C as the first project language file for this project (Bliss_Browser_OSMode) as the main configuration for OSMode is planned to be written in C. The shell will be placed on top of this, C is used to connect different variants of the shell to the operating system. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2023, Tuesday, January 17th at 6:56 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
