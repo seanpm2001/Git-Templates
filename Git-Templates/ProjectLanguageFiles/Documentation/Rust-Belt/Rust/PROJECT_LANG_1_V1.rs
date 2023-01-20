d40a014ce81af9e7abd71d1f1b5aeaa05bbfe621
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-20 14:12:24 -0800

    PROJECT_LANG_1_V1.rs
    PROJECT_LANG_1_V1.rs

---
+++ Git-Templates/ProjectLanguageFiles/Documentation/Rust-Belt/Rust/PROJECT_LANG_1_V1.rs
@@ -0,0 +1,26 @@
+// Start of script
+
+// Project language file 1
+// For: seanpm2001/Rust-Belt
+// About:
+// I decided to make Rust the main project language file for this project (seanpm2001/Rust-Belt) as this project is solely about projects written in the Rust programming language. Rust is being used, as it fits here. It is getting its own project language file, starting here.
+
+/* Program */
+fn project-language-file-one() {
+    println!("Project language file 1\n");
+    println!("For: seanpm2001/Rust-Belt\n");
+    println!("About:\n");
+    println!("I decided to make Rust the main project language file for this project (seanpm2001/Rust-Belt) as this project is solely about projects written in the Rust programming language. Rust is being used, as it fits here. It is getting its own project language file, starting here.\n");
+    break;
+}
+return project-language-file-one();
+break;
+/* End program */
+
+// File info
+
+// File version: 1 (2023, Friday, January 20th at 2:10 pm PST)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 27
+
+// End of script
