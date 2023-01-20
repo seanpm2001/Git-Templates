0d7dd03834230e5d69a0a2b635497556bf4bf70e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-20 14:05:05 -0800

    PROJECT_LANG_1_V1.elm
    PROJECT_LANG_1_V1.elm

---
+++ Git-Templates/ProjectLanguageFiles/Documentation/Climate-Activism/Elm/PROJECT_LANG_1_V1.elm
@@ -0,0 +1,16 @@
+-- Start of script
+-- Project language file 1
+-- For: seanpm2001/Climate-Activism
+-- About:
+-- I decided to make Elm the main (first) project language for this project (seanpm2001/Climate-Activism) as this project is purely documentation, but I didn't feel that Markdown was a significant enough language to represent this project. I wanted to represent it with a nature themed language, and I don't use Elm very much, so I chose it as the main project language file for this project. It is getting its own project language file, starting here.
+-- Start
+projectLanguageOne : String
+projectLanguageOne =
+    "Project language file 1\nFor: seanpm2001/Climate-Activism\nAbout:\nI decided to make Elm the main (first) project language for this project (seanpm2001/Climate-Activism) as this project is purely documentation, but I didn't feel that Markdown was a significant enough language to represent this project. I wanted to represent it with a nature themed language, and I don't use Elm very much, so I chose it as the main project language file for this project. It is getting its own project language file, starting here.\n"
+-- End
+{- File info
+File type: Elm source file (*.elm)
+File version: 1 (2023, Friday, January 20th at 2:04 pm PST)
+Line count (including blank lines and compiler line): 17
+-}
+-- End of script
