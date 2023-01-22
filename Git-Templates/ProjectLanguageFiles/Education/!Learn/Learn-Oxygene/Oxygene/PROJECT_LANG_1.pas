2344505a3dec09ce30e735703026b22f9e21bb95
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-21 20:19:42 -0800

    PROJECT_LANG_1.pas
    PROJECT_LANG_1.pas

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Oxygene/Oxygene/PROJECT_LANG_1.pas
@@ -0,0 +1,18 @@
+implementation
+// Start of script
+class method ConsoleApp.Main;
+begin
+  Console.WriteLine('Project language file 1');
+  Console.WriteLine('For: seanpm2001/Learn-Oxygene');
+  Console.WriteLine('About:');
+  Console.WriteLine('I decided to make Oxygene the main project language file for this project (Seanpm2001/Learn-Oxygene) as Oxygene is the language this project is dedicated to, because this project is about learning the Oxygene programming language. It only makes sense to Oxygene the official language for this project. It is getting its own project language file, starting here.');
+end;
+
+end.
+
+// File info
+// File type: Oxygene source file (*.oxygene *.pas)
+// File version: 1 (2023, Saturday, January 21st at 8:17 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
