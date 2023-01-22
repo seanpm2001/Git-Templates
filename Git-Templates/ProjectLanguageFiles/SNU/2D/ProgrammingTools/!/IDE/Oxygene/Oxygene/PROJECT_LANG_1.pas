c9a29851d26d2ca2b7d1d1686cd6f7493c053cd1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-21 20:22:55 -0800

    PROJECT_LANG_1.pas
    PROJECT_LANG_1.pas

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Oxygene/Oxygene/PROJECT_LANG_1.pas
@@ -0,0 +1,18 @@
+implementation
+// Start of script
+class method ConsoleApp.Main;
+begin
+  Console.WriteLine('Project language file 1');
+  Console.WriteLine('For: SNU/2D/ProgrammingTools/IDE/Oxygene');
+  Console.WriteLine('About:');
+  Console.WriteLine('I decided to make Oxygene the main project language file for this project (SNU / 2D / Programming Tools / IDE / Oxygene) as this is a Oxygene IDE, and it needs its main language to be represented here.');
+end;
+
+end.
+
+// File info
+// File type: Oxygene source file (*.oxygene *.pas)
+// File version: 1 (2023, Saturday, January 21st at 8:21 pm PST)
+// Line count (including blank lines and compiler line): 19
+
+// End of script
