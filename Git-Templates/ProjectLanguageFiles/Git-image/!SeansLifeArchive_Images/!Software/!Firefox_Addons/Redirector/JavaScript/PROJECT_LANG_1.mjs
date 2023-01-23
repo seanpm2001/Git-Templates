79ec645db047cbb7ac3ab67eb3d21aaea137870c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-22 16:12:24 -0800

    PROJECT_LANG_1.mjs
    PROJECT_LANG_1.mjs

---
+++ Git-Templates/ProjectLanguageFiles/Git-image/!SeansLifeArchive_Images/!Software/!Firefox_Addons/Redirector/JavaScript/PROJECT_LANG_1.mjs
@@ -0,0 +1,32 @@
+// Start of script
+
+class projectLanguageFileOne() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 1");
+    alert("Project language file 1");
+    console.log ("For: seanpm2001/SeansLifeArchive_Images_FirefoxAddon_Redirector");
+    alert("For: seanpm2001/SeansLifeArchive_Images_FirefoxAddon_Redirector");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose JavaScript as the first (main) project language for this project (seanpm2001/SeansLifeArchive_Images_FirefoxAddon_Redirector) as the addon this project is documenting is written mostly in JavaScript. I want to represent the original project, so JavaScript was chosen as the main project language file. It is getting its own project language file, starting here.");
+    alert("I chose JavaScript as the first (main) project language for this project (seanpm2001/SeansLifeArchive_Images_FirefoxAddon_Redirector) as the addon this project is documenting is written mostly in JavaScript. I want to represent the original project, so JavaScript was chosen as the main project language file. It is getting its own project language file, starting here.");
+    break;
+} // Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+}
+
+// Output: (2x)
+// Project language file 1
+// For: seanpm2001/SeansLifeArchive_Images_FirefoxAddon_Redirector
+// About:
+// I chose JavaScript as the first (main) project language for this project (seanpm2001/SeansLifeArchive_Images_FirefoxAddon_Redirector) as the addon this project is documenting is written mostly in JavaScript. I want to represent the original project, so JavaScript was chosen as the main project language file. It is getting its own project language file, starting here.
+
+/* File info
+* File version: 1 (2023, Sunday, January 22nd at 4:10 pm PST)
+* File type: JavaScript 1.8 source file (*.js *.cjs *.mjs)
+* Line count (including blank lines and compiler line): 33
+*/
+
+// End of script
