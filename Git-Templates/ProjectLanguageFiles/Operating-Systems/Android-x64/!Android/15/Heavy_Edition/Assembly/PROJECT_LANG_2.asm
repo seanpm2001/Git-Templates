674ad513f300561aa4d72c5d5ebe44df9b9225df
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-22 20:43:30 -0800

    PROJECT_LANG_2.asm
    PROJECT_LANG_2.asm

---
+++ Git-Templates/ProjectLanguageFiles/Operating-Systems/Android-x64/!Android/15/Heavy_Edition/Assembly/PROJECT_LANG_2.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 2
+; For: Android-x64/Android-15.0/Heavy_Edition
+; About:
+; I have decided to make Assembly the second project language for this project (Android-x64/Android-15.0/Heavy_Edition) as it is needed for core programming alongside C, notably for usage in the bootloader. It is getting its own project language file, starting here.
+
+; File info
+; File version: 1 (2023, Sunday, January 22nd at 8:43 pm PST)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
