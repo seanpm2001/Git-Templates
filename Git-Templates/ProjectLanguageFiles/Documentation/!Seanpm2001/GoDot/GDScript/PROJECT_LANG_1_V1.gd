10906d95d268c6ce7887ac77122a569237b759bf
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-26 20:42:58 -0800

    PROJECT_LANG_1_V1.gd
    PROJECT_LANG_1_V1.gd

---
+++ Git-Templates/ProjectLanguageFiles/Documentation/!Seanpm2001/GoDot/GDScript/PROJECT_LANG_1_V1.gd
@@ -0,0 +1,23 @@
+# Start of script
+
+# Project language file output
+func _ready():
+	print("Project language file 1")
+	print("For: seanpm2001/Seanpm2001-GoDot")
+	print("About")
+	print("I chose GDScript (GoDot Script) as the first project language file for this project (Seanpm2001/Learn-GDScript) as this project is about documenting my projects written in the GoDot language. Markdown is in use for the documentation, but GDScript will be used for representation. It is getting its own project language file, starting here.")
+	# Output
+	# Project language file 1
+	# For: seanpm2001/Seanpm2001-GoDot
+	# About:
+	# I chose GDScript (GoDot Script) as the first project language file for this project (Seanpm2001/Learn-GDScript) as this project is about documenting my projects written in the GoDot language. Markdown is in use for the documentation, but GDScript will be used for representation. It is getting its own project language file, starting here.
+	break
+return _ready()
+break
+
+# File info
+# File type: GDscript (GoDot Script) source file (*.gd)
+# File version: 1 (2023, Thursday, January 26th at 8:42 pm PST)
+# Line count (including blank lines and compiler line): 24
+
+# End of script
