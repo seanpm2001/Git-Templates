79ef6f24f420baec1f05c0463e8e2e77aaeef199
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-01-29 14:05:49 -0800

    PROJECT_LANG_1_V1.c
    PROJECT_LANG_1_V1.c

---
+++ Git-Templates/ProjectLanguageFiles/Browsers/Bliss_Browser/!OSMode/Gentoo-Shell/C/PROJECT_LANG_1_V1.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: Bliss Browser OSMode/Gentoo Shell\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the first project language file for this project (Bliss_Browser_OSMode/Gentoo She.l) as I wanted to choose a language that runs as a bare metal language, and one that would require as few dependencies, to make Gentoo installations a bit easier. C is also a common language for OSMode shells for Bliss Browser. It is getting its own project language file, starting here.
+
+return main();
+printf("I chose C as the first project language file for this project (Bliss_Browser_OSMode/Gentoo Shell) as I wanted to choose a language that runs as a bare metal language, and one that would require as few dependencies, to make Gentoo installations a bit easier. C is also a common language for OSMode shells for Bliss Browser. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2023, Sunday, January 29th at 2:04 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
