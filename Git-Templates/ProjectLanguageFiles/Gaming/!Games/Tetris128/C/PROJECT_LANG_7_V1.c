52317a6cfa36f68e63ffde8a90dfc8abf4e2d6dc
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-21 17:57:22 -0800

    PROJECT_LANG_7_V1.c
    PROJECT_LANG_7_V1.c

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Tetris128/C/PROJECT_LANG_7_V1.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileSeven {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 7\n");
+    printf("For: Tetris128\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the seventh project language file for this project (Tetris128) as C is needed for core application, bare metal functionality on several parts of this project. It is getting its own project language file, starting here.
+
+return main();
+printf("I chose C as the seventh project language file for this project (Tetris128) as C is needed for core application, bare metal functionality on several parts of this project. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2023, Tuesday, February 21st at 5:56 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
