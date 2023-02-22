1c426b1d4a27cf8e41c67f8acef34a719a14a8d8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-21 17:53:06 -0800

    PROJECT_LANG_6_V1.glsl
    PROJECT_LANG_6_V1.glsl

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Tetris128/GLSL/PROJECT_LANG_6_V1.glsl
@@ -0,0 +1,17 @@
+// Start of script
+// Sample file usage: https://www.lighthouse3d.com/tutorials/glsl-tutorial/hello-world/ (warning: insecure connection)
+#define Project_Language_File_Six
+// Project language file 6
+// For: Tetrs128
+// I decided to make GLSL (OpenGL Shading Language) the sixth project language file for this project (Tetris128) as GLSL is the main shading language used on this project. It is getting its own project languzge file, starting here.
+void main()
+{
+    outputF = vec4(1.0, 0.0, 0.0, 1.0);
+}
+#endif
+/* File info
+* File type: GLSL source file (*.glsl)
+* File version: 1 (2023, Tuesday, February 21st at 5:52 pm PST)
+* Line count (including blank lines and compiler line): 18
+*/
+// End of script
