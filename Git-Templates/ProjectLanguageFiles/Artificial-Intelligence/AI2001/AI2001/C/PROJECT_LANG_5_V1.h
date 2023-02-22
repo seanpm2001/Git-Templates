7c19fbf0891b0b495372f826810e73f7e2ce6116
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-22 11:20:24 -0800

    PROJECT_LANG_5_V1.h
    PROJECT_LANG_5_V1.h

---
+++ Git-Templates/ProjectLanguageFiles/Artificial-Intelligence/AI2001/AI2001/C/PROJECT_LANG_5_V1.h
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileFive {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 5\n");
+    printf("For: AI2001\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the fifth project language file for this project (AI2001) as the wrapper and core of the AI2001 engine is to be written in C for portability, stability, and efficiency purposes. It is getting its own project language file, starting here
+
+return main();
+printf("I chose C as the fifth project language file for this project (AI2001) as the wrapper and core of the AI2001 engine is to be written in C for portability, stability, and efficiency purposes. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2023, Wednesday, February 22nd at 11:18 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
