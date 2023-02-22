acbfc474885dda1d3205dff9628cd1df7a896781
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-21 17:48:06 -0800

    PROJECT_LANG_4_V1.rs
    PROJECT_LANG_4_V1.rs

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Tetris128/Rust/PROJECT_LANG_4_V1.rs
@@ -0,0 +1,16 @@
+// Start of script
+// I chose Rust as the first main project language for this project (SWave Starter) as Rust is a stable and fast language that I decided would be good for this project. I also wanted to maintain software diversity and use Rust instead of Python.
+fn main() {
+  println!("Project language file 4 (Rust)");
+  println!("For: Tetris128");
+  println!("About:");
+  println!("I chose Rust as the fourth project language file for this project (Seanpm2001/Tetris128) as Rust is one of the languages this project uses heavily (it has an engine written in the language) and I wanted to have a project language file to represent it.");
+  break;
+}
+return main();
+break;
+// File info
+// File version: 1 (Friday, 2021 December 17th at 6:48 pm)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 17
+// End of script
