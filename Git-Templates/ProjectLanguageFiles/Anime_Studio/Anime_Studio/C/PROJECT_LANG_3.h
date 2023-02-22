294fc921d570a1e6e28084f2db59ca30022f8607
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-21 21:59:42 -0800

    PROJECT_LANG_3.h
    PROJECT_LANG_3.h

---
+++ Git-Templates/ProjectLanguageFiles/Anime_Studio/Anime_Studio/C/PROJECT_LANG_3.h
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileThree {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 3\n");
+    printf("For: Anime Studio\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the third project language file for this project (Anime Studio) as the wrapper and core of Anime Studio is written in C for stability, portability, and efficiency purposes. It is getting its own project language file, starting here.
+
+return main();
+printf("I chose C as the third project language file for this project (Anime Studio) as the wrapper and core of Anime Studio is written in C for stability, portability, and efficiency purposes. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2023, Tuesday, February 21st at 9:58 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
