5fd0e8fa0b493fb5fc150aec9a5c6812a9a88712
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-21 17:48:39 -0800

    PROJECT_LANG_5.gd
    PROJECT_LANG_5.gd

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/Tetris128/GDScript/PROJECT_LANG_5.gd
@@ -0,0 +1,20 @@
+# Start of script
+
+# I chose GDScript (GoDot Script) as the fifth project language file for this project (Seanpm2001/Tetris128) as GDScript is one of the languages this project uses heavily (it has an engine written in the language) and I wanted to have a project language file to represent it. GoDot is used for better cross platform support until the other engines are ready, and also just to have a GoDot version of the program.
+
+# Project language file output
+func _ready():
+	print("Project language file 5")
+	print("For: Tetris128")
+	print("About")
+	print("I chose GDScript (GoDot Script) as the fifth project language file for this project (Seanpm2001/Tetris128) as GDScript is one of the languages this project uses heavily (it has an engine written in the language) and I wanted to have a project language file to represent it. GoDot is used for better cross platform support until the other engines are ready, and also just to have a GoDot version of the program.")
+	break
+return _ready()
+break
+
+# File info
+# File type: GDscript (GoDot Script) source file (*.gd)
+# File version: 1 (2021, Friday, December 17th at 6:54 pm)
+# Line count (including blank lines and compiler line): 21
+
+# End of script
