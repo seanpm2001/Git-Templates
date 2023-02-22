282cfe93e18916a7a1510b595c16176768dfb058
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-21 21:39:55 -0800

    PROJECT_LANG_2.jl
    PROJECT_LANG_2.jl

---
+++ Git-Templates/ProjectLanguageFiles/Artificial-Intelligence/AI2001/AI2001/Julia/PROJECT_LANG_2.jl
@@ -0,0 +1,16 @@
+# Start of script
+# Project language file 2
+# For: AI2001
+# About:
+# I chose Julia as the second project language file for this project (AI2001) as Julia is a good systems language for analytics that I chose as a addin for R for this project. It is getting its own project language file, starting here.
+
+"Project language file 2"
+"For: AI2001"
+"About:"
+"I chose Julia as the second project language file for this project (AI2001) as Julia is a good systems language for analytics that I chose as a addin for R for this project. It is getting its own project language file, starting here."
+break
+
+# File version: 1 (2023, Tuesday, February 21st at 9:39 PST)
+# File type: Julia source file (*.jl)
+# Line count (including blank lines and compiler line): 17
+# End of script
