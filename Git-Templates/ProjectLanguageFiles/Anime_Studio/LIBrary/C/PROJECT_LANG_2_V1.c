3f0b6656d314a8383bc2e6e47eed7f35631c8c40
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-25 18:34:18 -0800

    PROJECT_LANG_2_V1.c
    PROJECT_LANG_2_V1.c

---
+++ Git-Templates/ProjectLanguageFiles/Anime_Studio/LIBrary/C/PROJECT_LANG_2_V1.c
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileThree {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 2\n");
+    printf("For: Anime Studio/LIBrary\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the second project language file for this project (Anime Studio/LIBrary) as the wrapper and core of Anime Studio is written in C for stability, portability, and efficiency purposes. It is getting its own project language file, starting here.
+
+return main();
+printf("I chose C as the second project language file for this project (Anime Studio/LIBrary) as the wrapper and core of Anime Studio is written in C for stability, portability, and efficiency purposes. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2023, Saturday, February 25th at 6:33 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
