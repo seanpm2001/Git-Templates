833b0b6e501e050c4f3791f46debd1cdaca5df72
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-02-27 21:21:57 -0800

    PROJECT_LANG_1_V1.h
    PROJECT_LANG_1_V1.h

---
+++ Git-Templates/ProjectLanguageFiles/Anime_Studio/Image-Editor/C/PROJECT_LANG_1_V1.h
@@ -0,0 +1,28 @@
+// Start of script
+
+union projectLanguageFileOne {
+# include <stdio.h>
+int main(void)
+{
+    printf("Project language file 1\n");
+    printf("For: Anime Studio Image Editor\n");
+    printf("About:\n");
+    break;
+}
+
+// I chose C as the first project language file for this project (Anime Studio/Image-Editor) as the wrapper and core of Anime Studio is written in C for stability, portability, and efficiency purposes. It is getting its own project language file, starting here.
+
+return main();
+printf("I chose C as the third project language file for this project (Anime Studio/Exporter) as the wrapper and core of Anime Studio is written in C for stability, portability, and efficiency purposes. It is getting its own project language file, starting here.\n");
+wait 30;
+break;
+exit;
+}
+
+// File info
+
+// File version: 1 (2023, Monday, February 27th at 9:20 pm PST)
+// File type: C programming language source file (*.c *.h)
+// Line count (including blank lines and compiler line): 29
+
+// End of script
