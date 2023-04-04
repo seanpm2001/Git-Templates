d4dcfa94926ab432beea9fb57d37f99431b64cd7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-04-03 22:26:03 -0700

    PROJECT_LANG_1.mm
    PROJECT_LANG_1.mm

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-Objective-CSharp/Objective-C#/PROJECT_LANG_1.mm
@@ -0,0 +1,45 @@
+#!/bin/objective-csharp -x
+// Start of script
+#import </CoreFoundation/CFAttributedString.h>
+#import </Foundation/NSAttributedString.h>
+#import </Foundation/NSDictionary.h>
+#import </Foundation/NSMutableAttributedString.h>
+#import </UIKit/UIKit.h>
+using System.DotNet;
+using System.ObjectiveCSharp;
+
+@namespace objectivecsharp-projectLanguage {
+
+void projectLanguageFileOne(); {
+	@interface classMain : superclassMain {
+  	// instance variables
+	}
++ classMethod1;
++ (return_type)classMethod2;
++ (return_type)classMethod3:(param1_type)param1_varName;
+
+- (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+- (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+                              param2_callName:(param2_type)param2_varName;
+        NSLog.println("Project language file 1");
+        NSLog.println("For: seanpm2001/Learn-Objective-CSharp");
+        NSLog.println("About:");
+    	NSLog.println("I decided to make Objective-CSharp the main project language file for this project (Seanpm2001/Learn-Objective-CSharp) as Objective-CSharp is the language this project is dedicated to, because this project is about learning the Objective-CSharp programming language. It only makes sense to Objective-CSharp the official language for this project. It is getting its own project language file, starting here.");
+    	break;
+    	@end
+	}
+}
+
+/* Output
+Project language file 1
+For: seanpm2001/Learn-Objective-CSharp
+About:
+I decided to make Objective-CSharp the main project language file for this project (Seanpm2001/Learn-Objective-CSharp) as Objective-CSharp is the language this project is dedicated to, because this project is about learning the Objective-CSharp programming language. It only makes sense to Objective-CSharp the official language for this project. It is getting its own project language file, starting here.
+*/
+
+/* File info
+* File type: Objective-C# source file (*.mm, *.cs)
+* File version: 1 (2023, Monday, April 3rd at 10:25 pm PST)
+* Line count (including blank lines and compiler line): 46
+*/
+// End of script
