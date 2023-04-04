946773d6d0912da7dabef50d7a3c75caf74c5a35
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-04-03 22:28:38 -0700

    PROJECT_LANG_1.mm
    PROJECT_LANG_1.mm

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/Objective-CSharp/Objective-C#/PROJECT_LANG_1.mm
@@ -0,0 +1,45 @@
+#!/bin/objective-csharp -x
+// Start of script
+#import </CoreFoundation/CFAttributedString.h>
+#import </Foundation/NSAttributedString.h>
+#import </Foundation/NSDictionary.h>
+#import </Foundation/NSMutableAttributedString.h>
+#import </UIKit/UIKit.h>
+using System.DotNet;
+using System.ObjectiveCSharp;
+
+@namespace objectivecsharp-projectLanguage {
+
+void projectLanguageFileOne(); {
+	@interface classMain : superclassMain {
+  	// instance variables
+	}
++ classMethod1;
++ (return_type)classMethod2;
++ (return_type)classMethod3:(param1_type)param1_varName;
+
+- (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+- (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+                              param2_callName:(param2_type)param2_varName;
+        NSLog.println("Project language file 1");
+        NSLog.println("For: /SNU/2D/ProgrammingTools/IDE/Objective-CSharp");
+        NSLog.println("About:");
+    	NSLog.println("I decided to make Objective-CSharp the main project language file for this project (SNU / 2D / Programming Tools / IDE / Objective-CSharp) as this is a Objective-CSharp IDE, and it needs its main language to be represented here.");
+    	break;
+    	@end
+	}
+}
+
+/* Output
+Project language file 1
+For: /SNU/2D/ProgrammingTools/IDE/Objective-CSharp
+About:
+I decided to make Objective-CSharp the main project language file for this project (SNU / 2D / Programming Tools / IDE / Objective-CSharp) as this is a Objective-CSharp IDE, and it needs its main language to be represented here.
+*/
+
+/* File info
+* File type: Objective-C# source file (*.mm, *.cs)
+* File version: 1 (2023, Monday, April 3rd at 10:28 pm PST)
+* Line count (including blank lines and compiler line): 46
+*/
+// End of script
