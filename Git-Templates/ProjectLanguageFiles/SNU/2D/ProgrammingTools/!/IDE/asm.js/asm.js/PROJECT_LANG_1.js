8e2d0428449d03919b322a2f7f698e985da4d7fc
Sean P. Myrick V19.1.7.2 <seanpm2001-mailing-list@protonmail.com>
2023-12-29 23:47:51 -0800

    PROJECT_LANG_1.js

---
+++ Git-Templates/ProjectLanguageFiles/SNU/2D/ProgrammingTools/!/IDE/asm.js/asm.js/PROJECT_LANG_1.js
@@ -0,0 +1,18 @@
+// Start of script
+console.log("Project language file 1");
+console.log("For: SNU/2D/ProgrammingTools/IDE/asm.js");
+console.log("About:");
+console.log("I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / asm.js) as this is a asm.js IDE, and it needs its main language to be represented here.");
+break;
+/* Output
+Project language file 1
+For: SNU/2D/ProgrammingTools/IDE/asm.js
+About
+I decided to make JavaScript the main project language file for this project (SNU / 2D / Programming Tools / IDE / asm.js) as this is a asm.js IDE, and it needs its main language to be represented here.
+*/
+/* File info
+* File version: 1 (2023, Friday, December 29th at 11:47 pm PST)
+* File type: asm.js source file (*.js)
+* Line count (including blank lines and compiler line): 19
+*/
+// End of script
