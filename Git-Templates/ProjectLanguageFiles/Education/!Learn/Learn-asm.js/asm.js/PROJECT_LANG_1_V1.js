41fe2f853f3c54cdc8c986efa1909d6810cf3807
Sean P. Myrick V19.1.7.2 <seanpm2001-mailing-list@protonmail.com>
2023-12-29 23:44:23 -0800

    PROJECT_LANG_1_V1.js
    PROJECT_LANG_1_V1.js

---
+++ Git-Templates/ProjectLanguageFiles/Education/!Learn/Learn-asm.js/asm.js/PROJECT_LANG_1_V1.js
@@ -0,0 +1,18 @@
+// Start of script
+console.log("Project language file 1");
+console.log("For: seanpm2001/Learn-asm.js");
+console.log("About:");
+console.log("I decided to make JavaScript the main project language file for this project (Seanpm2001/Learn-asm.js) as asm.js is the language this project is dedicated to, because this project is about learning the asm.js programming language. It only makes sense to JavaScript the official language for this project. It is getting its own project language file, starting here.");
+break;
+/* Output
+Project language file 1
+For: seanpm2001/Learn-asm.js
+About
+I decided to make asm.js the main project language file for this project (Seanpm2001/Learn-asm.js) as asm.js is the language this project is dedicated to, because this project is about learning the asm.js programming language. It only makes sense to asm.js the official language for this project. It is getting its own project language file, starting here.
+*/
+/* File info
+* File version: 1 (2023, Friday, December 29th at 11:43 pm PST)
+* File type: asm.js source file (*.js)
+* Line count (including blank lines and compiler line): 19
+*/
+// End of script
