0b7a65ae9a354224ba6dcf73764c50f10ef04b48
Sean P. Myrick V19.1.7.2 <seanpm2001-mailing-list@protonmail.com>
2024-01-15 22:51:33 -0800

    LANG_V1.ex

---
+++ Git-Templates/ProjectLanguageFiles/Gaming/!Games/ClashRoyale/Elixir/LANG_V1.ex
@@ -0,0 +1,3 @@
+# Clash Royale is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of an in-game variable (elixir) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Elixir
+print ("Clash Royale is closed source, and I don't know what language it is written in, so I chose the one programming language with the name of an in-game variable (elixir) and added this file, as I want this project to have a language assigned to it, so I wrote this file in Elixir")
+# File version: 1 (Saturday, October 24th 2020 at 5:32 pm)
